b048ce38f9c33595300311ccc160e36d21b61ced
Hannes Schweighofer <hannes.schweighofer@splendit.at>
2016-08-02 17:27:13 +0200

    ADD simple and ugly assignment rule

--- core/src/main/java/at/splendit/simonykees/core/RefactorASTVisitor.java
+++ core/src/main/java/at/splendit/simonykees/core/RefactorASTVisitor.java
@@ -3,7 +3,11 @@ package at.splendit.simonykees.core;
 import org.eclipse.jdt.core.dom.ASTVisitor;
 import org.eclipse.jdt.core.dom.Assignment;
 import org.eclipse.jdt.core.dom.Assignment.Operator;
+import org.eclipse.jdt.core.dom.Expression;
 import org.eclipse.jdt.core.dom.IfStatement;
+import org.eclipse.jdt.core.dom.InfixExpression;
+import org.eclipse.jdt.core.dom.NumberLiteral;
+import org.eclipse.jdt.core.dom.SimpleName;
 
 public class RefactorASTVisitor extends ASTVisitor {
 
@@ -21,8 +25,26 @@ public class RefactorASTVisitor extends ASTVisitor {
 	
 	@Override
 	public boolean visit(Assignment node) {
-		if(node.getOperator() != null && node.getOperator().equals(Operator.ASSIGN)){
-			node.setOperator(Operator.PLUS_ASSIGN);
+		if(node.getOperator() != null && node.getOperator().equals(Operator.ASSIGN)) {
+			// TODO check if node.getNodeType() is also suitable!
+			SimpleName leftHandSide = (SimpleName) node.getLeftHandSide();
+			Expression rightHandSide = node.getRightHandSide();
+			if (rightHandSide instanceof InfixExpression) {
+				Expression left = ((InfixExpression) rightHandSide).getLeftOperand();
+				Expression right = ((InfixExpression) rightHandSide).getRightOperand();
+				if (left instanceof SimpleName) {
+					SimpleName name = (SimpleName) left;
+					if (leftHandSide.getIdentifier().equals(name.getIdentifier())) {
+						node.setOperator(Operator.PLUS_ASSIGN);
+						node.setRightHandSide(node.getRoot().getAST().newNumberLiteral(((NumberLiteral) right).getToken()));
+					}
+				}
+			} else if (rightHandSide instanceof NumberLiteral) {
+				((NumberLiteral) rightHandSide).getToken();
+			} else {
+				RefactorHandler.log("implement [" + rightHandSide.getClass().getSimpleName() + "]");
+			}
+//			node.setOperator(Operator.PLUS_ASSIGN);
 		}
 		return true;
 	}
