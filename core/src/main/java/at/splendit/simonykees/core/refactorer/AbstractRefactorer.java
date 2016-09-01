bc28bda7577a80fe606d7122f767ca97bfb6c721
Hannes Schweighofer <hannes.schweighofer@splendit.at>
2016-09-01 16:22:10 +0200

    LJA-86 ADD second try of a refactorer

--- core/src/main/java/at/splendit/simonykees/core/refactorer/AbstractRefactorer.java
+++ core/src/main/java/at/splendit/simonykees/core/refactorer/AbstractRefactorer.java
@@ -1,10 +1,15 @@
 package at.splendit.simonykees.core.refactorer;
 
-import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
+import org.eclipse.core.runtime.Status;
 import org.eclipse.jdt.core.ICompilationUnit;
 import org.eclipse.jdt.core.IJavaElement;
+import org.eclipse.jdt.core.IJavaProject;
+import org.eclipse.jdt.core.IPackageFragment;
+import org.eclipse.jdt.core.IPackageFragmentRoot;
 import org.eclipse.jdt.core.JavaModelException;
 import org.eclipse.jdt.core.dom.AST;
 import org.eclipse.jdt.core.dom.ASTParser;
@@ -13,7 +18,6 @@ import org.eclipse.jdt.core.dom.CompilationUnit;
 import org.eclipse.jdt.core.dom.rewrite.ASTRewrite;
 import org.eclipse.jface.text.Document;
 import org.eclipse.ltk.core.refactoring.DocumentChange;
-import org.eclipse.ltk.core.refactoring.TextChange;
 import org.eclipse.text.edits.TextEdit;
 
 import at.splendit.simonykees.core.Activator;
@@ -22,42 +26,66 @@ public abstract class AbstractRefactorer {
 	
 	protected List<IJavaElement> javaElements;
 	protected List<Class<? extends ASTVisitor>> rules;
+	protected List<DocumentChange> documentChanges = new ArrayList<>();
 	
 	public AbstractRefactorer(List<IJavaElement> javaElements, List<Class<? extends ASTVisitor>> rules) {
 		this.javaElements = javaElements;
 		this.rules = rules;
 	}
 	
-	protected ICompilationUnit getWorkingCopy(ICompilationUnit compilationUnit) throws JavaModelException {
-		return compilationUnit.getWorkingCopy(null);
+	public void doRefactoring() {
+		List<ICompilationUnit> compilationUnits = new ArrayList<>();
+		
+		try {
+			collectICompilationUnits(compilationUnits, javaElements);
+			
+			if (compilationUnits.isEmpty()) {
+				Activator.log(Status.WARNING, "No compilation units found", null);
+				return;
+			}
+			
+			for (ICompilationUnit compilationUnit : compilationUnits) {
+				for (Class<? extends ASTVisitor> ruleClazz : rules) {
+					ICompilationUnit workingCopy = compilationUnit.getWorkingCopy(null);
+					
+					try {
+						final ASTParser astParser = ASTParser.newParser(AST.JLS8);
+						resetParser(workingCopy, astParser);
+						final CompilationUnit astRoot = (CompilationUnit) astParser.createAST(null);
+						final ASTRewrite astRewrite = ASTRewrite.create(astRoot.getAST());
+						
+						Activator.log("Init rule [" + ruleClazz.getName() + "]");
+						ASTVisitor rule = ruleClazz.getConstructor(ASTRewrite.class).newInstance(astRewrite);
+						astRoot.accept(rule);
+						
+						String source = workingCopy.getSource();
+						Document document = new Document(source);
+						TextEdit edits = astRewrite.rewriteAST(document, workingCopy.getJavaProject().getOptions(true));
+						
+						DocumentChange documentChange = new DocumentChange(rule.getClass().getSimpleName(), document);
+						documentChange.setEdit(edits);
+						documentChanges.add(documentChange);
+						
+						workingCopy.applyTextEdit(edits, null);
+						workingCopy.reconcile(ICompilationUnit.NO_AST, false, null, null);
+						workingCopy.commitWorkingCopy(false, null);
+					} catch (ReflectiveOperationException e) {
+						Activator.log(Status.ERROR, "Cannot init rule [" + ruleClazz.getName() + "]", e);
+					}
+					
+					workingCopy.discardWorkingCopy();
+				}
+				
+			}
+			
+		} catch (JavaModelException e) {
+			Activator.log(Status.ERROR, e.getMessage(), null);
+			// FIXME should also throw an exception
+		}
 	}
 	
-	protected ASTRewrite applyRuleToWorkingCopy(ICompilationUnit workingCopy, Class<? extends ASTVisitor> ruleClazz) throws ReflectiveOperationException {
-		final ASTParser astParser = ASTParser.newParser(AST.JLS8);
-		resetParser(workingCopy, astParser);
-		final CompilationUnit astRoot = (CompilationUnit) astParser.createAST(null);
-		final ASTRewrite astRewrite = ASTRewrite.create(astRoot.getAST());
-		
-		Activator.log("Init rule [" + ruleClazz.getName() + "]");
-		ASTVisitor rule = ruleClazz.getConstructor(ASTRewrite.class).newInstance(astRewrite);
-		astRoot.accept(rule);
-		
-		return astRewrite;
-	}
-	
-	protected TextEdit getTextEdit(ICompilationUnit workingCopy, ASTRewrite astRewrite) throws JavaModelException {
-		String source = workingCopy.getSource();
-		Document document = new Document(source);
-		TextEdit edits = astRewrite.rewriteAST(document, workingCopy.getJavaProject().getOptions(true));
-		
-		return edits;
-	}
-	
-	protected void applyToWorkingCopy(ICompilationUnit workingCopy, TextEdit edits) throws JavaModelException {
-		workingCopy.applyTextEdit(edits, null);
-		workingCopy.reconcile(ICompilationUnit.NO_AST, false, null, null);
-		workingCopy.commitWorkingCopy(false, null);
-		workingCopy.discardWorkingCopy();
+	public List<DocumentChange> getDocumentChanges() {
+		return documentChanges;
 	}
 	
 	protected static void resetParser(ICompilationUnit compilationUnit, ASTParser astParser) {
@@ -70,6 +98,41 @@ public abstract class AbstractRefactorer {
 		DocumentChange documentChange = new DocumentChange(name, document);
 		documentChange.setEdit(edits);
 		return documentChange;
+	}
+	
+	protected static void collectICompilationUnits(List<ICompilationUnit> result, List<IJavaElement> javaElements) throws JavaModelException {
+		for (IJavaElement javaElement : javaElements) {
+			if (javaElement instanceof ICompilationUnit) {
+				ICompilationUnit compilationUnit = (ICompilationUnit) javaElement;
+				addCompilationUnit(result, compilationUnit);
+			} else if (javaElement instanceof IPackageFragment) {
+				IPackageFragment packageFragment = (IPackageFragment) javaElement;
+				addCompilationUnit(result, packageFragment.getCompilationUnits());
+			} else if (javaElement instanceof IPackageFragmentRoot) {
+				IPackageFragmentRoot packageFragmentRoot = (IPackageFragmentRoot) javaElement;
+				collectICompilationUnits(result, Arrays.asList(packageFragmentRoot.getChildren()));
+			} else if (javaElement instanceof IJavaProject) {
+				IJavaProject javaProject = (IJavaProject) javaElement;
+				for (IPackageFragment packageFragment : javaProject.getPackageFragments()) {
+					addCompilationUnit(result, packageFragment.getCompilationUnits());
+				}
+			}
+		}
+	}
+	
+	private static void addCompilationUnit(List<ICompilationUnit> result, ICompilationUnit compilationUnit) throws JavaModelException {
+		if (!compilationUnit.isConsistent()) {
+			compilationUnit.makeConsistent(null);
+		}
+		if (!compilationUnit.isReadOnly()) {
+			result.add(compilationUnit);
+		}
+	}
+	
+	private static void addCompilationUnit(List<ICompilationUnit> result, ICompilationUnit[] compilationUnits) throws JavaModelException {
+		for (ICompilationUnit compilationUnit : compilationUnits) {
+			addCompilationUnit(result, compilationUnit);
+		}
 	}
 
 }
