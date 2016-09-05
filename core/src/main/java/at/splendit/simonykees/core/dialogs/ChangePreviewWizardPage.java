47c3c9fa45ac6d38a62522ecab4edc62ad41873a
Ludwig Werzowa <ludwig.werzowa@splendit.at>
2016-09-05 17:08:36 +0200

    LJA-85 UDPATE formatting and comments

--- core/src/main/java/at/splendit/simonykees/core/dialogs/ChangePreviewWizardPage.java
+++ core/src/main/java/at/splendit/simonykees/core/dialogs/ChangePreviewWizardPage.java
@@ -13,9 +13,9 @@ import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
 
 public class ChangePreviewWizardPage extends WizardPage {
-	
+
 	private TextEditBasedChange textEditBasedChange;
-	
+
 	/**
 	 * Create the wizard.
 	 */
@@ -28,31 +28,32 @@ public class ChangePreviewWizardPage extends WizardPage {
 
 	@Override
 	public void createControl(Composite parent) {
-		
-		Composite container= new Composite(parent, SWT.NONE);
-		
+
+		Composite container = new Composite(parent, SWT.NONE);
+
 		container.setBounds(10, 10, 548, 296);
 		Display display = Display.getCurrent();
-		RGB rgb = new RGB(128, 128, 128);
-		container.setBackground(new Color(display, rgb));
-		
-		GridLayout layout= new GridLayout();
-		layout.marginHeight= 0; layout.marginWidth= 0;
-		container.setLayout(layout);
+		RGB rgb = new RGB(128, 128, 128); 
+		container.setBackground(new Color(display, rgb)); // useless; just to illustrate the size of the Composite
 
-		SashForm sashForm= new SashForm(container, SWT.VERTICAL);
-		
-		GridData gd= new GridData(GridData.FILL_BOTH);
-		gd.widthHint= convertWidthInCharsToPixels(80);
-		sashForm.setLayoutData(gd);
-		
+		GridLayout layout = new GridLayout();
+		layout.marginHeight = 0; // margin to the Composite
+		layout.marginWidth = 0;
+		container.setLayout(layout); // without setting the layout, nothing displays
+
 		setControl(container);
-		
+
+		SashForm sashForm = new SashForm(container, SWT.VERTICAL);
+
+		GridData gridData = new GridData(GridData.FILL_BOTH); // GridData works with GridLayout
+//		gridData.widthHint = convertWidthInCharsToPixels(80);
+		sashForm.setLayoutData(gridData);
+
 		TextEditChangePreviewViewer viewer = new TextEditChangePreviewViewer();
 		viewer.createControl(sashForm);
-//		viewer.setInput(new ChangePreviewViewerInput(textEditBasedChange));
+		// viewer.setInput(new ChangePreviewViewerInput(textEditBasedChange));
 		viewer.setInput(TextEditChangePreviewViewer.createInput(textEditBasedChange));
-		
+
 	}
-	
+
 }
