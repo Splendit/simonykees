d63d32cee0210edee4ae8f692105d15307ec8595
Hannes Schweighofer <hannes.schweighofer@splendit.at>
2016-09-08 14:46:36 +0200

    LJA-94 ADD keep changes for every working copy the rule is applied on

--- core/src/main/java/at/splendit/simonykees/core/rule/RefactoringRule.java
+++ core/src/main/java/at/splendit/simonykees/core/rule/RefactoringRule.java
@@ -9,6 +9,7 @@ import org.eclipse.jdt.core.JavaModelException;
 import org.eclipse.jdt.core.dom.ASTVisitor;
 import org.eclipse.ltk.core.refactoring.DocumentChange;
 
+import at.splendit.simonykees.core.Activator;
 import at.splendit.simonykees.core.util.SimonykeesUtil;
 
 public abstract class RefactoringRule<T extends ASTVisitor> {
@@ -20,6 +21,8 @@ public abstract class RefactoringRule<T extends ASTVisitor> {
 	protected boolean enabled = true;
 	
 	private Class<T> visitor;
+	
+	private Map<ICompilationUnit, DocumentChange> changes = new HashMap<ICompilationUnit, DocumentChange>();
 	
 	public RefactoringRule(Class <T> visitor) {
 		this.visitor = visitor;
@@ -42,19 +45,37 @@ public abstract class RefactoringRule<T extends ASTVisitor> {
 	}
 	
 	/**
-	 * Changes are applied to working copy but <b>not</b> committed
-	 * 
-	 * @param workingCopies
-	 * @return
-	 * @throws JavaModelException
-	 * @throws ReflectiveOperationException
+	 * Changes should be generated with {@code generateDocumentChanges} first  
+	 * @return Map containing {@code ICompilationUnit}s as key and corresponding {@code DocumentChange}s as value
 	 */
-	public Map<ICompilationUnit, DocumentChange> generateDocumentChanges(List<ICompilationUnit> workingCopies) throws JavaModelException, ReflectiveOperationException {
-		Map<ICompilationUnit, DocumentChange> changes = new HashMap<ICompilationUnit, DocumentChange>();
-		for (ICompilationUnit wc : workingCopies) {
-			changes.put(wc, SimonykeesUtil.applyRule(wc, visitor));
-		}
+	public Map<ICompilationUnit, DocumentChange> getDocumentChanges() {
 		return changes;
 	}
 	
+	/**
+	 * Changes are applied to working copy but <b>not</b> committed
+	 * 
+	 * @param workingCopies
+	 * @throws JavaModelException
+	 * @throws ReflectiveOperationException
+	 */
+	public void generateDocumentChanges(List<ICompilationUnit> workingCopies) throws JavaModelException, ReflectiveOperationException {
+		for (ICompilationUnit wc : workingCopies) {
+			applyRule(wc);
+		}
+	}
+	
+	private void applyRule(ICompilationUnit workingCopy) throws JavaModelException, ReflectiveOperationException {
+		if (changes.containsKey(workingCopy)) {
+			// already have changes
+			Activator.log("working copy alread contains changes for this rule [" + this.name + "]");
+		} else {
+			DocumentChange documentChange = SimonykeesUtil.applyRule(workingCopy, visitor);
+			if (documentChange != null) {
+				changes.put(workingCopy, documentChange);
+			} else {
+				// no changes
+			}
+		}
+	}
 }
