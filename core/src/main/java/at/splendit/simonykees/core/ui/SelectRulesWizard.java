be67649f44ad55cc7fce7d6bb026ea8883ff7274
Hannes Schweighofer <hannes.schweighofer@splendit.at>
2016-09-08 16:46:37 +0200

    LJA-94 CHANGE use new work flow and print changes

--- core/src/main/java/at/splendit/simonykees/core/ui/SelectRulesWizard.java
+++ core/src/main/java/at/splendit/simonykees/core/ui/SelectRulesWizard.java
@@ -1,12 +1,16 @@
 package at.splendit.simonykees.core.ui;
 
 import java.util.List;
+import java.util.Map;
 
+import org.eclipse.jdt.core.ICompilationUnit;
 import org.eclipse.jdt.core.IJavaElement;
 import org.eclipse.jdt.core.dom.ASTVisitor;
 import org.eclipse.jface.wizard.Wizard;
+import org.eclipse.ltk.core.refactoring.DocumentChange;
 
 import at.splendit.simonykees.core.Activator;
+import at.splendit.simonykees.core.refactorer.AbstractRefactorer;
 import at.splendit.simonykees.core.rule.RefactoringRule;
 
 public class SelectRulesWizard extends Wizard {
@@ -31,9 +35,31 @@ public class SelectRulesWizard extends Wizard {
 	@Override
 	public boolean performFinish() {
 		final List<RefactoringRule<? extends ASTVisitor>> rules = selectRulesPage.getSelectedRules();
-		rules.forEach(rule -> Activator.log(rule.getName()));
-//		new AbstractRefactorer(javaElements, rules) {}.doRefactoring();
+		AbstractRefactorer refactorer = new AbstractRefactorer(javaElements, rules) {};
+		
+		refactorer.prepareRefactoring();
+		refactorer.doRefactoring();
+		
+		Activator.log(formatRuleAndFileAndChange(refactorer.getRules()));
+		
+		refactorer.commitRefactoring();
+		
 		return true;
+	}
+	
+	private String formatRuleAndFileAndChange(List<RefactoringRule<? extends ASTVisitor>> rules) {
+		String output = "";
+		
+		for (RefactoringRule<? extends ASTVisitor> rule : rules) {
+			output += "\nRule [" + rule.getName() + "]\n";
+			Map<ICompilationUnit, DocumentChange> changes = rule.getDocumentChanges();
+			for (ICompilationUnit cu : changes.keySet()) {
+				output += "\tFile\t[" + cu.getPath().toString() + "]\n";
+				output += "\tChange\t[\n" + changes.get(cu).getEdit() + "\n\t]\n"; 
+			}
+		}
+		
+		return output;
 	}
 
 }
