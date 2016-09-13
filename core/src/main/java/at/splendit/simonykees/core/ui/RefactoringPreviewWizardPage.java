bb48e5d10a86a6bbc2b3ca4738625a9d726ade93
Ludwig Werzowa <ludwig.werzowa@splendit.at>
2016-09-13 14:42:34 +0200

    LJA-95 ADD selection change listener

--- core/src/main/java/at/splendit/simonykees/core/ui/RefactoringPreviewWizardPage.java
+++ core/src/main/java/at/splendit/simonykees/core/ui/RefactoringPreviewWizardPage.java
@@ -4,9 +4,14 @@
 package at.splendit.simonykees.core.ui;
 
 import org.eclipse.jdt.core.dom.ASTVisitor;
+import org.eclipse.jface.viewers.ISelectionChangedListener;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.viewers.SelectionChangedEvent;
 import org.eclipse.jface.viewers.TreeViewer;
 import org.eclipse.jface.wizard.WizardPage;
+import org.eclipse.ltk.core.refactoring.DocumentChange;
 import org.eclipse.ltk.internal.ui.refactoring.TextEditChangePreviewViewer;
+import org.eclipse.ltk.ui.refactoring.IChangePreviewViewer;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.SashForm;
 import org.eclipse.swt.layout.GridData;
@@ -17,50 +22,86 @@ import at.splendit.simonykees.core.rule.RefactoringRule;
 
 @SuppressWarnings("restriction")
 public class RefactoringPreviewWizardPage extends WizardPage {
-	
+
 	private RefactoringRule<? extends ASTVisitor> refactoringRule;
+	private CompilationUnitNode currentCompilationUnitNode;
+	private IChangePreviewViewer currentPreviewViewer;
 
 	public RefactoringPreviewWizardPage(RefactoringRule<? extends ASTVisitor> rule) {
 		super(rule.getName());
 		setTitle(rule.getName()); // FIXME
 		setDescription(rule.getDescription());
 		this.refactoringRule = rule;
+
+		// FIXME make it null safe
+		this.currentCompilationUnitNode = new CompilationUnitNode(
+				refactoringRule.getDocumentChanges().keySet().stream().findFirst().get());
 	}
 
-	/* (non-Javadoc)
-	 * @see org.eclipse.jface.dialogs.IDialogPage#createControl(org.eclipse.swt.widgets.Composite)
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.jface.dialogs.IDialogPage#createControl(org.eclipse.swt.
+	 * widgets.Composite)
 	 */
 	@Override
 	public void createControl(Composite parent) {
 		Composite container = new Composite(parent, SWT.NONE);
 
 		GridLayout layout = new GridLayout();
-		layout.marginHeight = 0; // margin from TextEditChangePreviewViewer to Composite
+
+		// margin from TextEditChangePreviewViewer to Composite
+		layout.marginHeight = 0;
 		layout.marginWidth = 0;
-		container.setLayout(layout); // without setting the layout, nothing displays
+
+		// without setting the layout, nothing displays
+		container.setLayout(layout);
 
 		setControl(container);
-		
+
 		SashForm sashForm = new SashForm(container, SWT.VERTICAL);
-		
-//		ViewerPane
+
 		TreeViewer treeViewer = new TreeViewer(sashForm);
 		CompilationUnitContentProvider contentProvider = new CompilationUnitContentProvider();
-		
+
 		treeViewer.setContentProvider(contentProvider);
-		treeViewer.setInput(refactoringRule.getDocumentChanges().keySet()); 
-		
-		GridData gridData = new GridData(GridData.FILL_BOTH); // GridData works with GridLayout
-//		gridData.widthHint = convertWidthInCharsToPixels(80);
+		treeViewer.addSelectionChangedListener(createSelectionChangedListener());
+		treeViewer.setInput(refactoringRule.getDocumentChanges().keySet());
+
+		// GridData works with GridLayout
+		GridData gridData = new GridData(GridData.FILL_BOTH);
 		sashForm.setLayoutData(gridData);
 
-		TextEditChangePreviewViewer viewer = new TextEditChangePreviewViewer();
-		viewer.createControl(sashForm);
-		
-		// FIXME show more than just the first element
-		// FIXME handle empty changes for a rule (changes only get created if there is a change)
-		viewer.setInput(TextEditChangePreviewViewer.createInput(refactoringRule.getDocumentChanges().values().stream().findFirst().get()));
+		currentPreviewViewer = new TextEditChangePreviewViewer();
+		currentPreviewViewer.createControl(sashForm);
 
+		populatePreviewViewer();
+
+	}
+
+	private ISelectionChangedListener createSelectionChangedListener() {
+		return new ISelectionChangedListener() {
+			@Override
+			public void selectionChanged(SelectionChangedEvent event) {
+				IStructuredSelection sel = (IStructuredSelection) event.getSelection();
+
+				if (sel.size() == 1) {
+					CompilationUnitNode newSelection = (CompilationUnitNode) sel.getFirstElement();
+					if (!newSelection.equals(currentCompilationUnitNode)) {
+						currentCompilationUnitNode = newSelection;
+						populatePreviewViewer();
+					}
+				}
+			}
+		};
+	}
+	
+	private void populatePreviewViewer() {
+		currentPreviewViewer.setInput(TextEditChangePreviewViewer.createInput(getCurrentDocumentChange()));
+	}
+
+	private DocumentChange getCurrentDocumentChange() {
+		return refactoringRule.getDocumentChanges().get(currentCompilationUnitNode.getCompilationUnit());
 	}
 
 }
