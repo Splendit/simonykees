982f9daa54125ddd8ac8a01b6e2e449e455ea2cd
Hannes Schweighofer <hannes.schweighofer@splendit.at>
2016-09-13 17:06:11 +0200

    LJA-98 ADD internationalisation to rules

--- core/src/main/java/at/splendit/simonykees/core/i18n/Messages.java
+++ core/src/main/java/at/splendit/simonykees/core/i18n/Messages.java
@@ -4,9 +4,15 @@ import org.eclipse.osgi.util.NLS;
 
 public class Messages extends NLS {
 	private static final String BUNDLE_NAME = "at.splendit.simonykees.core.i18n.messages"; //$NON-NLS-1$
+	public static String ArithmethicAssignmentRule_description;
+	public static String ArithmethicAssignmentRule_name;
 	public static String RefactoringRule_default_description;
 	public static String RefactoringRule_default_name;
 	public static String RefactoringRule_warning_workingcopy_already_present;
+	public static String StringUtilsRule_description;
+	public static String StringUtilsRule_name;
+	public static String TryWithResourceRule_description;
+	public static String TryWithResourceRule_name;
 	static {
 		// initialize resource bundle
 		NLS.initializeMessages(BUNDLE_NAME, Messages.class);
