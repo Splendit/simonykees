d3130c322fa75b1ad292c5adb073154705cbe649
Martin Huter <martin.huter@splendit.at>
2016-09-19 11:56:42 +0200

    LJA-91 used externalisation on Strings

--- core/src/main/java/at/splendit/simonykees/core/rule/FunctionalInterfaceRule.java
+++ core/src/main/java/at/splendit/simonykees/core/rule/FunctionalInterfaceRule.java
@@ -1,13 +1,14 @@
 package at.splendit.simonykees.core.rule;
 
+import at.splendit.simonykees.core.i18n.Messages;
 import at.splendit.simonykees.core.visitor.FunctionalInterfaceASTVisitor;
 
 public class FunctionalInterfaceRule extends RefactoringRule<FunctionalInterfaceASTVisitor> {
 
 	public FunctionalInterfaceRule(Class<FunctionalInterfaceASTVisitor> visitor) {
 		super(visitor);
-		this.name = "Working Title - FunctionalInterface";
-		this.description = "Converts annonym inner classes to equivalent lambda expressions";
+		this.name = Messages.FunctionalInterfaceRule_name;
+		this.description = Messages.FunctionalInterfaceRule_description;
 	}
 
 }
