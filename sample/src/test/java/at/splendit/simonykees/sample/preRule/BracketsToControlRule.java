8abea6091746cf2bd06e9776d115811ef75fd159
Hannes Schweighofer <hannes.schweighofer@splendit.at>
2016-09-19 17:05:54 +0200

    LJA-88 ADD samples for rule testing

---
+++ sample/src/test/java/at/splendit/simonykees/sample/preRule/BracketsToControlRule.java
@@ -0,0 +1,10 @@
+package at.splendit.simonykees.sample.preRule;
+
+public class BracketsToControlRule {
+	public static void main(String[] args) {
+		int i = 0;
+
+		if (i == 0) i = 0;
+		else i = 0;
+	}
+}
