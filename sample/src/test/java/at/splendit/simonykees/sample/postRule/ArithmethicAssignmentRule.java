924183e400735531c735b70502234856f69e19b9
Martin Huter <martin.huter@splendit.at>
2016-09-20 15:29:43 +0200

    LJA-107 added more tests to arithmeticAssignment

--- sample/src/test/java/at/splendit/simonykees/sample/postRule/ArithmethicAssignmentRule.java
+++ sample/src/test/java/at/splendit/simonykees/sample/postRule/ArithmethicAssignmentRule.java
@@ -1,7 +1,28 @@
 package at.splendit.simonykees.sample.postRule;
 
 public class ArithmethicAssignmentRule {
+
+	private int a = 3;
+	private int c = 3;
+	private int b = 3;
+	private int q = 3;
+
 	public void doSomething(int i) {
 		i += 1;
+		i = 7 + 4 - 3;
+		i += 3;
+		i -= 3;
+		i += 4 - 3;
+		i += 4 + 3;
+		i += 3 + 4;
+		i += 3 - 4;
+		i += a - 4;
+		i = a + b;
+		i += a;
+		i *= a;
+		b *= 4;
+		q *= (3 + 4);
+		q = a * b / c * q * 3 - i;
+		a /= 4;
 	}
 }
--- sample/src/test/java/at/splendit/simonykees/sample/preRule/ArithmethicAssignmentRule.java
+++ sample/src/test/java/at/splendit/simonykees/sample/preRule/ArithmethicAssignmentRule.java
@@ -1,7 +1,28 @@
 package at.splendit.simonykees.sample.preRule;
 
 public class ArithmethicAssignmentRule {
+
+	private int a = 3;
+	private int c = 3;
+	private int b = 3;
+	private int q = 3;
+
 	public void doSomething(int i) {
 		i = i + 1;
+		i = 7 + 4 - 3;
+		i = i + 3;
+		i = i - 3;
+		i = i + 4 - 3;
+		i = i + 4 + 3;
+		i = 3 + i + 4;
+		i = 3 + i - 4;
+		i = a + i - 4;
+		i = a + b;
+		i = a + i;
+		i = i * a;
+		b = 4 * b;
+		q = (3 + 4) * q;
+		q = a * b / c * q * 3 - i;
+		a = a / 4;
 	}
 }
