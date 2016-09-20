3e1b19977dd06556930286b0d5d77c817ee81659
Martin Huter <martin.huter@splendit.at>
2016-09-20 15:30:11 +0200

    LJA-111 added more tests to BracketsToControl

--- sample/src/test/java/at/splendit/simonykees/sample/postRule/BracketsToControlRule.java
+++ sample/src/test/java/at/splendit/simonykees/sample/postRule/BracketsToControlRule.java
@@ -1,5 +1,7 @@
 package at.splendit.simonykees.sample.postRule;
 
+import java.util.Arrays;
+
 public class BracketsToControlRule {
 	public static void main(String[] args) {
 		int i = 0;
@@ -8,6 +10,43 @@ public class BracketsToControlRule {
 			i = 0;
 		} else {
 			i = 0;
+		}
+	}
+	
+	public static void testforLoop() {
+		for (int i = 1; i < 2; i++) {
+			;
+		}
+		for (Integer i : Arrays.asList(1, 2, 3)) {
+			System.out.println(i);
+		}
+	}
+
+	public static void thestWhileLoop() {
+		String st = null;
+		while (st != null) {
+			;
+		}
+		do {
+			;
+		} while (st != null);
+	}
+
+	public static void testIf() {
+		String s = "a";
+		String t = "bb";
+		if (s == "a") {
+			if (t == "b") {
+				do {
+					;
+				} while (t != null);
+			} else if (s == "aaa") {
+				System.out.print("aa");
+			} else {
+				System.out.print("bbb");
+			}
+		} else {
+			;
 		}
 	}
 }
