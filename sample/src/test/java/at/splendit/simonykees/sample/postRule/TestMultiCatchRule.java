1c8ede8d51851e58d3e6d6c8e727d64ad25afb1c
Martin Huter <martin.huter@splendit.at>
2016-09-22 09:20:14 +0200

    LJA-110 added Mutlicatch test

---
+++ sample/src/test/java/at/splendit/simonykees/sample/postRule/TestMultiCatchRule.java
@@ -0,0 +1,35 @@
+package at.splendit.simonykees.sample.postRule;
+
+import java.lang.reflect.InvocationTargetException;
+
+import org.junit.Test;
+
+/**
+ * This test is a manual test to provide tests for the TryWithResource
+ * replacement
+ * 
+ * 
+ * How to Test: Do unit Test -> All tests should pass. Apply the [Usage] on the
+ * file. Test the file again -> All tests should still pass and all methods of
+ * Strings should be replaced by the corresponding StrinUtils implementation.
+ * 
+ * Usage: [Right Click in Editor] -> [Simoneykees/SelectRuleWizardHandler] ->
+ * [MultiCatchTest auswählen] -> Finish This triggers the Event.
+ * 
+ * @author mgh
+ *
+ */
+public class TestMultiCatchRule {
+
+	@Test
+	public void tryWithResourceCommentBugTest() {
+		// TODO meaningful Asserts?
+		try {
+			String.class.getConstructor(String.class).newInstance("aa");
+		} catch (InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException | NoSuchMethodException | SecurityException  e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
