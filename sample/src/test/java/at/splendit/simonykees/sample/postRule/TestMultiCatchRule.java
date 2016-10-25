b98d62629a849d1856716fe70aab113d8a299106
Ludwig Werzowa <ludwig.werzowa@splendit.at>
2016-10-25 18:28:50 +0200

    SIM-91 ADD corner cases for inheritance (see FIXME)

--- sample/src/test/java/at/splendit/simonykees/sample/postRule/TestMultiCatchRule.java
+++ sample/src/test/java/at/splendit/simonykees/sample/postRule/TestMultiCatchRule.java
@@ -4,10 +4,6 @@ import java.lang.reflect.InvocationTargetException;
 
 import org.junit.Test;
 
-/**
- * @author Martin Huter
- *
- */
 @SuppressWarnings("nls")
 public class TestMultiCatchRule {
 
@@ -22,4 +18,52 @@ public class TestMultiCatchRule {
 		}
 	}
 
+	public int cornerCase(int i) {
+		try {
+			throwSomething(i);
+		/*
+		 * FIXME (no ticket yet): 
+		 * 		as seen here, only FirstException and SecondException should be in the 
+		 * 		multi-catch clause, otherwise a compilation error will occur. 
+		 */
+		} catch (FirstException | SecondException e) { 
+			e.printStackTrace();
+		}
+		return i;
+	}
+
+	public void throwSomething(int i) throws FirstException, SecondChildChildException, SecondChildException,
+			SecondChildSecondException, SecondException {
+		switch (i) {
+		case 1:
+			throw new FirstException();
+		case 2:
+			throw new SecondException();
+		case 3:
+			throw new SecondChildException();
+		case 4:
+			throw new SecondChildChildException();
+		case 5:
+			throw new SecondChildSecondException();
+
+		default:
+			break;
+		}
+	}
+
+	class FirstException extends Exception {
+	}
+
+	class SecondException extends Exception {
+	}
+
+	class SecondChildException extends SecondException {
+	}
+
+	class SecondChildChildException extends SecondChildException {
+	}
+
+	class SecondChildSecondException extends SecondException {
+	}
+	
 }
--- sample/src/test/java/at/splendit/simonykees/sample/preRule/TestMultiCatchRule.java
+++ sample/src/test/java/at/splendit/simonykees/sample/preRule/TestMultiCatchRule.java
@@ -4,10 +4,6 @@ import java.lang.reflect.InvocationTargetException;
 
 import org.junit.Test;
 
-/**
- * @author Martin Huter
- *
- */
 @SuppressWarnings("nls")
 public class TestMultiCatchRule {
 
@@ -37,4 +33,55 @@ public class TestMultiCatchRule {
 		}
 	}
 
+	public int cornerCase(int i) {
+		try {
+			throwSomething(i);
+		} catch (SecondChildChildException e) {
+			e.printStackTrace();
+		} catch (SecondChildException e) {
+			e.printStackTrace();
+		} catch (SecondChildSecondException e) {
+			e.printStackTrace();
+		} catch (FirstException e) {
+			e.printStackTrace();
+		} catch (SecondException e) {
+			e.printStackTrace();
+		}
+		return i;
+	}
+
+	public void throwSomething(int i) throws FirstException, SecondChildChildException, SecondChildException,
+			SecondChildSecondException, SecondException {
+		switch (i) {
+		case 1:
+			throw new FirstException();
+		case 2:
+			throw new SecondException();
+		case 3:
+			throw new SecondChildException();
+		case 4:
+			throw new SecondChildChildException();
+		case 5:
+			throw new SecondChildSecondException();
+
+		default:
+			break;
+		}
+	}
+
+	class FirstException extends Exception {
+	}
+
+	class SecondException extends Exception {
+	}
+
+	class SecondChildException extends SecondException {
+	}
+
+	class SecondChildChildException extends SecondChildException {
+	}
+
+	class SecondChildSecondException extends SecondException {
+	}
+	
 }
