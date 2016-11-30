7d5782b1969a0266a2bd1ff9f30c3a65f5baf470
Martin Huter <martin.huter@splendit.at>
2016-11-30 13:30:00 +0100

    SIM-52 ADD tests for serial uid transformation

---
+++ sample/src/test/java/at/splendit/simonykees/sample/postRule/serial/TestSerialVersionUidRule.java
@@ -0,0 +1,13 @@
+package at.splendit.simonykees.sample.postRule.serial;
+
+import java.io.Serializable;
+
+@SuppressWarnings("nls")
+public class TestSerialVersionUidRule implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private long otherField;
+	private Object objectField;
+}
---
+++ sample/src/test/java/at/splendit/simonykees/sample/preRule/TestSerialVersionUidRule.java
@@ -0,0 +1,13 @@
+package at.splendit.simonykees.sample.preRule;
+
+import java.io.Serializable;
+
+@SuppressWarnings("nls")
+public class TestSerialVersionUidRule implements Serializable {
+	/**
+	 * 
+	 */
+	private long serialVersionUID = 1L;
+	private long otherField;
+	private Object objectField;
+}
