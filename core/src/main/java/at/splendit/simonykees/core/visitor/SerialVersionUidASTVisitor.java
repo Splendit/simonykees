95b981d44c1445f97b6e824d7707798647df6c8d
Martin Huter <martin.huter@splendit.at>
2016-11-30 15:58:30 +0100

    SIM-52 ADD implementation for single field declaration for serialuid

--- core/src/main/java/at/splendit/simonykees/core/visitor/SerialVersionUidASTVisitor.java
+++ core/src/main/java/at/splendit/simonykees/core/visitor/SerialVersionUidASTVisitor.java
@@ -1,7 +1,15 @@
 package at.splendit.simonykees.core.visitor;
 
-import org.eclipse.jdt.core.dom.CompilationUnit;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.apache.commons.lang3.StringUtils;
+import org.eclipse.jdt.core.dom.ASTVisitor;
 import org.eclipse.jdt.core.dom.FieldDeclaration;
+import org.eclipse.jdt.core.dom.Modifier;
+import org.eclipse.jdt.core.dom.Modifier.ModifierKeyword;
+import org.eclipse.jdt.core.dom.VariableDeclarationFragment;
+import org.eclipse.jdt.core.dom.rewrite.ListRewrite;
 
 /**
  * Checks if the serialversionUID is static and final and adds the modifier if
@@ -21,10 +29,54 @@ public class SerialVersionUidASTVisitor extends AbstractCompilationUnitASTVisito
 		this.fullyQuallifiedNameMap.put(COLLECTION_KEY,
 				generateFullyQuallifiedNameList(COLLECTION_FULLY_QUALLIFIED_NAME));
 	}
-	
+
 	@Override
-	public boolean visit(CompilationUnit node){
-		
+	public boolean visit(FieldDeclaration node) {
+		CheckSerialUidASTVisitor checkSerialUidASTVisitor = new CheckSerialUidASTVisitor();
+		node.accept(checkSerialUidASTVisitor);
+		if (checkSerialUidASTVisitor.getSerialUidNode() != null && !checkSerialUidASTVisitor.getWantedKeyWords().isEmpty()) {
+			if (1 == node.fragments().size()) {
+				ListRewrite modifieresRewrite = astRewrite.getListRewrite(node, FieldDeclaration.MODIFIERS2_PROPERTY);
+				for(ModifierKeyword modifierKeyword : checkSerialUidASTVisitor.getWantedKeyWords()){
+					modifieresRewrite.insertLast(node.getAST().newModifier(modifierKeyword), null);
+				}
+			}
+		}
 		return true;
+	}
+
+	private class CheckSerialUidASTVisitor extends ASTVisitor {
+
+		private Set<ModifierKeyword> wantedKeyWords;
+		private VariableDeclarationFragment serialUidNode = null;
+
+		public CheckSerialUidASTVisitor() {
+			wantedKeyWords = new HashSet<>();
+			wantedKeyWords.add(ModifierKeyword.FINAL_KEYWORD);
+			wantedKeyWords.add(ModifierKeyword.STATIC_KEYWORD);
+			wantedKeyWords.add(ModifierKeyword.PRIVATE_KEYWORD);
+		}
+		
+		@Override
+		public boolean visit(VariableDeclarationFragment node) {
+			if (StringUtils.equals(node.getName().getIdentifier(), "serialVersionUID")) {
+				serialUidNode = node;
+			}
+			return true;
+		}
+
+		@Override
+		public boolean visit(Modifier node) {
+			wantedKeyWords.remove(node.getKeyword());
+			return true;
+		}
+		
+		public Set<ModifierKeyword> getWantedKeyWords() {
+			return wantedKeyWords;
+		}
+
+		public VariableDeclarationFragment getSerialUidNode() {
+			return serialUidNode;
+		}
 	}
 }
