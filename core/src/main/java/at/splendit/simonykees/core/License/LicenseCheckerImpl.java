ea747d66b0e51ea8e07590354f34b37bfc50d13b
Elham.Cheriki <Elham.Cheriki@splendit.at>
2016-12-01 14:51:22 +0100

    SIM-160 ADD factory to make interface between Plugin and License

---
+++ core/src/main/java/at/splendit/simonykees/core/License/LicenseCheckerImpl.java
@@ -0,0 +1,25 @@
+package at.splendit.simonykees.core.License;
+
+public class LicenseCheckerImpl implements LicenseChecker{
+	
+
+	@Override
+	public LicenseType getType() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public LicenseStatus getStatus() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Long getExpireDate() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+
+}
