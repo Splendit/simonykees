a057b7d70611376032c58ea167c79077f8768bbb
ardit.ymeri <ardit.ymeri@dev-pc-05>
2016-12-02 11:53:25 +0100

    SIM-164 EDIT : moving test cases from TestForToForEachRuleTODO to TestForToForEachRule

--- sample/src/test/java/at/splendit/simonykees/sample/postRule/forToForEach/TestForToForEachRule.java
+++ sample/src/test/java/at/splendit/simonykees/sample/postRule/forToForEach/TestForToForEachRule.java
@@ -3,12 +3,23 @@ package at.splendit.simonykees.sample.postRule.forToForEach;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @SuppressWarnings("nls")
 public class TestForToForEachRule {
 	
 	private List<String> generateList(String input) {
 		return Arrays.asList(input.split(";"));
+	}
+	
+	private List<Integer> generateHashCodeList(String input) {
+		List<String> foo = generateList(input);
+		List<Integer>fooHashCodes = 
+				foo
+				.stream()
+				.map(s -> s.hashCode())
+				.collect(Collectors.toList());
+		return fooHashCodes;
 	}
 	
 	public String testForToForEach(String input) {
@@ -30,5 +41,104 @@ public class TestForToForEachRule {
 		    sb.append(s);
 		    sb.append(fooIterator);
 		}
+	}
+	
+	public String testForToForEachIteratingIndex(String input) {
+		List<String> foo = generateList(input);
+		
+		StringBuilder sb = new StringBuilder();
+		
+		int i;
+		for (java.lang.String fooIterator:foo){
+			String s = fooIterator;
+			sb.append(s);
+		}
+		
+		return sb.toString();
+	}
+	
+	public String testForToForEachBiggerThanOneIterationStep(String input) {
+		List<String> foo = generateList(input);
+
+		StringBuilder sb = new StringBuilder();
+		
+		int i;
+		for (i = 0; i < foo.size(); i = i + 2) {
+			String s = foo.get(i);
+			sb.append(s);
+		}
+		
+		return sb.toString();
+	}
+
+	public String testForToForEachMultipleInitForStatement(String input) {
+		List<String> foo = generateList(input);
+		
+		StringBuilder sb = new StringBuilder();
+
+		int i, a;
+		for (i = 0, a = 0; i < foo.size(); i++) {
+			String s = foo.get(i);
+			sb.append(s);
+		}
+		
+		return sb.toString();
+	}
+
+	public String testForToForEachMultipleInitAndIncStatement(String input) {
+		List<String> foo = generateList(input);
+		
+		StringBuilder sb = new StringBuilder();
+
+		int i, a;
+		for (i = 0, a = 0; i < foo.size(); i++, a++) {
+			String s = foo.get(i);
+			sb.append(s);
+		}
+		
+		return sb.toString();
+	}
+	
+	public String testForToForEachWithArray(String input) {
+		
+		List<String> foo = generateList(input);
+		String[] ms = (String[]) foo.toArray();
+		StringBuilder sb = new StringBuilder();
+
+		for (int i = 0; i < ms.length; i++) {
+			String s = ms[i];
+			sb.append(s);
+		}
+		
+		return sb.toString();
+	}
+	
+//	public String testForToForEachNumberCollection(String input) {
+//		List<? extends Number> foo = generateHashCodeList(input);
+//		
+//		StringBuilder sb = new StringBuilder();
+//		
+//		int i;
+//		for (i = 0; i < foo.size(); i = i + 1) { 
+// 			FIXME SIM-163 : if the collection type restricted to certain sub-types, the forEach iterator type shall be 'parent type'
+//			Number s = foo.get(i);
+//			sb.append(s.toString());
+//		}
+//		
+//		return sb.toString();
+//	}
+	
+	public String testForToForEachNumberCollectionBigIterayionStep(String input) {
+		List<? extends Number> foo = generateHashCodeList(input);
+		
+		StringBuilder sb = new StringBuilder();
+		
+		int i;
+		for (i = 0; i < foo.size(); i = i + 2) {
+			Number s = foo.get(i);
+			sb.append(s.toString());
+		}
+		
+		return sb.toString();
 	}
 }
