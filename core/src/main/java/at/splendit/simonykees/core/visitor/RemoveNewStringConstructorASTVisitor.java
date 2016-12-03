f1ac9f1a02a10b8b4103bc9c0c26f9ebe02d62e2
Martin Huter <martin.huter@splendit.at>
2016-12-03 15:36:22 +0100

    SIM-30 ADD implementation of visitor

--- core/src/main/java/at/splendit/simonykees/core/visitor/RemoveNewStringConstructorASTVisitor.java
+++ core/src/main/java/at/splendit/simonykees/core/visitor/RemoveNewStringConstructorASTVisitor.java
@@ -1,5 +1,13 @@
 package at.splendit.simonykees.core.visitor;
 
+import java.util.List;
+
+import org.eclipse.jdt.core.dom.ClassInstanceCreation;
+import org.eclipse.jdt.core.dom.Expression;
+import org.eclipse.jdt.core.dom.StringLiteral;
+
+import at.splendit.simonykees.core.util.ClassRelationUtil;
+
 /**
  * Finds all class instantiations of string over no input parameter or string
  * because its an useless construction. The wrapping of the string is resolved
@@ -19,5 +27,31 @@ public class RemoveNewStringConstructorASTVisitor extends AbstractCompilationUni
 	public RemoveNewStringConstructorASTVisitor() {
 		super();
 		this.fullyQuallifiedNameMap.put(STRING_KEY, generateFullyQuallifiedNameList(STRING_FULLY_QUALLIFIED_NAME));
+	}
+
+	@Override
+	public boolean visit(ClassInstanceCreation node) {
+		if (ClassRelationUtil.isContentOfRegistertITypes(node.getType().resolveBinding(),
+				this.iTypeMap.get(STRING_KEY))) {
+			/**
+			 * node.arguments() javadoc shows that it elements are at least
+			 * Expression
+			 */
+			@SuppressWarnings("unchecked")
+			List<Expression> arguments = (List<Expression>) node.arguments();
+			/**
+			 * new String() resolves to ""
+			 */
+			if (0 == arguments.size()) {
+				astRewrite.replace(node, node.getAST().newStringLiteral(), null);
+			} else if (1 == arguments.size()) {
+				Expression argument = arguments.get(0);
+				if (argument instanceof StringLiteral || ClassRelationUtil
+						.isContentOfRegistertITypes(argument.resolveTypeBinding(), iTypeMap.get(STRING_KEY))) {
+					astRewrite.replace(node, astRewrite.createMoveTarget(argument), null);
+				}
+			}
+		}
+		return true;
 	}
 }
