3c288a484989da393f38446c23f555a4dedb1011
ardit.ymeri <ardit.ymeri@dev-pc-05>
2016-12-06 13:27:10 +0100

    SIM-128 ADD: new class for caching result of a validation call

---
+++ core/src/main/java/at/splendit/simonykees/core/License/ValidationResultCache.java
@@ -0,0 +1,47 @@
+package at.splendit.simonykees.core.License;
+
+import java.time.Instant;
+import java.util.HashMap;
+
+import com.labs64.netlicensing.domain.Constants.ValidationResult;
+
+/**
+ * A cash implementation for storing the result of a single validation call.
+ * 
+ * @author ardit.ymeri
+ *
+ */
+public class ValidationResultCache {
+	private static ValidationResultCache instance;
+	
+	private final String TIME_STAMP_KEY = "time-stamp";
+	private final String VALIDATION_RESULT_KEY = "validation-result";
+	private HashMap<String, Object> cacheHashMap = new HashMap<>();
+
+
+	private ValidationResultCache() {
+
+	}
+	
+	public void updateCachedResult(ValidationResult validationResult, Instant timestamp) {
+		cacheHashMap.put(TIME_STAMP_KEY, timestamp);
+		cacheHashMap.put(VALIDATION_RESULT_KEY, validationResult);
+	}
+	
+	public ValidationResult getCachedValidationResult() {
+		// TODO: throw an exception if cache is empty
+		return (ValidationResult) cacheHashMap.get(VALIDATION_RESULT_KEY);
+	}
+	
+	public Instant getValidationTimestamp() {
+		// TODO: throw an exception if cache is empty
+		return (Instant) cacheHashMap.get(TIME_STAMP_KEY);
+	}
+
+	public synchronized static ValidationResultCache getInstance() {
+		if (instance == null) {
+			instance = new ValidationResultCache();
+		}
+		return instance;
+	}
+}
