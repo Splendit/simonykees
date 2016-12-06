78cfb2c34adcc9166ff097c934c3d0309eac501e
ardit.ymeri <ardit.ymeri@dev-pc-05>
2016-12-06 15:25:51 +0100

    SIM-128 UPDATE: LicenseManager gets necessary data from validation cache

--- core/src/main/java/at/splendit/simonykees/core/License/LicenseManager.java
+++ core/src/main/java/at/splendit/simonykees/core/License/LicenseManager.java
@@ -1,5 +1,9 @@
 package at.splendit.simonykees.core.License;
 
+import java.time.Instant;
+
+import com.labs64.netlicensing.domain.vo.ValidationResult;
+
 /**
  * Manager of the validation process. Starts the validation process, caches the
  * result of the validation, and provides relevant information of the validation
@@ -10,8 +14,8 @@ package at.splendit.simonykees.core.License;
  */
 public class LicenseManager {
 
-	private final String LICENSEE_NAME = "test-licensee-name"; // to be provided as a parameter, or to be read from an encrypted file.
-	private final String LICENSEE_NUMBER = "test-licensee-number"; // to be provided as a parameter, or to be read from an encrypted file.
+	private final String LICENSEE_NAME = "test-licensee-name"; // to be provided as a parameter.
+	private final String LICENSEE_NUMBER = "test-licensee-number"; // to be provided as a parameter.
 	private final boolean DO_VALIDATE = true;
 	private final long VALIDATE_INTERVAL = 5; // validation interval in seconds.
 	
@@ -39,6 +43,16 @@ public class LicenseManager {
 		licensee = new LicenseeEntity(LICENSEE_NAME, LICENSEE_NUMBER);
 		
 		ValidateExecutor.startSchedule(schedulerEntity, licensee);
+	}
+	
+	public LicenseChecker getValidationData() {
+		ValidationResultCache cache = ValidationResultCache.getInstance();
+		ValidationResult validationResult = cache.getCachedValidationResult();
+		Instant timestamp = cache.getValidationTimestamp();
+		
+		LicenseCheckerImpl checker = new LicenseCheckerImpl(validationResult, timestamp);
+		
+		return checker;
 	}
 	
 	// TODO: override clone()
