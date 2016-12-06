6a9940a9dfde0e2731bb359701c9da45515d42d4
ardit.ymeri <ardit.ymeri@dev-pc-05>
2016-12-06 15:23:43 +0100

    SIM-128 UPDATE: extracting relevant data from ValidatioResult

--- core/src/main/java/at/splendit/simonykees/core/License/LicenseCheckerImpl.java
+++ core/src/main/java/at/splendit/simonykees/core/License/LicenseCheckerImpl.java
@@ -1,33 +1,85 @@
 package at.splendit.simonykees.core.License;
 
 import java.time.Instant;
+import java.util.Map;
+
+import com.labs64.netlicensing.domain.vo.Composition;
+import com.labs64.netlicensing.domain.vo.ValidationResult;
 
 public class LicenseCheckerImpl implements LicenseChecker {
 
-	// TODO: all the values are to be read from cash!
+	private LicenseType licenseType;
+	private boolean status;
+	private Instant timestamp;
+	private String sessionId;
+
+	// TODO: check if the following keys match with the keys of the validation
+	// properties
+	private final String LICENSING_MODEL_KEY = "productModuleNumber";
+	private final String SESSION_ID_KEY = "sessionId";
+	private final String VALID_KEY = "valid";
+
+	public LicenseCheckerImpl(ValidationResult validationResult, Instant timestamp) {
+		setTimestamp(timestamp);
+		extractValidationData(validationResult);
+	}
+
+	private void extractValidationData(ValidationResult validationResult) {
+		Map<String, Composition> validations = validationResult.getValidations();
+		validations.values().forEach(composition -> {
+			composition.getProperties().forEach((key, value) -> {
+
+				switch (key) {
+				case LICENSING_MODEL_KEY:
+					LicenseType type = LicenseType.fromString(value.getValue());
+					setLicenseType(type);
+					break;
+				case SESSION_ID_KEY:
+					setSessionId(value.getValue());
+					break;
+				case VALID_KEY:
+					boolean valid = Boolean.valueOf(value.getValue());
+					setStatus(valid);
+					break;
+				}
+				
+			});
+		});
+	}
 
 	@Override
 	public LicenseType getType() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.licenseType;
 	}
 
 	@Override
 	public boolean getStatus() {
-		// TODO Auto-generated method stub
-		return false;
+		return this.status;
 	}
 
 	@Override
 	public Instant getValidationTimeStamp() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.timestamp;
 	}
 
 	@Override
 	public String getFloatingSessionId() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.sessionId;
 	}
 
+	private void setLicenseType(LicenseType licenseType) {
+		this.licenseType = licenseType;
+	}
+
+	private void setSessionId(String sessionId) {
+		this.sessionId = sessionId;
+	}
+
+	private void setStatus(boolean status) {
+		this.status = status;
+	}
+
+	private void setTimestamp(Instant timestamp) {
+		this.timestamp = timestamp;
+	}
 }
--- core/src/main/java/at/splendit/simonykees/core/License/LicenseType.java
+++ core/src/main/java/at/splendit/simonykees/core/License/LicenseType.java
@@ -4,7 +4,26 @@ public enum LicenseType {
 	
 	TRIAL,
 	FLOATING,
-	NODE_LOCKED
+	NODE_LOCKED;
 	
-
+	public static LicenseType fromString(String value) {
+		LicenseType licenseType = TRIAL;
+		
+		// TODO: check if the cases really match with the returned values.
+		
+		switch(value.toLowerCase()) {
+		case "trial":
+			licenseType = TRIAL;
+			break;
+		case "floating":
+			licenseType = FLOATING;
+			break;
+		case "nodelocked":
+			licenseType = NODE_LOCKED;
+			break;
+		}
+		
+		return licenseType;
+	}
+	
 }
