a89f4971c563f6a18d79b66bd69e6455cdfd69b1
Martin Huter <martin.huter@splendit.at>
2016-12-12 11:18:41 +0100

    SIM-55 ADD implementation, more tests

--- sample/src/test/java/at/splendit/simonykees/sample/postRule/allRules/TestStringConcatToPlusRule.java
+++ sample/src/test/java/at/splendit/simonykees/sample/postRule/allRules/TestStringConcatToPlusRule.java
@@ -1,37 +1,37 @@
-package at.splendit.simonykees.sample.preRule;
+package at.splendit.simonykees.sample.postRule.allRules;
 
 @SuppressWarnings("nls")
 public class TestStringConcatToPlusRule {
 
 	public String testConcatWithLiteral(String input) {
-		return input.concat("abc");
+		return input + "abc";
 	}
 
 	public String testConcatWithVariable(String input, String param) {
-		return input.concat(param);
+		return input + param;
 	}
 
 	public String testConcatOnlyLiterals() {
-		return "abc".concat("def");
+		return "abc" + "def";
 	}
 
 	public String testConcatRecursionWithLiteral(String input) {
-		return input.concat("abc".concat("def"));
+		return input + "abc" + "def";
 	}
 
 	public String testConcatRecursionWithParam(String input, String param) {
-		return input.concat(param.concat(param));
+		return input + param + param;
 	}
 
 	public String testConcatRecursionWithParamAndCast(String input, String param) {
-		return input.concat((String) param.concat(param));
+		return input + (String) param + param;
 	}
 
 	public String testConcatMixedWithPlus(String input, String param) {
-		return input.concat(param.concat(param)) + input.concat(param.concat(param));
+		return input + param + param + input + param + param;
 	}
 
 	public String testConcatWithToString(String input, String param) {
-		return input.concat(param).toString();
+		return input + param;
 	}
 }
--- sample/src/test/java/at/splendit/simonykees/sample/postRule/stringConcat/TestStringConcatToPlusRule.java
+++ sample/src/test/java/at/splendit/simonykees/sample/postRule/stringConcat/TestStringConcatToPlusRule.java
@@ -10,16 +10,28 @@ public class TestStringConcatToPlusRule {
 	public String testConcatWithVariable(String input, String param) {
 		return input + param;
 	}
-	
+
 	public String testConcatOnlyLiterals() {
 		return "abc" + "def";
 	}
-	
+
 	public String testConcatRecursionWithLiteral(String input) {
 		return input + "abc" + "def";
 	}
-	
+
 	public String testConcatRecursionWithParam(String input, String param) {
 		return input + param + param;
+	}
+
+	public String testConcatRecursionWithParamAndCast(String input, String param) {
+		return input + (String) param + param;
+	}
+
+	public String testConcatMixedWithPlus(String input, String param) {
+		return input + param + param + input + param + param;
+	}
+
+	public String testConcatWithToString(String input, String param) {
+		return (input + param).toString();
 	}
 }
