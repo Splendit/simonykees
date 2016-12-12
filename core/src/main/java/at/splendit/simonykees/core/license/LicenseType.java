025770c652611b860f963ef9bf1ef64a51ee7281
ardit.ymeri <ardit.ymeri@dev-pc-05>
2016-12-12 19:34:00 +0100

    SIM-128 UPDATE: fixing license type enumeration

--- core/src/main/java/at/splendit/simonykees/core/license/LicenseType.java
+++ core/src/main/java/at/splendit/simonykees/core/license/LicenseType.java
@@ -2,23 +2,23 @@ package at.splendit.simonykees.core.license;
 
 public enum LicenseType {
 	
-	TRIAL,
+	TRY_AND_BUY,
 	FLOATING,
 	NODE_LOCKED;
 	
 	public static LicenseType fromString(String value) {
-		LicenseType licenseType = TRIAL;
+		LicenseType licenseType = TRY_AND_BUY;
 		
 		// TODO: check if the cases really match with the returned values.
 		
 		switch(value.toLowerCase()) {
-		case "trial":
-			licenseType = TRIAL;
+		case "tryandbuy":
+			licenseType = TRY_AND_BUY;
 			break;
 		case "floating":
 			licenseType = FLOATING;
 			break;
-		case "nodelocked":
+		case "multifeature":
 			licenseType = NODE_LOCKED;
 			break;
 		}
