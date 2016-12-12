b182521fa9eb0e50955524e5172057b3a935a091
ardit.ymeri <ardit.ymeri@dev-pc-05>
2016-12-12 19:35:07 +0100

    SIM-128 UPDATE: setting new PASS_APIKEY as an authentication token

--- core/src/main/java/at/splendit/simonykees/core/license/RestApiConnection.java
+++ core/src/main/java/at/splendit/simonykees/core/license/RestApiConnection.java
@@ -12,7 +12,7 @@ import com.labs64.netlicensing.domain.vo.SecurityMode;
 public class RestApiConnection {
 	private static final String REST_API_PATH = "/core/v2/rest";
 	private static final String BASE_URL_PROD = "https://go.netlicensing.io";
-	private static final String PASS_APIKEY = "5e6b99b7-4a6f-4a33-b94e-67f3552e0925";
+	private static final String PASS_APIKEY = "bf7f1092-3c88-492f-8a52-d00823d225a8";
 	private static final String BASE_URL = BASE_URL_PROD + REST_API_PATH;
 	private static final Context context = new Context();
 	private static RestApiConnection instance;
