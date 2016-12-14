fa51d118a9d43784fba5b798e6e0fbe0b8207ac6
Ludwig Werzowa <ludwig.werzowa@splendit.at>
2016-12-14 18:01:59 +0100

    SIM-42 UPDATE changed to a final class with a private constructor as described as best practice by http://stackoverflow.com/a/479582 and http://stackoverflow.com/a/320642.

--- core/src/main/java/at/splendit/simonykees/core/constants/ReservedNames.java
+++ core/src/main/java/at/splendit/simonykees/core/constants/ReservedNames.java
@@ -1,26 +1,29 @@
 package at.splendit.simonykees.core.constants;
 
 @SuppressWarnings("nls")
-abstract public class ReservedNames {
+public final class ReservedNames {
 	public static final String INTEGER = "Integer";
 	public static final String INTEGER_PRIMITIVE = "int";
-	
-	
+
 	public static final String LONG = "Long";
 	public static final String LONG_PRIMITIVE = "long";
 	public static final String LONG_LITERAL = "L";
-	
+
 	public static final String FLOAT = "Float";
 	public static final String FLOAT_PRIMITIVE = "float";
 	public static final String FLOAT_LITERAL = "F";
-	
+
 	public static final String DOUBLE = "Double";
 	public static final String DOUBLE_PRIMITIVE = "double";
 	public static final String DOUBLE_LITERAL = "D";
-	
+
 	/** MI = Method Invocation */
 	public static final String MI_TO_STRING = "toString";
 	/** MI = Method Invocation */
 	public static final String MI_VALUE_OF = "valueOf";
-	
+
+	private ReservedNames() {
+		// this class should not be instantiated
+	}
+
 }
