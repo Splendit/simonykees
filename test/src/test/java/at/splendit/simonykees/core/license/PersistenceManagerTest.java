17aa1c46ec1f1a1c8077fbb369caf78aaad4e287
ardit.ymeri <ardit.ymeri@splendit.at>
2017-01-02 11:33:45 +0100

    SIM-128 UPDATE: adding test cases for validating with persisted data

--- test/src/test/java/at/splendit/simonykees/core/license/PersistenceManagerTest.java
+++ test/src/test/java/at/splendit/simonykees/core/license/PersistenceManagerTest.java
@@ -94,4 +94,214 @@ public class PersistenceManagerTest {
 		assertEquals(orgModel.getSubscriptionExpirationDate(), decModel.getSubscriptionExpirationDate());
 		assertEquals(orgModel.getSubscriptionStatus(), decModel.getSubscriptionStatus());
 	}
+	
+	@Test
+	public void validateNodeLocked() {
+		// having stored a valid persistence model of node locked license...
+		PersistenceManager persistenceMng = PersistenceManager.getInstance();
+		Instant nowMin30sec = Instant.now().minusSeconds(30);
+		Instant nowMin35sec = Instant.now().minusSeconds(35);
+		ZonedDateTime demoExpireDate  = ZonedDateTime.now().plusDays(4);
+		ZonedDateTime subscriptionExpires = ZonedDateTime.now().plusDays(350);
+		PersistenceModel orgModel = new PersistenceModel(
+				"test-number", 
+				"test-name", 
+				true,  // last validation status
+				LicenseType.NODE_LOCKED, // license type
+				nowMin30sec, // validation time stamp
+				demoExpireDate, 
+				null,  // expiration time stamp
+				subscriptionExpires, 
+				true, // last subscription status
+				nowMin35sec, 
+				LicenseType.NODE_LOCKED);
+		persistenceMng.setPersistenceModel(orgModel);
+		persistenceMng.persist();
+		
+		// when validating using the persisted data...
+		LicenseChecker checker = persistenceMng.vlidateUsingPersistedData();
+		
+		// expecting license to be valid...
+		assertTrue(checker.isValid());
+		assertEquals(LicenseStatus.CONNECTION_FAILURE, checker.getLicenseStatus());
+		assertEquals(LicenseType.NODE_LOCKED, checker.getType());
+		assertEquals(subscriptionExpires, checker.getExpirationDate());
+		assertEquals("test-name", checker.getLicenseeName());
+		assertEquals(nowMin30sec, checker.getValidationTimeStamp());
+	}
+	
+	@Test
+	public void validateFloating() {
+		// having stored a valid persistence model of some floating license...
+		PersistenceManager persistenceMng = PersistenceManager.getInstance();
+		Instant nowMin30sec = Instant.now().minusSeconds(30);
+		Instant nowMin35sec = Instant.now().minusSeconds(35);
+		ZonedDateTime demoExpireDate  = ZonedDateTime.now().plusDays(4);
+		ZonedDateTime subscriptionExpires = ZonedDateTime.now().plusDays(350);
+		PersistenceModel orgModel = new PersistenceModel(
+				"test-number", 
+				"floating test-name", 
+				true,  // last validation status
+				LicenseType.FLOATING, // license type
+				nowMin30sec, // validation time stamp
+				demoExpireDate, 
+				null,  // expiration time stamp
+				subscriptionExpires, 
+				true, // last subscription status
+				nowMin35sec, 
+				LicenseType.FLOATING);
+		persistenceMng.setPersistenceModel(orgModel);
+		persistenceMng.persist();
+		
+		// when validating using the persisted data...
+		LicenseChecker checker = persistenceMng.vlidateUsingPersistedData();
+		
+		// expecting license to be valid...
+		assertTrue(checker.isValid());
+		assertEquals(LicenseStatus.CONNECTION_FAILURE, checker.getLicenseStatus());
+		assertEquals(LicenseType.FLOATING, checker.getType());
+		assertEquals(subscriptionExpires, checker.getExpirationDate());
+		assertEquals("floating test-name", checker.getLicenseeName());
+		assertEquals(nowMin30sec, checker.getValidationTimeStamp());
+	}
+	
+	@Test
+	public void validateTryAndBuy() {
+		// having stored a valid persistence model of some demo license...
+		PersistenceManager persistenceMng = PersistenceManager.getInstance();
+		Instant nowMin30sec = Instant.now().minusSeconds(30);
+		Instant nowMin35sec = Instant.now().minusSeconds(35);
+		ZonedDateTime demoExpireDate  = ZonedDateTime.now().plusDays(4);
+		ZonedDateTime subscriptionExpires = ZonedDateTime.now().plusDays(350);
+		PersistenceModel orgModel = new PersistenceModel(
+				"test-number", 
+				"floating test-name", 
+				true,  // last validation status
+				LicenseType.TRY_AND_BUY, // license type
+				nowMin30sec, // validation time stamp
+				demoExpireDate, 
+				null,  // expiration time stamp
+				subscriptionExpires, 
+				true, // last subscription status
+				nowMin35sec, 
+				LicenseType.TRY_AND_BUY);
+		persistenceMng.setPersistenceModel(orgModel);
+		persistenceMng.persist();
+		
+		// when validating using the persisted data...
+		LicenseChecker checker = persistenceMng.vlidateUsingPersistedData();
+		
+		// expecting license to be valid...
+		assertTrue(checker.isValid());
+		assertEquals(LicenseStatus.CONNECTION_FAILURE, checker.getLicenseStatus());
+		assertEquals(LicenseType.TRY_AND_BUY, checker.getType());
+		assertEquals(subscriptionExpires, checker.getExpirationDate());
+		assertEquals("floating test-name", checker.getLicenseeName());
+		assertEquals(nowMin30sec, checker.getValidationTimeStamp());
+	}
+	
+	@Test
+	public void validateTryAndBuyExiredDemo() {
+		// having stored an expired persistence model of some demo license...
+		PersistenceManager persistenceMng = PersistenceManager.getInstance();
+		Instant nowMin1300sec = Instant.now().minusSeconds(1300);
+		Instant nowMin1350sec = Instant.now().minusSeconds(1350);
+		ZonedDateTime demoExpireDate  = ZonedDateTime.now();
+		ZonedDateTime subscriptionExpires = null;
+		PersistenceModel orgModel = new PersistenceModel(
+				"test-number", 
+				"floating test-name", 
+				true,  // last validation status
+				LicenseType.TRY_AND_BUY, // license type
+				nowMin1300sec, // validation time stamp
+				demoExpireDate, 
+				null,  // expiration time stamp
+				subscriptionExpires, 
+				true, // last subscription status
+				nowMin1350sec, 
+				LicenseType.TRY_AND_BUY);
+		persistenceMng.setPersistenceModel(orgModel);
+		persistenceMng.persist();
+		
+		// when validating using the persisted data...
+		LicenseChecker checker = persistenceMng.vlidateUsingPersistedData();
+		
+		// expecting license to NOT be valid...
+		assertFalse(checker.isValid());
+		assertEquals(LicenseStatus.CONNECTION_FAILURE, checker.getLicenseStatus());
+		assertEquals(LicenseType.TRY_AND_BUY, checker.getType());
+		assertEquals(subscriptionExpires, checker.getExpirationDate());
+		assertEquals("floating test-name", checker.getLicenseeName());
+		assertEquals(nowMin1300sec, checker.getValidationTimeStamp());
+	}
+	
+	@Test
+	public void validateExpiredValidationTimestamp() {
+		// having stored a PersistedModel with an expired validation time-stamp...
+		PersistenceManager persistenceMng = PersistenceManager.getInstance();
+		Instant nowMin3601sec = Instant.now().minusSeconds(3601);
+		Instant nowMin3605sec = Instant.now().minusSeconds(3605);
+		ZonedDateTime demoExpireDate  = ZonedDateTime.now().plusDays(4);
+		ZonedDateTime subscriptionExpires = ZonedDateTime.now().plusDays(350);
+		PersistenceModel orgModel = new PersistenceModel(
+				"test-number", 
+				"test-name", 
+				true,  // last validation status
+				LicenseType.NODE_LOCKED, // license type
+				nowMin3601sec, // validation time-stamp
+				demoExpireDate, 
+				null,  // expiration time stamp
+				subscriptionExpires, 
+				true, // last subscription status
+				nowMin3605sec, 
+				LicenseType.NODE_LOCKED);
+		persistenceMng.setPersistenceModel(orgModel);
+		persistenceMng.persist();
+		
+		// when validating using the persisted data...
+		LicenseChecker checker = persistenceMng.vlidateUsingPersistedData();
+		
+		// expecting license to NOT be valid...
+		assertFalse(checker.isValid());
+		assertEquals(LicenseStatus.CONNECTION_FAILURE, checker.getLicenseStatus());
+		assertEquals(LicenseType.NODE_LOCKED, checker.getType());
+		assertEquals(subscriptionExpires, checker.getExpirationDate());
+		assertEquals("test-name", checker.getLicenseeName());
+		assertEquals(nowMin3601sec, checker.getValidationTimeStamp());
+	}
+	
+	@Test
+	public void validateExpiredSubscription() {
+		// having stored a PersistedModel with an expired subscription date...
+		PersistenceManager persistenceMng = PersistenceManager.getInstance();
+		Instant nowMin1000sec = Instant.now().minusSeconds(1000);
+		Instant nowMin1005sec = Instant.now().minusSeconds(1005);
+		ZonedDateTime demoExpireDate  = ZonedDateTime.now().plusDays(4);
+		ZonedDateTime subscriptionExpires = ZonedDateTime.now();
+		PersistenceModel orgModel = new PersistenceModel(
+				"test-number", 
+				"test-name", 
+				true,  // last validation status
+				LicenseType.NODE_LOCKED, // license type
+				nowMin1000sec, // validation time stamp
+				demoExpireDate, 
+				null,  // expiration time stamp
+				subscriptionExpires, 
+				true, // last subscription status
+				nowMin1005sec, 
+				LicenseType.NODE_LOCKED);
+		persistenceMng.setPersistenceModel(orgModel);
+		persistenceMng.persist();
+		
+		// when validating using the persisted data...
+		LicenseChecker checker = persistenceMng.vlidateUsingPersistedData();
+		
+		// expecting license to NOT be valid...
+		assertFalse(checker.isValid());
+		assertEquals(LicenseStatus.CONNECTION_FAILURE, checker.getLicenseStatus());
+		assertEquals(LicenseType.NODE_LOCKED, checker.getType());
+		assertEquals(subscriptionExpires, checker.getExpirationDate());
+		assertEquals("test-name", checker.getLicenseeName());
+		assertEquals(nowMin1000sec, checker.getValidationTimeStamp());
+	}
 }
