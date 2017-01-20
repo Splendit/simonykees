dd739a4d4143f32a0d697f3e49ac63180854110d
ardit.ymeri <ardit.ymeri@splendit.at>
2017-01-20 17:35:34 +0100

    SIM-179 UPDATE: new corner test cases for removeNewString ctor

--- sample/src/test/java/at/splendit/simonykees/sample/postRule/removeNewString/TestRemoveNewStringConstructorRule.java
+++ sample/src/test/java/at/splendit/simonykees/sample/postRule/removeNewString/TestRemoveNewStringConstructorRule.java
@@ -3,6 +3,14 @@ package at.splendit.simonykees.sample.postRule.removeNewString;
 @SuppressWarnings("nls")
 public class TestRemoveNewStringConstructorRule {
 
+	private String sampleMethod() {
+		return "sample-method";
+	}
+
+	private String sampleMethod(String input) {
+		return "sample-method-" + input;
+	}
+
 	public String testNewEmptyStrig() {
 		return "";
 	}
@@ -21,5 +29,22 @@ public class TestRemoveNewStringConstructorRule {
 
 	public String testNewStringOnNonStringElement(StringBuilder sb) {
 		return new String(sb);
+	}
+
+	public String testNewStringOnMethodInvocation() {
+		return sampleMethod();
+	}
+
+	public String testNewStringOnMethodInvocationWithParams(String input) {
+		return sampleMethod(input);
+	}
+
+	public String testNewStringOnByteArray(String input) {
+		byte[] bytes = input.getBytes();
+		return new String(bytes);
+	}
+
+	public String testNestedNewStrings(String input) {
+		return input;
 	}
 }
--- sample/src/test/java/at/splendit/simonykees/sample/preRule/TestRemoveNewStringConstructorRule.java
+++ sample/src/test/java/at/splendit/simonykees/sample/preRule/TestRemoveNewStringConstructorRule.java
@@ -2,6 +2,14 @@ package at.splendit.simonykees.sample.preRule;
 
 @SuppressWarnings("nls")
 public class TestRemoveNewStringConstructorRule {
+	
+	private String sampleMethod() {
+		return "sample-method";
+	}
+
+	private String sampleMethod(String input) {
+		return "sample-method-" + input;
+	}
 
 	public String testNewEmptyStrig() {
 		return new String();
@@ -21,5 +29,22 @@ public class TestRemoveNewStringConstructorRule {
 
 	public String testNewStringOnNonStringElement(StringBuilder sb) {
 		return new String(sb);
+	}
+	
+	public String testNewStringOnMethodInvocation() {
+		return new String(sampleMethod());
+	}
+	
+	public String testNewStringOnMethodInvocationWithParams(String input) {
+		return new String(sampleMethod(input));
+	}
+	
+	public String testNewStringOnByteArray(String input) {
+		byte [] bytes = input.getBytes();
+		return new String(bytes);
+	}
+	
+	public String testNestedNewStrings(String input) {
+		return new String(new String(input));
 	}
 }
