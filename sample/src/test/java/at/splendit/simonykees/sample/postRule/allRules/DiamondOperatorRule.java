ae78c372058390ab6b1773fbc9a21827813a72d5
ardit.ymeri <ardit.ymeri@splendit.at>
2017-03-02 17:22:27 +0100

    SIM-280 ADD: testing diamond operator rule

---
+++ sample/src/test/java/at/splendit/simonykees/sample/postRule/allRules/DiamondOperatorRule.java
@@ -0,0 +1,107 @@
+package at.splendit.simonykees.sample.postRule.allRules;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class DiamondOperatorRule {
+
+	private class GenericSample<T> {
+		private T t;
+
+		public GenericSample(T t) {
+			this.t = t;
+		}
+
+		public T getValue() {
+			return t;
+		}
+	}
+
+	private String concatList(List objects) {
+		objects.add(new Object());
+		Object val = objects.stream().map(o -> o.toString()).collect(Collectors.joining(", "));
+		return val.toString();
+	}
+
+	public void inferListType() {
+		List<String> list = new ArrayList<String>();
+		list.add(BigDecimal.TEN.toString());
+	}
+
+	public void inferMapTypes() {
+		Map<String, Object> map = new HashMap<String, Object>();
+		Integer ten = Integer.valueOf(10);
+		map.put(BigDecimal.TEN.toString(), ten);
+	}
+
+	public void inferGenericTypes() {
+		List<? extends Number> list = new ArrayList<Number>();
+	}
+
+	public void multipleDeclarationStatement(String input) {
+		List<String> list1 = new ArrayList<String>(), list2 = new ArrayList<String>(), list3 = new ArrayList<>();
+		list1.add(input);
+		list2.add(input);
+		list3.add(input);
+	}
+
+	public void lasyInstatiation(String input) {
+		List<String> list;
+		Map<String, GenericSample<String>> map;
+		Integer ten = Integer.valueOf(10);
+		if (ten > 0) {
+			list = new ArrayList<String>();
+			map = new HashMap<String, GenericSample<String>>();
+			list.add(ten.toString());
+			map.put(input, new GenericSample<String>(ten.toString()));
+		}
+	}
+
+	public void multipleMapDeclarationStatement(String input) {
+		Map<String, Number> map1 = new HashMap<String, Number>(), map2 = new HashMap<String, Number>(),
+				map3 = new HashMap<String, Number>();
+		map1.put(input, 10);
+		map2.put(input, 11);
+		map3.put(input, 12);
+	}
+
+	public void blockWithMapAndLists(String input) {
+		List<String> list = new ArrayList<String>();
+		List<Integer> numList = new ArrayList<Integer>();
+		Map<String, Integer> map = new HashMap<String, Integer>();
+		Set<String> set = new HashSet<String>();
+		if (list.isEmpty()) {
+			Set<Integer> numSet = new HashSet<Integer>();
+			list.add(input);
+			numSet.add(input.hashCode());
+			numList.add(input.length());
+			map.put(input, input.hashCode());
+			set.add(input);
+		}
+	}
+
+	public String userDefinedGeneric(String input) {
+		GenericSample<String> userDefinedGeneric = new GenericSample<String>(input);
+
+		return userDefinedGeneric.getValue();
+	}
+
+	public void nestedGenericTypes(String input) {
+		GenericSample<String> userDefined = new GenericSample<String>(input);
+		List<GenericSample<String>> list = new ArrayList<GenericSample<String>>();
+		list.add(userDefined);
+
+		Map<String, GenericSample<String>> map = new HashMap<String, GenericSample<String>>();
+		map.put(input, userDefined);
+	}
+
+	public void diamondRuleOnMethodInvocation(String input) {
+		concatList(new ArrayList<String>());
+	}
+}
