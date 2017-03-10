5dbcf4b0fec15b2bd3b4b8bfb1078d5cd5788a8f
ardit.ymeri <ardit.ymeri@splendit.at>
2017-03-10 15:38:33 +0100

    SIM-283 UPDATE: renaming parameters conflicting with local variables

--- core/src/main/java/at/splendit/simonykees/core/visitor/loop/VariableDefinitionASTVisitor.java
+++ core/src/main/java/at/splendit/simonykees/core/visitor/loop/VariableDefinitionASTVisitor.java
@@ -1,11 +1,13 @@
 package at.splendit.simonykees.core.visitor.loop;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.jdt.core.dom.ASTNode;
+import org.eclipse.jdt.core.dom.Block;
 import org.eclipse.jdt.core.dom.ForStatement;
 import org.eclipse.jdt.core.dom.SimpleName;
-import org.eclipse.jdt.core.dom.Statement;
 import org.eclipse.jdt.core.dom.VariableDeclarationFragment;
-import org.eclipse.jdt.core.dom.VariableDeclarationStatement;
-import org.eclipse.jdt.core.dom.WhileStatement;
 
 import at.splendit.simonykees.core.visitor.AbstractCompilationUnitASTVisitor;
 
@@ -16,66 +18,65 @@ import at.splendit.simonykees.core.visitor.AbstractCompilationUnitASTVisitor;
  * @author Martin Huter
  * @since 0.9.2
  */
-class VariableDefinitionASTVisitor extends AbstractCompilationUnitASTVisitor {
-	private SimpleName variableName;
-	private Statement statement;
-	private boolean useableLoopVariable = false;
-	private VariableDeclarationStatement variableDeclarationStatement = null;
+public class VariableDefinitionASTVisitor extends AbstractCompilationUnitASTVisitor {
+	private ASTNode beforeThis;
+	private boolean endThis;
+	private List<SimpleName> variableNames;
+	private List<ASTNode> relevantBlocks;
 
-	public VariableDefinitionASTVisitor(SimpleName variableName, Statement statement) {
-		this.variableName = variableName;
-		this.statement = statement;
+	public VariableDefinitionASTVisitor() {
+		variableNames = new ArrayList<>();
+		endThis = true;
+	}
+	
+	public VariableDefinitionASTVisitor(ASTNode beforeThis, List<ASTNode>relevantBlocks) {
+		this();
+		this.beforeThis = beforeThis;
+		this.relevantBlocks = relevantBlocks;
+		
+	}
+	
+	public boolean preVisit2(ASTNode node) {
+		if(node==beforeThis){
+			endThis = false;
+		}
+		return endThis;
+	}
+	
+	@Override
+	public boolean visit(Block node) {
+		boolean inScope = false;
+		for(ASTNode block : relevantBlocks) {
+			if(block == node) {
+				inScope = true;
+			}
+		}
+		
+		return inScope;
+	}
+	
+	@Override
+	public boolean visit(ForStatement node) {
+		boolean inScope = false;
+		for(ASTNode block : relevantBlocks) {
+			if(block == node) {
+				inScope = true;
+			}
+		}
+		
+		return inScope;
 	}
 
 	@Override
 	public boolean visit(VariableDeclarationFragment node) {
-		if (node.getName().getIdentifier().equals(variableName.getIdentifier())) {
-			// this case can only happen once in the scope of the tree
-			useableLoopVariable = true;
-			return false;
+		if(endThis) {
+			variableNames.add(node.getName());
 		}
-		return true;
+		
+		return endThis;
 	}
-
-	@Override
-	public void endVisit(VariableDeclarationStatement node) {
-		if (useableLoopVariable && variableDeclarationStatement == null) {
-			variableDeclarationStatement = node;
-		}
-
-	}
-
-	@Override
-	public boolean visit(WhileStatement node) {
-		if (statement.equals(node)) {
-			return false;
-		}
-		return true;
-	}
-
-	@Override
-	public boolean visit(ForStatement node) {
-		if (statement.equals(node)) {
-			return false;
-		}
-		return true;
-	}
-
-	@Override
-	public boolean visit(SimpleName node) {
-		if ((node.getFullyQualifiedName().equals(variableName.getFullyQualifiedName()))) {
-			useableLoopVariable = false;
-			variableDeclarationStatement = null;
-			return false;
-		}
-		return true;
-	}
-
-	public VariableDeclarationStatement getVariableDeclarationStatement() {
-		return variableDeclarationStatement;
-	}
-
-	public boolean isUseableLoopVariable() {
-		return useableLoopVariable;
+	
+	public List<SimpleName> getScopeVariableNames() {
+		return variableNames;
 	}
 }
