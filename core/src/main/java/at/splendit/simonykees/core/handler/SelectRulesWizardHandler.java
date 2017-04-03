6f4001e8364cb9767c2aae0e62b67c19039fef11
Ludwig Werzowa <ludwig.werzowa@splendit.at>
2017-04-03 12:51:53 +0200

    SIM-338 ADD license check before SelectRulesWizard

--- core/src/main/java/at/splendit/simonykees/core/handler/SelectRulesWizardHandler.java
+++ core/src/main/java/at/splendit/simonykees/core/handler/SelectRulesWizardHandler.java
@@ -3,8 +3,10 @@ package at.splendit.simonykees.core.handler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.jface.wizard.WizardDialog;
+import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.handlers.HandlerUtil;
 
+import at.splendit.simonykees.core.ui.LicenseUtil;
 import at.splendit.simonykees.core.ui.SelectRulesWizard;
 
 /**
@@ -17,16 +19,24 @@ public class SelectRulesWizardHandler extends AbstractSimonykeesHandler {
 
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
-		final WizardDialog dialog = new WizardDialog(HandlerUtil.getActiveShell(event),
-				new SelectRulesWizard(getSelectedJavaElements(event)));
 
-		/*
-		 * the dialog is made as small as possible horizontally (we want line
-		 * breaks for rule descriptions)
-		 */
-		dialog.setPageSize(750, 500);
+		if (LicenseUtil.isValid()) {
+			final WizardDialog dialog = new WizardDialog(HandlerUtil.getActiveShell(event),
+					new SelectRulesWizard(getSelectedJavaElements(event)));
 
-		dialog.open();
+			/*
+			 * the dialog is made as smaller than necessary horizontally (we
+			 * want line breaks for rule descriptions)
+			 */
+			dialog.setPageSize(750, 500);
+
+			dialog.open();
+		} else {
+			// do not display the SelectRulesWizard if the license is invalid
+			final Shell shell = HandlerUtil.getActiveShell(event);
+			LicenseUtil.displayLicenseErrorDialog(shell);
+		}
+
 		return null;
 	}
 
