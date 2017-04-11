dfbd95c6e3a68061a28ead70588c14565d086e12
Ludwig Werzowa <ludwig.werzowa@splendit.at>
2017-04-11 10:32:04 +0200

    SIM-323 UPDATE formatting and added a comment

--- core/src/main/java/at/splendit/simonykees/core/Activator.java
+++ core/src/main/java/at/splendit/simonykees/core/Activator.java
@@ -48,12 +48,14 @@ public class Activator extends AbstractUIPlugin {
 		plugin = this;
 
 		/*
-		 * JNA first trys to read from jna.boot.library.path. If system property
-		 * jna.boot.library.path is set to wrong version from another project in
-		 * Eclipse where jSparrow is installed, jSparrow throws exception. If
-		 * property is reset it will try to read from jna.library.path. To avoid
-		 * that jna.nosys is set to true. This should force libraries to be
-		 * unpacked from the jar.
+		 * JNA first tries to read from jna.boot.library.path. If system
+		 * property jna.boot.library.path is set to wrong version from another
+		 * project in Eclipse where jSparrow is installed, jSparrow throws
+		 * exception. If property is reset it will try to read from
+		 * jna.library.path. To avoid that jna.nosys is set to true. This should
+		 * force libraries to be unpacked from the jar.
+		 * 
+		 * See SIM-323 and the explanatory comment. 
 		 */
 		System.setProperty("jna.boot.library.path", ""); //$NON-NLS-1$ //$NON-NLS-2$
 		System.setProperty("jna.nosys", "true"); //$NON-NLS-1$ //$NON-NLS-2$
