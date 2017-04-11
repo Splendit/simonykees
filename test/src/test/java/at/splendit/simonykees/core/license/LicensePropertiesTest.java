ebdbc046ec885e6f06f26f107fac00c77297f7f1
Martin Huter <martin.huter@splendit.at>
2017-04-11 11:45:39 +0200

    SIM-352 moved license configuration

--- test/src/test/java/at/splendit/simonykees/core/license/LicensePropertiesTest.java
+++ test/src/test/java/at/splendit/simonykees/core/license/LicensePropertiesTest.java
@@ -3,6 +3,8 @@ package at.splendit.simonykees.core.license;
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
+import at.splendit.simonykees.license.LicenseProperties;
+
 /**
  * This test class only ensures that the test environment is being used, because we basically run our license tests against
  * the Net Licensing test account.    
