5eaf3325cb213d558ed9c0d30d80c9ef1c969874
matthias.webhofer <matthias.webhofer@splendit.at>
2017-04-26 12:34:00 +0200

    SIM-379 ADD new module license.api
    this is the description interface for declarative services

---
+++ license.api/src/main/java/at/splendit/simonykees/license/api/LicenseValidationService.java
@@ -0,0 +1,39 @@
+package at.splendit.simonykees.license.api;
+
+public interface LicenseValidationService {
+	/**
+	 * starts the heart beat of the validation process
+	 */
+	void startValidation();
+	
+	/**
+	 * stops the validation process
+	 */
+	void stopValidation();
+	
+	/**
+	 * checks, if a valid license is present
+	 * @return true, if a valid license is present, false otherwise
+	 */
+	boolean isValid();
+	
+	/**
+	 * updates the license key
+	 * @param licenseKey
+	 * @param licenseName
+	 * @return ?
+	 */
+	boolean updateLicenseeNumber(String licenseKey, String licenseName);
+	
+	/**
+	 * produces human readable license information 
+	 * @return human readable license information
+	 */
+	String getDisplayableLicenseInformation();
+	
+	/**
+	 * produces human readable information about the status of the license
+	 * @return human readable status information
+	 */
+	String getLicenseStautsUserMessage();
+}
