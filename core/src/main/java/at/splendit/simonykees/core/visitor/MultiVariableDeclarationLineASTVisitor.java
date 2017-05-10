444bf99cef99f2a28fa60c360a17e2811451bce5
matthias.webhofer <matthias.webhofer@splendit.at>
2017-05-10 14:24:18 +0200

    SIM-367 UPDATE unnecessary code and commetns removed

--- core/src/main/java/at/splendit/simonykees/core/visitor/MultiVariableDeclarationLineASTVisitor.java
+++ core/src/main/java/at/splendit/simonykees/core/visitor/MultiVariableDeclarationLineASTVisitor.java
@@ -10,10 +10,7 @@ import org.eclipse.jdt.core.dom.AnnotationTypeDeclaration;
 import org.eclipse.jdt.core.dom.Block;
 import org.eclipse.jdt.core.dom.ChildListPropertyDescriptor;
 import org.eclipse.jdt.core.dom.EnumDeclaration;
-import org.eclipse.jdt.core.dom.Expression;
 import org.eclipse.jdt.core.dom.FieldDeclaration;
-import org.eclipse.jdt.core.dom.MarkerAnnotation;
-import org.eclipse.jdt.core.dom.Modifier;
 import org.eclipse.jdt.core.dom.TypeDeclaration;
 import org.eclipse.jdt.core.dom.VariableDeclarationFragment;
 import org.eclipse.jdt.core.dom.VariableDeclarationStatement;
@@ -23,6 +20,8 @@ import org.eclipse.jdt.core.dom.rewrite.ListRewrite;
 import at.splendit.simonykees.core.util.ASTNodeUtil;
 
 /**
+ * Declaring multiple variables on one line is difficult to read. This Visitor
+ * extracts these declarations and puts them in a single line for each variable.
  * 
  * @author Matthias Webhofer
  * @since 1.2
@@ -32,44 +31,26 @@ public class MultiVariableDeclarationLineASTVisitor extends AbstractASTRewriteAS
 
 	@Override
 	public boolean visit(FieldDeclaration fieldDeclaration) {
+
 		List<VariableDeclarationFragment> fragments = ASTNodeUtil.convertToTypedList(fieldDeclaration.fragments(),
 				VariableDeclarationFragment.class);
 
 		ASTRewrite astRewrite = getAstRewrite();
-//		AST ast = astRewrite.getAST();
+		AST ast = astRewrite.getAST();
 
 		if (fragments.size() > 1) {
 
 			List<FieldDeclaration> newFieldDeclarations = fragments.stream().skip(1).map(fragment -> {
 
-//				FieldDeclaration newFieldDeclaration = (FieldDeclaration) ASTNode.copySubtree(ast, fieldDeclaration);
-//				FieldDeclaration newFieldDeclaration = (FieldDeclaration) astRewrite.createCopyTarget(fieldDeclaration);
-				VariableDeclarationFragment newFragment = (VariableDeclarationFragment)astRewrite.createMoveTarget(fragment);
-				FieldDeclaration newFieldDeclaration = fieldDeclaration.getParent().getAST().newFieldDeclaration(newFragment);
+				FieldDeclaration newFieldDeclaration = (FieldDeclaration) ASTNode.copySubtree(ast, fieldDeclaration);
+				VariableDeclarationFragment newFragment = (VariableDeclarationFragment) astRewrite
+						.createMoveTarget(fragment);
 
-
-
-//				newFieldDeclaration.fragments().clear();
-				ListRewrite newFieldListRewrite = astRewrite.getListRewrite(newFieldDeclaration, FieldDeclaration.FRAGMENTS_PROPERTY);
-//				ASTNodeUtil.convertToTypedList(newFieldDeclaration.fragments(), VariableDeclarationFragment.class)
-//				.forEach(ef -> newFieldListRewrite.remove(ef, null));
-//				newFieldListRewrite.insertFirst(newFragment, null);
-				ASTNodeUtil.convertToTypedList(fieldDeclaration.modifiers(), Modifier.class).forEach(modifier -> {
-					
-					astRewrite.getListRewrite(newFieldDeclaration, FieldDeclaration.MODIFIERS2_PROPERTY).insertLast(modifier, null);
-				}
-				);
+				newFieldDeclaration.fragments().clear();
 				
-				ASTNodeUtil.convertToTypedList(fieldDeclaration.modifiers(), MarkerAnnotation.class).forEach(modifier -> {
-					MarkerAnnotation m = (MarkerAnnotation)astRewrite.createCopyTarget(modifier);
-					astRewrite.getListRewrite(newFieldDeclaration, FieldDeclaration.MODIFIERS2_PROPERTY).insertLast(m, null);
-				}
-				);
-				
-				
-//				newFieldDeclaration.fragments().add(newFragment);
-
-//				astRewrite.getListRewrite(fieldDeclaration, FieldDeclaration.FRAGMENTS_PROPERTY).remove(fragment, null);
+				ListRewrite fieldRewrite = astRewrite.getListRewrite(newFieldDeclaration,
+						FieldDeclaration.FRAGMENTS_PROPERTY);
+				fieldRewrite.insertLast(newFragment, null);
 
 				return newFieldDeclaration;
 			}).collect(Collectors.toList());
@@ -110,17 +91,15 @@ public class MultiVariableDeclarationLineASTVisitor extends AbstractASTRewriteAS
 						VariableDeclarationStatement newVariableDeclarationStatement = (VariableDeclarationStatement) ASTNode
 								.copySubtree(ast, variableDeclarationStatement);
 						VariableDeclarationFragment newFragment = (VariableDeclarationFragment) astRewrite
-								.createCopyTarget(fragment);
+								.createMoveTarget(fragment);
 
 						newVariableDeclarationStatement.fragments().clear();
-						newVariableDeclarationStatement.fragments().add(newFragment);
-
-						astRewrite.remove(fragment, null);
+						ListRewrite variableRewrite = astRewrite.getListRewrite(newVariableDeclarationStatement,
+								VariableDeclarationStatement.FRAGMENTS_PROPERTY);
+						variableRewrite.insertLast(newFragment, null);
 
 						return newVariableDeclarationStatement;
 					}).collect(Collectors.toList());
-
-			ChildListPropertyDescriptor propertyDescriptor;
 
 			ListRewrite listRewrite = astRewrite.getListRewrite(variableDeclarationStatement.getParent(),
 					Block.STATEMENTS_PROPERTY);
