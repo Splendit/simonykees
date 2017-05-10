e2f9b752f60ff1b792298e6765bfe8a7c4b73e49
Ludwig Werzowa <ludwig.werzowa@splendit.at>
2017-05-10 16:57:29 +0200

    SIM-380 ADD re-added the OrganiseImport and CodeFormatter rules

--- core/src/main/java/at/splendit/simonykees/core/rule/Tag.java
+++ core/src/main/java/at/splendit/simonykees/core/rule/Tag.java
@@ -7,6 +7,7 @@ import java.util.stream.Collectors;
 
 import at.splendit.simonykees.core.rule.impl.ArithmethicAssignmentRule;
 import at.splendit.simonykees.core.rule.impl.BracketsToControlRule;
+import at.splendit.simonykees.core.rule.impl.CodeFormatterRule;
 import at.splendit.simonykees.core.rule.impl.CollectionRemoveAllRule;
 import at.splendit.simonykees.core.rule.impl.DiamondOperatorRule;
 import at.splendit.simonykees.core.rule.impl.FieldNameConventionRule;
@@ -14,6 +15,7 @@ import at.splendit.simonykees.core.rule.impl.ForToForEachRule;
 import at.splendit.simonykees.core.rule.impl.FunctionalInterfaceRule;
 import at.splendit.simonykees.core.rule.impl.InefficientConstructorRule;
 import at.splendit.simonykees.core.rule.impl.MultiCatchRule;
+import at.splendit.simonykees.core.rule.impl.OrganiseImportsRule;
 import at.splendit.simonykees.core.rule.impl.OverrideAnnotationRule;
 import at.splendit.simonykees.core.rule.impl.PrimitiveBoxedForStringRule;
 import at.splendit.simonykees.core.rule.impl.RearrangeClassMembersRule;
@@ -37,8 +39,17 @@ import at.splendit.simonykees.core.rule.impl.WhileToForEachRule;
 @SuppressWarnings("nls")
 public enum Tag {
 
-	LOOP("loop"), JAVA_0_9("0.9"), JAVA_1_1("1.1", "1"), JAVA_1_2("1.2", "2"), JAVA_1_3("1.3", "3"), JAVA_1_4("1.4",
-			"4"), JAVA_1_5("1.5", "5"), JAVA_1_6("1.6", "6"), JAVA_1_7("1.7", "7"), JAVA_1_8("1.8", "8"), EMPTY();
+	LOOP("loop"),
+	JAVA_0_9("0.9"),
+	JAVA_1_1("1.1", "1"),
+	JAVA_1_2("1.2", "2"),
+	JAVA_1_3("1.3", "3"),
+	JAVA_1_4("1.4", "4"),
+	JAVA_1_5("1.5", "5"),
+	JAVA_1_6("1.6", "6"),
+	JAVA_1_7("1.7", "7"),
+	JAVA_1_8("1.8", "8"),
+	EMPTY();
 	// not yet arrived! JAVA_1_9("1.5","5");
 
 	private List<String> tagName;
@@ -58,6 +69,8 @@ public enum Tag {
 			return Arrays.asList(JAVA_1_4);
 		} else if (BracketsToControlRule.class == clazz) {
 			return Arrays.asList(JAVA_1_1);
+		} else if (CodeFormatterRule.class == clazz) {
+			return Arrays.asList(JAVA_1_1);
 		} else if (CollectionRemoveAllRule.class == clazz) {
 			return Arrays.asList(JAVA_1_2);
 		} else if (DiamondOperatorRule.class == clazz) {
@@ -72,6 +85,8 @@ public enum Tag {
 			return Arrays.asList(JAVA_1_5);
 		} else if (MultiCatchRule.class == clazz) {
 			return Arrays.asList(JAVA_1_7);
+		} else if (OrganiseImportsRule.class == clazz) {
+			return Arrays.asList(JAVA_1_1);
 		} else if (OverrideAnnotationRule.class == clazz) {
 			return Arrays.asList(JAVA_1_6);
 		} else if (PrimitiveBoxedForStringRule.class == clazz) {
