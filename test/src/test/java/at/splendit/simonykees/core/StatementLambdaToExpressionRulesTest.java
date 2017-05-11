3bac11ec24846d3e6336a7ccf9cf0c180e75b1a4
matthias.webhofer <matthias.webhofer@splendit.at>
2017-05-11 16:44:03 +0200

    SIM-48 UPDATE postrule package

--- test/src/test/java/at/splendit/simonykees/core/StatementLambdaToExpressionRulesTest.java
+++ test/src/test/java/at/splendit/simonykees/core/StatementLambdaToExpressionRulesTest.java
@@ -8,15 +8,15 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
 
-import at.splendit.simonykees.core.rule.impl.StringLiteralEqualityCheckRule;
+import at.splendit.simonykees.core.rule.impl.StatementLambdaToExpressionRule;
 import at.splendit.simonykees.core.util.RulesTestUtil;
-import at.splendit.simonykees.core.visitor.StringLiteralEqualityCheckASTVisitor;
+import at.splendit.simonykees.core.visitor.StatementLambdaToExpressionASTVisitor;
 
 @RunWith(Parameterized.class)
 @SuppressWarnings("nls")
 public class StatementLambdaToExpressionRulesTest extends AbstractRulesTest {
-	private static final String POSTRULE_PACKAGE = RulesTestUtil.BASE_PACKAGE + ".postRule.stringLiteralEqualityCheck";
-	private static final String POSTRULE_DIRECTORY = RulesTestUtil.BASE_DIRECTORY + "/postRule/stringLiteralEqualityCheck";
+	private static final String POSTRULE_PACKAGE = RulesTestUtil.BASE_PACKAGE + ".postRule.statementLambdaToExpression";
+	private static final String POSTRULE_DIRECTORY = RulesTestUtil.BASE_DIRECTORY + "/postRule/statementLambdaToExpression";
 
 	private String fileName;
 	private Path preRule;
@@ -26,7 +26,7 @@ public class StatementLambdaToExpressionRulesTest extends AbstractRulesTest {
 		this.fileName = fileName;
 		this.preRule = preRule;
 		this.postRule = postRule;
-		rulesList.add(new StringLiteralEqualityCheckRule(StringLiteralEqualityCheckASTVisitor.class));
+		rulesList.add(new StatementLambdaToExpressionRule(StatementLambdaToExpressionASTVisitor.class));
 	}
 	
 	@Parameters(name = "{index}: test file[{0}]")
