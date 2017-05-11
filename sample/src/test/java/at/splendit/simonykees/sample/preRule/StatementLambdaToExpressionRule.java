2185e5f0e15c45aa8ae299977d71b8545cba84c7
matthias.webhofer <matthias.webhofer@splendit.at>
2017-05-11 16:44:57 +0200

    SIM-48 ADD rule samples implemented

--- sample/src/test/java/at/splendit/simonykees/sample/preRule/StatementLambdaToExpressionRule.java
+++ sample/src/test/java/at/splendit/simonykees/sample/preRule/StatementLambdaToExpressionRule.java
@@ -1,12 +1,31 @@
 package at.splendit.simonykees.sample.preRule;
 
+import java.util.List;
+import java.util.function.Function;
+
 /**
  * 
  * @author Matthias Webhofer
  * @since 1.2
  *
  */
-@SuppressWarnings({ "unused", "nls" })
+@SuppressWarnings({ "unchecked", "rawtypes", "unused" })
 public class StatementLambdaToExpressionRule {
+	public Function<Function, Function> f = (Function function) -> {
+		return function.compose(function);
+	};
 
+	private Function<Function, Function> g = (Function function) -> function.compose(function);
+
+	private void testMethod(List<Integer> list) {
+		list.stream().map(element -> element * 2);
+		list.stream().map(element -> {
+			element *= 2;
+			element += 1;
+			return element;
+		});
+		list.stream().map(element -> {
+			return element * 2;
+		});
+	}
 }
