607d71d140bf0d798d5037e8650416ebdc47cb7b
matthias.webhofer <matthias.webhofer@splendit.at>
2017-05-12 12:44:01 +0200

    SIM-48 UPDATE javadoc added and bugs fixed

--- core/src/main/java/at/splendit/simonykees/core/visitor/StatementLambdaToExpressionASTVisitor.java
+++ core/src/main/java/at/splendit/simonykees/core/visitor/StatementLambdaToExpressionASTVisitor.java
@@ -3,11 +3,23 @@ package at.splendit.simonykees.core.visitor;
 import org.eclipse.jdt.core.dom.ASTNode;
 import org.eclipse.jdt.core.dom.Block;
 import org.eclipse.jdt.core.dom.Expression;
+import org.eclipse.jdt.core.dom.ExpressionStatement;
 import org.eclipse.jdt.core.dom.LambdaExpression;
 import org.eclipse.jdt.core.dom.ReturnStatement;
 import org.eclipse.jdt.core.dom.Statement;
 
 /**
+ * 
+ * If the body of the {@link LambdaExpression} is a {@link Block} and it only
+ * contains a single {@link ExpressionStatement} or a {@link ReturnStatement},
+ * this rule will replace the {@link Block} with the containing {@link Expression}. Hence,
+ * the statement lambda becomes an expression lambda.
+ * 
+ * before: list.stream().map(element -> { 
+ * 				return element * 2; 
+ * 			}
+ * 
+ * after: list.stream.map(element -> element * 2);
  * 
  * @author Matthias Webhofer
  * @since 1.2
@@ -25,16 +37,12 @@ public class StatementLambdaToExpressionASTVisitor extends AbstractASTRewriteAST
 				Statement statement = (Statement) block.statements().get(0);
 				if (statement instanceof ReturnStatement) {
 					ReturnStatement returnStatement = (ReturnStatement) statement;
-					Expression expression = (Expression) astRewrite.createMoveTarget(returnStatement.getExpression());
-					astRewrite.replace(block, expression, null);
+					astRewrite.replace(block, returnStatement.getExpression(), null);
+				} else if (statement instanceof ExpressionStatement) {
+					ExpressionStatement expressionStatemnet = (ExpressionStatement) statement;
+					astRewrite.replace(block, expressionStatemnet.getExpression(), null);
 				}
-			} else {
-				// do nothing
 			}
-		} else if (lambdaBody instanceof Expression) {
-			// allready finished
-		} else {
-			// error?
 		}
 
 		return true;
