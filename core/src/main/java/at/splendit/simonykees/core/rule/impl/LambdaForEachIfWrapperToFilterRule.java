ae3b287833867c4fb659bc47a21c18ec17af648b
matthias.webhofer <matthias.webhofer@splendit.at>
2017-05-23 10:43:05 +0200

    SIM-400 UPDATE use i18n messages

--- core/src/main/java/at/splendit/simonykees/core/rule/impl/LambdaForEachIfWrapperToFilterRule.java
+++ core/src/main/java/at/splendit/simonykees/core/rule/impl/LambdaForEachIfWrapperToFilterRule.java
@@ -4,13 +4,20 @@ import org.apache.commons.lang3.JavaVersion;
 
 import at.splendit.simonykees.core.rule.RefactoringRule;
 import at.splendit.simonykees.core.visitor.LambdaForEachIfWrapperToFilterASTVisitor;
+import at.splendit.simonykees.i18n.Messages;
 
+/**
+ * @see LambdaForEachIfWrapperToFilterASTVisitor
+ * 
+ * @author Matthias Webhofer
+ * @since 1.2
+ */
 public class LambdaForEachIfWrapperToFilterRule extends RefactoringRule<LambdaForEachIfWrapperToFilterASTVisitor> {
 
 	public LambdaForEachIfWrapperToFilterRule(Class<LambdaForEachIfWrapperToFilterASTVisitor> visitor) {
 		super(visitor);
-		this.name = "LambdaForEachIfWrapperToFilterRule";
-		this.description = "LambdaForEachIfWrapperToFilterRule";
+		this.name = Messages.LambdaForEachIfWrapperToFilterRule_name;
+		this.description = Messages.LambdaForEachIfWrapperToFilterRule_description;
 	}
 
 	@Override
