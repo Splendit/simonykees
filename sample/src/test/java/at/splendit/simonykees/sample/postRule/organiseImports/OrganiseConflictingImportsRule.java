77ee1a24ba50c6df74fddc73ddceb7ccfe9c0b81
Martin Huter <martin.huter@splendit.at>
2017-05-30 12:16:08 +0200

    SIM-376 READDED useless imports for mvn tests

--- sample/src/test/java/at/splendit/simonykees/sample/postRule/organiseImports/OrganiseConflictingImportsRule.java
+++ sample/src/test/java/at/splendit/simonykees/sample/postRule/organiseImports/OrganiseConflictingImportsRule.java
@@ -1,5 +1,8 @@
 package at.splendit.simonykees.sample.postRule.organiseImports;
 
+import java.lang.reflect.*;
+import java.math.BigDecimal;
+
 import at.splendit.simonykees.sample.utilities.NumberUtils;
 import at.splendit.simonykees.sample.utilities.StringUtils;
 
