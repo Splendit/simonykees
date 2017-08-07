fdf0cb68aefcab7add5f96b180e65fd36200496c
ardit.ymeri <ardit.ymeri@splendit.at>
2017-08-07 16:11:13 +0200

    SIM-644 UPDATE: fixing unit tests after merging

--- sample/src/test/java/at/splendit/simonykees/sample/postRule/allRules/LambdaToMethodReferenceRule.java
+++ sample/src/test/java/at/splendit/simonykees/sample/postRule/allRules/LambdaToMethodReferenceRule.java
@@ -137,13 +137,13 @@ public class LambdaToMethodReferenceRule {
 
 			@Override
 			public boolean hasNext() {
-				personList.stream().forEach((person) -> doSomething(person));
+				personList.forEach((person) -> doSomething(person));
 				return false;
 			}
 
 			@Override
 			public Object next() {
-				personList.stream().forEach((person) -> doSomething(person));
+				personList.forEach((person) -> doSomething(person));
 				return null;
 			}
 
