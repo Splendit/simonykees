ff15efbae2e7532db4e4e586c9163fc65fba5b60
matthias.webhofer <matthias.webhofer@splendit.at>
2017-10-17 15:54:29 +0200

    SIM-853 ADD JdtUnitFixture from core test
    this should be considered for refactoring dpending on what else is needed in the ui test. many things from core test have been removed from this file because it was not needed so far.

---
+++ ui.test/src/test/java/eu/jsparrow/ui/test/jdtunit/JdtUnitException.java
@@ -0,0 +1,11 @@
+package eu.jsparrow.ui.test.jdtunit;
+
+public class JdtUnitException extends Exception {
+
+	private static final long serialVersionUID = -9217858914225093699L;
+
+	public JdtUnitException(String string) {
+		super(string);
+	}
+
+}
