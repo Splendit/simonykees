3dd94e8d75e3495bdb27355f767ac3ed0e7e8795
matthias.webhofer <matthias.webhofer@splendit.at>
2017-10-17 16:35:08 +0200

    SIM-864 UPDATE not discarding original working copy

--- core/src/main/java/eu/jsparrow/core/refactorer/RefactoringState.java
+++ core/src/main/java/eu/jsparrow/core/refactorer/RefactoringState.java
@@ -40,7 +40,6 @@ public class RefactoringState {
 	private List<RefactoringRule<? extends AbstractASTRewriteASTVisitor>> ignoredRules = new ArrayList<>();
 
 	public RefactoringState(ICompilationUnit original, ICompilationUnit workingCopy) {
-		super();
 		this.original = original;
 		this.workingCopy = workingCopy;
 	}
@@ -152,7 +151,6 @@ public class RefactoringState {
 	public void discardWorkingCopy() {
 		try {
 			workingCopy.discardWorkingCopy();
-			original.discardWorkingCopy();
 			workingCopy.close();
 			original.close();
 		} catch (JavaModelException e) {
