b12c7e16123c13d697401342de5317336a3c54f6
ardit.ymeri <ardit.ymeri@splendit.at>
2017-10-19 11:04:49 +0200

    SIM-732 UPDATE testing class consisting of unreferenced fields

---
+++ sample/src/test/java/at/splendit/simonykees/sample/postRule/publicFieldRenaming/renaming/ClassWithUnreferencedFields.java
@@ -0,0 +1,8 @@
+package at.splendit.simonykees.sample.postRule.publicFieldRenaming.renaming;
+
+@SuppressWarnings({"nls", "unused"})
+public class ClassWithUnreferencedFields {
+	String field = "toBeRenamedTo field";
+	public String privateField = "toBeRenamedTo privateField";
+	public static String staticField = "toBeRenamedTo staticField";
+}
---
+++ sample/src/test/java/at/splendit/simonykees/sample/preRule/renaming/ClassWithUnreferencedFields.java
@@ -0,0 +1,8 @@
+package at.splendit.simonykees.sample.preRule.renaming;
+
+@SuppressWarnings({"nls", "unused"})
+public class ClassWithUnreferencedFields {
+	String FIELD = "toBeRenamedTo field";
+	public String private_field = "toBeRenamedTo privateField";
+	public static String Static_field = "toBeRenamedTo staticField";
+}
