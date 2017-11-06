db28d8c5df8ac3e0b9a1b72b3ef2a39fd519cff7
matthias.webhofer <matthias.webhofer@splendit.at>
2017-11-06 12:23:18 +0100

    SIM-610 UPDATE i18n

--- i18n/src/main/java/eu/jsparrow/i18n/Messages.java
+++ i18n/src/main/java/eu/jsparrow/i18n/Messages.java
@@ -41,6 +41,8 @@ public class Messages extends NLS {
 	public static String Activator_standalone_RulesDoNotExist;
 	public static String Activator_standalone_ConfigFileReadSuccessfully;
 
+	public static String Activator_standalone_None;
+
 	public static String Activator_standalone_noRulesSelected;
 	public static String Activator_standalone_UsingDefaultConfiguration;
 	public static String ArithmethicAssignmentRule_description;
