840aacba3f8b6689d69fcf8eb027c503a0ce8d63
Ludwig Werzowa <ludwig.werzowa@splendit.at>
2017-11-08 16:30:28 +0100

    SIM-732 UPDATE formatted reserved keyowords so we hopefully never get format changes again

--- core/src/main/java/eu/jsparrow/core/visitor/renaming/JavaReservedKeyWords.java
+++ core/src/main/java/eu/jsparrow/core/visitor/renaming/JavaReservedKeyWords.java
@@ -20,17 +20,11 @@ public class JavaReservedKeyWords {
 
 	@SuppressWarnings("nls")
 	static final String[] javaKeyWords = { "abstract", "assert", "boolean", "break", "byte", "case", "catch", "char",
-			"class", "const",
-
-			"continue", "default", "do", "double", "else", "enum", "extends", "false", "final", "finally", "float",
-
-			"for", "goto", "if", "implements", "import", "instanceof", "int", "interface", "long", "native",
-
-			"new", "null", "package", "private", "protected", "public", "return", "short", "static", "strictfp",
-			"super",
-
-			"switch", "synchronized", "this", "throw", "throws", "transient", "true", "try", "void", "volatile",
-			"while" };
+			"class", "const", "continue", "default", "do", "double", "else", "enum", "extends", "false", "final",
+			"finally", "float", "for", "goto", "if", "implements", "import", "instanceof", "int", "interface", "long",
+			"native", "new", "null", "package", "private", "protected", "public", "return", "short", "static",
+			"strictfp", "super", "switch", "synchronized", "this", "throw", "throws", "transient", "true", "try",
+			"void", "volatile", "while" };
 
 	public static boolean isKeyWord(String keyword) {
 		return (Arrays.binarySearch(javaKeyWords, keyword) >= 0);
