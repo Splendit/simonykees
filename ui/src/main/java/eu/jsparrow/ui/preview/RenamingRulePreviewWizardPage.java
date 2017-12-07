3e7ad380d270b105661df9a74d29fadb8e8d45d0
ardit.ymeri <ardit.ymeri@splendit.at>
2017-12-07 15:41:27 +0100

    SIM-963 UPDATE avoid recalculation reaching existing state

--- ui/src/main/java/eu/jsparrow/ui/preview/RenamingRulePreviewWizardPage.java
+++ ui/src/main/java/eu/jsparrow/ui/preview/RenamingRulePreviewWizardPage.java
@@ -217,18 +217,29 @@ public class RenamingRulePreviewWizardPage extends WizardPage {
 
 		FieldMetaData selectedFieldData = selectedWrapper.getFieldData();
 		if (checked) {
-			if(uncheckedFields.contains(selectedFieldData)) {
-				uncheckedFields.remove(selectedFieldData);
-			} else if (!recheckedFields.contains(selectedFieldData)) {
-				recheckedFields.add(selectedFieldData);
-			}
+			markAsNewCheck(selectedFieldData);
 			wizard.addMetaData(selectedFieldData);
 		} else {
-			uncheckedFields.add(selectedFieldData);
-			recheckedFields.remove(selectedFieldData);
+			markAsNewUncheck(selectedFieldData);
 			wizard.removeMetaData(selectedFieldData);
 		}
 		populatePreviewViewer();
+	}
+
+	private void markAsNewUncheck(FieldMetaData selectedFieldData) {
+		if(recheckedFields.contains(selectedFieldData)) {				
+			recheckedFields.remove(selectedFieldData);
+		} else if(!uncheckedFields.contains(selectedFieldData)) {				
+			uncheckedFields.add(selectedFieldData);
+		}
+	}
+
+	private void markAsNewCheck(FieldMetaData selectedFieldData) {
+		if(uncheckedFields.contains(selectedFieldData)) {
+			uncheckedFields.remove(selectedFieldData);
+		} else if (!recheckedFields.contains(selectedFieldData)) {
+			recheckedFields.add(selectedFieldData);
+		}
 	}
 
 	/**
