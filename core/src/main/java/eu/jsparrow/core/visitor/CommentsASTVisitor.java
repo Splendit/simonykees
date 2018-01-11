d801af2dbd3677c37cb8bb80248884e0dfa9d1c3
ardit.ymeri <ardit.ymeri@splendit.at>
2018-01-11 18:30:41 +0100

    SIM-727 UPDATE saving comments in try with resource rule

--- core/src/main/java/eu/jsparrow/core/visitor/CommentsASTVisitor.java
+++ core/src/main/java/eu/jsparrow/core/visitor/CommentsASTVisitor.java
@@ -1,52 +1,96 @@
 package eu.jsparrow.core.visitor;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import org.eclipse.jdt.core.ICompilationUnit;
+import org.eclipse.jdt.core.JavaModelException;
 import org.eclipse.jdt.core.dom.ASTVisitor;
 import org.eclipse.jdt.core.dom.BlockComment;
+import org.eclipse.jdt.core.dom.Comment;
+import org.eclipse.jdt.core.dom.CompilationUnit;
 import org.eclipse.jdt.core.dom.Javadoc;
 import org.eclipse.jdt.core.dom.LineComment;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
- * A visitor for collecting the comments. 
+ * A visitor for collecting the comments.
  * 
  * @author Ardit Ymeri
  * @since 2.5.0
  */
 public class CommentsASTVisitor extends ASTVisitor {
 
-	private List<LineComment> lineComments = new ArrayList<>();
-	private List<BlockComment> blockComments = new ArrayList<>();
-	private List<Javadoc> javadocs = new ArrayList<>();
+	private static final Logger logger = LoggerFactory.getLogger(CommentsASTVisitor.class);
+
+	private Map<LineComment, String> lineComments = new HashMap<>();
+	private Map<BlockComment, String> blockComments = new HashMap<>();
+	private Map<Javadoc, String> javadocs = new HashMap<>();
+	private String source;
+
+	public CommentsASTVisitor() {
+
+	}
+
+	public void parseSource(CompilationUnit compilationUnit) {
+		try {
+			String source1 = ((ICompilationUnit) compilationUnit.getJavaElement()).getSource();
+			setSource(source1);
+		} catch (JavaModelException e) {
+			logger.error("Cannot read the source of the compilation unit", e); //$NON-NLS-1$
+		}
+	}
+
+	private void setSource(String source2) {
+		this.source = source2;
+	}
 
 	@Override
 	public boolean visit(LineComment lineComment) {
-		lineComments.add(lineComment);
+		String content = findContent(lineComment);
+		lineComments.put(lineComment, content);
 		return true;
 	}
 
 	@Override
 	public boolean visit(BlockComment blockComment) {
-		blockComments.add(blockComment);
+		blockComments.put(blockComment, findContent(blockComment));
 		return true;
 	}
 
 	@Override
 	public boolean visit(Javadoc javaDoc) {
-		javadocs.add(javaDoc);
+		javadocs.put(javaDoc, findContent(javaDoc));
 		return true;
 	}
-	
-	public List<LineComment> getLineComments() {
+
+	public Map<LineComment, String> getLineComments() {
 		return this.lineComments;
 	}
-	
-	public List<BlockComment> getBlockComments() {
+
+	public Map<BlockComment, String> getBlockComments() {
 		return this.blockComments;
 	}
-	
-	public List<Javadoc> getJavadocs() {
+
+	public Map<Javadoc, String> getJavadocs() {
 		return this.javadocs;
+	}
+	
+	private String findContent(Comment comment) {
+		if(this.source == null) {
+			return null;
+		}
+		
+		int start = comment.getStartPosition();
+		int length = comment.getLength();
+		int end = start + length;
+		if(source.length() < end) {
+			return null;
+		}
+		
+		return source.substring(start, end);
 	}
 }
--- core/src/main/java/eu/jsparrow/core/visitor/impl/trycatch/TryWithResourceASTVisitor.java
+++ core/src/main/java/eu/jsparrow/core/visitor/impl/trycatch/TryWithResourceASTVisitor.java
@@ -2,17 +2,25 @@ package eu.jsparrow.core.visitor.impl.trycatch;
 
 import java.io.Closeable;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
+import org.apache.commons.lang3.StringUtils;
+import org.eclipse.jdt.core.ICompilationUnit;
+import org.eclipse.jdt.core.JavaModelException;
 import org.eclipse.jdt.core.dom.ASTNode;
 import org.eclipse.jdt.core.dom.Block;
 import org.eclipse.jdt.core.dom.CatchClause;
+import org.eclipse.jdt.core.dom.Comment;
+import org.eclipse.jdt.core.dom.CompilationUnit;
 import org.eclipse.jdt.core.dom.ITypeBinding;
+import org.eclipse.jdt.core.dom.LineComment;
 import org.eclipse.jdt.core.dom.MethodInvocation;
 import org.eclipse.jdt.core.dom.Modifier;
 import org.eclipse.jdt.core.dom.SimpleName;
+import org.eclipse.jdt.core.dom.Statement;
 import org.eclipse.jdt.core.dom.TryStatement;
 import org.eclipse.jdt.core.dom.Type;
 import org.eclipse.jdt.core.dom.VariableDeclarationExpression;
@@ -24,6 +32,7 @@ import eu.jsparrow.core.builder.NodeBuilder;
 import eu.jsparrow.core.util.ASTNodeUtil;
 import eu.jsparrow.core.util.ClassRelationUtil;
 import eu.jsparrow.core.visitor.AbstractASTRewriteASTVisitor;
+import eu.jsparrow.core.visitor.CommentsASTVisitor;
 
 /**
  * The {@link TryWithResourceASTVisitor} is used to find resources in an
@@ -39,6 +48,16 @@ public class TryWithResourceASTVisitor extends AbstractASTRewriteASTVisitor {
 	private static final String AUTO_CLOSEABLE_FULLY_QUALIFIED_NAME = java.lang.AutoCloseable.class.getName();
 	private static final String CLOSEABLE_FULLY_QUALIFIED_NAME = java.io.Closeable.class.getName();
 	static final String CLOSE = "close"; //$NON-NLS-1$
+	private CompilationUnit compilationUnit;
+	private List<Comment> comments;
+
+	@Override
+	public boolean visit(CompilationUnit cu) {
+		this.compilationUnit = cu;
+		comments = ASTNodeUtil.convertToTypedList(cu.getCommentList(), Comment.class);
+
+		return true;
+	}
 
 	// TODO improvement for suppressed deprecation needed, see SIM-878
 	@SuppressWarnings("unchecked")
@@ -58,7 +77,7 @@ public class TryWithResourceASTVisitor extends AbstractASTRewriteASTVisitor {
 			 * after first non resource object
 			 */
 			ITypeBinding typeBind = varDeclStatmentNode.getType()
-					.resolveBinding();
+				.resolveBinding();
 			if (!ClassRelationUtil.isInheritingContentOfTypes(typeBind, generateFullyQualifiedNameList(
 					AUTO_CLOSEABLE_FULLY_QUALIFIED_NAME, CLOSEABLE_FULLY_QUALIFIED_NAME))) {
 				break;
@@ -82,8 +101,8 @@ public class TryWithResourceASTVisitor extends AbstractASTRewriteASTVisitor {
 					VariableDeclarationExpression variableDeclarationExpression = varDeclStatmentNode.getAST()
 						.newVariableDeclarationExpression((VariableDeclarationFragment) ASTNode
 							.copySubtree(variableDeclarationFragment.getAST(), variableDeclarationFragment));
-					variableDeclarationExpression.setType((Type) ASTNode.copySubtree(varDeclStatmentNode.getAST(),
-							varDeclStatmentNode.getType()));
+					variableDeclarationExpression.setType(
+							(Type) ASTNode.copySubtree(varDeclStatmentNode.getAST(), varDeclStatmentNode.getType()));
 
 					List<Modifier> modifierList = ASTNodeUtil.convertToTypedList(varDeclStatmentNode.modifiers(),
 							Modifier.class);
@@ -97,23 +116,77 @@ public class TryWithResourceASTVisitor extends AbstractASTRewriteASTVisitor {
 
 					resourceList.add(variableDeclarationExpression);
 					resourceNameList.add(variableDeclarationFragment.getName());
+					List<Comment> relatedComments;
 
 					if (numFragments > 1) {
 						astRewrite.remove(variableDeclarationFragment, null);
+						relatedComments = findComments(variableDeclarationFragment);
 						numFragments--;
 					} else {
 						astRewrite.remove(varDeclStatmentNode, null);
+						relatedComments = findComments(varDeclStatmentNode);
 					}
+					
+					CommentsASTVisitor commentsVisitor = new CommentsASTVisitor();
+					commentsVisitor.parseSource(compilationUnit);
+					relatedComments.forEach(comment -> comment.accept(commentsVisitor));
+					commentsVisitor.getLineComments().forEach((key, value) -> addLineComment(node, value));
+					commentsVisitor.getBlockComments().forEach((key, value) -> addBlockComment(node, value));
+					
 				}
 			}
 		}
-
 
 		if (!resourceList.isEmpty()) {
 			replaceTryStatement(node, resourceList, resourceNameList, toBeMovedToResources);
 
 		}
 		return true;
+	}
+
+	private void addBlockComment(TryStatement node, String value) {
+		Block block = ASTNodeUtil.getSpecificAncestor(node, Block.class);
+		
+		value = StringUtils.replace(value, "\\n\\t", "\\n");
+		ListRewrite listRewrite = astRewrite.getListRewrite(block, Block.STATEMENTS_PROPERTY);
+		Statement placeHolder = (Statement) astRewrite.createStringPlaceholder(value,
+				ASTNode.EMPTY_STATEMENT);
+		listRewrite.insertBefore(placeHolder, node, null);
+	}
+	
+	private void addLineComment(TryStatement node, String content) {
+
+		Block block = ASTNodeUtil.getSpecificAncestor(node, Block.class);
+
+		ListRewrite listRewrite = astRewrite.getListRewrite(block, Block.STATEMENTS_PROPERTY);
+		Statement placeHolder = (Statement) astRewrite.createStringPlaceholder(content,
+				ASTNode.EMPTY_STATEMENT);
+		listRewrite.insertBefore(placeHolder, node, null);
+	}
+
+	private List<Comment> findComments(ASTNode node) {
+		List<Comment> relatedComments = new ArrayList<>();
+		relatedComments.addAll(findInternalComments(node));
+		int leadingCommentIndex = this.compilationUnit.firstLeadingCommentIndex(node);
+		if (leadingCommentIndex >= 0) {
+			relatedComments.add(0, comments.get(leadingCommentIndex));
+		}
+		
+		int trailCommentIndex = this.compilationUnit.lastTrailingCommentIndex(node);
+		if (trailCommentIndex >= 0) {
+			relatedComments.add(comments.get(trailCommentIndex));
+		}
+
+		return relatedComments;
+	}
+
+	private Collection<? extends Comment> findInternalComments(ASTNode node) {
+		int nodeStartPos = node.getStartPosition();
+		int nodeEndPos = nodeStartPos + node.getLength();
+
+		return comments.stream()
+			.filter(comment -> comment.getStartPosition() > nodeStartPos && comment.getStartPosition() < nodeEndPos)
+			.collect(Collectors.toList());
 	}
 
 	@SuppressWarnings("deprecation")
@@ -128,7 +201,8 @@ public class TryWithResourceASTVisitor extends AbstractASTRewriteASTVisitor {
 			.map(mapper)
 			.collect(Collectors.toList());
 
-		if (node.resources().isEmpty() && resourceList.size() != 1) {
+		if (node.resources()
+			.isEmpty() && resourceList.size() != 1) {
 
 			TryStatement tryStatement = createNewTryStatement(node, resourceList, toBeMovedToResources,
 					closeInvocations);
@@ -148,7 +222,7 @@ public class TryWithResourceASTVisitor extends AbstractASTRewriteASTVisitor {
 	@SuppressWarnings("unchecked")
 	private TryStatement createNewTryStatement(TryStatement node, List<VariableDeclarationExpression> resourceList,
 			List<VariableDeclarationFragment> toBeMovedToResources, List<MethodInvocation> closeInvocations) {
-		
+
 		TryStatement tryStatement = getASTRewrite().getAST()
 			.newTryStatement();
 		tryStatement.resources()
@@ -166,7 +240,7 @@ public class TryWithResourceASTVisitor extends AbstractASTRewriteASTVisitor {
 		tryStatement.catchClauses()
 			.addAll(newCatchClauses);
 		tryStatement.setFinally((Block) ASTNode.copySubtree(node.getAST(), node.getFinally()));
-		
+
 		return tryStatement;
 	}
 }
