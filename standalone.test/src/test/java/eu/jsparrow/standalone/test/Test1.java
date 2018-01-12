f5f81352b57de2cec873eb5ce111f75095d1d224
matthias.webhofer <matthias.webhofer@splendit.at>
2018-01-12 14:14:20 +0100

    SIM-937 ADD new test module for standalone

---
+++ standalone.test/src/test/java/eu/jsparrow/standalone/test/Test1.java
@@ -0,0 +1,12 @@
+package eu.jsparrow.standalone.test;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class Test1 {
+	@Test
+	public void test1() {
+		assertTrue(true);
+	}
+}
