13dcdc1a41448a95b774588925f314e76e2a1a28
ardit.ymeri <ardit.ymeri@splendit.at>
2018-01-16 17:29:47 +0100

    SIM-727 UPDATE saving comments in String::findFirst rule

--- core/src/main/java/eu/jsparrow/core/visitor/loop/stream/EnhancedForLoopToStreamFindFirstASTVisitor.java
+++ core/src/main/java/eu/jsparrow/core/visitor/loop/stream/EnhancedForLoopToStreamFindFirstASTVisitor.java
@@ -142,6 +142,7 @@ public class EnhancedForLoopToStreamFindFirstASTVisitor extends AbstractEnhanced
 
 			astRewrite.replace(varDeclFragment.getInitializer(), methodInvocation, null);
 			replaceLoopWithFragment(forLoop, varDeclFragment);
+			saveRelatedComments(forLoop);
 			onRewrite();
 
 		} else if ((returnStatement = isConvertableWithReturn(thenStatement, forLoop, loopParameter.getName(),
@@ -160,6 +161,7 @@ public class EnhancedForLoopToStreamFindFirstASTVisitor extends AbstractEnhanced
 					loopParameter, orElseExpression, mapCopyTargets);
 			astRewrite.replace(returnStatement.getExpression(), methodInvocation, null);
 			astRewrite.remove(forLoop, null);
+			saveRelatedComments(forLoop);
 			onRewrite();
 		}
 
