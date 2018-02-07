45987a235069a89fbcff1218fd14a368daebe9e9
matthias.webhofer <matthias.webhofer@splendit.at>
2018-02-07 17:24:44 +0100

    SIM-986 UPDATE test cases

--- standalone.test/src/test/java/eu/jsparrow/standalone/StandaloneConfigTest.java
+++ standalone.test/src/test/java/eu/jsparrow/standalone/StandaloneConfigTest.java
@@ -3,7 +3,6 @@ package eu.jsparrow.standalone;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Matchers.any;
-import static org.mockito.Matchers.anyString;
 import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
@@ -51,6 +50,9 @@ public class StandaloneConfigTest {
 	private File mavenDepsFolder;
 	private IClasspathEntry classpathEntry;
 	private StandaloneConfig standaloneConfig;
+	private MavenInvoker mavenInvoker;
+
+	private static final String ECLIPSE = "eclipse"; //$NON-NLS-1$
 
 	@BeforeClass
 	public static void setUpClass() throws IOException {
@@ -72,6 +74,7 @@ public class StandaloneConfigTest {
 		javaProject = mock(IJavaProject.class);
 		mavenDepsFolder = mock(File.class);
 		classpathEntry = mock(IClasspathEntry.class);
+		mavenInvoker = mock(MavenInvoker.class);
 		standaloneConfig = new TestableStandaloneConfig("standaloneTest", path.toString(), "1.8", true); //$NON-NLS-1$ //$NON-NLS-2$
 	}
 
@@ -86,24 +89,14 @@ public class StandaloneConfigTest {
 		Files.deleteIfExists(tempFilePath);
 	}
 
-	@Test
-	public void getProjectDescription_projectDescriptionDoesNotExist_projectDescriptionCreated() throws Exception {
+	@Test(expected = IllegalStateException.class)
+	public void getProjectDescription_projectDescriptionDoesNotExist_projectDescriptionCreated_IllegalStateExceptionThrwon()
+			throws Exception {
 		tempFilePath = Paths.get("/", ".jsparrow-project"); //$NON-NLS-1$//$NON-NLS-2$
-
-		when(workspace.newProjectDescription(anyString())).thenReturn(projectDescription);
-		when(projectDescription.getNatureIds()).thenReturn(new String[] {});
 
 		standaloneConfig.getProjectDescription();
 
-		verify(workspace).newProjectDescription(anyString());
-		verify(projectDescription).getNatureIds();
-
-		ArgumentCaptor<String[]> captor = ArgumentCaptor.forClass(String[].class);
-		verify(projectDescription).setNatureIds(captor.capture());
-		assertTrue(captor.getValue().length == 2);
-
-		verify(projectDescription).setLocation(eq(new org.eclipse.core.runtime.Path(path.toString())));
-
+		verify(mavenInvoker).invoke(eq(ECLIPSE), eq(ECLIPSE), eq(null));
 		assertTrue(standaloneConfig.isDescriptionGenerated());
 	}
 
@@ -173,51 +166,51 @@ public class StandaloneConfigTest {
 
 		assertTrue(entries.isEmpty());
 	}
-	
+
 	@Test
 	public void collectMavenDependenciesAsClasspathEntries_fileListIsNotEmpty_returnsNonEmptyList() {
 		File jarFile = mock(File.class);
-		when(mavenDepsFolder.listFiles()).thenReturn(new File[] {jarFile});
+		when(mavenDepsFolder.listFiles()).thenReturn(new File[] { jarFile });
 
 		List<IClasspathEntry> entries = standaloneConfig.collectMavenDependenciesAsClasspathEntries();
 
 		assertFalse(entries.isEmpty());
 	}
-	
+
 	@Test
 	public void addToClasspath_emptyClasspathList_doesNotCallAnyMethods() throws Exception {
 		standaloneConfig.setJavaProject(javaProject);
-		
+
 		standaloneConfig.addToClasspath(Collections.emptyList());
-		
+
 		verifyZeroInteractions(javaProject);
 	}
-	
+
 	@Test
 	public void addToClasspath_nonEmptyClasspathList_oldEntriesIsEmpty_setRawClasspathCalled() throws Exception {
 		standaloneConfig.setJavaProject(javaProject);
 		when(javaProject.getRawClasspath()).thenReturn(new IClasspathEntry[] {});
-		
+
 		standaloneConfig.addToClasspath(Collections.singletonList(classpathEntry));
-		
+
 		verify(javaProject).getRawClasspath();
-		
+
 		ArgumentCaptor<IClasspathEntry[]> captor = ArgumentCaptor.forClass(IClasspathEntry[].class);
 		verify(javaProject).setRawClasspath(captor.capture(), any());
 		assertTrue(captor.getValue().length == 1);
 	}
-	
+
 	@Test
 	public void addToClasspath_nonEmptyClasspathList_oldEntriesIsNotEmpty_setRawClasspathCalled() throws Exception {
 		IClasspathEntry oldEntry = mock(IClasspathEntry.class);
-		
+
 		standaloneConfig.setJavaProject(javaProject);
-		when(javaProject.getRawClasspath()).thenReturn(new IClasspathEntry[] {oldEntry});
-		
+		when(javaProject.getRawClasspath()).thenReturn(new IClasspathEntry[] { oldEntry });
+
 		standaloneConfig.addToClasspath(Collections.singletonList(classpathEntry));
-		
+
 		verify(javaProject).getRawClasspath();
-		
+
 		ArgumentCaptor<IClasspathEntry[]> captor = ArgumentCaptor.forClass(IClasspathEntry[].class);
 		verify(javaProject).setRawClasspath(captor.capture(), any());
 		assertTrue(captor.getValue().length > 1);
@@ -229,7 +222,8 @@ public class StandaloneConfigTest {
 			this(name, path, compilerCompliance, false);
 		}
 
-		public TestableStandaloneConfig(String name, String path, String compilerCompliance, boolean testMode) throws Exception {
+		public TestableStandaloneConfig(String name, String path, String compilerCompliance, boolean testMode)
+				throws Exception {
 			super(name, path, compilerCompliance, "", testMode); //$NON-NLS-1$
 		}
 
@@ -257,10 +251,15 @@ public class StandaloneConfigTest {
 		protected File getMavenDependencyFolder() {
 			return mavenDepsFolder;
 		}
-		
+
 		@Override
 		protected IClasspathEntry createLibraryClasspathEntry(String jarPath) {
 			return classpathEntry;
+		}
+
+		@Override
+		protected MavenInvoker getMavenInvoker() {
+			return mavenInvoker;
 		}
 	}
 }
