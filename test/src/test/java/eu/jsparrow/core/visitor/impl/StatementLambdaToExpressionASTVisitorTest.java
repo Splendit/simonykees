836a7e8a7b671cf3a7cbc273da69bb798c7a2043
Hans-Jörg Schrödl <hans-joerg.schroedl@splendit.at>
2018-02-07 13:08:56 +0100

    SIM-1017 UPDATE: Improve test

--- test/src/test/java/eu/jsparrow/core/visitor/impl/StatementLambdaToExpressionASTVisitorTest.java
+++ test/src/test/java/eu/jsparrow/core/visitor/impl/StatementLambdaToExpressionASTVisitorTest.java
@@ -19,13 +19,13 @@ public class StatementLambdaToExpressionASTVisitorTest  extends UsesJDTUnitFixtu
 	private String blockTemplate = "new ArrayList<>().forEach(element -> %s);";
 	
 	@Before
-	public void setUp() {
+	public void setUp() throws Exception {
 		visitor = new StatementLambdaToExpressionASTVisitor();
+		fixture.addImport("java.util.ArrayList");
 	}
 	
 	@Test
 	public void visit_simpleExpressionLambda_shouldReplace() throws Exception {
-		fixture.addImport("java.util.ArrayList");
 		String lambda = "{ new String();}";
 		String block = String.format(blockTemplate, lambda);
 		fixture.addMethodBlock(block);
@@ -36,13 +36,11 @@ public class StatementLambdaToExpressionASTVisitorTest  extends UsesJDTUnitFixtu
 	
 		Block expected = createBlock(String.format(blockTemplate, "new String()"));
 		assertMatch(expected, fixture.getMethodBlock());
-		
-		new ArrayList<>().forEach(element -> new String());
 	}
 	
 	@Test
 	public void visit_simpleReturnLambda_shouldReplace() throws Exception {
-		fixture.addImport("java.util.ArrayList");
+		
 		String lambda = "{ new String(); return; }";
 		String block = String.format(blockTemplate, lambda);
 		fixture.addMethodBlock(block);
@@ -57,7 +55,6 @@ public class StatementLambdaToExpressionASTVisitorTest  extends UsesJDTUnitFixtu
 	
 	@Test
 	public void visit_tooManyStatements_shouldNotReplace() throws Exception {
-		fixture.addImport("java.util.ArrayList");
 		String lambda = "{ new String(); new String(); return; }";
 		String block = String.format(blockTemplate, lambda);
 		fixture.addMethodBlock(block);
@@ -71,7 +68,6 @@ public class StatementLambdaToExpressionASTVisitorTest  extends UsesJDTUnitFixtu
 	
 	@Test
 	public void visit_invalidStatement_shouldNotReplace() throws Exception {
-		fixture.addImport("java.util.ArrayList");
 		String lambda = "{ int d = 0; }";
 		String block = String.format(blockTemplate, lambda);
 		fixture.addMethodBlock(block);
@@ -85,8 +81,6 @@ public class StatementLambdaToExpressionASTVisitorTest  extends UsesJDTUnitFixtu
 	
 	@Test
 	public void visit_simplePredicate_shouldReplace() throws Exception {
-		fixture.addImport("java.util.ArrayList");
-
 		String block = "new ArrayList<>().stream().filter(element -> { return true; });";
 		fixture.addMethodBlock(block);
 		visitor.setASTRewrite(fixture.getAstRewrite());
@@ -101,7 +95,6 @@ public class StatementLambdaToExpressionASTVisitorTest  extends UsesJDTUnitFixtu
 	public void visit_whenReplacementHappens_ShouldUpdateListeners() throws Exception {
 		ASTRewriteVisitorListenerStub listener = new ASTRewriteVisitorListenerStub();
 		visitor.addRewriteListener(listener);
-		fixture.addImport("java.util.ArrayList");
 		String lambda = "{ new String();}";
 		String block = String.format(blockTemplate, lambda);
 		fixture.addMethodBlock(block);
