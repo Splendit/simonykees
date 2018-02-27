c33030be9707a2f17bb55c020b42e01f75e48909
Andreja <andreja.sambolec@splendit.at>
2018-02-27 11:28:43 +0100

    SIM-812 UPDATE increesed min width

--- bundles/ui/src/main/java/eu/jsparrow/ui/handler/SelectRulesWizardHandler.java
+++ bundles/ui/src/main/java/eu/jsparrow/ui/handler/SelectRulesWizardHandler.java
@@ -194,7 +194,7 @@ public class SelectRulesWizardHandler extends AbstractHandler {
 					@Override
 					protected void configureShell(Shell newShell) {
 						super.configureShell(newShell);
-						newShell.setSize(1000, 1000);
+						newShell.setSize(1050, 1000);
 						newShell.setMinimumSize(630, 600);
 					}
 				};
