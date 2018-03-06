60524630a8d5d46b7512a1de38c2cb30d8e59d54
ardit.ymeri <ardit.ymeri@splendit.at>
2018-03-06 17:52:12 +0100

    SIM-1041 UPDATE avoid calendar name conflicts

--- bundles/core/src/main/java/eu/jsparrow/core/visitor/impl/DateDeprecatedASTVisitor.java
+++ bundles/core/src/main/java/eu/jsparrow/core/visitor/impl/DateDeprecatedASTVisitor.java
@@ -1,19 +1,29 @@
 package eu.jsparrow.core.visitor.impl;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.eclipse.jdt.core.dom.AST;
 import org.eclipse.jdt.core.dom.ASTNode;
+import org.eclipse.jdt.core.dom.AbstractTypeDeclaration;
 import org.eclipse.jdt.core.dom.Block;
 import org.eclipse.jdt.core.dom.ClassInstanceCreation;
 import org.eclipse.jdt.core.dom.Expression;
 import org.eclipse.jdt.core.dom.ExpressionStatement;
+import org.eclipse.jdt.core.dom.FieldDeclaration;
 import org.eclipse.jdt.core.dom.InfixExpression;
+import org.eclipse.jdt.core.dom.Initializer;
+import org.eclipse.jdt.core.dom.MethodDeclaration;
 import org.eclipse.jdt.core.dom.MethodInvocation;
 import org.eclipse.jdt.core.dom.NumberLiteral;
+import org.eclipse.jdt.core.dom.SimpleName;
 import org.eclipse.jdt.core.dom.Statement;
 import org.eclipse.jdt.core.dom.Type;
+import org.eclipse.jdt.core.dom.TypeDeclaration;
 import org.eclipse.jdt.core.dom.VariableDeclarationFragment;
 import org.eclipse.jdt.core.dom.VariableDeclarationStatement;
 import org.eclipse.jdt.core.dom.rewrite.ListRewrite;
@@ -21,6 +31,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import eu.jsparrow.core.builder.NodeBuilder;
+import eu.jsparrow.core.visitor.sub.VariableDeclarationsVisitor;
 import eu.jsparrow.rules.common.util.ASTNodeUtil;
 import eu.jsparrow.rules.common.util.ClassRelationUtil;
 import eu.jsparrow.rules.common.visitor.AbstractAddImportASTVisitor;
@@ -29,7 +40,7 @@ import eu.jsparrow.rules.common.visitor.AbstractAddImportASTVisitor;
  * ASTVisitor that searches deprecated Date constructs and replaces it with
  * legal ones.
  * 
- * @author Martin Huter
+ * @author Martin Huter, Ardit Ymeri
  * @since 2.5
  *
  */
@@ -40,8 +51,11 @@ public class DateDeprecatedASTVisitor extends AbstractAddImportASTVisitor {
 	private static final String DATE_QUALIFIED_NAME = java.util.Date.class.getName();
 	private static final String CALENDAR_QUALIFIED_NAME = java.util.Calendar.class.getName();
 	private static final String CALENDAR_NAME = java.util.Calendar.class.getSimpleName();
-	
-	private static final String CAL = "cal"; //$NON-NLS-1$
+
+	private static final String CALENDAR = "calendar"; //$NON-NLS-1$
+
+	private Map<AbstractTypeDeclaration, List<String>> fieldNames = new HashMap<>();
+	private Map<ASTNode, List<String>> localVariableNames = new HashMap<>();
 
 	@Override
 	public boolean visit(ClassInstanceCreation node) {
@@ -53,20 +67,28 @@ public class DateDeprecatedASTVisitor extends AbstractAddImportASTVisitor {
 			case 3:
 			case 5:
 			case 6:
-				astRewrite.replace(node, getMethodInvocation(node.getAST(), CAL), null);
+				ASTNode scope = findEnclosingScope(node).orElse(null);
+				if (scope == null) {
+					logger.warn("The scope of the Date declaration cannot be found!"); //$NON-NLS-1$
+					break;
+				}
+				lazyLoadScopeNames(scope);
+				String calendarName = findCalendarName(scope);
+				AST ast = node.getAST();
+				astRewrite.replace(node, getMethodInvocation(ast, calendarName), null);
 				Statement ancestorStatment = ASTNodeUtil.getSpecificAncestor(node, Statement.class);
 				Block surroundingBlock;
 				if (ASTNode.BLOCK == ancestorStatment.getParent()
 					.getNodeType()) {
 					surroundingBlock = (Block) ancestorStatment.getParent();
 					ListRewrite lrw = astRewrite.getListRewrite(surroundingBlock, Block.STATEMENTS_PROPERTY);
-					generateCalendar(node.getAST(), CAL, expressionList)
+					generateCalendar(ast, calendarName, expressionList)
 						.forEach(s -> lrw.insertBefore(s, ancestorStatment, null));
 				} else {
-					Block injectionBlock = node.getAST().newBlock();
+					Block injectionBlock = ast.newBlock();
 					@SuppressWarnings("unchecked")
 					List<Statement> blockStatements = (List<Statement>) injectionBlock.statements();
-					blockStatements.addAll(generateCalendar(node.getAST(), CAL, expressionList));
+					blockStatements.addAll(generateCalendar(ast, calendarName, expressionList));
 					blockStatements.add((Statement) astRewrite.createMoveTarget(ancestorStatment));
 					astRewrite.replace(ancestorStatment, injectionBlock, null);
 				}
@@ -78,6 +100,106 @@ public class DateDeprecatedASTVisitor extends AbstractAddImportASTVisitor {
 			}
 		}
 		return true;
+	}
+
+	@Override
+	public void endVisit(TypeDeclaration typeDeclaration) {
+		fieldNames.remove(typeDeclaration);
+		localVariableNames.remove(typeDeclaration);
+	}
+
+	@Override
+	public void endVisit(MethodDeclaration methodDeclaration) {
+		localVariableNames.remove(methodDeclaration);
+	}
+
+	@Override
+	public void endVisit(Initializer initializer) {
+		localVariableNames.remove(initializer);
+	}
+
+	private String findCalendarName(ASTNode scope) {
+		String name = CALENDAR;
+		int suffix = 1;
+		while (isInScope(name)) {
+			name = CALENDAR + suffix;
+			suffix++;
+		}
+		saveIntroducedName(scope, name);
+		return name;
+	}
+
+	private void lazyLoadScopeNames(ASTNode scope) {
+
+		if (!localVariableNames.containsKey(scope)) {
+			VariableDeclarationsVisitor declarationsVisitor = new VariableDeclarationsVisitor();
+			scope.accept(declarationsVisitor);
+			List<String> declaredInScope = declarationsVisitor.getVariableDeclarationNames()
+				.stream()
+				.map(SimpleName::getIdentifier)
+				.collect(Collectors.toList());
+
+			this.localVariableNames.put(scope, declaredInScope);
+		}
+
+		if (TypeDeclaration.BODY_DECLARATIONS_PROPERTY != scope.getLocationInParent()) {
+			return;
+		}
+
+		TypeDeclaration typeDeclaration = (TypeDeclaration) scope.getParent();
+		if (fieldNames.containsKey(typeDeclaration)) {
+			return;
+		}
+
+		FieldDeclaration[] fields = typeDeclaration.getFields();
+		List<String> names = new ArrayList<>();
+		for (FieldDeclaration field : fields) {
+			names.addAll(ASTNodeUtil.convertToTypedList(field.fragments(), VariableDeclarationFragment.class)
+				.stream()
+				.map(VariableDeclarationFragment::getName)
+				.map(SimpleName::getIdentifier)
+				.collect(Collectors.toList()));
+		}
+		fieldNames.put(typeDeclaration, names);
+	}
+
+	private void saveIntroducedName(ASTNode scope, String name) {
+		List<String> names = localVariableNames.get(scope);
+		if (names != null) {
+			names.add(name);
+		} else {
+			names = new ArrayList<>();
+			names.add(name);
+			localVariableNames.put(scope, names);
+		}
+	}
+
+	private boolean isInScope(String name) {
+		return fieldNames.values()
+			.stream()
+			.anyMatch(names -> names.contains(name))
+				|| localVariableNames.values()
+					.stream()
+					.anyMatch(names -> names.contains(name));
+	}
+
+	private Optional<ASTNode> findEnclosingScope(ClassInstanceCreation node) {
+		Initializer initalizer = ASTNodeUtil.getSpecificAncestor(node, Initializer.class);
+		if (initalizer != null) {
+			return Optional.of(initalizer);
+		}
+
+		MethodDeclaration methodDeclaration = ASTNodeUtil.getSpecificAncestor(node, MethodDeclaration.class);
+		if (methodDeclaration != null) {
+			return Optional.of(methodDeclaration);
+		}
+
+		FieldDeclaration fieldDeclaration = ASTNodeUtil.getSpecificAncestor(node, FieldDeclaration.class);
+		if (fieldDeclaration != null) {
+			return Optional.of(fieldDeclaration.getParent());
+		}
+
+		return Optional.empty();
 	}
 
 	private MethodInvocation getMethodInvocation(AST ast, String nameOfCalendar) {
@@ -99,25 +221,27 @@ public class DateDeprecatedASTVisitor extends AbstractAddImportASTVisitor {
 		// cal.set(1900 + 99, 1, 1); set arguments need to be added
 		MethodInvocation setMethod = NodeBuilder.newMethodInvocation(ast,
 				NodeBuilder.newSimpleName(ast, nameOfCalendar), NodeBuilder.newSimpleName(ast, "set")); //$NON-NLS-1$
-		
-		//Preparing the year
+
+		// Preparing the year
 		Expression firstArgument = arguments.remove(0);
-		if(ASTNode.NUMBER_LITERAL == firstArgument.getNodeType()) {
+		if (ASTNode.NUMBER_LITERAL == firstArgument.getNodeType()) {
 			try {
-				Integer year = Integer.parseInt(((NumberLiteral)firstArgument).getToken());
+				Integer year = Integer.parseInt(((NumberLiteral) firstArgument).getToken());
 				// Conversion to Calendar initialization
-				year=year+1900;
-				setMethod.arguments().add(ast.newNumberLiteral(year.toString()));
-			}catch (NumberFormatException e) {
-				//If number could not be parsed use the expression
+				year = year + 1900;
 				setMethod.arguments()
-				.add(NodeBuilder.newInfixExpression(ast, InfixExpression.Operator.PLUS, ast.newNumberLiteral("1900"), //$NON-NLS-1$
-						(Expression) astRewrite.createMoveTarget(firstArgument)));
+					.add(ast.newNumberLiteral(year.toString()));
+			} catch (NumberFormatException e) {
+				// If number could not be parsed use the expression
+				setMethod.arguments()
+					.add(NodeBuilder.newInfixExpression(ast, InfixExpression.Operator.PLUS,
+							ast.newNumberLiteral("1900"), //$NON-NLS-1$
+							(Expression) astRewrite.createMoveTarget(firstArgument)));
 			}
-			
+
 		}
-		
-		//Adding other arguments
+
+		// Adding other arguments
 		arguments.forEach(a -> setMethod.arguments()
 			.add(astRewrite.createMoveTarget(a)));
 		ExpressionStatement setStatement = NodeBuilder.newExpressionStatement(ast, setMethod);
