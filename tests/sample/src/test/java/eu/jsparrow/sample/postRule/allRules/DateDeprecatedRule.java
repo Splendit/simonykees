316fc6e001016ec698d68f11301e86ad1e842982
ardit.ymeri <ardit.ymeri@splendit.at>
2018-03-07 15:29:25 +0100

    SIM-1041 UPDATE saving and testing comments

--- tests/sample/src/test/java/eu/jsparrow/sample/postRule/allRules/DateDeprecatedRule.java
+++ tests/sample/src/test/java/eu/jsparrow/sample/postRule/allRules/DateDeprecatedRule.java
@@ -7,6 +7,40 @@ public class DateDeprecatedRule {
 
 	private String calendar1 = "don't hide me!";
 	private Date unInitializedDate;
+	private Date field;
+	{
+		Calendar calendar = Calendar.getInstance();
+		calendar.set(1999, 1, 1);
+		field = calendar.getTime();
+	}
+
+	private Date /* name leading comment */ fieldWithComments /*
+																 * name trailing
+																 * comment
+																 */; // trailing
+																		// comment
+	{
+		/* init leading comment */
+		/* init inner comment */
+		/* init trailing comment */
+		Calendar calendar = Calendar.getInstance();
+		calendar.set(1999, 1, 1);
+		fieldWithComments /* name trailing comment */ = calendar.getTime();
+	}
+
+	private Date // name leading comment
+	fieldWithLineComments
+	// name trailing comment
+	; // trailing comment
+	{
+		// init inner comment
+		// init trailing comment
+		Calendar calendar = Calendar.getInstance();
+		calendar.set(1999, 1, 1);
+		fieldWithLineComments
+		// name trailing comment
+				= calendar.getTime();
+	}
 
 	private final Date instantiateMeInInitializerBlock;
 	{
@@ -44,6 +78,43 @@ public class DateDeprecatedRule {
 		Calendar calendar = Calendar.getInstance();
 		calendar.set(1999, 1, 1);
 		unInitializedDate = calendar.getTime();
+	}
+
+	public void notDeprecated_dontReplace() {
+		Date date = new Date();
+	}
+
+	public void savingComments() {
+		Calendar calendar = Calendar.getInstance();
+		calendar.set(1999, 1, 1);
+		/* leading init comment */
+		/* inner init comment */
+		/* trailing init comment */
+		// leading statement comment
+		Date /* leading name comment */ date /* trailing name comment */ = calendar.getTime(); // trailing
+																								// statement
+																								// comment
+
+		Calendar calendar2 = Calendar.getInstance();
+		calendar2.set(1999, 1, 1);
+		// inner init comment /
+		// trailing init comment
+		Date
+		/* leading name comment */
+		dateSurroundedByLineComments
+		// trailing name comment
+				= // leading init comment
+				calendar2.getTime(); // trailing statement comment
+
+		if (date != null) {
+			Calendar calendar3 = Calendar.getInstance();
+			calendar3.set(1990, 1, 31);
+			/* leading initializer comment */
+			/* inner initializer comment */
+			/* trailing initializer comment */
+			// leading comment
+			date /* name comment */ = calendar3.getTime(); // trailing comment
+		}
 	}
 
 	class InnerClass {
--- tests/sample/src/test/java/eu/jsparrow/sample/preRule/DateDeprecatedRule.java
+++ tests/sample/src/test/java/eu/jsparrow/sample/preRule/DateDeprecatedRule.java
@@ -6,6 +6,18 @@ public class DateDeprecatedRule {
 	
 	private String calendar1 = "don't hide me!";
 	private Date unInitializedDate;
+	private Date field = new Date(99, 1, 1);
+	
+	private Date /* name leading comment */ fieldWithComments /* name trailing comment */ = /* init leading comment */ new  /* init inner comment */ Date(99, 1, 1) /* init trailing comment */; // trailing comment
+	
+	private Date // name leading comment
+	fieldWithLineComments 
+	// name trailing comment
+	= // init leading comment 
+	new  
+	// init inner comment  
+	Date(99, 1, 1) // init trailing comment 
+	; // trailing comment
 	
 	private final Date instantiateMeInInitializerBlock;
 	{
@@ -34,6 +46,27 @@ public class DateDeprecatedRule {
 		unInitializedDate = new Date(99, 1, 1);
 	}
 	
+	public void notDeprecated_dontReplace() {
+		Date date = new Date();
+	}
+	
+	public void savingComments() {
+		// leading statement comment
+		Date /* leading name comment */ date /* trailing name comment */ = /* leading init comment*/new /* inner init comment */ Date(99, 1, 1)/* trailing init comment */ ; // trailing statement comment
+		
+		Date 
+		/* leading name comment */ 
+		dateSurroundedByLineComments 
+		// trailing name comment 
+		= // leading init comment
+		new // inner init comment /
+		Date(99, 1, 1)// trailing init comment
+		; // trailing statement comment
+		
+		if(date != null) 
+			// leading comment
+			date /* name comment */ = /* leading initializer comment */ new /* inner initializer comment */ Date(90, 1, 31) /* trailing initializer comment */ ; // trailing comment
+	}
 	
 	class InnerClass {
 		
