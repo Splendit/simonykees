f9ac03aedcadbf1e52c42a1998a9e76858236050
Hans-Jörg Schnedlitz <hans-joerg.schnedlitz@splendit.at>
2018-03-23 11:33:05 +0100

    SIM-1072 UPDATE Refactor persistence and write tests

--- bundles/license.netlicensing/src/main/java/eu/jsparrow/license/netlicensing/cleanslate/LicensePersistence.java
+++ bundles/license.netlicensing/src/main/java/eu/jsparrow/license/netlicensing/cleanslate/LicensePersistence.java
@@ -1,10 +1,11 @@
 package eu.jsparrow.license.netlicensing.cleanslate;
 
 import eu.jsparrow.license.netlicensing.cleanslate.model.LicenseModel;
+import eu.jsparrow.license.netlicensing.cleanslate.model.ValidationException;
 
 public interface LicensePersistence {
 
-	LicenseModel load();
+	LicenseModel load() throws ValidationException;
 	
-	void save(LicenseModel model);
+	void save(LicenseModel model) throws ValidationException;
 }
---
+++ tests/license.netlicensing.test/src/test/java/eu/jsparrow/license/netlicensing/cleanslate/persistence/SecureStoragePersistenceTest.java
@@ -0,0 +1,81 @@
+package eu.jsparrow.license.netlicensing.cleanslate.persistence;
+
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.*;
+
+import org.eclipse.equinox.security.storage.ISecurePreferences;
+import org.eclipse.equinox.security.storage.StorageException;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.mockito.Matchers.*;
+
+import eu.jsparrow.license.netlicensing.cleanslate.DummyLicenseModel;
+import eu.jsparrow.license.netlicensing.cleanslate.model.LicenseModel;
+import eu.jsparrow.license.netlicensing.cleanslate.model.ValidationException;
+
+@SuppressWarnings("nls")
+@RunWith(MockitoJUnitRunner.class)
+public class SecureStoragePersistenceTest {
+
+	@Mock
+	private ISecurePreferences simonykeesNode;
+
+	@Mock
+	private IEncryption encryption;
+
+	private SecureStoragePersistence secureStoragePersistence;
+
+	@Before
+	public void setUp() {
+		ISecurePreferences securePreferences = mock(ISecurePreferences.class);
+		when(securePreferences.node(anyString())).thenReturn(simonykeesNode);
+
+		this.secureStoragePersistence = new SecureStoragePersistence(securePreferences, encryption);
+	}
+
+
+	@Test
+	public void save_validModel_encryptsAndSavesData() throws Exception {
+		byte[] modelBytes = ModelSerializer.serialize(new DummyLicenseModel());
+		byte[] encryptedModelBytes = "encryptedModel".getBytes();
+	
+		when(encryption.encrypt(modelBytes)).thenReturn(encryptedModelBytes);
+
+		secureStoragePersistence.save(new DummyLicenseModel());
+
+		verify(simonykeesNode).putByteArray(eq("credentials"), eq(encryptedModelBytes), eq(false));
+	}
+
+	@Test
+	public void load_validModel_decryptsAndLoadsModel() throws Exception {
+		LicenseModel model = new DummyLicenseModel();
+		byte[] encryptedModelBytes = "encryptedModel".getBytes();
+		byte[] decryptedModel = ModelSerializer.serialize(model);
+		when(simonykeesNode.getByteArray(eq("credentials"), any(byte[].class))).thenReturn(encryptedModelBytes);
+		when(encryption.decrypt(encryptedModelBytes)).thenReturn(decryptedModel);
+
+		LicenseModel result= secureStoragePersistence.load();
+		
+		// assertEquals fails as the deserialized object is not the same as the original but a clone
+		assertNotNull(result);
+	}
+
+	@Test(expected = ValidationException.class)
+	public void save_withCorruptedSecureStorage_throwsValidationException() throws Exception {
+		byte[] modelBytes = "model".getBytes();
+		byte[] encryptedModelBytes = "encryptedModel".getBytes();
+		when(encryption.encrypt(eq(modelBytes))).thenReturn(encryptedModelBytes);
+
+		doThrow(StorageException.class).when(simonykeesNode)
+			.putByteArray(anyString(), any(byte[].class), anyBoolean());
+
+		secureStoragePersistence.save(new DummyLicenseModel());
+	}
+
+	
+
+}
