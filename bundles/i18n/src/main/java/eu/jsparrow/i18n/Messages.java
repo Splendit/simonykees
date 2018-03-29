52b07d0042d6d58e38aea66669d9aacce161c85e
matthias.webhofer <matthias.webhofer@splendit.at>
2018-03-29 13:29:43 +0200

    SIM-1055 UPDATE i18n

--- bundles/i18n/src/main/java/eu/jsparrow/i18n/Messages.java
+++ bundles/i18n/src/main/java/eu/jsparrow/i18n/Messages.java
@@ -372,6 +372,8 @@ public class Messages extends NLS {
 	public static String SimonykeesUpdateLicenseDialog_update_license_dialog_title;
 	public static String SimonykeesUpdateLicenseDialog_update_llicense_dialog_button;
 	public static String SimonykeesUpdateLicenseDialog_update_license_dialog_group_title;
+	public static String StandaloneActivator_bundleStarted;
+	public static String StandaloneActivator_startingBundle;
 	public static String StandaloneConfig_AddProjectNaturesToProjectDescription;
 	public static String StandaloneConfig_collectCompilationUnits;
 	public static String StandaloneConfig_CompilerComplianceSetTo;
