8fcb638c45eeaaa0208077912808c78e8f8b710e
Ludwig Werzowa <ludwig.werzowa@splendit.at>
2018-06-05 11:26:51 +0200

    SIM-1125 ADD convenience script to install local or remote JMP versions

---
+++ installMavenPluginLatest.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+MAVEN_PLUGIN_VERSION="0.5.0-SNAPSHOT"
+
+function usage() {
+   cat <<EOF
+Usage: $0 (-u|-j)
+where:
+    -j install the maven plugin that is already locally built
+    -u install the latest develop version from packagedrone
+EOF
+   exit 0
+}
+
+function installUrl() {
+  jsparrow-maven-plugin/install.sh -u "http://packagedrone-vm-01.splendit.loc:8080/maven/jenkins-jSparrow-maven-plugin-develop/eu/jsparrow/jsparrow-maven-plugin/$MAVEN_PLUGIN_VERSION/jsparrow-maven-plugin-$MAVEN_PLUGIN_VERSION.jar"
+}
+
+function installJar() {
+  jsparrow-maven-plugin/install.sh -j "jsparrow-maven-plugin/target/jsparrow-maven-plugin-$MAVEN_PLUGIN_VERSION.jar"
+}
+
+# parse arguments
+while getopts ":ujh" option
+do
+  case "${option}" in
+    u) installUrl;;
+    j) installJar;;
+    h) usage;;
+  esac
+done
+
+if [[ -z "$1" ]] 
+  then 
+  usage
+fi
