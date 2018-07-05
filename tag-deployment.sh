1c48719f4acc262e4592a655bc20b144f0b56772
Ludwig Werzowa <ludwig.werzowa@splendit.at>
2018-07-05 15:39:06 +0200

    SIM-1190 UPDATE fixed typos

--- tag-deployment.sh
+++ tag-deployment.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-# command to tag the development build
-# returns exit code 128 with error if tag already excists
-# first paramter is required an has to be develop or master and results in the directory of the tag
-# second parameter is optinal and allows to set a subdirector for the tag
+# command to tag the builds
+# returns exit code 128 with error if tag already exists
+# first parameter is required and has to be develop or master and results in the directory of the tag
+# second parameter is optional and allows to set a subdirector for the tag
 
 # prefix definition for develop
 if [ "$1" == "develop" ]; then dir=$1/; fi
 
 # prefix definition for master
 if [ "$1" == "master" ]; then dir=$1/; fi
+if [ -z ${dir+x} ]; then echo "Executed with wrong argument '$1' or argument is unset"; exit 1; fi
 
-if [ -z ${dir+x} ]; then echo "Exectued with wrong argument '$1' or argument is unset"; exit 1; fi
 
-# second parameter is the sub dircetory
+# second parameter is the sub directory
 if [ -n ${2+x} ]; then subdir="$2"/; fi
 
 # building the tag
@@ -27,5 +27,5 @@ echo completeTag=$completeTag
 # tag the build
 git tag $completeTag
 
-# push the tage to the remote
+# push the tag to the remote
 git push origin $completeTag 
