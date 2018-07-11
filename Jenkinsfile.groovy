0ac06b94ebc59833272bc2bb7fc4c021168b75e6
Ludwig Werzowa <ludwig.werzowa@splendit.at>
2018-07-11 16:30:27 +0200

    SIM-1190 UPDATE moved "main" method up and made sshCredentials into a static method

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -9,7 +9,7 @@
 def mvnBin() { "${tool 'mvn system'}/bin/mvn" }
 
 // jenkins git ssh credentials
-def sshCredentials = '7f15bb8a-a1db-4cdf-978f-3ae5983400b6'
+static def sshCredentials() { '7f15bb8a-a1db-4cdf-978f-3ae5983400b6' }
 
 enum Profile {
 	MASTER_PRODUCTION_PROGUARD("-Dproduction -Dproguard -Pmaster-production-proguard", ""),
@@ -33,174 +33,6 @@ enum Profile {
 
 	// TODO remove this getter once a nicer solution for uploadMappingFile(Profile profile) is found
 	String getQualifier() { qualifier }
-}
-
-
-void checkout() {
-	stage('Preparation') { // for display purposes
-		checkout scm
-	}
-}
-
-void pushToGithub() {
-	// defines the backup repository to push to
-	def backupOrigin = 'git@github.com:Splendit/simonykees.git'
-
-	stage('Push to Github') {
-		println "Pushing to GitHub..."
-		sshagent([sshCredentials]) { //key id of ssh-rsa key in remote repository within jenkins
-			// pushing the repository to github
-			sh("git push $backupOrigin HEAD:$env.BRANCH_NAME")
-		}
-	}
-}
-
-void compileEclipsePlugin() {
-	stage('Compile Eclipse Plugin') {
-		def mvnCommand = 'clean verify -DskipTests'
-		sh "'${mvnBin()}' ${mvnCommand}"
-	}
-}
-
-/**
- * this only tests whether or not the sources compile.
- * see deployMavenPluginWithDependencies for a full build that includes copying dependencies
- */
-void compileMavenPlugin() {
-	ncies.
-			stage('Compile Maven Plugin') {
-				def mvnCommand = 'clean install -DskipTests'
-
-				dir('jsparrow-maven-plugin') {
-					sh "'${mvnBin()}/bin/mvn' ${mvnCommand}"
-				}
-			}
-}
-
-/**
- * Creates a virtual X windows display and runs integration tests.
- * Uses setTestStatus to set the result.
- */
-void runIntegrationTests() {
-	wrap([$class: 'Xvfb', additionalOptions: '', assignedLabels: '', autoDisplayName: true, debug: true, screen: '1366x768x24', shutdownWithBuild: true, timeout: 10]) {
-		// X virtual framebuffer (virtual X window display) is needed for plugin tests
-		// wrap([$class: 'Xvfb']) {
-		stage('Integration-Tests') {
-			// Run the maven build
-			def mvnCommand = 'clean verify -fae -Dsurefire.rerunFailingTestsCount=2'
-
-			// def mvnCommand = 'surefire:test -fae -Dsurefire.rerunFailingTestsCount=2'
-			def statusCode = sh(returnStatus: true, script: "'${mvnBin()}/bin/mvn' ${mvnCommand}")
-
-			// in case of failing tests, there will be 'repeats' number of reruns
-			int i = 0
-			int repeats = 1
-			while (statusCode != 0 && i < repeats) {
-				def rerunTests = 'clean verify -fae'
-				statusCode = sh(returnStatus: true, script: "'${mvnBin()}/bin/mvn' ${rerunTests}")
-				i = i + 1
-			}
-
-			setTestStatus(statusCode)
-
-			// collects unit test results
-			junit '**/target/surefire-reports/TEST-*.xml'
-			archive 'target/*.jar'
-		}
-	}
-}
-
-/**
- * Run SonarQube analysis, server configuration takes place in the Jenkins config
- */
-void runSonarQubeAnalysis() {
-
-	stage('SonarQube Analysis') {
-		withSonarQubeEnv('SonarQube Server') {
-			sh 'mvn sonar:sonar'
-		}
-	}
-}
-
-void tagCommit(def branchName, String subdirectory) {
-
-	stage('Tag Commit') {
-		// tag build in repository
-		sshagent([sshCredentials]) { //key id of ssh-rsa key in remote repository within jenkins
-			// first parameter is the dir, second parameter is the subdirectory and optional
-			sh("./tag-deployment.sh $branchName $subdirectory")
-			sh("git push $backupOrigin --tags")
-		}
-	}
-}
-
-void buildEclipsePlugin(Profile profile, String timestamp) {
-
-	def mvnCommand = "clean verify -DskipTests -B ${profile.mvnOptions(timestamp)}"
-
-	stage("Eclipse Build: ${profile.formattedName()}") {
-		sh "'${mvnBin()}' $mvnCommand"
-	}
-}
-
-void deployEclipsePlugin(Profile profile, String timestamp) {
-
-	def mvnCommand = "clean deploy -DskipTests -B ${profile.mvnOptions(timestamp)}"
-
-	stage("Eclipse Deploy: ${profile.formattedName()}") {
-		sh "'${mvnBin()}' $mvnCommand"
-	}
-}
-
-/**
- * IMPORTANT: the Eclipse plugin has to be built first! This means that either buildEclipsePlugin or deployEclipsePlugin has to be executed with the same profile!
- *
- * @param profile the same profile that has been used to build the Eclipse plugin
- */
-void deployMavenPlugin(Profile profile, String timestamp) {
-
-	/*
-	 * for the JMP -DforceContextQualifier will do nothing on master because there is no qualifier
-	 * (1.0.0 vs 1.0.0-SNAPSHOT -> SNAPSHOT will be replaced with the qualifier for the jar name)
-	 */
-	def mvnCommand = "clean deploy -DskipTests ${profile.mvnOptions(timestamp)}"
-
-	def pluginResourcePath = 'jsparrow-maven-plugin/src/main/resources'
-	def jSparrowTargetPath = 'releng/eu.jsparrow.product/target/repository/plugins'
-	def manifest = 'manifest.standalone'
-	def manifestContent = sh(script: "ls $jSparrowTargetPath", returnStdout: true)
-
-	stage("JMP Deploy: ${profile.formattedName()}") {
-		dir('jsparrow-maven-plugin/src/main/resources') {
-			writeFile file: "${manifest}", text: "${manifestContent}"
-		}
-
-		// Copy required dependencies into plugin resource folder
-		sh("cp ${jSparrowTargetPath}/* ${pluginResourcePath}")
-
-		dir('jsparrow-maven-plugin') {
-			sh "'${mvnBin()}' ${mvnCommand}"
-		}
-	}
-}
-
-void uploadMappingFile(Profile profile) {
-
-	// TODO there has to be a nicer way to get the version (and then the directory), when we already have the qualifier
-	def buildNumber = sh(returnStdout: true, script: "pcregrep -o1 \"name='eu.jsparrow\\.feature\\.feature\\.group' range='\\[.*,((\\d*\\.){3}\\d{8}-\\d{4})\" releng/site/target/p2content.xml").trim()
-	def directory = "${buildNumber}${profile.qualifier}"
-
-	stage ("Upload Mapping Files") {
-		uploadMappingFiles(directory)
-	}
-}
-
-void runStandardSteps() {
-	checkout()
-	pushToGithub()
-	compileEclipsePlugin()
-	compileMavenPlugin()
-	runIntegrationTests()
 }
 
 timestamps {
@@ -296,6 +128,173 @@ timestamps {
 
 		step([$class: 'StashNotifier']) // Notifies the Stash Instance of the build result
 	}
+}
+
+void checkout() {
+	stage('Preparation') { // for display purposes
+		checkout scm
+	}
+}
+
+void pushToGithub() {
+	// defines the backup repository to push to
+	def backupOrigin = 'git@github.com:Splendit/simonykees.git'
+
+	stage('Push to Github') {
+		println "Pushing to GitHub..."
+		sshagent([sshCredentials()]) { //key id of ssh-rsa key in remote repository within jenkins
+			// pushing the repository to github
+			sh("git push $backupOrigin HEAD:$env.BRANCH_NAME")
+		}
+	}
+}
+
+void compileEclipsePlugin() {
+	stage('Compile Eclipse Plugin') {
+		def mvnCommand = 'clean verify -DskipTests'
+		sh "'${mvnBin()}' ${mvnCommand}"
+	}
+}
+
+/**
+ * this only tests whether or not the sources compile.
+ * see deployMavenPluginWithDependencies for a full build that includes copying dependencies
+ */
+void compileMavenPlugin() {
+	ncies.
+			stage('Compile Maven Plugin') {
+				def mvnCommand = 'clean install -DskipTests'
+
+				dir('jsparrow-maven-plugin') {
+					sh "'${mvnBin()}/bin/mvn' ${mvnCommand}"
+				}
+			}
+}
+
+/**
+ * Creates a virtual X windows display and runs integration tests.
+ * Uses setTestStatus to set the result.
+ */
+void runIntegrationTests() {
+	wrap([$class: 'Xvfb', additionalOptions: '', assignedLabels: '', autoDisplayName: true, debug: true, screen: '1366x768x24', shutdownWithBuild: true, timeout: 10]) {
+		// X virtual framebuffer (virtual X window display) is needed for plugin tests
+		// wrap([$class: 'Xvfb']) {
+		stage('Integration-Tests') {
+			// Run the maven build
+			def mvnCommand = 'clean verify -fae -Dsurefire.rerunFailingTestsCount=2'
+
+			// def mvnCommand = 'surefire:test -fae -Dsurefire.rerunFailingTestsCount=2'
+			def statusCode = sh(returnStatus: true, script: "'${mvnBin()}/bin/mvn' ${mvnCommand}")
+
+			// in case of failing tests, there will be 'repeats' number of reruns
+			int i = 0
+			int repeats = 1
+			while (statusCode != 0 && i < repeats) {
+				def rerunTests = 'clean verify -fae'
+				statusCode = sh(returnStatus: true, script: "'${mvnBin()}/bin/mvn' ${rerunTests}")
+				i = i + 1
+			}
+
+			setTestStatus(statusCode)
+
+			// collects unit test results
+			junit '**/target/surefire-reports/TEST-*.xml'
+			archive 'target/*.jar'
+		}
+	}
+}
+
+/**
+ * Run SonarQube analysis, server configuration takes place in the Jenkins config
+ */
+void runSonarQubeAnalysis() {
+
+	stage('SonarQube Analysis') {
+		withSonarQubeEnv('SonarQube Server') {
+			sh 'mvn sonar:sonar'
+		}
+	}
+}
+
+void tagCommit(def branchName, String subdirectory) {
+
+	stage('Tag Commit') {
+		// tag build in repository
+		sshagent([sshCredentials()]) { //key id of ssh-rsa key in remote repository within jenkins
+			// first parameter is the dir, second parameter is the subdirectory and optional
+			sh("./tag-deployment.sh $branchName $subdirectory")
+			sh("git push $backupOrigin --tags")
+		}
+	}
+}
+
+void buildEclipsePlugin(Profile profile, String timestamp) {
+
+	def mvnCommand = "clean verify -DskipTests -B ${profile.mvnOptions(timestamp)}"
+
+	stage("Eclipse Build: ${profile.formattedName()}") {
+		sh "'${mvnBin()}' $mvnCommand"
+	}
+}
+
+void deployEclipsePlugin(Profile profile, String timestamp) {
+
+	def mvnCommand = "clean deploy -DskipTests -B ${profile.mvnOptions(timestamp)}"
+
+	stage("Eclipse Deploy: ${profile.formattedName()}") {
+		sh "'${mvnBin()}' $mvnCommand"
+	}
+}
+
+/**
+ * IMPORTANT: the Eclipse plugin has to be built first! This means that either buildEclipsePlugin or deployEclipsePlugin has to be executed with the same profile!
+ *
+ * @param profile the same profile that has been used to build the Eclipse plugin
+ */
+void deployMavenPlugin(Profile profile, String timestamp) {
+
+	/*
+	 * for the JMP -DforceContextQualifier will do nothing on master because there is no qualifier
+	 * (1.0.0 vs 1.0.0-SNAPSHOT -> SNAPSHOT will be replaced with the qualifier for the jar name)
+	 */
+	def mvnCommand = "clean deploy -DskipTests ${profile.mvnOptions(timestamp)}"
+
+	def pluginResourcePath = 'jsparrow-maven-plugin/src/main/resources'
+	def jSparrowTargetPath = 'releng/eu.jsparrow.product/target/repository/plugins'
+	def manifest = 'manifest.standalone'
+	def manifestContent = sh(script: "ls $jSparrowTargetPath", returnStdout: true)
+
+	stage("JMP Deploy: ${profile.formattedName()}") {
+		dir('jsparrow-maven-plugin/src/main/resources') {
+			writeFile file: "${manifest}", text: "${manifestContent}"
+		}
+
+		// Copy required dependencies into plugin resource folder
+		sh("cp ${jSparrowTargetPath}/* ${pluginResourcePath}")
+
+		dir('jsparrow-maven-plugin') {
+			sh "'${mvnBin()}' ${mvnCommand}"
+		}
+	}
+}
+
+void uploadMappingFile(Profile profile) {
+
+	// TODO there has to be a nicer way to get the version (and then the directory), when we already have the qualifier
+	def buildNumber = sh(returnStdout: true, script: "pcregrep -o1 \"name='eu.jsparrow\\.feature\\.feature\\.group' range='\\[.*,((\\d*\\.){3}\\d{8}-\\d{4})\" releng/site/target/p2content.xml").trim()
+	def directory = "${buildNumber}${profile.qualifier}"
+
+	stage ("Upload Mapping Files") {
+		uploadMappingFiles(directory)
+	}
+}
+
+void runStandardSteps() {
+	checkout()
+	pushToGithub()
+	compileEclipsePlugin()
+	compileMavenPlugin()
+	runIntegrationTests()
 }
 
 def setTestStatus(testStatus) {
