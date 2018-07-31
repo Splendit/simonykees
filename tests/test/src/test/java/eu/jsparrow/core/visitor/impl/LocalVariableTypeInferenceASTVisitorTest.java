9d39c0e1ef14c9296e407507481ce2a6649d4994
ardit.ymeri <ardit.ymeri@splendit.at>
2018-07-31 14:57:22 +0200

    SIM-1223 UPDATE adding visitor tests

--- tests/test/src/test/java/eu/jsparrow/core/visitor/impl/LocalVariableTypeInferenceASTVisitorTest.java
+++ tests/test/src/test/java/eu/jsparrow/core/visitor/impl/LocalVariableTypeInferenceASTVisitorTest.java
@@ -8,6 +8,7 @@ import org.junit.Test;
 
 import eu.jsparrow.rules.java10.LocalVariableTypeInferenceASTVisitor;
 
+@SuppressWarnings("nls")
 public class LocalVariableTypeInferenceASTVisitorTest extends UsesJDTUnitFixture {
 
 	private LocalVariableTypeInferenceASTVisitor visitor;
@@ -15,6 +16,35 @@ public class LocalVariableTypeInferenceASTVisitorTest extends UsesJDTUnitFixture
 	@Before
 	public void setUp() {
 		visitor = new LocalVariableTypeInferenceASTVisitor();
+	}
+	
+	@Test
+	public void visit_simpleTypeInitialization_shouldReplace() throws Exception {
+		fixture.addImport("java.util.Date");
+		String block = "Date date = new Date();";
+		String varDeclaration = "var date = new Date();";
+
+		fixture.addMethodBlock(block);
+		visitor.setASTRewrite(fixture.getAstRewrite());
+		
+		fixture.accept(visitor);
+		
+		Block expectedBlock = createBlock(varDeclaration);
+		assertMatch(expectedBlock, fixture.getMethodBlock());
+	}
+	
+	@Test
+	public void visit_enhancedForLoopDeclaration_shouldReplace() throws Exception {
+		String block = "for(String string : Arrays.asList(\"1\", \"2\", \"3\")) {}";
+		String varDeclaration = "for(var string : Arrays.asList(\"1\", \"2\", \"3\")) {}";
+
+		fixture.addMethodBlock(block);
+		visitor.setASTRewrite(fixture.getAstRewrite());
+		
+		fixture.accept(visitor);
+		
+		Block expectedBlock = createBlock(varDeclaration);
+		assertMatch(expectedBlock, fixture.getMethodBlock());
 	}
 
 	@Test
@@ -32,5 +62,84 @@ public class LocalVariableTypeInferenceASTVisitorTest extends UsesJDTUnitFixture
 		Block expected = createBlock(block);
 		assertMatch(expected, fixture.getMethodBlock());
 	}
+	
+	@Test
+	public void visit_initizationHavingDiamond_shouldNotReplace() throws Exception {
+		fixture.addImport("java.util.HashMap");
+		String block = "HashMap<String, String> map = new HashMap<>();";
+		fixture.addMethodBlock(block);
+		visitor.setASTRewrite(fixture.getAstRewrite());
+		
+		fixture.accept(visitor);
+		
+		Block expectedBlock = createBlock(block);
+		assertMatch(expectedBlock, fixture.getMethodBlock());
+	}
+	
+	@Test
+	public void visit_initizationWithSubtype_shouldNotReplace() throws Exception {
+		fixture.addImport("java.util.HashMap");
+		fixture.addImport("java.util.Map");
+		String block = "Map<String, String> map = new HashMap<String, String>();";
+		fixture.addMethodBlock(block);
+		visitor.setASTRewrite(fixture.getAstRewrite());
+		
+		fixture.accept(visitor);
+		
+		Block expectedBlock = createBlock(block);
+		assertMatch(expectedBlock, fixture.getMethodBlock());
+	}
+	
+	@Test
+	public void visit_simpleTypeWithoutInitialization_shouldNotReplace() throws Exception {
+		fixture.addImport("java.util.Date");
+		String block = "Date date; date = new Date();";
 
+		fixture.addMethodBlock(block);
+		visitor.setASTRewrite(fixture.getAstRewrite());
+		
+		fixture.accept(visitor);
+		
+		Block expectedBlock = createBlock(block);
+		assertMatch(expectedBlock, fixture.getMethodBlock());
+	}
+	
+	@Test
+	public void visit_singleVariableDeclarationInCatchBlock_shouldNotReplace() throws Exception {
+		fixture.addImport("java.io.File");
+		String block = "try {new File(\"file.name\");} catch(NullPointerException e) {}";
+
+		fixture.addMethodBlock(block);
+		visitor.setASTRewrite(fixture.getAstRewrite());
+		
+		fixture.accept(visitor);
+		
+		Block expectedBlock = createBlock(block);
+		assertMatch(expectedBlock, fixture.getMethodBlock());
+	}
+
+	@Test
+	public void visit_initializationWithLambda_shouldNotReplace() throws Exception {
+		String block = "Runnable r = () -> {};";
+		fixture.addMethodBlock(block);
+		visitor.setASTRewrite(fixture.getAstRewrite());
+		
+		fixture.accept(visitor);
+		
+		Block expectedBlock = createBlock(block);
+		assertMatch(expectedBlock, fixture.getMethodBlock());
+	}
+	
+	@Test
+	public void visit_singleDeclarationInLambdaParameter_shouldNotReplace() throws Exception {
+		fixture.addImport("java.util.Predicate");
+		String block = "Predicate<String> myPredicate; myPredicate = (String value) -> value.isEmpty();";
+		fixture.addMethodBlock(block);
+		visitor.setASTRewrite(fixture.getAstRewrite());
+		
+		fixture.accept(visitor);
+		
+		Block expectedBlock = createBlock(block);
+		assertMatch(expectedBlock, fixture.getMethodBlock());
+	}
 }
