bf851a8d4db61045dd7fabb4e13fdbf11afb4cc7
ardit.ymeri <ardit.ymeri@splendit.at>
2018-08-27 09:57:22 +0200

    SIM-1211 UPDATE adding more unit tests and updating existing ones

--- tests/standalone.test/src/test/java/eu/jsparrow/standalone/RuleConfigurationWrapperTest.java
+++ tests/standalone.test/src/test/java/eu/jsparrow/standalone/RuleConfigurationWrapperTest.java
@@ -2,24 +2,34 @@ package eu.jsparrow.standalone;
 
 import java.util.Collections;
 import java.util.List;
-
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.*;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import java.util.Map;
 
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
-import static org.mockito.Mockito.*;
-
 import eu.jsparrow.core.config.YAMLConfig;
 import eu.jsparrow.core.config.YAMLProfile;
+import eu.jsparrow.core.config.YAMLRenamingRule;
+import eu.jsparrow.core.config.YAMLLoggerRule;
 import eu.jsparrow.core.rule.impl.CodeFormatterRule;
+import eu.jsparrow.core.rule.impl.logger.LogLevelEnum;
 import eu.jsparrow.rules.common.RefactoringRule;
 import eu.jsparrow.standalone.exceptions.StandaloneException;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.hasEntry;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @SuppressWarnings("nls")
 public class RuleConfigurationWrapperTest {
@@ -30,7 +40,7 @@ public class RuleConfigurationWrapperTest {
 
 	private String selectedProfileName = "selectedProfileName";
 	private RefactoringRule refactoringRule = new CodeFormatterRule();
-	
+
 	@Rule
 	public ExpectedException expectedException = ExpectedException.none();
 
@@ -39,34 +49,30 @@ public class RuleConfigurationWrapperTest {
 		config = mock(YAMLConfig.class);
 		selectedProfile = mock(YAMLProfile.class);
 		refactoringRules = Collections.singletonList(refactoringRule);
-		
-		when(config.getSelectedProfile()).thenReturn(selectedProfileName);
+
 		when(selectedProfile.getName()).thenReturn(selectedProfileName);
 		when(config.getProfiles()).thenReturn(Collections.singletonList(selectedProfile));
-
-		
 	}
 
 	@Test
 	public void computeConfiguration_selectedProfile_shouldComputeConfigFromSelectedProfile() throws Exception {
-
+		when(config.getSelectedProfile()).thenReturn(selectedProfileName);
 
 		new RuleConfigurationWrapper(config, refactoringRules);
 
 		verify(selectedProfile, times(1)).getLoggerRule();
 		verify(selectedProfile, times(1)).getRenamingRule();
 	}
-	
+
 	@Test
 	public void computeConfiguration_noSelectedProfile_shouldComputeConfigFromRoot() throws Exception {
-		when(config.getSelectedProfile()).thenReturn("");
 
 		new RuleConfigurationWrapper(config, refactoringRules);
 
 		verify(config, times(1)).getLoggerRule();
 		verify(config, times(1)).getRenamingRule();
 	}
-	
+
 	@Test
 	public void computeConfiguration_invalidSelectedProfile_shouldThrowException() throws Exception {
 		String invalidSelectedProfileName = "invalidProfileName";
@@ -79,67 +85,112 @@ public class RuleConfigurationWrapperTest {
 		verify(config, never()).getLoggerRule();
 		verify(config, never()).getRenamingRule();
 	}
-	
+
 	@Test
 	public void getSelectedAutomaticRules_selectedProfile_shouldReturnRulesFromProfile() throws Exception {
+		when(config.getSelectedProfile()).thenReturn(selectedProfileName);
 		when(selectedProfile.getRules()).thenReturn(Collections.singletonList(refactoringRule.getId()));
-		
+
 		RuleConfigurationWrapper ruleConfigurationWrapper = new RuleConfigurationWrapper(config, refactoringRules);
 		List<RefactoringRule> rules = ruleConfigurationWrapper.getSelectedAutomaticRules();
-		
-		assertThat(rules, allOf(hasSize(1), contains(refactoringRule)));
+
+		assertThat(rules, hasSize(1));
+		assertThat(rules, contains(refactoringRule));
 	}
-	
+
 	@Test
 	public void getSelectedAutomaticRules_noSelectedProfile_shouldReturnRulesRoot() throws Exception {
-		when(config.getSelectedProfile()).thenReturn("");
 		when(config.getRules()).thenReturn(Collections.singletonList(refactoringRule.getId()));
-	
+
 		RuleConfigurationWrapper ruleConfigurationWrapper = new RuleConfigurationWrapper(config, refactoringRules);
 		List<RefactoringRule> rules = ruleConfigurationWrapper.getSelectedAutomaticRules();
-		
-		assertThat(rules, allOf(hasSize(1), contains(refactoringRule)));
+
+		assertThat(rules, hasSize(1));
+		assertThat(rules, contains(refactoringRule));
 	}
-	
+
 	@Test
 	public void isSelectedRule_selectedProfile_shouldReturnTrue() throws Exception {
+		when(config.getSelectedProfile()).thenReturn(selectedProfileName);
 		when(selectedProfile.getRules()).thenReturn(Collections.singletonList(refactoringRule.getId()));
-		
+
 		RuleConfigurationWrapper ruleConfigurationWrapper = new RuleConfigurationWrapper(config, refactoringRules);
 		boolean selectedRefactoring = ruleConfigurationWrapper.isSelectedRule(refactoringRule.getId());
-		
+
 		assertTrue(selectedRefactoring);
 	}
-	
+
 	@Test
 	public void isSelectedRule_selectedProfile_shouldReturnFalse() throws Exception {
+		when(config.getSelectedProfile()).thenReturn(selectedProfileName);
 		when(selectedProfile.getRules()).thenReturn(Collections.singletonList(refactoringRule.getId()));
-		
+
 		RuleConfigurationWrapper ruleConfigurationWrapper = new RuleConfigurationWrapper(config, refactoringRules);
 		boolean selectedRefactoring = ruleConfigurationWrapper.isSelectedRule("some-other-rule-id");
-		
+
 		assertFalse(selectedRefactoring);
 	}
-	
+
 	@Test
 	public void isSelectedRule_noSelectedProfile_shouldReturnTrue() throws Exception {
-		when(config.getSelectedProfile()).thenReturn("");
 		when(config.getRules()).thenReturn(Collections.singletonList(refactoringRule.getId()));
-		
+
 		RuleConfigurationWrapper ruleConfigurationWrapper = new RuleConfigurationWrapper(config, refactoringRules);
 		boolean selectedRefactoring = ruleConfigurationWrapper.isSelectedRule(refactoringRule.getId());
-		
+
 		assertTrue(selectedRefactoring);
 	}
-	
+
 	@Test
 	public void isSelectedRule_noSelectedProfile_shouldReturnFalse() throws Exception {
-		when(config.getSelectedProfile()).thenReturn("");
 		when(config.getRules()).thenReturn(Collections.singletonList(refactoringRule.getId()));
-		
+
 		RuleConfigurationWrapper ruleConfigurationWrapper = new RuleConfigurationWrapper(config, refactoringRules);
 		boolean selectedRefactoring = ruleConfigurationWrapper.isSelectedRule("some-other-rule");
-		
+
 		assertFalse(selectedRefactoring);
+	}
+
+	@Test
+	public void getLoggerConfiguration_emptyConfiguration_shouldReturnEmptyMap() throws Exception {
+		YAMLLoggerRule loggerRuleConfiguration = new YAMLLoggerRule();
+		when(config.getLoggerRule()).thenReturn(loggerRuleConfiguration);
+
+		RuleConfigurationWrapper ruleConfigurationWrapper = new RuleConfigurationWrapper(config, refactoringRules);
+		Map<String, String> loggerConfigurationOptions = ruleConfigurationWrapper.getLoggerConfigurationOptions();
+
+		assertTrue(loggerConfigurationOptions.isEmpty());
+	}
+
+	@SuppressWarnings("unchecked")
+	@Test
+	public void getLoggerConfiguration_defaultConfiguration_shouldDefaultOptions() throws Exception {
+		YAMLLoggerRule loggerRuleConfiguration = new YAMLLoggerRule(LogLevelEnum.INFO, LogLevelEnum.ERROR,
+				LogLevelEnum.ERROR, LogLevelEnum.INFO, LogLevelEnum.ERROR, LogLevelEnum.ERROR, true);
+		when(config.getLoggerRule()).thenReturn(loggerRuleConfiguration);
+
+		RuleConfigurationWrapper ruleConfigurationWrapper = new RuleConfigurationWrapper(config, refactoringRules);
+		Map<String, String> loggerConfigurationOptions = ruleConfigurationWrapper.getLoggerConfigurationOptions();
+
+		assertThat(loggerConfigurationOptions,
+				allOf(hasEntry("system-out-print", "info"), hasEntry("system-err-print", "error"),
+						hasEntry("print-stacktrace", "error"), hasEntry("system-out-print-exception", "info"),
+						hasEntry("system-err-print-exception", "error"), hasEntry("new-logging-statement", "error"),
+						hasEntry("attach-exception-object", "true")));
+
+	}
+
+	@Test
+	public void getFieldRenamingOptions_emptyConfiguration_shouldReturnEmptyMap() throws Exception {
+		YAMLRenamingRule renamingConfiguration = new YAMLRenamingRule();
+		when(config.getRenamingRule()).thenReturn(renamingConfiguration);
+
+		RuleConfigurationWrapper ruleConfigurationWrapper = new RuleConfigurationWrapper(config, refactoringRules);
+		Map<String, Boolean> renamingConfigurationOptions = ruleConfigurationWrapper.getFieldRenamingOptions();
+
+		assertThat(renamingConfigurationOptions,
+				allOf(hasEntry("public", true), hasEntry("package-private", true), hasEntry("protected", true),
+						hasEntry("private", true), hasEntry("add-todo", false),
+						hasEntry("uppercase-after-underscore", true)));
 	}
 }
