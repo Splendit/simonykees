401937ff376bc349a85935e79f26b79d51cc351f
ardit.ymeri <ardit.ymeri@splendit.at>
2018-09-03 17:02:15 +0200

    SIM-1247 ADD legacy bundle for organize imports rule

---
+++ bundles/rules.imports.old/src/main/java/eu/jsparrow/rules/imports/OldImportsRuleServiceImpl.java
@@ -0,0 +1,25 @@
+package eu.jsparrow.rules.imports;
+
+import java.util.Collections;
+import java.util.List;
+
+import org.osgi.service.component.annotations.Component;
+
+import eu.jsparrow.rules.api.RuleService;
+import eu.jsparrow.rules.common.RefactoringRule;
+
+/**
+ * Implementor of {@link RuleService}. Currently provides only {@link OrganiseImportsRule}.  
+ *
+ * @author Hans-Jörg Schrödl
+ *
+ */
+@Component
+public class OldImportsRuleServiceImpl implements RuleService {
+
+	@Override
+	public List<RefactoringRule> loadRules() {
+		return Collections.singletonList(new OrganiseImportsRule());
+	}
+
+}
