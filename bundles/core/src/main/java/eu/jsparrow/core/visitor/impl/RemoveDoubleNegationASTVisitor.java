05cf58d3cc31447f462c14869ef3a6732cb3f1c2
ardit.ymeri <ardit.ymeri@splendit.at>
2018-10-23 18:12:10 +0200

    SIM-1309 UPDATE preserving comments

--- bundles/core/src/main/java/eu/jsparrow/core/visitor/impl/RemoveDoubleNegationASTVisitor.java
+++ bundles/core/src/main/java/eu/jsparrow/core/visitor/impl/RemoveDoubleNegationASTVisitor.java
@@ -1,9 +1,15 @@
 package eu.jsparrow.core.visitor.impl;
 
-import org.eclipse.jdt.core.dom.ASTNode;
-import org.eclipse.jdt.core.dom.PrefixExpression;
+import java.util.List;
 
+import org.eclipse.jdt.core.dom.ASTNode;
+import org.eclipse.jdt.core.dom.Comment;
+import org.eclipse.jdt.core.dom.PrefixExpression;
+import org.eclipse.jdt.core.dom.Statement;
+
+import eu.jsparrow.rules.common.util.ASTNodeUtil;
 import eu.jsparrow.rules.common.visitor.AbstractASTRewriteASTVisitor;
+import eu.jsparrow.rules.common.visitor.helper.CommentRewriter;
 
 /**
  * ASTVisitor that searches control statements for non-block bodies and wraps it
@@ -19,27 +25,40 @@ public class RemoveDoubleNegationASTVisitor extends AbstractASTRewriteASTVisitor
 		ASTNode replaceNode = unwrapNegations(prefixExpression, true);
 
 		if (null != replaceNode && prefixExpression != replaceNode) {
-			astRewrite.replace(prefixExpression, replaceNode, null);
-			onRewrite();			
+			astRewrite.replace(prefixExpression, astRewrite.createCopyTarget(replaceNode), null);
+			saveComments(prefixExpression, replaceNode);
+			onRewrite();
 		}
 
 		return true;
 	}
 
-	
+	private void saveComments(PrefixExpression prefixExpression, ASTNode replaceNode) {
+		CommentRewriter commentRewriter = getCommentRewriter();
+		List<Comment> comments = commentRewriter.findRelatedComments(prefixExpression);
+		List<Comment> remainingComents = commentRewriter.findRelatedComments(replaceNode);
+		comments.removeAll(remainingComents);
+		Statement parentStatement = ASTNodeUtil.getSpecificAncestor(prefixExpression, Statement.class);
+		commentRewriter.saveBeforeStatement(parentStatement, comments);
+	}
+
 	/**
 	 * Recursive search for possible replacement node
-	 * @param node input node
-	 * @param selector alternating selector for node
+	 * 
+	 * @param node
+	 *            input node
+	 * @param selector
+	 *            alternating selector for node
 	 * @return node to replace the expression with
 	 */
-	ASTNode unwrapNegations(ASTNode node, boolean selector) {
+	private ASTNode unwrapNegations(ASTNode node, boolean selector) {
 		PrefixExpression prefixExpression = null;
 		if (ASTNode.PREFIX_EXPRESSION == node.getNodeType()) {
 			prefixExpression = (PrefixExpression) node;
 		}
-		
-		boolean isNotOperator = prefixExpression != null && PrefixExpression.Operator.NOT == prefixExpression.getOperator();
+
+		boolean isNotOperator = prefixExpression != null
+				&& PrefixExpression.Operator.NOT == prefixExpression.getOperator();
 
 		if (isNotOperator) {
 			ASTNode returnValue = unwrapNegations(prefixExpression.getOperand(), !selector);
@@ -48,9 +67,8 @@ public class RemoveDoubleNegationASTVisitor extends AbstractASTRewriteASTVisitor
 			} else {
 				return returnValue;
 			}
-		}
-		else {
-			if(selector) {
+		} else {
+			if (selector) {
 				return node;
 			}
 			return null;
