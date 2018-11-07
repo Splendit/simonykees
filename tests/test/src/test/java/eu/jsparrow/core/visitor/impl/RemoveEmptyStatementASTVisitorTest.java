a96a102fa04343fe4084a4b7325aac48d5317400
ardit.ymeri <ardit.ymeri@splendit.at>
2018-11-07 10:39:07 +0100

    SIM-1304 UPDATE removing unnecessary semicolons in switch statements

--- tests/test/src/test/java/eu/jsparrow/core/visitor/impl/RemoveEmptyStatementASTVisitorTest.java
+++ tests/test/src/test/java/eu/jsparrow/core/visitor/impl/RemoveEmptyStatementASTVisitorTest.java
@@ -6,20 +6,21 @@ import org.eclipse.jdt.core.dom.Block;
 import org.junit.Before;
 import org.junit.Test;
 
+@SuppressWarnings("nls")
 public class RemoveEmptyStatementASTVisitorTest extends UsesJDTUnitFixture {
-	
+
 	private RemoveEmptyStatementASTVisitor visitor;
-	
+
 	@Before
 	public void setUp() {
-		 visitor = new RemoveEmptyStatementASTVisitor();
+		visitor = new RemoveEmptyStatementASTVisitor();
 	}
-	
+
 	@Test
 	public void visit_removeEmptyStatementFromMethodBody_shouldRefactor() throws Exception {
 		String block = "; int i = 0;";
 		String expected = "int i = 0;";
-		
+
 		fixture.addMethodBlock(block);
 		visitor.setASTRewrite(fixture.getAstRewrite());
 		fixture.accept(visitor);
@@ -27,12 +28,38 @@ public class RemoveEmptyStatementASTVisitorTest extends UsesJDTUnitFixture {
 		Block expectedBlock = createBlock(expected);
 		assertMatch(expectedBlock, fixture.getMethodBlock());
 	}
-	
+
+	@Test
+	public void visit_removeEmptyStatementFromSwitchCaseStatement_shouldRefactor() throws Exception {
+		String block = "switch (\"\") { case \"\": ; break; default: break; }";
+		String expected = "switch (\"\") { case \"\":  break; default: break; }";
+
+		fixture.addMethodBlock(block);
+		visitor.setASTRewrite(fixture.getAstRewrite());
+		fixture.accept(visitor);
+
+		Block expectedBlock = createBlock(expected);
+		assertMatch(expectedBlock, fixture.getMethodBlock());
+	}
+
+	@Test
+	public void visit_removeEmptyStatementFromSwitchDefaultStatement_shouldRefactor() throws Exception {
+		String block = "switch (\"\") { case \"\": break; default: ; break; }";
+		String expected = "switch (\"\") { case \"\":  break; default: break; }";
+
+		fixture.addMethodBlock(block);
+		visitor.setASTRewrite(fixture.getAstRewrite());
+		fixture.accept(visitor);
+
+		Block expectedBlock = createBlock(expected);
+		assertMatch(expectedBlock, fixture.getMethodBlock());
+	}
+
 	@Test
 	public void visit_removeMultipleEmptyStatementFromMethodBody_shouldRefactor() throws Exception {
 		String block = ";;;;;; ;;; ;;;; ;; int i = 0;;;;; ;;; ;;";
 		String expected = "int i = 0;";
-		
+
 		fixture.addMethodBlock(block);
 		visitor.setASTRewrite(fixture.getAstRewrite());
 		fixture.accept(visitor);
@@ -40,13 +67,12 @@ public class RemoveEmptyStatementASTVisitorTest extends UsesJDTUnitFixture {
 		Block expectedBlock = createBlock(expected);
 		assertMatch(expectedBlock, fixture.getMethodBlock());
 	}
-	
-	
+
 	@Test
 	public void visit_removeEmptyStatementFromIfBody_shouldRefactor() throws Exception {
 		String block = "if(true) {;} int i = 0;";
 		String expected = "if(true) { } int i = 0;";
-		
+
 		fixture.addMethodBlock(block);
 		visitor.setASTRewrite(fixture.getAstRewrite());
 		fixture.accept(visitor);
@@ -54,11 +80,11 @@ public class RemoveEmptyStatementASTVisitorTest extends UsesJDTUnitFixture {
 		Block expectedBlock = createBlock(expected);
 		assertMatch(expectedBlock, fixture.getMethodBlock());
 	}
-	
+
 	@Test
 	public void visit_singleBodyIfStatement_shouldNotRefactor() throws Exception {
 		String block = "if(true) ; int i = 0;";
-		
+
 		fixture.addMethodBlock(block);
 		visitor.setASTRewrite(fixture.getAstRewrite());
 		fixture.accept(visitor);
@@ -66,12 +92,11 @@ public class RemoveEmptyStatementASTVisitorTest extends UsesJDTUnitFixture {
 		Block expectedBlock = createBlock(block);
 		assertMatch(expectedBlock, fixture.getMethodBlock());
 	}
-	
-	
+
 	@Test
 	public void visit_singleBodyForStatement_shouldNotRefactor() throws Exception {
 		String block = "for(String string : Arrays.asList(\"\")) ;";
-		
+
 		fixture.addImport(java.util.Arrays.class.getName());
 		fixture.addMethodBlock(block);
 		visitor.setASTRewrite(fixture.getAstRewrite());
@@ -80,11 +105,11 @@ public class RemoveEmptyStatementASTVisitorTest extends UsesJDTUnitFixture {
 		Block expectedBlock = createBlock(block);
 		assertMatch(expectedBlock, fixture.getMethodBlock());
 	}
-	
+
 	@Test
 	public void visit_emptyForLoopHeader_shouldNotRefactor() throws Exception {
 		String block = "for(String string : Arrays.asList(\"\")) ;";
-		
+
 		fixture.addImport(java.util.Arrays.class.getName());
 		fixture.addMethodBlock(block);
 		visitor.setASTRewrite(fixture.getAstRewrite());
@@ -93,7 +118,4 @@ public class RemoveEmptyStatementASTVisitorTest extends UsesJDTUnitFixture {
 		Block expectedBlock = createBlock(block);
 		assertMatch(expectedBlock, fixture.getMethodBlock());
 	}
-	
-	
-
 }
