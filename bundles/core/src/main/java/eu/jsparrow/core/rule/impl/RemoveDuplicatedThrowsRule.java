7861ceb21821c9d2964579b4e0eb7ae06329ec1d
ardit.ymeri <ardit.ymeri@splendit.at>
2018-11-07 17:40:02 +0100

    SIM-1310 ADD rule - removing duplicated, subtypes and runtime exceptions

---
+++ bundles/core/src/main/java/eu/jsparrow/core/rule/impl/RemoveDuplicatedThrowsRule.java
@@ -0,0 +1,33 @@
+package eu.jsparrow.core.rule.impl;
+
+import java.time.Duration;
+import java.util.Arrays;
+
+import org.eclipse.jdt.core.JavaCore;
+
+import eu.jsparrow.core.visitor.impl.RemoveDuplicatedThrowsASTVisitor;
+import eu.jsparrow.rules.common.RefactoringRuleImpl;
+import eu.jsparrow.rules.common.RuleDescription;
+import eu.jsparrow.rules.common.Tag;
+
+/**
+ * @see RemoveDuplicatedThrowsASTVisitor
+ * 
+ * @since 2.7.0
+ *
+ */
+public class RemoveDuplicatedThrowsRule extends RefactoringRuleImpl<RemoveDuplicatedThrowsASTVisitor> {
+
+	public RemoveDuplicatedThrowsRule() {
+		this.visitorClass = RemoveDuplicatedThrowsASTVisitor.class;
+		this.id = "RemoveDuplicatedThrows"; //$NON-NLS-1$
+		this.ruleDescription = new RuleDescription("Remove Duplicated Throws", "", Duration.ofMinutes(2),
+				Arrays.asList(Tag.JAVA_1_1, Tag.READABILITY));
+	}
+
+	@Override
+	protected String provideRequiredJavaVersion() {
+		return JavaCore.VERSION_1_1;
+	}
+
+}
