75bd0d6da394ec524abd6dedbe031f690f00cd03
matthias.webhofer <matthias.webhofer@splendit.at>
2018-11-14 12:23:38 +0100

    SIM-1318 ADD entities for statistics metadata

---
+++ bundles/core/src/main/java/eu/jsparrow/core/refactorer/StandaloneStatisticsMetadata.java
@@ -0,0 +1,47 @@
+package eu.jsparrow.core.refactorer;
+
+public class StandaloneStatisticsMetadata {
+
+	private long startTime;
+	private String repoOwner;
+	private String repoName;
+
+	public StandaloneStatisticsMetadata() {
+
+	}
+
+	public StandaloneStatisticsMetadata(long startTime, String repoOwner, String repoName) {
+		this.startTime = startTime;
+		this.repoOwner = repoOwner;
+		this.repoName = repoName;
+	}
+
+	public boolean isValid() {
+		return startTime > 0 && repoOwner != null && !repoOwner.isEmpty() && repoName != null && !repoName.isEmpty();
+	}
+
+	public long getStartTime() {
+		return startTime;
+	}
+
+	public void setStartTime(long startTime) {
+		this.startTime = startTime;
+	}
+
+	public String getRepoOwner() {
+		return repoOwner;
+	}
+
+	public void setRepoOwner(String repoOwner) {
+		this.repoOwner = repoOwner;
+	}
+
+	public String getRepoName() {
+		return repoName;
+	}
+
+	public void setRepoName(String repoName) {
+		this.repoName = repoName;
+	}
+
+}
