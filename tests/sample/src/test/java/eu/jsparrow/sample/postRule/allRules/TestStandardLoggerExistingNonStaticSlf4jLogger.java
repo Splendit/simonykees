97ac42fce3cd8fe7f2fde2d5839d2c80e02f830c
matthias.webhofer <matthias.webhofer@splendit.at>
2018-11-26 17:49:16 +0100

    SIM-1337 UPDATE test cases

--- tests/sample/src/test/java/eu/jsparrow/sample/postRule/allRules/TestStandardLoggerExistingNonStaticSlf4jLogger.java
+++ tests/sample/src/test/java/eu/jsparrow/sample/postRule/allRules/TestStandardLoggerExistingNonStaticSlf4jLogger.java
@@ -5,14 +5,19 @@ import org.slf4j.LoggerFactory;
 
 import eu.jsparrow.sample.postRule.standardLoggerSlf4j.TestStandardLoggerExistingSlf4jLogger;
 
+/**
+ * SIM-1337
+ *
+ */
 public class TestStandardLoggerExistingNonStaticSlf4jLogger {
-	private final Logger logger = LoggerFactory.getLogger(TestStandardLoggerExistingSlf4jLogger.class);
+
+	private final Logger logger = LoggerFactory.getLogger(TestStandardLoggerExistingNonStaticSlf4jLogger.class);
 
 	public void testingLogger() {
 		logger.info("");
 		logger.info("my val");
 	}
-	
+
 	public static void noChangesInStaticMethods() {
 		System.out.println("no changes here");
 	}
