699240029d211bb3b73123b25fac266234879f0c
Andreja <andreja.sambolec@splendit.at>
2018-12-12 12:07:25 +0100

    SIM-1353 ADD free icon for free rule in free license

--- bundles/ui/src/main/java/eu/jsparrow/ui/wizard/impl/TreeLabelProvider.java
+++ bundles/ui/src/main/java/eu/jsparrow/ui/wizard/impl/TreeLabelProvider.java
@@ -1,6 +1,14 @@
 package eu.jsparrow.ui.wizard.impl;
 
+import java.net.URL;
+import java.util.HashMap;
+
+import org.eclipse.core.runtime.FileLocator;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.core.runtime.Platform;
 import org.eclipse.jface.dialogs.Dialog;
+import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.jface.resource.JFaceResources;
 import org.eclipse.jface.resource.LocalResourceManager;
 import org.eclipse.jface.resource.ResourceManager;
@@ -9,20 +17,37 @@ import org.eclipse.jface.viewers.LabelProvider;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.graphics.ImageData;
 import org.eclipse.swt.widgets.Display;
+import org.osgi.framework.Bundle;
 
 import eu.jsparrow.rules.common.RefactoringRule;
+import eu.jsparrow.ui.Activator;
+import eu.jsparrow.ui.util.LicenseUtil;
 
 /**
  * Label provider for left view in select rules wizard
  * 
- * @author Andreja Sambolec
  * @since 1.2
  *
  */
 public class TreeLabelProvider extends LabelProvider implements IColorProvider {
 
 	private ResourceManager resourceManager;
+
+	private Image greenFreeRuleImage;
+	private static final String F_GREEN_ICON_PATH = "icons/f-icon-green-14px.png"; //$NON-NLS-1$
+
+	public TreeLabelProvider() {
+		Bundle bundle = Platform.getBundle(Activator.PLUGIN_ID);
+
+		IPath iPathTickMarkGreen = new Path(F_GREEN_ICON_PATH);
+		URL urlTickMarkGreen = FileLocator.find(bundle, iPathTickMarkGreen, new HashMap<>());
+		ImageDescriptor imageDescTickMarkGreen = ImageDescriptor.createFromURL(urlTickMarkGreen);
+		Image tickmarkGreenIconImage = imageDescTickMarkGreen.createImage();
+		ImageData imageDataTickmarkGreen = tickmarkGreenIconImage.getImageData();
+		greenFreeRuleImage = new Image(Display.getCurrent(), imageDataTickmarkGreen);
+	}
 
 	@Override
 	public String getText(Object element) {
@@ -39,12 +64,15 @@ public class TreeLabelProvider extends LabelProvider implements IColorProvider {
 	@Override
 	public Image getImage(Object element) {
 		if (element instanceof RefactoringRule) {
-			if (((RefactoringRule) element).isEnabled()) {
-				// without icon
-			} else {
+			RefactoringRule rule = (RefactoringRule) element;
+			if (!rule.isEnabled()) {
 				// info icon that rule is disabled, explanation appears in
 				// description text when rule is clicked
 				return JFaceResources.getImage(Dialog.DLG_IMG_MESSAGE_INFO);
+			} else if (rule.isFree() && LicenseUtil.get().isFreeLicense()) {
+				return greenFreeRuleImage;
+			} else {
+				// without icon
 			}
 		}
 		return super.getImage(element);
