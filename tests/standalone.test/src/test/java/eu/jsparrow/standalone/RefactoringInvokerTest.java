a6666d85fa2807cd0192fa18247d60c5b530b3ef
Matthias Webhofer <matthias.webhofer@splendit.at>
2019-03-13 17:27:58 +0100

    SIM-1484 UPDATE RefactoringInvokerTest

--- tests/standalone.test/src/test/java/eu/jsparrow/standalone/RefactoringInvokerTest.java
+++ tests/standalone.test/src/test/java/eu/jsparrow/standalone/RefactoringInvokerTest.java
@@ -1,95 +1,108 @@
-//package eu.jsparrow.standalone;
-//
-//import static org.mockito.Mockito.doThrow;
-//import static org.mockito.Mockito.mock;
-//import static org.mockito.Mockito.never;
-//import static org.mockito.Mockito.verify;
-//import static org.mockito.Mockito.when;
-//
-//import java.util.Arrays;
-//
-//import org.eclipse.jdt.core.IJavaProject;
-//import org.junit.Before;
-//import org.junit.Rule;
-//import org.junit.Test;
-//import org.junit.rules.ExpectedException;
-//import org.osgi.framework.BundleContext;
-//
-//import eu.jsparrow.core.refactorer.RefactoringPipeline;
-//import eu.jsparrow.standalone.exceptions.StandaloneException;
-//
-///**
-// * test class for {@link RefactoringInvoker}
-// * 
-// * @author Matthias Webhofer, Hans-Jörg Schrödl
-// * @since 2.5.0
-// */
-//public class RefactoringInvokerTest {
-//
-//	private RefactoringInvoker refactoringInvoker;
-//	private StandaloneConfig standaloneConfig;
-//
-//	@Rule
-//	public ExpectedException expectedException = ExpectedException.none();
-//
-//	@Before
-//	public void setUp() {
-//		refactoringInvoker = new TestableRefactoringInvoker();
-//
-//		IJavaProject javaProject = mock(IJavaProject.class);
-//		when(javaProject.getElementName()).thenReturn("projectName"); //$NON-NLS-1$
-//
-//		standaloneConfig = mock(StandaloneConfig.class);
-//		when(standaloneConfig.getJavaProject()).thenReturn(javaProject);
-//	}
-//
-//	@Test
-//	public void startRefactoring() throws Exception {
-//		BundleContext context = mock(BundleContext.class);
-//		RefactoringPipeline refactoringPipeline = mock(RefactoringPipeline.class);
-//
-//		IJavaProject javaProject = mock(IJavaProject.class);
-//		when(javaProject.getElementName()).thenReturn(""); //$NON-NLS-1$
-//		when(refactoringPipeline.getRulesWithChangesAsString()).thenReturn(""); //$NON-NLS-1$
-//
-//		refactoringInvoker.startRefactoring(context);
-//
-//		verify(standaloneConfig).createRefactoringStates();
-//		verify(standaloneConfig).computeRefactoring();
-//		verify(standaloneConfig).commitRefactoring();
-//	}
-//
-//	@Test
-//	public void startRefactoring_exceptionsInCreateRefactoringState_shouldNotCommit() throws Exception {
-//		BundleContext context = mock(BundleContext.class);
-//
-//		doThrow(StandaloneException.class).when(standaloneConfig)
-//			.createRefactoringStates();
-//
-//		expectedException.expect(StandaloneException.class);
-//		refactoringInvoker.startRefactoring(context);
-//
-//		verify(standaloneConfig, never()).computeRefactoring();
-//		verify(standaloneConfig, never()).commitRefactoring();
-//	}
-//
-//	@Test
-//	public void startRefactoring_exceptionsInDoRefactoring_shouldNotCommit() throws Exception {
-//		BundleContext context = mock(BundleContext.class);
-//		doThrow(StandaloneException.class).when(standaloneConfig)
-//			.computeRefactoring();
-//
-//		expectedException.expect(StandaloneException.class);
-//		refactoringInvoker.startRefactoring(context);
-//
-//		verify(standaloneConfig, never()).commitRefactoring();
-//	}
-//
-//	class TestableRefactoringInvoker extends RefactoringInvoker {
-//
-//		@Override
-//		protected void loadStandaloneConfig(BundleContext context) {
-//			super.standaloneConfigs = Arrays.asList(standaloneConfig);
-//		}
-//	}
-//}
+package eu.jsparrow.standalone;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.eclipse.jdt.core.IJavaProject;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.osgi.framework.BundleContext;
+
+import eu.jsparrow.core.refactorer.RefactoringPipeline;
+import eu.jsparrow.standalone.exceptions.StandaloneException;
+
+/**
+ * test class for {@link RefactoringInvoker}
+ * 
+ * @author Matthias Webhofer, Hans-Jörg Schrödl
+ * @since 2.5.0
+ */
+public class RefactoringInvokerTest {
+
+	private RefactoringInvoker refactoringInvoker;
+	private StandaloneConfig standaloneConfig;
+
+	private MavenProjectImporter mavenImporter;
+
+	@Rule
+	public ExpectedException expectedException = ExpectedException.none();
+
+	@Before
+	public void setUp() throws Exception {
+		refactoringInvoker = new TestableRefactoringInvoker();
+		mavenImporter = mock(MavenProjectImporter.class);
+
+		IJavaProject javaProject = mock(IJavaProject.class);
+		when(javaProject.getElementName()).thenReturn("projectName"); //$NON-NLS-1$
+
+		standaloneConfig = mock(StandaloneConfig.class);
+		when(standaloneConfig.getJavaProject()).thenReturn(javaProject);
+
+		when(mavenImporter.importProjects(any(File.class), any(String.class)))
+			.thenReturn(Collections.singletonList(javaProject));
+		refactoringInvoker.setImporter(mavenImporter);
+	}
+
+	@Test
+	public void startRefactoring() throws Exception {
+		BundleContext context = mock(BundleContext.class);
+		RefactoringPipeline refactoringPipeline = mock(RefactoringPipeline.class);
+
+		IJavaProject javaProject = mock(IJavaProject.class);
+		when(javaProject.getElementName()).thenReturn(""); //$NON-NLS-1$
+		when(refactoringPipeline.getRulesWithChangesAsString()).thenReturn(""); //$NON-NLS-1$
+
+		refactoringInvoker.startRefactoring(context);
+
+		verify(standaloneConfig).createRefactoringStates();
+		verify(standaloneConfig).computeRefactoring();
+		verify(standaloneConfig).commitRefactoring();
+	}
+
+	@Test
+	public void startRefactoring_exceptionsInCreateRefactoringState_shouldNotCommit() throws Exception {
+		BundleContext context = mock(BundleContext.class);
+
+		doThrow(StandaloneException.class).when(standaloneConfig)
+			.createRefactoringStates();
+
+		expectedException.expect(StandaloneException.class);
+		refactoringInvoker.startRefactoring(context);
+
+		verify(standaloneConfig, never()).computeRefactoring();
+		verify(standaloneConfig, never()).commitRefactoring();
+	}
+
+	@Test
+	public void startRefactoring_exceptionsInDoRefactoring_shouldNotCommit() throws Exception {
+		BundleContext context = mock(BundleContext.class);
+		doThrow(StandaloneException.class).when(standaloneConfig)
+			.computeRefactoring();
+
+		expectedException.expect(StandaloneException.class);
+		refactoringInvoker.startRefactoring(context);
+
+		verify(standaloneConfig, never()).commitRefactoring();
+	}
+
+	class TestableRefactoringInvoker extends RefactoringInvoker {
+
+		@Override
+		protected void loadStandaloneConfig(List<IJavaProject> importedProjects, BundleContext context) {
+			super.standaloneConfigs = Arrays.asList(standaloneConfig);
+		}
+		
+	}
+}
