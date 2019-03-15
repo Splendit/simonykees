cf3d283eba916620ecb8c015d47aeb7ce3be1128
Matthias Webhofer <matthias.webhofer@splendit.at>
2019-03-15 12:27:07 +0100

    SIM-1312 UPDATE formatting and NLS Tags

--- bundles/core/src/main/java/eu/jsparrow/core/rule/impl/RemoveModifiersInInterfacePropertiesRule.java
+++ bundles/core/src/main/java/eu/jsparrow/core/rule/impl/RemoveModifiersInInterfacePropertiesRule.java
@@ -10,16 +10,16 @@ import eu.jsparrow.rules.common.RefactoringRuleImpl;
 import eu.jsparrow.rules.common.RuleDescription;
 import eu.jsparrow.rules.common.Tag;
 
-public class RemoveModifiersInInterfacePropertiesRule extends RefactoringRuleImpl<RemoveModifiersInInterfacePropertiesASTVisitor> {
-	
+public class RemoveModifiersInInterfacePropertiesRule
+		extends RefactoringRuleImpl<RemoveModifiersInInterfacePropertiesASTVisitor> {
 
 	public RemoveModifiersInInterfacePropertiesRule() {
 		this.visitorClass = RemoveModifiersInInterfacePropertiesASTVisitor.class;
 		this.id = "RemoveModifiersFromInterfaceProperties"; //$NON-NLS-1$
-		this.ruleDescription = new RuleDescription("Remove Modifiers from Interface Properties",
-				"", Duration.ofMinutes(1),
-				Arrays.asList(Tag.JAVA_1_1, Tag.READABILITY));
+		this.ruleDescription = new RuleDescription("Remove Modifiers from Interface Properties", "",
+				Duration.ofMinutes(1), Arrays.asList(Tag.JAVA_1_1, Tag.READABILITY));
 	}
+
 	@Override
 	protected String provideRequiredJavaVersion() {
 		return JavaCore.VERSION_1_1;
--- bundles/core/src/main/java/eu/jsparrow/core/visitor/impl/RemoveModifiersInInterfacePropertiesASTVisitor.java
+++ bundles/core/src/main/java/eu/jsparrow/core/visitor/impl/RemoveModifiersInInterfacePropertiesASTVisitor.java
@@ -11,39 +11,37 @@ import eu.jsparrow.rules.common.util.ASTNodeUtil;
 import eu.jsparrow.rules.common.visitor.AbstractASTRewriteASTVisitor;
 
 public class RemoveModifiersInInterfacePropertiesASTVisitor extends AbstractASTRewriteASTVisitor {
-	
+
 	@Override
 	public boolean visit(TypeDeclaration type) {
-		
-		if(!type.isInterface()) {
+
+		if (!type.isInterface()) {
 			return true;
 		}
-		
+
 		FieldDeclaration[] fields = type.getFields();
-		
-		for(FieldDeclaration field : fields) {
+
+		for (FieldDeclaration field : fields) {
 			List<Modifier> modifiers = ASTNodeUtil.convertToTypedList(field.modifiers(), Modifier.class);
-			for(Modifier modifier : modifiers) {
-				if(modifier.isPublic() || modifier.isStatic() || modifier.isFinal()) {
+			for (Modifier modifier : modifiers) {
+				if (modifier.isPublic() || modifier.isStatic() || modifier.isFinal()) {
 					astRewrite.remove(modifier, null);
 					onRewrite();
 				}
 			}
 		}
-		
-		
+
 		MethodDeclaration[] methods = type.getMethods();
-		for(MethodDeclaration method : methods) {
+		for (MethodDeclaration method : methods) {
 			List<Modifier> modifiers = ASTNodeUtil.convertToTypedList(method.modifiers(), Modifier.class);
-			for(Modifier modifier : modifiers) {
-				if(modifier.isPublic()) {
+			for (Modifier modifier : modifiers) {
+				if (modifier.isPublic()) {
 					astRewrite.remove(modifier, null);
 					onRewrite();
 				}
 			}
 		}
-		
-		
+
 		return true;
 	}
 
