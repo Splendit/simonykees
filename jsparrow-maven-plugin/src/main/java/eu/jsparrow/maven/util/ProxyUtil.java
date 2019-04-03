e34074038b8c10e58f728581c576e693dc37fa08
Matthias Webhofer <matthias.webhofer@splendit.at>
2019-04-03 11:50:51 +0200

    SIM-1503 UPDATE minor fixes

--- jsparrow-maven-plugin/src/main/java/eu/jsparrow/maven/util/ProxyUtil.java
+++ jsparrow-maven-plugin/src/main/java/eu/jsparrow/maven/util/ProxyUtil.java
@@ -7,7 +7,7 @@ import org.apache.maven.execution.MavenSession;
 import org.apache.maven.settings.Proxy;
 
 public class ProxyUtil {
-	
+
 	private static final String PROXY_DELIMITER = "§"; //$NON-NLS-1$
 	private static final String SETTINGS_DELIMITER = "^"; //$NON-NLS-1$
 
@@ -21,39 +21,41 @@ public class ProxyUtil {
 			.stream()
 			.filter(Proxy::isActive)
 			.filter(p -> "https".equalsIgnoreCase(p.getProtocol()) || "http".equalsIgnoreCase(p.getProtocol())) //$NON-NLS-1$ //$NON-NLS-2$
-			.filter(p -> p.getHost() != null && p.getHost() != "" && p.getPort() != 0); //$NON-NLS-1$
+			.filter(p -> p.getHost() != null && !p.getHost()
+				.isEmpty())
+			.filter(p -> p.getPort() > 0 && p.getPort() < 65535);
 	}
 
 	public static String getSettingsStringFrom(Stream<Proxy> proxies) {
-		 return proxies
-				.map(p -> {
-					StringBuilder proxySB = new StringBuilder();
-					appendParameter(proxySB, "type=",p.getProtocol()); //$NON-NLS-1$
-					appendParameter(proxySB, "host=",p.getHost()); //$NON-NLS-1$
-					appendParameter(proxySB, "port=",String.valueOf(p.getPort())); //$NON-NLS-1$
-					
-					String username = p.getNonProxyHosts();
-					if (username != null && !username.isEmpty()) {
-						appendParameter(proxySB, "username=", username); //$NON-NLS-1$
-					}
-					
-					String password = p.getNonProxyHosts();
-					if (password != null && !password.isEmpty()) {
-						appendParameter(proxySB, "password=", password); //$NON-NLS-1$
-					}
-					
-					String nonProxyHosts = p.getNonProxyHosts();
-					if (nonProxyHosts != null && !nonProxyHosts.isEmpty()) {
-						appendParameter(proxySB, "nonProxyHosts=", nonProxyHosts); //$NON-NLS-1$
-					}
-					return proxySB.toString();
-				}).collect(Collectors.joining(PROXY_DELIMITER));
+		return proxies.map(p -> {
+			StringBuilder proxySB = new StringBuilder();
+			appendParameter(proxySB, "type=", p.getProtocol()); //$NON-NLS-1$
+			appendParameter(proxySB, "host=", p.getHost()); //$NON-NLS-1$
+			appendParameter(proxySB, "port=", String.valueOf(p.getPort())); //$NON-NLS-1$
+
+			String username = p.getUsername();
+			if (username != null && !username.isEmpty()) {
+				appendParameter(proxySB, "username=", username); //$NON-NLS-1$
+			}
+
+			String password = p.getPassword();
+			if (password != null && !password.isEmpty()) {
+				appendParameter(proxySB, "password=", password); //$NON-NLS-1$
+			}
+
+			String nonProxyHosts = p.getNonProxyHosts();
+			if (nonProxyHosts != null && !nonProxyHosts.isEmpty()) {
+				appendParameter(proxySB, "nonProxyHosts=", nonProxyHosts); //$NON-NLS-1$
+			}
+			return proxySB.toString();
+		})
+			.collect(Collectors.joining(PROXY_DELIMITER));
 	}
-	
+
 	private static void appendParameter(StringBuilder sb, String key, String value) {
 		sb.append(key)
-		.append(value)
-		.append(SETTINGS_DELIMITER);
+			.append(value)
+			.append(SETTINGS_DELIMITER);
 	}
 
 }
