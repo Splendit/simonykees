d9deea7fdc6b5dae95c727f4bc53dee51463c9fd
Matthias Webhofer <matthias.webhofer@splendit.at>
2019-04-04 12:44:12 +0200

    SIM-1474 ADD key store service exception

---
+++ bundles/crypto/src/main/java/eu/jsparrow/crypto/exception/KeyStoreServiceException.java
@@ -0,0 +1,25 @@
+package eu.jsparrow.crypto.exception;
+
+import eu.jsparrow.crypto.service.KeyStoreService;
+
+/**
+ * This {@link Exception} is used for potential {@link KeyStoreService} problems
+ * 
+ * @since 3.4.0
+ */
+public class KeyStoreServiceException extends Exception {
+
+	private static final long serialVersionUID = 2764814363815811777L;
+
+	public KeyStoreServiceException() {
+		super();
+	}
+
+	public KeyStoreServiceException(String message) {
+		super(message);
+	}
+
+	public KeyStoreServiceException(String message, Throwable t) {
+		super(message, t);
+	}
+}
