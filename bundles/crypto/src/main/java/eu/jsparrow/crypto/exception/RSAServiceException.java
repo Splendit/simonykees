69424f97ccd38dfc6ad644469e28c0b08e922c48
Matthias Webhofer <matthias.webhofer@splendit.at>
2019-04-23 15:46:17 +0200

    SIM-1477 UPDATE add missing javadoc

--- bundles/crypto/src/main/java/eu/jsparrow/crypto/exception/RSAServiceException.java
+++ bundles/crypto/src/main/java/eu/jsparrow/crypto/exception/RSAServiceException.java
@@ -1,5 +1,12 @@
 package eu.jsparrow.crypto.exception;
 
+import eu.jsparrow.crypto.service.RSAService;
+
+/**
+ * This {@link Exception} is used for potential {@link RSAService} problems.
+ * 
+ * @since 3.5.0
+ */
 public class RSAServiceException extends Exception {
 
 	private static final long serialVersionUID = 6148629667426307741L;
