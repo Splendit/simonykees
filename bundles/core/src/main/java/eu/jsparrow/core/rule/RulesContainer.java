322ae3e5596fc8657a1c3a12c0a113de30482f8b
Ludwig Werzowa <ludwig.werzowa@splendit.at>
2019-05-16 11:13:30 +0200

    SIM-1547 FIX re-added import for MapGetOrDefaultRule

--- bundles/core/src/main/java/eu/jsparrow/core/rule/RulesContainer.java
+++ bundles/core/src/main/java/eu/jsparrow/core/rule/RulesContainer.java
@@ -40,6 +40,7 @@ import eu.jsparrow.core.rule.impl.LambdaForEachCollectRule;
 import eu.jsparrow.core.rule.impl.LambdaForEachIfWrapperToFilterRule;
 import eu.jsparrow.core.rule.impl.LambdaForEachMapRule;
 import eu.jsparrow.core.rule.impl.LambdaToMethodReferenceRule;
+import eu.jsparrow.core.rule.impl.MapGetOrDefaultRule;
 import eu.jsparrow.core.rule.impl.MultiCatchRule;
 import eu.jsparrow.core.rule.impl.MultiVariableDeclarationLineRule;
 import eu.jsparrow.core.rule.impl.OptionalIfPresentRule;
