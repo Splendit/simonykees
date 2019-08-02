a016b83925b05b65241692e8c13bf796bb171871
Ardit Ymeri <ardit.ymeri@splendit.at>
2019-08-02 13:45:05 +0200

    SIM-1284 ADD visitor tests for Collections singletonList

---
+++ tests/test/src/test/java/eu/jsparrow/core/visitor/impl/UseCollectionsSingletonListASTVisitorTest.java
@@ -0,0 +1,81 @@
+package eu.jsparrow.core.visitor.impl;
+
+import static eu.jsparrow.jdtunit.Matchers.assertMatch;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+@SuppressWarnings("nls")
+public class UseCollectionsSingletonListASTVisitorTest extends UsesJDTUnitFixture {
+
+	private UseCollectionsSingletonListASTVisitor visitor;
+	
+	@BeforeEach
+	public void setUp() throws Exception {
+		visitor = new UseCollectionsSingletonListASTVisitor();
+		fixture.addImport(java.util.Arrays.class.getName());
+	}
+	
+	@Test
+	public void visit_asListZeroArguments_shouldTransform() throws Exception {
+		String original = "List<String> strings = Arrays.asList();";
+		String expected = "List<String> strings = Collections.emptyList();";
+		fixture.addMethodBlock(original);
+		visitor.setASTRewrite(fixture.getAstRewrite());
+		
+		fixture.accept(visitor);
+		
+		assertMatch(createBlock(expected), fixture.getMethodBlock());
+	}
+	
+	@Test
+	public void visit_asListOneArgument_shouldTransform() throws Exception {
+		String original = "List<String> strings = Arrays.asList(\"value\");";
+		String expected = "List<String> strings = Collections.singletonList(\"value\");";
+		fixture.addMethodBlock(original);
+		visitor.setASTRewrite(fixture.getAstRewrite());
+		
+		fixture.accept(visitor);
+		
+		assertMatch(createBlock(expected), fixture.getMethodBlock());
+	}
+	
+	@Test
+	public void visit_asListStaticImport_shouldTransform() throws Exception {
+		String original = "List<String> strings = asList(\"value\");";
+		String expected = "List<String> strings = singletonList(\"value\");";
+		fixture.addImport(java.util.Arrays.class.getName() + ".asList", true, false);
+		fixture.addMethodBlock(original);
+		visitor.setASTRewrite(fixture.getAstRewrite());
+		
+		fixture.accept(visitor);
+		
+		assertMatch(createBlock(expected), fixture.getMethodBlock());
+	}
+	
+	
+	@Test
+	public void visit_asListEmptyListStaticImport_shouldTransform() throws Exception {
+		String original = "List<String> strings = asList();";
+		String expected = "List<String> strings = emptyList();";
+		fixture.addImport(java.util.Arrays.class.getName() + ".asList", true, false);
+		fixture.addMethodBlock(original);
+		visitor.setASTRewrite(fixture.getAstRewrite());
+		
+		fixture.accept(visitor);
+		
+		assertMatch(createBlock(expected), fixture.getMethodBlock());
+	}
+	
+	@Test
+	public void visit_asListMoreThanOneArguments_shouldNotTransform() throws Exception {
+		String original = "List<String> strings = Arrays.asList(\"value1\", \"value2\");";
+		fixture.addMethodBlock(original);
+		visitor.setASTRewrite(fixture.getAstRewrite());
+		
+		fixture.accept(visitor);
+		
+		assertMatch(createBlock(original), fixture.getMethodBlock());
+	}
+	
+}
