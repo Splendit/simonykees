9b9b43b71bed1ecf07034bf1a1f907e6967a72fe
Ludwig Werzowa <ludwig.werzowa@splendit.at>
2019-08-06 11:37:34 +0200

    SIM-1607 UPDATE added additional fixture tests

--- tests/test/src/test/java/eu/jsparrow/core/visitor/impl/RemoveNullCheckBeforeInstanceofASTVisitorTest.java
+++ tests/test/src/test/java/eu/jsparrow/core/visitor/impl/RemoveNullCheckBeforeInstanceofASTVisitorTest.java
@@ -30,6 +30,19 @@ public class RemoveNullCheckBeforeInstanceofASTVisitorTest extends UsesJDTUnitFi
 	}
 
 	@Test
+	public void visit_nullLiteralInLHS_shouldReplace() throws Exception {
+		String block = "String value = \"\"; if (null != value && value instanceof String) { }";
+		String expectedContent = "String value = \"\"; if (value instanceof String) { }";
+		fixture.addMethodBlock(block);
+		visitor.setASTRewrite(fixture.getAstRewrite());
+
+		fixture.accept(visitor);
+
+		Block expected = createBlock(expectedContent);
+		assertMatch(expected, fixture.getMethodBlock());
+	}
+
+	@Test
 	public void visit_initializationExpression_shouldReplace() throws Exception {
 		String block = "String value = \"\"; boolean b = value != null && value instanceof String;";
 		String expectedContent = "String value = \"\"; boolean b = value instanceof String;";
@@ -144,7 +157,68 @@ public class RemoveNullCheckBeforeInstanceofASTVisitorTest extends UsesJDTUnitFi
 
 	@Test
 	public void visit_missingPrefixOperator_shouldNotReplace() throws Exception {
-		String block = "String value = \"\"; if (value != null || (value instanceof String)) { }";
+		String block = "String value = \"\"; if (value == null || (value instanceof String)) { }";
+		fixture.addMethodBlock(block);
+		visitor.setASTRewrite(fixture.getAstRewrite());
+
+		fixture.accept(visitor);
+
+		Block expected = createBlock(block);
+		assertMatch(expected, fixture.getMethodBlock());
+	}
+
+	@Test
+	public void visit_missingPrefixOperatorNestedParenthesizedExpression_shouldNotReplace() throws Exception {
+		String block = "String value = \"\"; if (value == null || (((((value instanceof String)))))) { }";
+		fixture.addMethodBlock(block);
+		visitor.setASTRewrite(fixture.getAstRewrite());
+
+		fixture.accept(visitor);
+
+		Block expected = createBlock(block);
+		assertMatch(expected, fixture.getMethodBlock());
+	}
+
+	@Test
+	public void visit_missingSimpleNameInInstanceof_shouldNotReplace() throws Exception {
+		String block = "String block = \" \"; if(block != null && block.substring(0) instanceof String) {}";
+		fixture.addMethodBlock(block);
+		visitor.setASTRewrite(fixture.getAstRewrite());
+
+		fixture.accept(visitor);
+
+		Block expected = createBlock(block);
+		assertMatch(expected, fixture.getMethodBlock());
+	}
+
+	@Test
+	public void visit_mismatchingInfixCondition_shouldNotReplace() throws Exception {
+		String block = "String value = \"\"; if (value != null || value instanceof String) { }";
+		fixture.addMethodBlock(block);
+		visitor.setASTRewrite(fixture.getAstRewrite());
+
+		fixture.accept(visitor);
+
+		Block expected = createBlock(block);
+		assertMatch(expected, fixture.getMethodBlock());
+	}
+
+	@Test
+	public void visit_mismatchingNullCheckAndInstanceofLHSs_shouldNotReplace() throws Exception {
+		String block = "" + "BufferedReader r;\n" + "String line;\n"
+				+ "while ((line = r.readLine()) != null && line instanceof String) {}";
+		fixture.addMethodBlock(block);
+		visitor.setASTRewrite(fixture.getAstRewrite());
+
+		fixture.accept(visitor);
+
+		Block expected = createBlock(block);
+		assertMatch(expected, fixture.getMethodBlock());
+	}
+
+	@Test
+	public void visit_missingNullLiteral_shouldNotReplace() throws Exception {
+		String block = "String value, value2 = \"\"; if (value != value2 && value instanceof String) { }";
 		fixture.addMethodBlock(block);
 		visitor.setASTRewrite(fixture.getAstRewrite());
 
