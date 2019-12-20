405fec31fb16dedfd4e8e94c9afa4564ad11f34e
Ardit Ymeri <ardit.ymeri@splendit.at>
2019-12-20 17:32:07 +0100

    SIM-1033 ADD visitor test cases

--- tests/test/src/test/java/eu/jsparrow/core/visitor/impl/optional/OptionalMapASTVisitorTest.java
+++ tests/test/src/test/java/eu/jsparrow/core/visitor/impl/optional/OptionalMapASTVisitorTest.java
@@ -2,8 +2,6 @@ package eu.jsparrow.core.visitor.impl.optional;
 
 import static eu.jsparrow.jdtunit.Matchers.assertMatch;
 
-import java.util.Optional;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -24,10 +22,6 @@ public class OptionalMapASTVisitorTest extends UsesSimpleJDTUnitFixture {
 	
 	@Test
 	public void test_baseCase_shouldTransform() throws Exception {
-		Optional<String> optional = Optional.of("value");
-		optional
-			.map(value -> value.replace("t", "o"))
-			.ifPresent(test -> System.out.print(test));
 		String original = "" + 
 				"		Optional<String> optional = Optional.of(\"value\");\n" + 
 				"		optional.ifPresent(value -> {\n" + 
@@ -46,5 +40,134 @@ public class OptionalMapASTVisitorTest extends UsesSimpleJDTUnitFixture {
 
 		assertMatch(ASTNodeBuilder.createBlockFromString(expected), fixture.getMethodBlock());
 	}
+	
+	@Test
+	public void test_multipleRemainingStatements_shouldTransform() throws Exception {
+		String original = "" + 
+				"		Optional<String> optional = Optional.of(\"value\");\n" + 
+				"		optional.ifPresent(value -> {\n" + 
+				"			String test = value.replace(\"t\", \"o\");\n" + 
+				"			System.out.print(test);\n" + 
+				"			System.out.print(test);\n" + 
+				"		});";
+		String expected = "" +
+				"		Optional<String> optional = Optional.of(\"value\");\n" + 
+				"		optional.map(value -> value.replace(\"t\", \"o\")).ifPresent(test -> {\n" + 
+				"			System.out.print(test);\n" + 
+				"			System.out.print(test);\n" + 
+				"		});";
+		fixture.addMethodBlock(original);
+		visitor.setASTRewrite(fixture.getAstRewrite());
+
+		fixture.accept(visitor);
+
+		assertMatch(ASTNodeBuilder.createBlockFromString(expected), fixture.getMethodBlock());
+	}
+	
+	@Test
+	public void test_multipleExtractedStatements_shouldTransform() throws Exception {
+		String original = "" + 
+				"		Optional<String> optional = Optional.of(\"value\");\n" + 
+				"		optional.ifPresent(value -> {\n" + 
+				"			System.out.print(value);\n" + 
+				"			String test = value.replace(\"t\", \"o\");\n" + 
+				"			System.out.print(test);\n" + 
+				"		});";
+		String expected = "" +
+				"		Optional<String> optional = Optional.of(\"value\");\n" + 
+				"		optional.map(value -> {\n" + 
+				"			System.out.print(value);\n" + 
+				"			return value.replace(\"t\", \"o\");\n" + 
+				"		}).ifPresent(test -> System.out.print(test));";
+		fixture.addMethodBlock(original);
+		visitor.setASTRewrite(fixture.getAstRewrite());
+
+		fixture.accept(visitor);
+
+		assertMatch(ASTNodeBuilder.createBlockFromString(expected), fixture.getMethodBlock());
+	}
+	
+	@Test
+	public void test_primitiveTypes_shouldTransform() throws Exception {
+		String original = "" + 
+				"		Optional<String> optional = Optional.of(\"value\");\n" + 
+				"		optional.ifPresent(value -> {\n" + 
+				"			int length = value.length();\n" + 
+				"			if(length > 0) {\n" + 
+				"				System.out.println(\"Length is \" + length);\n" + 
+				"			}\n" + 
+				"		});";
+		String expected = "" +
+				"		Optional<String> optional = Optional.of(\"value\");\n" + 
+				"		optional.map(value -> value.length()).ifPresent(length -> {\n" + 
+				"			if(length > 0) {\n" + 
+				"				System.out.println(\"Length is \" + length);\n" + 
+				"			}\n" + 
+				"		});";
+		fixture.addMethodBlock(original);
+		visitor.setASTRewrite(fixture.getAstRewrite());
+
+		fixture.accept(visitor);
+
+		assertMatch(ASTNodeBuilder.createBlockFromString(expected), fixture.getMethodBlock());
+	}
+	
+	@Test
+	public void test_usingParameterType_shouldTransform() throws Exception {
+		String original = "" + 
+				"	public void usingParameterType() {\n" + 
+				"		Optional<String> optional = Optional.of(\"value\");\n" + 
+				"		optional.ifPresent((String value) -> {\n" + 
+				"			String test = value.replace(\"t\", \"o\"); \n" + 
+				"			System.out.print(test);\n" + 
+				"		});\n" + 
+				"	}";
+		String expected = "" +
+				"		Optional<String> optional = Optional.of(\"value\");\n" + 
+				"		optional.map((String value) -> value.replace(\"t\", \"o\")).ifPresent((String test) -> System.out.print(test));";
+		fixture.addMethodBlock(original);
+		visitor.setASTRewrite(fixture.getAstRewrite());
+
+		fixture.accept(visitor);
+
+		assertMatch(ASTNodeBuilder.createBlockFromString(expected), fixture.getMethodBlock());
+	}
+	
+	// Negative test cases 
+	
+	@Test
+	public void test_nonExtractableBody_shouldNotTransform() throws Exception {
+		String original = "" + 
+				"		Optional<String> optional = Optional.of(\"value\");\n" + 
+				"		optional.ifPresent(value -> {\n" + 
+				"			String test = value.replace(\"t\", \"o\");\n" + 
+				"			System.out.print(test);\n" + 
+				"			System.out.print(value);\n" + 
+				"		});";
+		fixture.addMethodBlock(original);
+		visitor.setASTRewrite(fixture.getAstRewrite());
+
+		fixture.accept(visitor);
+
+		assertMatch(ASTNodeBuilder.createBlockFromString(original), fixture.getMethodBlock());
+	}
+	
+	@Test
+	public void test_singleStatementLambdaBlock_shouldNotTransform() throws Exception {
+		String original = "" + 
+				"		Optional<String> optional = Optional.of(\"value\");\n" + 
+				"		optional.ifPresent(value -> {\n" + 
+				"			if(value.length() > 1) {				\n" + 
+				"				System.out.print(value);\n" + 
+				"			}\n" + 
+				"		});";
+		fixture.addMethodBlock(original);
+		visitor.setASTRewrite(fixture.getAstRewrite());
+
+		fixture.accept(visitor);
+
+		assertMatch(ASTNodeBuilder.createBlockFromString(original), fixture.getMethodBlock());
+	}
+	
 
 }
