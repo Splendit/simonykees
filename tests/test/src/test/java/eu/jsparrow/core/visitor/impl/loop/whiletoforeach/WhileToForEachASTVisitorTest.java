377ee883c6b3d8a959b66b8d968f5eed19d44389
Ardit Ymeri <ardit.ymeri@splendit.at>
2020-03-03 16:08:56 +0100

    SIM-1704 ADD tests while loops updating collections

---
+++ tests/test/src/test/java/eu/jsparrow/core/visitor/impl/loop/whiletoforeach/WhileToForEachASTVisitorTest.java
@@ -0,0 +1,48 @@
+package eu.jsparrow.core.visitor.impl.loop.whiletoforeach;
+
+import static eu.jsparrow.jdtunit.Matchers.assertMatch;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import eu.jsparrow.core.visitor.impl.UsesSimpleJDTUnitFixture;
+import eu.jsparrow.core.visitor.loop.whiletoforeach.WhileToForEachASTVisitor;
+import eu.jsparrow.jdtunit.util.ASTNodeBuilder;
+
+@SuppressWarnings("nls")
+public class WhileToForEachASTVisitorTest extends UsesSimpleJDTUnitFixture {
+	
+	private WhileToForEachASTVisitor visitor;
+
+	@BeforeEach
+	public void beforeEach() {
+		visitor = new WhileToForEachASTVisitor();
+	}
+	
+	
+	@Test
+	public void visit_whileLoopUpdatingIterator_shouldNotTransform() throws Exception {
+		String original = "" +
+				"		List<String> list = new ArrayList<>();\n" + 
+				"		int i = 0;\n" + 
+				"		while(i < list.size()) {\n" + 
+				"			String value = list.get(i);\n" + 
+				"			if(value.contains(\"0\")) {\n" + 
+				"				list.add(\"-\");\n" + 
+				"			}\n" + 
+				"			System.out.println(value);\n" + 
+				"			i++;\n" + 
+				"		}";
+		fixture.addImport(java.util.List.class.getName());
+		fixture.addImport(java.util.ArrayList.class.getName());
+		fixture.addMethodBlock(original);
+		
+		visitor.setASTRewrite(fixture.getAstRewrite());
+		fixture.accept(visitor);
+		assertMatch(ASTNodeBuilder.createBlockFromString(original), fixture.getMethodBlock());
+		
+	}
+}
