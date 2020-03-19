7ae37eae8318ccf1ff1eb7ff21df6958813da661
gregor.balint <gregor.balint@splendit.at>
2020-03-19 16:27:46 +0100

    SIM-1706 UPDATE use  assertChange and assertNoChange in the visitor tests

--- tests/test/src/test/java/eu/jsparrow/core/visitor/impl/loop/whiletoforeach/WhileToForEachASTVisitorTest.java
+++ tests/test/src/test/java/eu/jsparrow/core/visitor/impl/loop/whiletoforeach/WhileToForEachASTVisitorTest.java
@@ -1,19 +1,14 @@
 package eu.jsparrow.core.visitor.impl.loop.whiletoforeach;
 
-import static eu.jsparrow.jdtunit.Matchers.assertMatch;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import eu.jsparrow.core.visitor.impl.UsesSimpleJDTUnitFixture;
 import eu.jsparrow.core.visitor.loop.whiletoforeach.WhileToForEachASTVisitor;
-import eu.jsparrow.jdtunit.util.ASTNodeBuilder;
 
 @SuppressWarnings("nls")
 public class WhileToForEachASTVisitorTest extends UsesSimpleJDTUnitFixture {
 	
-	private WhileToForEachASTVisitor visitor;
-
 	@BeforeEach
 	public void beforeEach() {
 		visitor = new WhileToForEachASTVisitor();
@@ -21,7 +16,10 @@ public class WhileToForEachASTVisitorTest extends UsesSimpleJDTUnitFixture {
 	
 	@Test
 	public void visit_whileLoopUpdatingIterator_shouldNotTransform() throws Exception {
-		String original = "" +
+		fixture.addImport(java.util.List.class.getName());
+		fixture.addImport(java.util.ArrayList.class.getName());
+		
+		assertNoChange("" +
 				"		List<String> list = new ArrayList<>();\n" + 
 				"		int i = 0;\n" + 
 				"		while(i < list.size()) {\n" + 
@@ -31,19 +29,15 @@ public class WhileToForEachASTVisitorTest extends UsesSimpleJDTUnitFixture {
 				"			}\n" + 
 				"			System.out.println(value);\n" + 
 				"			i++;\n" + 
-				"		}";
-		fixture.addImport(java.util.List.class.getName());
-		fixture.addImport(java.util.ArrayList.class.getName());
-		fixture.addMethodBlock(original);
-		
-		visitor.setASTRewrite(fixture.getAstRewrite());
-		fixture.accept(visitor);
-		assertMatch(ASTNodeBuilder.createBlockFromString(original), fixture.getMethodBlock());
+				"		}");
 	}
 	
 	@Test
 	public void visit_reassigningIterator_shouldNotTransform() throws Exception {
-		String original = "" +
+		fixture.addImport(java.util.List.class.getName());
+		fixture.addImport(java.util.ArrayList.class.getName());
+		
+		assertNoChange("" +
 				"		List<String> list = new ArrayList<>();\n" + 
 				"		int i = 0;\n" + 
 				"		while(i < list.size()) {\n" + 
@@ -53,13 +47,6 @@ public class WhileToForEachASTVisitorTest extends UsesSimpleJDTUnitFixture {
 				"			}\n" + 
 				"			System.out.println(value);\n" + 
 				"			i++;\n" + 
-				"		}";
-		fixture.addImport(java.util.List.class.getName());
-		fixture.addImport(java.util.ArrayList.class.getName());
-		fixture.addMethodBlock(original);
-		
-		visitor.setASTRewrite(fixture.getAstRewrite());
-		fixture.accept(visitor);
-		assertMatch(ASTNodeBuilder.createBlockFromString(original), fixture.getMethodBlock());
+				"		}");
 	}
 }
