fb1117673128141f7c198a804f68def54aaa0c6d
Ardit Ymeri <ardit.ymeri@splendit.at>
2020-06-12 11:01:48 +0200

    SIM-1747 REMOVE old implementation of non specialized streams

--- bundles/core/src/main/java/eu/jsparrow/core/visitor/impl/UseArraysStreamASTVisitor.java
+++ bundles/core/src/main/java/eu/jsparrow/core/visitor/impl/UseArraysStreamASTVisitor.java
@@ -167,91 +167,6 @@ public class UseArraysStreamASTVisitor extends AbstractASTRewriteASTVisitor {
 		return !visitor.isIncompatible();
 	}
 
-	private boolean oldImplementation(MethodInvocation methodInvocation) {
-		boolean hasRightType = isMethodDeclaredOnType(methodInvocation, "asList", java.util.Arrays.class.getName());
-		if (!hasRightType) {
-			return true;
-		}
-		StructuralPropertyDescriptor propertyDescriptor = methodInvocation.getLocationInParent();
-		if (propertyDescriptor != MethodInvocation.EXPRESSION_PROPERTY) {
-			return true;
-		}
-		MethodInvocation parent = (MethodInvocation) methodInvocation.getParent();
-		if (!isMethodDeclaredOnType(parent, "stream", java.util.Collection.class.getName())) {
-			return false;
-		}
-
-		List<Expression> arguments = ASTNodeUtil.convertToTypedList(methodInvocation.arguments(), Expression.class);
-		if (arguments.isEmpty()) {
-			return true;
-		}
-		Expression argument = arguments.get(0);
-		ITypeBinding argumentTypeBinding = argument.resolveTypeBinding();
-		if (arguments.size() > 1 || !argumentTypeBinding.isArray()) {
-			if (hasSpecializedStream(argumentTypeBinding)) {
-				List<MethodInvocation> methodChain = extractStreamChain(parent);
-
-				// 1. the last element in the method chain should be forEach
-				// 2. every method in the chain should be a higher order
-				// function
-				// 3. the tricky part: the parameter should never be used as a
-				// boxed primitive. otherwise, there will be a compilation
-				// error.
-				return false;
-			}
-
-			/*
-			 * FIXME: doesn't seem like a good idea to convert:
-			 * Arrays.asList("1", "2", "3").stream() to: Arrays.stream(new
-			 * String[]{"1", "2", "3"})
-			 * 
-			 * The new code just looks longer. Additionally, it is not trivial
-			 * how to derive/import/treat the type of the components type of the
-			 * array creation. Afterall, there is no performance benefit unless
-			 * we avoid implicit boxing, which is handled in the previous if
-			 * block.
-			 */
-
-			AST ast = methodInvocation.getAST();
-			ArrayCreation arrayCreation = ast.newArrayCreation();
-			// FIXME: make sure the type of the argument is a simle type,
-			// without wildcards, boundaries, etc...
-			ArrayType arrayType = ast.newArrayType(ast.newSimpleType(ast.newName(argumentTypeBinding.getName())));
-			arrayCreation.setType(arrayType);
-			ArrayInitializer initializer = ast.newArrayInitializer();
-			@SuppressWarnings("unchecked")
-			List<Expression> initializerExpressions = initializer.expressions();
-			arguments.stream()
-				.map(arg -> (Expression) astRewrite.createCopyTarget(arg))
-				.forEach(initializerExpressions::add);
-			arrayCreation.setInitializer(initializer);
-
-			Expression experssion = methodInvocation.getExpression();
-			ListRewrite listRewrite = astRewrite.getListRewrite(parent, MethodInvocation.ARGUMENTS_PROPERTY);
-			listRewrite.insertFirst(arrayCreation, null);
-			astRewrite.replace(parent.getExpression(), astRewrite.createCopyTarget(experssion), null);
-			onRewrite();
-			/*
-			 * TODO: create a new array for the parameter of Arrays.stream()
-			 */
-		} else {
-			ITypeBinding componentType = argumentTypeBinding.getComponentType();
-			if (hasSpecializedStream(componentType)) {
-				return false;
-			}
-
-			// FIXME: consider the case when the expression is missing. (e.g. is
-			// in static imports)
-			// FIXME: make sure that java.util.Stream is imported.
-			Expression experssion = methodInvocation.getExpression();
-			ListRewrite listRewrite = astRewrite.getListRewrite(parent, MethodInvocation.ARGUMENTS_PROPERTY);
-			listRewrite.insertLast(astRewrite.createCopyTarget(argument), null);
-			astRewrite.replace(parent.getExpression(), astRewrite.createCopyTarget(experssion), null);
-			onRewrite();
-		}
-		return true;
-	}
-
 	private List<MethodInvocation> extractStreamChain(MethodInvocation methodInvocation) {
 		List<MethodInvocation> chain = new ArrayList<>();
 
@@ -292,32 +207,6 @@ public class UseArraysStreamASTVisitor extends AbstractASTRewriteASTVisitor {
 		IMethodBinding methodBinding = methodInvocation.resolveMethodBinding();
 		ITypeBinding declaringClass = methodBinding.getDeclaringClass();
 		return ClassRelationUtil.isContentOfType(declaringClass, expectedType);
-	}
-	
-	private boolean checkMethodChain(List<MethodInvocation> methodChain) {
-		if (!methodChain.isEmpty()) {
-
-			MethodInvocation last = methodChain.get(methodChain.size() - 1);
-			IMethodBinding lastMethodBinding = last.resolveMethodBinding();
-			ITypeBinding lastReturnType = lastMethodBinding.getReturnType();
-			boolean isVoid = "void".equals(lastReturnType.getName());
-
-			for (MethodInvocation method : methodChain) {
-				ITypeBinding typeBinding = method.resolveTypeBinding();
-				ITypeBinding[] typeArguments = typeBinding.getTypeArguments();
-				if (typeArguments.length == 1) {
-					ITypeBinding typeArgument = typeArguments[0];
-					if (hasSpecializedStream(typeArgument)) {
-						boolean compatible = isCompatibleWithUnboxedParameter(method);
-						if (!compatible) {
-							return true;
-						}
-					}
-				}
-			}
-
-		}
-		return false;
 	}
 
 	class UnboxCompatibilityVisitor extends ASTVisitor {
