e0cca1346b147d6133d5c352f7b72824a16d0f29
Ardit Ymeri <ardit.ymeri@splendit.at>
2020-07-03 17:18:09 +0200

    SIM-1763 UPDATE replacing LDAP query concats with parameters

--- bundles/core/src/main/java/eu/jsparrow/core/visitor/security/UseParameterizedLDAPQueryASTVisitor.java
+++ bundles/core/src/main/java/eu/jsparrow/core/visitor/security/UseParameterizedLDAPQueryASTVisitor.java
@@ -3,10 +3,16 @@ package eu.jsparrow.core.visitor.security;
 import java.util.Collections;
 import java.util.List;
 
+import org.eclipse.jdt.core.dom.AST;
 import org.eclipse.jdt.core.dom.ASTNode;
+import org.eclipse.jdt.core.dom.ArrayCreation;
+import org.eclipse.jdt.core.dom.ArrayInitializer;
+import org.eclipse.jdt.core.dom.ArrayType;
 import org.eclipse.jdt.core.dom.Expression;
 import org.eclipse.jdt.core.dom.InfixExpression;
 import org.eclipse.jdt.core.dom.MethodInvocation;
+import org.eclipse.jdt.core.dom.SimpleType;
+import org.eclipse.jdt.core.dom.rewrite.ListRewrite;
 
 public class UseParameterizedLDAPQueryASTVisitor extends AbstractDynamicQueryASTVisitor {
 
@@ -28,6 +34,15 @@ public class UseParameterizedLDAPQueryASTVisitor extends AbstractDynamicQueryAST
 			return true;
 		}
 
+		// perform replacement
+		replaceQuery(replaceableParameters);
+		ArrayCreation searchParameters = createSearchParameters(replaceableParameters);
+		ListRewrite listRewrite = astRewrite.getListRewrite(methodInvocation, MethodInvocation.ARGUMENTS_PROPERTY);
+		listRewrite.insertAfter(searchParameters, filterExpression, null);
+
+		return true;
+	}
+
 	public List<Expression> findDynamicQueryComponents(Expression filterExpression) {
 
 		if (filterExpression.getNodeType() == ASTNode.INFIX_EXPRESSION) {
@@ -43,7 +58,29 @@ public class UseParameterizedLDAPQueryASTVisitor extends AbstractDynamicQueryAST
 		}
 
 		return sqlVariableVisitor.getDynamicQueryComponents();
-
 	}
 
+	@SuppressWarnings("unchecked")
+	private ArrayCreation createSearchParameters(List<ReplaceableParameter> replaceableParameters) {
+		AST ast = astRewrite.getAST();
+		ArrayCreation arrayCreation = ast.newArrayCreation();
+
+		SimpleType elementType = ast.newSimpleType(ast.newSimpleName(String.class.getSimpleName()));
+		ArrayType stringArrayType = ast.newArrayType(elementType);
+		arrayCreation.setType(stringArrayType);
+		ArrayInitializer initializer = ast.newArrayInitializer();
+		for (ReplaceableParameter parameter : replaceableParameters) {
+			initializer.expressions()
+				.add(astRewrite.createCopyTarget(parameter.getParameter()));
+		}
+		arrayCreation.setInitializer(initializer);
+		return arrayCreation;
+	}
+
+	@Override
+	protected String getNewPreviousLiteralValue(ReplaceableParameter parameter) {
+		String original = parameter.getPrevious()
+			.getLiteralValue();
+		return String.format("%s{%s}", original, parameter.getPosition()); //$NON-NLS-1$
+	}
 }
