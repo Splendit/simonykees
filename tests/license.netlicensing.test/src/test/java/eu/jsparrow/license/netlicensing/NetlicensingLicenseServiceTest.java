e40021fda3a20fc57705e8f047a6e265b2298328
Ardit Ymeri <ardit.ymeri@splendit.at>
2020-07-06 23:25:44 +0200

    SIM-1765 UPDATE formatting and removing warnings

--- tests/license.netlicensing.test/src/test/java/eu/jsparrow/license/netlicensing/NetlicensingLicenseServiceTest.java
+++ tests/license.netlicensing.test/src/test/java/eu/jsparrow/license/netlicensing/NetlicensingLicenseServiceTest.java
@@ -11,43 +11,44 @@ import eu.jsparrow.license.netlicensing.model.DemoLicenseModel;
 import eu.jsparrow.license.netlicensing.model.NetlicensingLicenseModel;
 
 public class NetlicensingLicenseServiceTest {
-	
+
 	private NetlicensingLicenseService netlicensingLicenseValidation;
-	
+
 	@BeforeEach
 	public void setUp() {
 		netlicensingLicenseValidation = new NetlicensingLicenseService();
 	}
-	
+
 	@Test
 	public void test_verifySecretDemoLicense_shouldReturnTrue() {
 		String secret = "HARDWARE-INFORMATION-ID";
 		DemoLicenseModel licenseMode = new DemoLicenseModel();
-		
+
 		boolean valid = netlicensingLicenseValidation.verifySecretKey(licenseMode, secret);
-		
+
 		assertTrue(valid);
 	}
-	
-	
+
 	@Test
 	public void test_verifySecret_shouldReturnTrue() {
 		String secret = "HARDWARE-INFORMATION-ID";
-		NetlicensingLicenseModel licenseMode = new NetlicensingLicenseModel("key", secret, "product-nr", "module-nr", LicenseType.NODE_LOCKED);
-		
+		NetlicensingLicenseModel licenseMode = new NetlicensingLicenseModel("key", secret, "product-nr", "module-nr",
+				LicenseType.NODE_LOCKED);
+
 		boolean valid = netlicensingLicenseValidation.verifySecretKey(licenseMode, secret);
-		
+
 		assertTrue(valid);
 	}
-	
+
 	@Test
 	public void test_verifySecret_shouldReturnFalse() {
 		String expectedSecret = "HARDWARE-INFORMATION-ID";
 		String actualSecret = "ACTUAL-SECRET-ID-DIFFERENT-FROM-ORIGINAL";
-		NetlicensingLicenseModel licenseMode = new NetlicensingLicenseModel("key", expectedSecret, "product-nr", "module-nr", LicenseType.NODE_LOCKED);
-		
+		NetlicensingLicenseModel licenseMode = new NetlicensingLicenseModel("key", expectedSecret, "product-nr",
+				"module-nr", LicenseType.NODE_LOCKED);
+
 		boolean valid = netlicensingLicenseValidation.verifySecretKey(licenseMode, actualSecret);
-		
+
 		assertFalse(valid);
 	}
 
