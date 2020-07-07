703369290f39457c128ab867a17f452cf739376b
gregor.balint <gregor.balint@splendit.at>
2020-07-07 15:59:02 +0200

    SIM-1763 ADD new test cases

--- tests/test/src/test/java/eu/jsparrow/core/visitor/security/UseParameterizedLDAPQueryASTVisitorTest.java
+++ tests/test/src/test/java/eu/jsparrow/core/visitor/security/UseParameterizedLDAPQueryASTVisitorTest.java
@@ -1,11 +1,5 @@
 package eu.jsparrow.core.visitor.security;
 
-import javax.naming.NamingEnumeration;
-import javax.naming.NamingException;
-import javax.naming.directory.DirContext;
-import javax.naming.directory.SearchControls;
-import javax.naming.directory.SearchResult;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -17,9 +11,10 @@ public class UseParameterizedLDAPQueryASTVisitorTest extends UsesSimpleJDTUnitFi
 	public void setUp() throws Exception {
 		setVisitor(new UseParameterizedLDAPQueryASTVisitor());
 		fixture.addImport(javax.naming.directory.DirContext.class.getName());
-		fixture.addImport(javax.naming.NamingEnumeration.class.getName());
 		fixture.addImport(javax.naming.directory.SearchResult.class.getName());
 		fixture.addImport(javax.naming.directory.SearchControls.class.getName());
+		fixture.addImport(javax.naming.NamingEnumeration.class.getName());
+		fixture.addImport(javax.naming.NamingException.class.getName());
 	}
 
 	@Test
@@ -30,45 +25,157 @@ public class UseParameterizedLDAPQueryASTVisitorTest extends UsesSimpleJDTUnitFi
 				"DirContext ctx = null;\n" +
 				"String filter = \"(&(uid=\" + user + \")(userPassword=\" + pass + \"))\";\n" +
 				"try {\n" +
-				"	NamingEnumeration<SearchResult> results = ctx.search(\"ou=system\", filter, new SearchControls());\n" +
-				"} catch (NamingException e) {}";
+				"	NamingEnumeration<SearchResult> results = ctx.search(\"ou=system\", filter, new SearchControls());\n"
+				+
+				"} catch (NamingException e) {\n" +
+				"	e.printStackTrace();\n" +
+				"}";
 		String expected = "" +
 				"String user = null;\n" +
 				"String pass = null;\n" +
 				"DirContext ctx = null;\n" +
 				"String filter = \"(&(uid={0}\" + \")(userPassword={1}\" + \"))\";\n" +
 				"try {\n" +
-				"	NamingEnumeration<SearchResult> results = ctx.search(\"ou=system\", filter, new Object[] {user, pass}, new SearchControls());\n" +
-				"} catch (NamingException e) {}";
+				"NamingEnumeration<SearchResult> results = ctx.search(\"ou=system\", filter, new Object[] {user, pass}, new SearchControls());\n"
+				+
+				"} catch (NamingException e) {\n" +
+				"	e.printStackTrace();\n" +
+				"}";
+		assertChange(original, expected);
+	}
+
+	@Test
+	public void visit_FilterArgumentNotStoredInVariable_shouldTransform() throws Exception {
+		String original = "" +
+				"			String user = null;\n" +
+				"			String pass = null;\n" +
+				"			DirContext ctx = null;\n" +
+				"			try {\n" +
+				"				NamingEnumeration<SearchResult> results = ctx.search(\"ou=system\",\n" +
+				"						\"(&(uid=\" + user + \")(userPassword=\" + pass + \"))\", new SearchControls());\n"
+				+
+				"			} catch (NamingException e) {\n" +
+				"				e.printStackTrace();\n" +
+				"			}";
+		String expected = "" +
+				"			String user = null;\n" +
+				"			String pass = null;\n" +
+				"			DirContext ctx = null;\n" +
+				"			try {\n" +
+				"				NamingEnumeration<SearchResult> results = ctx.search(\"ou=system\",\n" +
+				"						\"(&(uid={0}\" + \")(userPassword={1}\" + \"))\", new Object[] { user, pass }, new SearchControls());\n"
+				+
+				"			} catch (NamingException e) {\n" +
+				"				e.printStackTrace();\n" +
+				"			}";
+		assertChange(original, expected);
+	}
+
+	@Test
+	public void visit_FilterInitializedAfterDeclaration_shouldTransform() throws Exception {
+		String original = "" +
+				"			String user = null;\n" +
+				"			String pass = null;\n" +
+				"			DirContext ctx = null;\n" +
+				"			String filter;\n" +
+				"			filter = \"(&(uid=\";\n" +
+				"			filter += user;\n" +
+				"			filter += \")(userPassword=\";\n" +
+				"			filter += pass;\n" +
+				"			filter += \"))\";\n" +
+				"			try {\n" +
+				"				NamingEnumeration<SearchResult> results = ctx.search(\"ou=system\", filter, new SearchControls());\n"
+				+
+				"			} catch (NamingException e) {\n" +
+				"				e.printStackTrace();\n" +
+				"			}";
+
+		String expected = "" +
+				"			String user = null;\n" +
+				"			String pass = null;\n" +
+				"			DirContext ctx = null;\n" +
+				"			String filter;\n" +
+				"			filter = \"(&(uid={0}\";\n" +
+				"			filter += \")(userPassword={1}\";\n" +
+				"			filter += \"))\";\n" +
+				"			try {\n" +
+				"				NamingEnumeration<SearchResult> results = ctx.search(\"ou=system\", filter, new Object[] { user, pass },\n"
+				+
+				"						new SearchControls());\n" +
+				"			} catch (NamingException e) {\n" +
+				"				e.printStackTrace();\n" +
+				"			}";
 
 		assertChange(original, expected);
 	}
 
-	public void sampleCode() {
-		String user = null;
-		String pass = null;
-		DirContext ctx = null;
+	@Test
+	public void visit_FilterInitializedWithNullAtDeclaration_shouldTransform() throws Exception {
+		String original = "" +
+				"			String user = null;\n" +
+				"			String pass = null;\n" +
+				"			DirContext ctx = null;\n" +
+				"			String filter = null;\n" +
+				"			filter = \"(&(uid=\";\n" +
+				"			filter += user;\n" +
+				"			filter += \")(userPassword=\";\n" +
+				"			filter += pass;\n" +
+				"			filter += \"))\";\n" +
+				"			try {\n" +
+				"				NamingEnumeration<SearchResult> results = ctx.search(\"ou=system\", filter, new SearchControls());\n"
+				+
+				"			} catch (NamingException e) {\n" +
+				"				e.printStackTrace();\n" +
+				"			}";
 
-		String filter = "(&(uid=" + user + ")(userPassword=" + pass + "))";
-		try {
-			NamingEnumeration<SearchResult> results = ctx.search("ou=system", filter, new SearchControls());
-		} catch (NamingException e) {
-			e.printStackTrace();
-		}
+		String expected = "" +
+				"			String user = null;\n" +
+				"			String pass = null;\n" +
+				"			DirContext ctx = null;\n" +
+				"			String filter = null;\n" +
+				"			filter = \"(&(uid={0}\";\n" +
+				"			filter += \")(userPassword={1}\";\n" +
+				"			filter += \"))\";\n" +
+				"			try {\n" +
+				"				NamingEnumeration<SearchResult> results = ctx.search(\"ou=system\", filter, new Object[] { user, pass },\n"
+				+
+				"						new SearchControls());\n" +
+				"			} catch (NamingException e) {\n" +
+				"				e.printStackTrace();\n" +
+				"			}";
+
+		assertChange(original, expected);
 	}
 
-	public void expectedCode() {
-		String user = null;
-		String pass = null;
-		DirContext ctx = null;
-
-		String filter = "(&(uid={0}" + ")(userPassword={1}" + "))";
-		try {
-			NamingEnumeration<SearchResult> results = ctx.search("ou=system", filter, new String[] { user, pass },
-					new SearchControls());
-		} catch (NamingException e) {
-			e.printStackTrace();
-		}
+	@Test
+	public void visit_useSearchWithInstanceOfName_shouldTransform() throws Exception {
+		fixture.addImport(javax.naming.Name.class.getName());
+		String original = "" +
+				"			String user = null;\n" +
+				"			String pass = null;\n" +
+				"			DirContext ctx = null;\n" +
+				"			Name name = null;\n" +
+				"			String filter = \"(&(uid=\" + user + \")(userPassword=\" + pass + \"))\";\n" +
+				"			try {\n" +
+				"				NamingEnumeration<SearchResult> results = ctx.search(name, filter, new SearchControls());\n"
+				+
+				"			} catch (NamingException e) {\n" +
+				"				e.printStackTrace();\n" +
+				"			}";
+		String expected = "" +
+				"			String user = null;\n" +
+				"			String pass = null;\n" +
+				"			DirContext ctx = null;\n" +
+				"			Name name = null;\n" +
+				"			String filter = \"(&(uid={0}\" + \")(userPassword={1}\" + \"))\";\n" +
+				"			try {\n" +
+				"				NamingEnumeration<SearchResult> results = ctx.search(name, filter, new Object[] { user, pass },\n"
+				+
+				"						new SearchControls());\n" +
+				"			} catch (NamingException e) {\n" +
+				"				e.printStackTrace();\n" +
+				"			}";
+		assertChange(original, expected);
 	}
 
 }
