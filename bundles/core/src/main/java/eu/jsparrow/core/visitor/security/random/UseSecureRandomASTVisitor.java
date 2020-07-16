76d2ff39a6a46375b7c3b510ff1dcad7abfdf10a
gregor.balint <gregor.balint@splendit.at>
2020-07-16 08:54:20 +0200

    SIM-1772 ADD visitor UseSecureRandomASTVisitor replacing new Random()

---
+++ bundles/core/src/main/java/eu/jsparrow/core/visitor/security/random/UseSecureRandomASTVisitor.java
@@ -0,0 +1,64 @@
+package eu.jsparrow.core.visitor.security.random;
+
+import org.eclipse.jdt.core.dom.AST;
+import org.eclipse.jdt.core.dom.ClassInstanceCreation;
+import org.eclipse.jdt.core.dom.ITypeBinding;
+import org.eclipse.jdt.core.dom.Name;
+import org.eclipse.jdt.core.dom.Type;
+import org.eclipse.jdt.core.dom.VariableDeclarationStatement;
+
+import eu.jsparrow.rules.common.visitor.AbstractAddImportASTVisitor;
+
+public class UseSecureRandomASTVisitor extends AbstractAddImportASTVisitor {
+
+	@Override
+	public boolean visit(ClassInstanceCreation node) {
+
+		ITypeBinding typeBinding = node.resolveTypeBinding();
+		if (!typeBinding.getQualifiedName()
+			.equals(java.util.Random.class.getName())) {
+			return false;
+		}
+
+		replaceUnsafeRandomInstanceCreation(node);
+		return true;
+
+	}
+
+	@Override
+	public boolean visit(VariableDeclarationStatement node) {
+
+		ITypeBinding typeBinding = node.getType()
+			.resolveBinding();
+		if (!typeBinding.getQualifiedName()
+			.equals(java.util.Random.class.getName())) {
+			return false;
+		}
+
+		replaceUnsafeRandomVariableType(node);
+		return true;
+
+	}
+
+	Type getSecureRandomType() {
+		String secureRandomQualifiedName = java.security.SecureRandom.class.getName();
+		Name secureRandomName;
+		AST ast = getCompilationUnit().getAST();
+		if (isSafeToAddImport(getCompilationUnit(), secureRandomQualifiedName)) {
+			this.addImports.add(secureRandomQualifiedName);
+			secureRandomName = ast
+				.newSimpleName(java.security.SecureRandom.class.getSimpleName());
+		} else {
+			secureRandomName = ast.newName(secureRandomQualifiedName);
+		}
+		return ast.newSimpleType(secureRandomName);
+	}
+
+	void replaceUnsafeRandomInstanceCreation(ClassInstanceCreation instanceCreation) {
+		astRewrite.replace(instanceCreation.getType(), getSecureRandomType(), null);
+	}
+
+	void replaceUnsafeRandomVariableType(VariableDeclarationStatement variableDeclarationStatement) {
+		astRewrite.replace(variableDeclarationStatement.getType(), getSecureRandomType(), null);
+	}
+}
---
+++ tests/test/src/test/java/eu/jsparrow/core/visitor/security/random/UseSecureRandomASTVisitorTest.java
@@ -0,0 +1,70 @@
+package eu.jsparrow.core.visitor.security.random;
+
+import java.util.Random;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import eu.jsparrow.core.visitor.impl.UsesJDTUnitFixture;
+
+public class UseSecureRandomASTVisitorTest extends UsesJDTUnitFixture {
+
+	@BeforeEach
+	public void setUp() throws Exception {
+		setDefaultVisitor(new UseSecureRandomASTVisitor());
+		defaultFixture.addImport(java.util.Random.class.getName());
+	}
+
+	@AfterEach
+	public void tearDown() throws Exception {
+		fixtureProject.clear();
+	}
+
+	@Test
+	public void visit_NewRandomInvocation_shouldTransform() throws Exception {
+
+		String original = "" +
+				"	void test() {\n" +
+				"		Random random = new Random();\n" +
+				"		byte bytes[] = new byte[20];\n" +
+				"		random.nextBytes(bytes);\n" +
+				"	}";
+
+		String expected = "" +
+				"	void test() {\n" +
+				"		SecureRandom random = new SecureRandom();\n" +
+				"		byte bytes[] = new byte[20];\n" +
+				"		random.nextBytes(bytes);" +
+				"	}";
+
+		assertChange(original, expected);
+	}
+	
+	
+	@Test
+	public void visit_ImportNotPossible_shouldTransform() throws Exception {
+
+		String original = "" +
+				"	class SecureRandom {}\n" + 
+				"	void test() {\n" + 
+				"		Random random = new Random();\n" + 
+				"		byte bytes[] = new byte[20];\n" + 
+				"		random.nextBytes(bytes);\n" + 
+				"	}";
+
+		String expected = "" +
+				"	class SecureRandom {}\n" + 
+				"	void test() {\n" + 
+				"		java.security.SecureRandom random = new java.security.SecureRandom();\n" + 
+				"		byte bytes[] = new byte[20];\n" + 
+				"		random.nextBytes(bytes);\n" + 
+				"	}";
+
+		assertChange(original, expected);
+	}
+	
+	
+
+
+}
