6d2af47ec1015c97710712a7a364570737db2fe9
gregor.balint <gregor.balint@splendit.at>
2020-07-30 15:27:23 +0200

    SIM-1772 UPDATE improve test coverage

--- tests/test/src/test/java/eu/jsparrow/core/visitor/security/random/UseClassSecureRandomNegativeASTVisitorTest.java
+++ tests/test/src/test/java/eu/jsparrow/core/visitor/security/random/UseClassSecureRandomNegativeASTVisitorTest.java
@@ -57,4 +57,52 @@ public class UseClassSecureRandomNegativeASTVisitorTest extends UsesJDTUnitFixtu
 		assertNoChange(original);
 	}
 
+	@Test
+	public void visit_NewRandomWithSeedAssignedToField_shouldNotTransform() throws Exception {
+
+		String original = "" +
+				"		Random random = new Random(0L);";
+
+		assertNoChange(original);
+	}
+	
+
+	@Test
+	public void visit_NewRandomWithSeedAssignmentAsArgument_shouldNotTransform() throws Exception {
+
+		String original = "" +
+				"		void useRandom(Random random) {\n" + 
+				"		}\n" + 
+				"		void test() {\n" + 
+				"			Random random;\n" + 
+				"			useRandom(random = new Random(0L));\n" + 
+				"		}";
+
+		assertNoChange(original);
+	}
+	
+	@Test
+	public void visit_NewRandomWithSeedAssignmentInThenClause_shouldNotTransform() throws Exception {
+
+		String original = "" +
+				"		void test() {\n" + 
+				"			Random random;\n" + 
+				"			if (true)\n" + 
+				"				random = new Random(0L);\n" + 
+				"		}";
+
+		assertNoChange(original);
+	}
+	
+	@Test
+	public void visit_NewRandomWithSeedAsExpressionStatement_shouldNotTransform() throws Exception {
+
+		String original = "" +
+				"		void test() {\n" + 
+				"			new Random(0L);\n" + 
+				"		}";
+
+		assertNoChange(original);
+	}
+
 }
