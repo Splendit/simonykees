6b5bf0f017df44ecb7cc890f8e2f96c26ca061d3
gregor.balint <gregor.balint@splendit.at>
2020-08-25 19:15:49 +0200

    SIM-1746 UPDATE improve test coverage

--- tests/test/src/test/java/eu/jsparrow/core/visitor/impl/UseOffsetBasedStringMethodsASTVisitorTest.java
+++ tests/test/src/test/java/eu/jsparrow/core/visitor/impl/UseOffsetBasedStringMethodsASTVisitorTest.java
@@ -111,16 +111,16 @@ public class UseOffsetBasedStringMethodsASTVisitorTest extends UsesSimpleJDTUnit
 	}
 	
 	@Test
-	public void visit_ImportClashForMax_shouldTransform() throws Exception {
+	public void visit_StaticImportOfMaxAlreadyExists_shouldTransform() throws Exception {
 		boolean isStatic = true;
 		boolean isOnDemand = false;		
-		fixture.addImport("samples.sim1746.staticmembers.StaticMembers.max", isStatic, isOnDemand);
+		fixture.addImport(java.lang.Math.class.getName() + ".max", isStatic, isOnDemand);
 		String original = "" +
 				"		String str = \"Hello World!\";\n" +
 				"		int index = str.substring(6).indexOf('d');\n";
 		String expected = "" +
 				"		String str = \"Hello World!\";\n" +
-				"		int index=Math.max(str.indexOf('d',6) - 6,-1);\n";
+				"		int index=max(str.indexOf('d',6) - 6,-1);\n";
 
 		assertChange(original, expected);
 	}
