7b9a918208d87c18805cf260689f9f9c9375fa99
gregor.balint <gregor.balint@splendit.at>
2020-09-24 11:40:39 +0200

    SIM-1799 ADD rule with name 'Use Files.newBufferedWriter'

---
+++ bundles/core/src/main/java/eu/jsparrow/core/visitor/files/UseFilesBufferedWriterASTVisitor.java
@@ -0,0 +1,31 @@
+package eu.jsparrow.core.visitor.files;
+
+import org.eclipse.jdt.core.dom.VariableDeclarationFragment;
+
+import eu.jsparrow.rules.common.visitor.AbstractAddImportASTVisitor;
+
+/**
+ * Replaces the initializations of {@link java.io.BufferedWriter} objects with
+ * the non-blocking alternative
+ * {@link java.nio.file.Files#newBufferedWriter(java.nio.file.Path, java.nio.charset.Charset)}.
+ * 
+ * For example, the following code:
+ * <p>
+ * {@code BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(new File("path/to/file")));}
+ * <p>
+ * is transformed to:
+ * <p>
+ * {@code BufferedWriter bufferedWriter = Files.newBufferedWriter(Paths.get("path/to/file"), Charset.defaultCharset());}
+ * <p>
+ * 
+ * @since 3.22.0
+ *
+ */
+public class UseFilesBufferedWriterASTVisitor extends AbstractAddImportASTVisitor {
+
+	@Override
+	public boolean visit(VariableDeclarationFragment fragment) {
+		return super.visit(fragment);
+	}
+
+}
