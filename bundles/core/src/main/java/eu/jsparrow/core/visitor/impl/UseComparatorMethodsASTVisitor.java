c381cc9d72824aa8da9c4b6c97f68ecbab5c1d08
gregor.balint <gregor.balint@splendit.at>
2020-10-14 21:52:51 +0200

    SIM-1816 UPDATE implement transformation

--- bundles/core/src/main/java/eu/jsparrow/core/visitor/impl/UseComparatorMethodsASTVisitor.java
+++ bundles/core/src/main/java/eu/jsparrow/core/visitor/impl/UseComparatorMethodsASTVisitor.java
@@ -1,12 +1,15 @@
 package eu.jsparrow.core.visitor.impl;
 
+import java.awt.dnd.DropTargetListener;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import org.eclipse.jdt.core.dom.AST;
 import org.eclipse.jdt.core.dom.ASTNode;
 import org.eclipse.jdt.core.dom.Expression;
+import org.eclipse.jdt.core.dom.ExpressionMethodReference;
 import org.eclipse.jdt.core.dom.IMethodBinding;
 import org.eclipse.jdt.core.dom.ITypeBinding;
 import org.eclipse.jdt.core.dom.LambdaExpression;
@@ -32,11 +35,64 @@ public class UseComparatorMethodsASTVisitor extends AbstractAddImportASTVisitor 
 	public boolean visit(LambdaExpression lambda) {
 
 		ComparatorTransformationData transformationData = analyzeLambda(lambda);
-
+		MethodInvocation lambdaReplacement;
 		if (transformationData != null) {
-			transformationData.getOrder();
+			lambdaReplacement = createInvocationCreatingComparator(transformationData);
+			astRewrite.replace(lambda, lambdaReplacement, null);
+			onRewrite();
 		}
 		return true;
+	}
+
+	private MethodInvocation createInvocationCreatingComparator(ComparatorTransformationData transformationData) {
+		boolean isReverse = transformationData.isReverseOrder();
+		Optional<IMethodBinding> optionalComparisonKeyMethod = transformationData.getComparisonKeyMethod();
+		AST ast = astRewrite.getAST();
+
+		MethodInvocation methodInvocation = ast.newMethodInvocation();
+		String methodName;
+		if (!optionalComparisonKeyMethod.isPresent()) {
+
+			if (isReverse) {
+				methodName = "reverseOrder"; //$NON-NLS-1$
+			} else {
+				methodName = "naturalOrder"; //$NON-NLS-1$
+			}
+			methodInvocation.setName(ast.newSimpleName(methodName));
+			methodInvocation.setExpression(ast.newSimpleName("Comparator")); //$NON-NLS-1$
+			return methodInvocation;
+		}
+
+		IMethodBinding iMethodBinding = optionalComparisonKeyMethod.get();
+		ITypeBinding returnType = iMethodBinding.getReturnType();
+		if (ClassRelationUtil.isContentOfType(returnType, java.lang.Integer.class.getName())) {
+			methodName = "comparingInt"; //$NON-NLS-1$
+		} else if (ClassRelationUtil.isContentOfType(returnType, java.lang.Long.class.getName())) {
+			methodName = "comparingLong"; //$NON-NLS-1$
+		} else if (ClassRelationUtil.isContentOfType(returnType, java.lang.Double.class.getName())) {
+			methodName = "comparingDouble"; //$NON-NLS-1$
+		} else {
+			methodName = "comparing"; //$NON-NLS-1$
+		}
+		methodInvocation.setName(ast.newSimpleName(methodName));
+		methodInvocation.setExpression(ast.newSimpleName("Comparator")); //$NON-NLS-1$
+		ExpressionMethodReference methodReference = ast.newExpressionMethodReference();
+		methodReference.setName(ast.newSimpleName(iMethodBinding.getName()));
+		methodReference.setExpression(ast.newName(iMethodBinding.getDeclaringClass()
+			.getErasure()
+			.getQualifiedName()));
+
+		@SuppressWarnings("unchecked")
+		List<Expression> arguments = methodInvocation.arguments();
+		arguments.add(methodReference);
+		if (!isReverse) {
+			return methodInvocation;
+		}
+		MethodInvocation reverseMethodInvocation = ast.newMethodInvocation();
+		// reversed
+		reverseMethodInvocation.setName(ast.newSimpleName("reversed")); //$NON-NLS-1$
+		reverseMethodInvocation.setExpression(methodInvocation);
+		return reverseMethodInvocation;
 	}
 
 	private ComparatorTransformationData analyzeLambda(LambdaExpression lambda) {
@@ -213,8 +269,8 @@ public class UseComparatorMethodsASTVisitor extends AbstractAddImportASTVisitor 
 			this.order = order;
 		}
 
-		public Order getOrder() {
-			return order;
+		public boolean isReverseOrder() {
+			return order == Order.REVERSE_ORDER;
 		}
 
 		public Optional<IMethodBinding> getComparisonKeyMethod() {
