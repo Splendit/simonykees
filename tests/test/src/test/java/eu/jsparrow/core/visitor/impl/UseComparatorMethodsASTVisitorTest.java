c38a0b245b8d7d4e5d35fb4f1900ed33475629c5
gregor.balint <gregor.balint@splendit.at>
2020-10-15 18:02:11 +0200

    SIM-1816 ADD JUnit test methods

--- tests/test/src/test/java/eu/jsparrow/core/visitor/impl/UseComparatorMethodsASTVisitorTest.java
+++ tests/test/src/test/java/eu/jsparrow/core/visitor/impl/UseComparatorMethodsASTVisitorTest.java
@@ -11,19 +11,207 @@ public class UseComparatorMethodsASTVisitorTest extends UsesJDTUnitFixture {
 		setDefaultVisitor(new UseComparatorMethodsASTVisitor());
 		defaultFixture.addImport(java.util.Comparator.class.getName());
 	}
-	
+
 	@AfterEach
 	public void tearDown() throws Exception {
 		fixtureProject.clear();
 	}
-	
+
+	@Test
+	public void visit_Comparator4Integer_shouldTransform() throws Exception {
+		String original = "" +
+				"void test() {\n" +
+				"	Comparator<Integer> comparator = (lhs, rhs) -> lhs.compareTo(rhs);\n" +
+				"}";
+		String expected = "" +
+				"void test() {\n" +
+				"	Comparator<Integer> comparator=Comparator.naturalOrder();\n" +
+				"}";
+		assertChange(original, expected);
+	}
+
+	@Test
+	public void visit_Comparator4IntegerReversed_shouldTransform() throws Exception {
+		String original = "" +
+				"void test() {\n" +
+				"	Comparator<Integer> comparator = (lhs, rhs) -> rhs.compareTo(lhs);\n" +
+				"}";
+		String expected = "" +
+				"void test() {\n" +
+				"	Comparator<Integer> comparator=Comparator.reverseOrder();\n" +
+				"}";
+		assertChange(original, expected);
+	}
+
+	@Test
+	public void visit_Comparator4ArrayDequeOfInteger_shouldTransform() throws Exception {
+		defaultFixture.addImport(java.util.ArrayDeque.class.getName());
+		String original = "" +
+				"void test() {\n" +
+				"	Comparator<ArrayDeque<Integer>> comparator = (lhs, rhs) -> lhs.getFirst().compareTo(rhs.getFirst());\n"
+				+
+				"}";
+
+		String expected = "" +
+				"void test() {\n" +
+				"	Comparator<ArrayDeque<Integer>> comparator=Comparator.comparingInt(ArrayDeque::getFirst);\n"
+				+
+				"}";
+
+		assertChange(original, expected);
+	}
+
+	@Test
+	public void visit_Comparator4ArrayDequeOfLong_shouldTransform() throws Exception {
+		defaultFixture.addImport(java.util.ArrayDeque.class.getName());
+		String original = "" +
+				"void test() {\n" +
+				"	Comparator<ArrayDeque<Long>> comparator = (lhs, rhs) -> lhs.getFirst().compareTo(rhs.getFirst());\n"
+				+
+				"}";
+
+		String expected = "" +
+				"void test() {\n" +
+				"	Comparator<ArrayDeque<Long>> comparator=Comparator.comparingLong(ArrayDeque::getFirst);\n"
+				+
+				"}";
+
+		assertChange(original, expected);
+	}
+
+	@Test
+	public void visit_Comparator4ArrayDequeOfDouble_shouldTransform() throws Exception {
+		defaultFixture.addImport(java.util.ArrayDeque.class.getName());
+		String original = "" +
+				"void test() {\n" +
+				"	Comparator<ArrayDeque<Double>> comparator = (lhs, rhs) -> lhs.getFirst().compareTo(rhs.getFirst());\n"
+				+
+				"}";
+
+		String expected = "" +
+				"void test() {\n" +
+				"	Comparator<ArrayDeque<Double>> comparator=Comparator.comparingDouble(ArrayDeque::getFirst);\n"
+				+
+				"}";
+
+		assertChange(original, expected);
+	}
+
+	@Test
+	public void visit_Comparator4ArrayDequeOfString_shouldTransform() throws Exception {
+		defaultFixture.addImport(java.util.ArrayDeque.class.getName());
+		String original = "" +
+				"void test() {\n" +
+				"	Comparator<ArrayDeque<String>> comparator = (lhs, rhs) -> lhs.getFirst().compareTo(rhs.getFirst());\n"
+				+
+				"}";
+
+		String expected = "" +
+				"void test() {\n" +
+				"	Comparator<ArrayDeque<String>> comparator=Comparator.comparing(ArrayDeque::getFirst);\n"
+				+
+				"}";
+
+		assertChange(original, expected);
+	}
+
+	@Test
+	public void visit_Comparator4ArrayDequeOfStringReversed_shouldTransform() throws Exception {
+		defaultFixture.addImport(java.util.ArrayDeque.class.getName());
+		String original = "" +
+				"void test() {\n" +
+				"	Comparator<ArrayDeque<String>> comparator = (lhs, rhs) -> rhs.getFirst().compareTo(lhs.getFirst());\n"
+				+
+				"}";
+
+		String expected = "" +
+				"void test() {\n" +
+				"	Comparator<ArrayDeque<String>> comparator=Comparator.comparing(ArrayDeque::getFirst).reversed();\n"
+				+
+				"}";
+
+		assertChange(original, expected);
+	}
+
+	@Test
+	public void visit_Comparator4IntegerUsingOnlyLHSLambdaParameter_shouldNotTransform() throws Exception {
+		String original = "" +
+				"void test() {\n" +
+				"	Comparator<Integer> comparator = (lhs, rhs) -> lhs.compareTo(lhs);\n" +
+				"}";
+		assertNoChange(original);
+	}
+
+	@Test
+	public void visit_Comparator4IntegerUsingOnlyRHSLambdaParameter_shouldNotTransform() throws Exception {
+		String original = "" +
+				"void test() {\n" +
+				"	Comparator<Integer> comparator = (lhs, rhs) -> rhs.compareTo(rhs);\n" +
+				"}";
+		assertNoChange(original);
+	}
+
+	@Test
+	public void visit_ArrayDequeComparatorUsingDifferentGetters_shouldNotTransform() throws Exception {
+		defaultFixture.addImport(java.util.ArrayDeque.class.getName());
+		String original = "" +
+				"void test() {\n" +
+				"	Comparator<ArrayDeque<Integer>> comparator = (lhs, rhs) -> lhs.getFirst().compareTo(rhs.getLast());\n"
+				+
+				"}";
+		assertNoChange(original);
+	}
+
+	@Test
+	public void visit_DequeComparatorUsingOnlyLHSLambdaParameter_shouldNotTransform() throws Exception {
+		defaultFixture.addImport(java.util.ArrayDeque.class.getName());
+		String original = "" +
+				"void test() {\n" +
+				"	Comparator<ArrayDeque<Integer>> comparator = (lhs, rhs) -> lhs.getFirst().compareTo(lhs.getFirst());\n"
+				+
+				"}";
+		assertNoChange(original);
+	}
+
+	@Test
+	public void visit_DequeComparatorUsingOnlyRHSLambdaParameter_shouldNotTransform() throws Exception {
+		defaultFixture.addImport(java.util.ArrayDeque.class.getName());
+		String original = "" +
+				"void test() {\n" +
+				"	Comparator<ArrayDeque<Integer>> comparator = (lhs, rhs) -> rhs.getFirst().compareTo(rhs.getFirst());\n"
+				+
+				"}";
+		assertNoChange(original);
+	}
+
 	@Test
 	public void visit_Comparator4ArrayListByFirstItem_shouldNotTransform() throws Exception {
 		defaultFixture.addImport(java.util.ArrayList.class.getName());
 		String original = "" +
-				"	void test() {\n" +
-				"		Comparator<ArrayList<Integer>> arrayListComparator = (l1, l2) -> l1.get(0).compareTo(l2.get(0));\n" +
-				"	}";
+				"void test() {\n" +
+				"	Comparator<ArrayList<Integer>> comparator = (lhs, rhs) -> lhs.get(0).compareTo(rhs.get(0));\n"
+				+
+				"}";
+
+		assertNoChange(original);
+	}
+
+	/**
+	 * This test will fail as soon as the transformation with jokers is
+	 * supported. TODO for Implementation of isComparable: find out whether
+	 * typeBinding.isCapture() is true, and if this is the case, then get the
+	 * bounds and determine whether comparable is within the bounds.
+	 * 
+	 * 
+	 * @throws Exception
+	 */
+	@Test
+	public void visit_ComparatorForComparableOfJoker_shouldButDoesNotTransform() throws Exception {
+		String original = "" +
+				"void test() {\n" +
+				"	Comparator<Comparable<? super Comparable<?>>> comparableComparatorReversed = (u1, u2) -> u2.compareTo(u1);\n"
+				+
+				"}";
 
 		assertNoChange(original);
 	}
