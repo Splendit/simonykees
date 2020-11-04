f87a3e0986b27c42bf4bd43aa4aec926202477ef
Ardit Ymeri <ardit.ymeri@splendit.at>
2020-11-04 18:25:36 +0100

    SIM1796 UPDATE comments and docs to denote where the template is taken from

--- bundles/standalone/src/main/java/eu/jsparrow/standalone/report/ReportGenerator.java
+++ bundles/standalone/src/main/java/eu/jsparrow/standalone/report/ReportGenerator.java
@@ -30,6 +30,9 @@ public class ReportGenerator {
 	private static final String TOTAL_FILES_CHANGED = "totalFilesChanged"; //$NON-NLS-1$
 	private static final String TOTAL_FILES_COUNT = "totalFilesCount"; //$NON-NLS-1$
 	private static final String PROJECT_NAME = "projectName"; //$NON-NLS-1$
+	/**
+	 * The template is taken from {@linkplain https://jsparrow.github.io/statistics/statistics.html}.
+	 */
 	private static final String REPORT_FILE_NAME = "jSparrowReport.html"; //$NON-NLS-1$
 
 	/**
