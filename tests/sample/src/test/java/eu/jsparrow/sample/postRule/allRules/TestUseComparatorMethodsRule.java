9967d393c065b0ddac6da59e77aab013c30a7ddd
Ardit Ymeri <ardit.ymeri@splendit.at>
2020-11-13 16:49:23 +0100

    SIM-1816 UPDATE sync allRules test file with postRules

--- tests/sample/src/test/java/eu/jsparrow/sample/postRule/allRules/TestUseComparatorMethodsRule.java
+++ tests/sample/src/test/java/eu/jsparrow/sample/postRule/allRules/TestUseComparatorMethodsRule.java
@@ -3,6 +3,8 @@ package eu.jsparrow.sample.postRule.allRules;
 import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.Deque;
+import java.util.function.Supplier;
 
 @SuppressWarnings({ "unused" })
 public class TestUseComparatorMethodsRule {
@@ -13,30 +15,259 @@ public class TestUseComparatorMethodsRule {
 
 	void testComparatorsForInteger() {
 		Comparator<Integer> comparator = Comparator.naturalOrder();
-		comparator = Comparator.naturalOrder();
-		comparator = Comparator.naturalOrder();
+		comparator = (lhs, rhs) -> {
+			return lhs.compareTo(rhs);
+		};
+		comparator = new Comparator<Integer>() {
+
+			@Override
+			public int compare(Integer lhs, Integer rhs) {
+				return lhs.compareTo(rhs);
+			}
+		};
 	}
 
 	void testComparatorsForIntegerReversed() {
 		Comparator<Integer> comparator = Comparator.reverseOrder();
-		comparator = Comparator.reverseOrder();
-		comparator = Comparator.reverseOrder();
+		comparator = (lhs, rhs) -> {
+			return rhs.compareTo(lhs);
+		};
+		comparator = new Comparator<Integer>() {
+
+			@Override
+			public int compare(Integer lhs, Integer rhs) {
+				return rhs.compareTo(lhs);
+			}
+		};
 	}
 
 	void testComparatorsForArrayDeque() {
 		Comparator<ArrayDeque<Integer>> comparator = Comparator.comparingInt(ArrayDeque::getFirst);
-		comparator = Comparator.comparingInt(ArrayDeque::getFirst);
-		comparator = Comparator.comparingInt(ArrayDeque::getFirst);
+		comparator = (lhs, rhs) -> {
+			return lhs.getFirst()
+				.compareTo(rhs.getFirst());
+		};
+		comparator = new Comparator<ArrayDeque<Integer>>() {
+
+			@Override
+			public int compare(ArrayDeque<Integer> lhs, ArrayDeque<Integer> rhs) {
+				return lhs.getFirst()
+					.compareTo(rhs.getFirst());
+			}
+		};
 	}
 
 	void testTransformationWithoutLossOfInformation() {
-		final Comparator<?> comparator1 = Comparator.<Integer>naturalOrder();
-		final Comparator<? extends Comparable<?>> comparator2 = Comparator.<Integer>naturalOrder();
+		Comparator<?> comparator1 = Comparator.<Integer>naturalOrder();
+		Comparator<? extends Comparable<?>> comparator2 = Comparator.<Integer>naturalOrder();
+		comparator1 = Comparator.<Integer>reverseOrder();
+		comparator2 = Comparator.<Integer>reverseOrder();
+		class LocalClass<T> implements Comparable<LocalClass<T>> {
+
+			@Override
+			public int compareTo(LocalClass<T> o) {
+				return 0;
+			}
+		}
+		comparator1 = Comparator.<LocalClass<Object>>naturalOrder();
+		comparator2 = Comparator.<LocalClass<Object>>naturalOrder();
+		comparator1 = Comparator.<LocalClass<Object>>reverseOrder();
+		comparator2 = Comparator.<LocalClass<Object>>reverseOrder();
 	}
 
 	void testGetCollectionForIntegerComparator() {
-		final ArrayList<?> arrayList = getCollectionForComparator(Comparator.<Integer>naturalOrder());
-		final ArrayList<? extends Comparable<?>> arrayList5 = getCollectionForComparator(
+		ArrayList<?> arrayList = getCollectionForComparator(Comparator.<Integer>naturalOrder());
+		ArrayList<? extends Comparable<?>> arrayList5 = getCollectionForComparator(
 				Comparator.<Integer>naturalOrder());
+	}
+
+	class TestUseComparatorOfDequeOfInteger {
+		void useComparator(Comparator<Deque<Integer>> comparator) {
+		}
+
+		void test() {
+			useComparator(
+					Comparator.comparingInt(Deque::getFirst));
+		}
+	}
+
+	class TestUseComparatorOfJoker {
+
+		Comparator<?> useComparator(Comparator<?> comparator) {
+			return comparator;
+		}
+
+		void testWithComparatorOfInteger() {
+			Comparator<?> comparator0 = useComparator((Comparator<Integer>) Comparator.<Integer>naturalOrder());
+			Comparator<?> comparator1 = useComparator(Comparator.<Integer>naturalOrder());
+		}
+
+		void testWithComparatorOfJokerExtendingComparableRawtype() {
+			@SuppressWarnings({ "rawtypes", "unchecked" })
+			Comparator<?> comparator = useComparator((Comparator<? extends Comparable>) (t1, t2) -> t1.compareTo(t2));
+		}
+
+		void testWithComparatorOfComparableRawtype() {
+			@SuppressWarnings({ "rawtypes", "unchecked" })
+			Comparator<?> comparator0 = useComparator((Comparator<Comparable>) (t1, t2) -> t1.compareTo(t2));
+			@SuppressWarnings({ "rawtypes", "unchecked" })
+			Comparator<?> comparator1 = useComparator((Comparable t1, Comparable t2) -> t1.compareTo(t2));
+		}
+
+		void testWithComparatorOfDeque() {
+			Comparator<?> comparator1 = useComparator(
+					(Comparator<Deque<Integer>>) Comparator.comparingInt((Deque<Integer> x1) -> x1.getFirst()));
+			Comparator<?> comparator0 = useComparator(
+					Comparator.comparingInt((Deque<Integer> lhs) -> lhs.getFirst()));
+		}
+	}
+
+	class TestUseObjectWithTypeParameter {
+		<T> T useObject(T t) {
+			return t;
+		}
+
+		void testWithComparatorOfInteger() {
+			Comparator<Integer> comparator;
+			comparator = this.useObject((Comparator<Integer>) Comparator.<Integer>naturalOrder());
+			comparator = this.<Comparator<Integer>>useObject(Comparator.naturalOrder());
+		}
+
+		@SuppressWarnings({ "rawtypes", "unchecked" })
+		void testWithComparatorOfJoker() {
+			Comparator<?> comparator;
+			comparator = this.<Comparator<?>>useObject(Comparator.<Integer>naturalOrder());
+			comparator = this.<Comparator<?>>useObject((Comparable t1, Comparable t2) -> t1.compareTo(t2));
+		}
+
+		void testWithComparatorOfDequeOfInteger() {
+			Comparator<Deque<Integer>> comparator;
+			comparator = this.useObject(
+					(Comparator<Deque<Integer>>) Comparator.comparingInt((Deque<Integer> lhs) -> lhs.getFirst()));
+			comparator = this.<Comparator<Deque<Integer>>>useObject((Comparator.comparingInt(Deque::getFirst)));
+		}
+	}
+
+	class TestNotTransformedDueToLambdaStructure {
+		Integer useInteger(Integer integer) {
+			return integer;
+		}
+
+		<T> T useObject(T object) {
+			return object;
+		}
+
+		Integer getFirst(Deque<Integer> deque) {
+			return deque.getFirst();
+		}
+
+		void test(Integer x1, Integer x2) {
+			Comparator<Integer> comparator0 = (lhs, rhs) -> lhs.compareTo(x2);
+			Comparator<Integer> comparator1 = (lhs, rhs) -> x1.compareTo(rhs);
+			Comparator<Integer> comparator2 = (lhs, rhs) -> x1.compareTo(x2);
+
+			Comparator<Integer> comparator3 = (lhs, rhs) -> lhs.compareTo(useInteger(rhs));
+			Comparator<Integer> comparator4 = (lhs, rhs) -> useInteger(lhs).compareTo(rhs);
+		}
+
+		void test(Deque<Integer> x1, Deque<Integer> x2) {
+
+			Comparator<Deque<Integer>> comparator0 = (lhs, rhs) -> x1.getFirst()
+				.compareTo(rhs.getFirst());
+			Comparator<Deque<Integer>> comparator1 = (lhs, rhs) -> lhs.getFirst()
+				.compareTo(x2.getFirst());
+			Comparator<Deque<Integer>> comparator2 = (lhs, rhs) -> x1.getFirst()
+				.compareTo(x2.getFirst());
+
+			Comparator<Deque<Integer>> comparator3 = (lhs, rhs) -> useObject(lhs).getFirst()
+				.compareTo(rhs.getFirst());
+			Comparator<Deque<Integer>> comparator4 = (lhs, rhs) -> lhs.getFirst()
+				.compareTo(useObject(rhs).getFirst());
+
+			Comparator<Deque<Integer>> comparator5 = (lhs, rhs) -> getFirst(lhs).compareTo(rhs.getFirst());
+			Comparator<Deque<Integer>> comparator6 = (lhs, rhs) -> lhs.getFirst()
+				.compareTo(getFirst(rhs));
+		}
+	}
+
+	class TestClassWithComparableTypeParameter<T extends Comparable<T>> {
+
+		Comparator<T> comparatorOfT;
+
+		@SuppressWarnings("unchecked")
+		void test() {
+			comparatorOfT = Comparator.naturalOrder();
+			comparatorOfT = Comparator.reverseOrder();
+
+			comparatorOfT = Comparator.naturalOrder();
+			comparatorOfT = Comparator.reverseOrder();
+
+			comparatorOfT = (Comparator<T>) Comparator.<T>naturalOrder();
+			comparatorOfT = (Comparator<T>) Comparator.<T>reverseOrder();
+
+			comparatorOfT = (Comparator<T>) (Comparator<Integer>) Comparator.<Integer>naturalOrder();
+		}
+	}
+
+	class TestMethodWithComparableTypeParameter {
+
+		@SuppressWarnings("unchecked")
+		<T extends Comparable<T>> void test() {
+			Comparator<T> comparatorOfT;
+
+			comparatorOfT = Comparator.naturalOrder();
+			comparatorOfT = Comparator.reverseOrder();
+
+			comparatorOfT = Comparator.naturalOrder();
+			comparatorOfT = Comparator.reverseOrder();
+
+			comparatorOfT = (Comparator<T>) Comparator.<T>naturalOrder();
+			comparatorOfT = (Comparator<T>) Comparator.<T>reverseOrder();
+
+			comparatorOfT = (Comparator<T>) (Comparator<Integer>) Comparator.<Integer>naturalOrder();
+		}
+	}
+
+	class TestClassWithTypeParameterSupplyingComparable<T extends Supplier<Integer>> {
+
+		Comparator<T> comparatorOfT;
+
+		void test() {
+			comparatorOfT = Comparator.comparingInt(T::get);
+			comparatorOfT = Comparator.comparingInt(T::get)
+				.reversed();
+
+			comparatorOfT = Comparator.comparingInt(T::get);
+			comparatorOfT = Comparator.comparingInt(T::get)
+				.reversed();
+
+			comparatorOfT = (Comparator<T>) Comparator.comparingInt((T x1) -> x1.get());
+			comparatorOfT = (Comparator<T>) Comparator.comparingInt((T x1) -> x1.get())
+				.reversed();
+		}
+	}
+
+	class TestSortingOfArrayList {
+
+		<T extends Comparable<T>> void testWithArrayListOfT() {
+			ArrayList<T> arrayList = new ArrayList<>();
+			arrayList.sort(Comparator.naturalOrder());
+			arrayList.sort((Comparator<T>) Comparator.<T>naturalOrder());
+			arrayList.sort(Comparator.<T>naturalOrder());
+		}
+
+		<T extends Supplier<Integer>> void testWithArrayListOfTSupplyingInteger() {
+			ArrayList<T> arrayList = new ArrayList<>();
+			arrayList.sort(Comparator.comparingInt(T::get));
+			arrayList.sort((Comparator<T>) Comparator.comparingInt((T x1) -> x1.get()));
+			arrayList.sort(Comparator.comparingInt((T x1) -> x1.get()));
+		}
+
+		void testWithArrayListOfInteger() {
+			ArrayList<Integer> arrayList = new ArrayList<>();
+			arrayList.sort(Comparator.naturalOrder());
+			arrayList.sort((Comparator<Integer>) Comparator.<Integer>naturalOrder());
+			arrayList.sort(Comparator.<Integer>naturalOrder());
+		}
 	}
 }
