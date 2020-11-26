bcbb74badd8e128ef340dcf5f3a00bc6ce91589d
Ardit Ymeri <ardit.ymeri@splendit.at>
2020-11-26 16:18:00 +0100

    SIM-1841 UPDATE pull up checking versions of a third party library
    We need this functionality in two places. UseStringUtils and replace ExpectedException with assertThrows.

--- bundles/core/src/main/java/eu/jsparrow/core/rule/impl/ReplaceExpectedExceptionByAssertThrowsRule.java
+++ bundles/core/src/main/java/eu/jsparrow/core/rule/impl/ReplaceExpectedExceptionByAssertThrowsRule.java
@@ -1,39 +1,20 @@
 package eu.jsparrow.core.rule.impl;
 
-import java.io.File;
-import java.io.IOException;
 import java.time.Duration;
 import java.util.Arrays;
 import java.util.function.Predicate;
-import java.util.jar.Attributes;
-import java.util.jar.Attributes.Name;
-import java.util.jar.JarFile;
-import java.util.jar.Manifest;
 
-import org.eclipse.core.runtime.IPath;
-import org.eclipse.jdt.core.IJavaElement;
 import org.eclipse.jdt.core.IJavaProject;
-import org.eclipse.jdt.core.IPackageFragmentRoot;
-import org.eclipse.jdt.core.IType;
 import org.eclipse.jdt.core.JavaCore;
-import org.eclipse.jdt.core.JavaModelException;
-import org.eclipse.osgi.util.NLS;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.osgi.framework.Version;
 
-import eu.jsparrow.core.exception.runtime.ITypeNotFoundRuntimeException;
 import eu.jsparrow.core.visitor.junit.ReplaceExpectedExceptionByAssertThrowsASTVisitor;
-import eu.jsparrow.i18n.Messages;
 import eu.jsparrow.rules.common.RefactoringRuleImpl;
 import eu.jsparrow.rules.common.RuleDescription;
 import eu.jsparrow.rules.common.Tag;
 
 public class ReplaceExpectedExceptionByAssertThrowsRule
 		extends RefactoringRuleImpl<ReplaceExpectedExceptionByAssertThrowsASTVisitor> {
-
-	private static final Logger logger = LoggerFactory
-		.getLogger(ReplaceExpectedExceptionByAssertThrowsRule.class);
 
 	private static final String ORG_JUNIT_ASSERT = "org.junit.Assert";
 	private static final String MIN_JUNIT_4_VERSION = "4.13";
@@ -50,6 +31,9 @@ public class ReplaceExpectedExceptionByAssertThrowsRule
 
 	@Override
 	protected String provideRequiredJavaVersion() {
+		/*
+		 * assertThrows expects a lambda expression. 
+		 */
 		return JavaCore.VERSION_1_8;
 	}
 
@@ -60,60 +44,7 @@ public class ReplaceExpectedExceptionByAssertThrowsRule
 			.compareTo(Version.parseVersion(MIN_JUNIT_4_VERSION)) >= 0
 				|| version.compareTo(Version.parseVersion(MIN_JUNIT_5_VERSION)) >= 0;
 
-		try {
-			IType classtype = project.findType(fullyQuallifiedClassName);
-			if (classtype != null) {
-
-				IPackageFragmentRoot thirdPartyLib = getProject(classtype.getParent());
-				if (thirdPartyLib != null) {
-					// file with path to library jar
-					IPath resourcePath = thirdPartyLib.getPath();
-					File file = new File(resourcePath.toString());
-
-					return isImplementationVersionValid(file, versionComparator);
-				}
-			} else {
-				String loggerDebug = NLS.bind(Messages.StringUtilsRule_classNotInClassPath, fullyQuallifiedClassName);
-				logger.debug(loggerDebug);
-			}
-		} catch (JavaModelException e) {
-			logger.error(e.getMessage(), new ITypeNotFoundRuntimeException());
-		}
-		return false;
-	}
-
-	private IPackageFragmentRoot getProject(IJavaElement iJavaElement) {
-		if (null == iJavaElement) {
-			return null;
-		}
-		IJavaElement parent = iJavaElement.getParent();
-		if (null == parent || parent instanceof IPackageFragmentRoot) {
-			return (IPackageFragmentRoot) parent;
-		}
-		return getProject(parent);
-	}
-
-	private boolean isImplementationVersionValid(File file, Predicate<Version> versionComparator) {
-		try (JarFile jar = new JarFile(file)) {
-
-			Manifest manifest = jar.getManifest();
-			Attributes attributes = manifest.getMainAttributes();
-
-			if (attributes != null) {
-				for (Object attribute : attributes.keySet()) {
-					Name key = (Name) attribute;
-					String keyword = key.toString();
-					if ("Implementation-Version".equals(keyword)) { //$NON-NLS-1$
-						Version actualVersion = Version.parseVersion(attributes.getValue(key));
-						return versionComparator.test(actualVersion);
-					}
-				}
-			}
-		} catch (IOException e) {
-			logger.debug("Jar Manifest load error in:", e); //$NON-NLS-1$
-			// Resolving version failed, rule can't be executed
-		}
-		return false;
+		return isInProjectLibraries(project, fullyQuallifiedClassName, versionComparator);
 	}
 
 }
--- bundles/rules.api/src/main/java/eu/jsparrow/rules/common/RefactoringRuleImpl.java
+++ bundles/rules.api/src/main/java/eu/jsparrow/rules/common/RefactoringRuleImpl.java
@@ -1,7 +1,19 @@
 package eu.jsparrow.rules.common;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.function.Predicate;
+import java.util.jar.Attributes;
+import java.util.jar.Attributes.Name;
+import java.util.jar.JarFile;
+import java.util.jar.Manifest;
+
+import org.eclipse.core.runtime.IPath;
 import org.eclipse.jdt.core.ICompilationUnit;
+import org.eclipse.jdt.core.IJavaElement;
 import org.eclipse.jdt.core.IJavaProject;
+import org.eclipse.jdt.core.IPackageFragmentRoot;
+import org.eclipse.jdt.core.IType;
 import org.eclipse.jdt.core.JavaCore;
 import org.eclipse.jdt.core.JavaModelException;
 import org.eclipse.jdt.core.dom.CompilationUnit;
@@ -10,11 +22,13 @@ import org.eclipse.jface.text.Document;
 import org.eclipse.ltk.core.refactoring.DocumentChange;
 import org.eclipse.osgi.util.NLS;
 import org.eclipse.text.edits.TextEdit;
+import org.osgi.framework.Version;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import eu.jsparrow.i18n.Messages;
 import eu.jsparrow.rules.common.exception.RefactoringException;
+import eu.jsparrow.rules.common.exception.runtime.ITypeNotFoundRuntimeException;
 import eu.jsparrow.rules.common.statistics.RuleApplicationCount;
 import eu.jsparrow.rules.common.util.RefactoringUtil;
 import eu.jsparrow.rules.common.visitor.AbstractASTRewriteASTVisitor;
@@ -225,6 +239,64 @@ public abstract class RefactoringRuleImpl<T extends AbstractASTRewriteASTVisitor
 	@Override
 	public RuleDescription getRuleDescription() {
 		return this.ruleDescription;
+	}
+
+	protected boolean isInProjectLibraries(IJavaProject project, String fullyQuallifiedClassName,
+			Predicate<Version> versionComparator) {
+		try {
+			IType classtype = project.findType(fullyQuallifiedClassName);
+			if (classtype != null) {
+
+				IPackageFragmentRoot thirdPartyLib = getProject(classtype.getParent());
+				if (thirdPartyLib != null) {
+					// file with path to library jar
+					IPath resourcePath = thirdPartyLib.getPath();
+					File file = new File(resourcePath.toString());
+
+					return isImplementationVersionValid(file, versionComparator);
+				}
+			} else {
+				String loggerDebug = NLS.bind(Messages.StringUtilsRule_classNotInClassPath, fullyQuallifiedClassName);
+				logger.debug(loggerDebug);
+			}
+		} catch (JavaModelException e) {
+			logger.error(e.getMessage(), new ITypeNotFoundRuntimeException());
+		}
+		return false;
+	}
+
+	protected IPackageFragmentRoot getProject(IJavaElement iJavaElement) {
+		if (null == iJavaElement) {
+			return null;
+		}
+		IJavaElement parent = iJavaElement.getParent();
+		if (null == parent || parent instanceof IPackageFragmentRoot) {
+			return (IPackageFragmentRoot) parent;
+		}
+		return getProject(parent);
+	}
+
+	private boolean isImplementationVersionValid(File file, Predicate<Version> versionComparator) {
+		try (JarFile jar = new JarFile(file)) {
+
+			Manifest manifest = jar.getManifest();
+			Attributes attributes = manifest.getMainAttributes();
+
+			if (attributes != null) {
+				for (Object attribute : attributes.keySet()) {
+					Name key = (Name) attribute;
+					String keyword = key.toString();
+					if ("Implementation-Version".equals(keyword)) { //$NON-NLS-1$
+						Version actualVersion = Version.parseVersion(attributes.getValue(key));
+						return versionComparator.test(actualVersion);
+					}
+				}
+			}
+		} catch (IOException e) {
+			logger.debug("Jar Manifest load error in:", e); //$NON-NLS-1$
+			// Resolving version failed, rule can't be executed
+		}
+		return false;
 	}
 
 	@Override
