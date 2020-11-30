4c60be73a23b4f0f229187f19dedbb7f49d7405a
Ardit Ymeri <ardit.ymeri@splendit.at>
2020-11-30 12:37:07 +0100

    SIM-1817 ADD method to replace TryStatement in UseFilesWriteString
    This is a workaround to removing resources in try statements. Because of a JDT bug, we cannot simply remove the resources from try statement. We replace the entire TryStatement instead.

--- bundles/core/src/main/java/eu/jsparrow/core/visitor/files/UseFilesWriteStringASTVisitor.java
+++ bundles/core/src/main/java/eu/jsparrow/core/visitor/files/UseFilesWriteStringASTVisitor.java
@@ -2,17 +2,27 @@ package eu.jsparrow.core.visitor.files;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import org.eclipse.jdt.core.dom.AST;
 import org.eclipse.jdt.core.dom.ASTNode;
+import org.eclipse.jdt.core.dom.Block;
+import org.eclipse.jdt.core.dom.CatchClause;
+import org.eclipse.jdt.core.dom.Comment;
 import org.eclipse.jdt.core.dom.CompilationUnit;
 import org.eclipse.jdt.core.dom.Expression;
 import org.eclipse.jdt.core.dom.MethodInvocation;
 import org.eclipse.jdt.core.dom.Name;
+import org.eclipse.jdt.core.dom.Statement;
+import org.eclipse.jdt.core.dom.TryStatement;
+import org.eclipse.jdt.core.dom.VariableDeclarationExpression;
 
+import eu.jsparrow.core.visitor.impl.trycatch.TwrCommentsUtil;
 import eu.jsparrow.rules.common.builder.NodeBuilder;
+import eu.jsparrow.rules.common.util.ASTNodeUtil;
 import eu.jsparrow.rules.common.visitor.AbstractAddImportASTVisitor;
+import eu.jsparrow.rules.common.visitor.helper.CommentRewriter;
 
 /**
  * 
@@ -48,6 +58,16 @@ public class UseFilesWriteStringASTVisitor extends AbstractAddImportASTVisitor {
 		MethodInvocation writeStringMethodInvocation = createFilesWriteStringMethodInvocation(transformationData);
 		astRewrite.replace(methodInvocation, writeStringMethodInvocation, null);
 
+		/*
+		 * TODO: 
+		 * 1. use the method createNewTryStatement to completely replace the try statement with a new one. 
+		 * This is a JDT bug that cannot handle TWR statements properly. For this reason, the TryStatement 
+		 * has to be provided from the transformationData. I tried to track it back but it is very complicated
+		 * process. Maybe you can do it faster. 
+		 * 2. If you have a transformationData object, that should provide everything 
+		 * you need for the transformation. You should not have more logic here that
+		 * gets the parents of a node until you reach the node to transform. 
+		 */
 		Expression bufferedIOInitializer = transformationData.getBufferedIOInitializer();
 		ASTNode bufferedIODeclarationFragment = bufferedIOInitializer.getParent();
 		ASTNode bufferedIOResourceToRemove = bufferedIODeclarationFragment.getParent();
@@ -95,4 +115,45 @@ public class UseFilesWriteStringASTVisitor extends AbstractAddImportASTVisitor {
 		return NodeBuilder.newMethodInvocation(ast, filesTypeName,
 				ast.newSimpleName("writeString"), arguments); //$NON-NLS-1$
 	}
+
+	@SuppressWarnings("unchecked")
+	private TryStatement createNewTryStatement(TryStatement node, List<VariableDeclarationExpression> resourceList) {
+
+		TryStatement tryStatement = getASTRewrite().getAST()
+			.newTryStatement();
+		tryStatement.resources()
+			.addAll(resourceList);
+
+		Block newBody = (Block) ASTNode.copySubtree(node.getAST(), node.getBody());
+		
+		List<Statement> newBodyStatements = ASTNodeUtil.convertToTypedList(newBody.statements(), Statement.class);
+		Map<Integer, List<Comment>> comments = TwrCommentsUtil.findBodyComments(node, getCommentRewriter());
+		CommentRewriter commentRewriter = getCommentRewriter();
+		comments.forEach((key, value) -> {
+			int newBodySize = newBodyStatements.size();
+			if (newBodySize > key) {
+				Statement statement = newBodyStatements.get(key);
+				commentRewriter.saveBeforeStatement(statement, value);
+			} else if (!newBodyStatements.isEmpty()) {
+				Statement statement = newBodyStatements.get(newBodySize - 1);
+				commentRewriter.saveAfterStatement(statement, value);
+			} else {
+				commentRewriter.saveBeforeStatement(node, value);
+			}
+		});
+
+		List<CatchClause> newCatchClauses = ASTNodeUtil.convertToTypedList(node.catchClauses(), CatchClause.class)
+			.stream()
+			.map(clause -> (CatchClause) ASTNode.copySubtree(node.getAST(), clause))
+			.collect(Collectors.toList());
+
+		tryStatement.setBody(newBody);
+		tryStatement.catchClauses()
+			.addAll(newCatchClauses);
+		tryStatement.setFinally((Block) ASTNode.copySubtree(node.getAST(), node.getFinally()));
+
+		return tryStatement;
+	}
+	
+
 }
