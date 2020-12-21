05d09eaa6e0319e77c3a05960146bec334008852
Ardit Ymeri <ardit.ymeri@splendit.at>
2020-12-21 13:25:02 +0100

    SIM-1851 ADD unit tests for Free Rules profile

--- bundles/ui/src/main/java/eu/jsparrow/ui/preference/profile/FreeRulesProfile.java
+++ bundles/ui/src/main/java/eu/jsparrow/ui/preference/profile/FreeRulesProfile.java
@@ -1,24 +1,11 @@
 package eu.jsparrow.ui.preference.profile;
 
-import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
-import eu.jsparrow.core.rule.impl.EnumsWithoutEqualsRule;
-import eu.jsparrow.core.rule.impl.ForToForEachRule;
-import eu.jsparrow.core.rule.impl.InefficientConstructorRule;
-import eu.jsparrow.core.rule.impl.LambdaToMethodReferenceRule;
-import eu.jsparrow.core.rule.impl.MultiVariableDeclarationLineRule;
-import eu.jsparrow.core.rule.impl.OverrideAnnotationRule;
-import eu.jsparrow.core.rule.impl.PrimitiveBoxedForStringRule;
-import eu.jsparrow.core.rule.impl.RemoveDoubleNegationRule;
-import eu.jsparrow.core.rule.impl.RemoveEmptyStatementRule;
-import eu.jsparrow.core.rule.impl.RemoveToStringOnStringRule;
-import eu.jsparrow.core.rule.impl.RemoveUnnecessaryThrownExceptionsRule;
-import eu.jsparrow.core.rule.impl.StringLiteralEqualityCheckRule;
-import eu.jsparrow.core.rule.impl.TryWithResourceRule;
-import eu.jsparrow.core.rule.impl.UseIsEmptyOnCollectionsRule;
+import eu.jsparrow.core.rule.RulesContainer;
 import eu.jsparrow.i18n.Messages;
-import eu.jsparrow.rules.common.OrganiseImportsRuleBase;
+import eu.jsparrow.rules.common.RefactoringRule;
 
 /**
  * Profile containing free rules.
@@ -33,21 +20,11 @@ public class FreeRulesProfile implements SimonykeesProfile {
 	boolean isBuiltInProfile = true;
 
 	public FreeRulesProfile() {
-		enabledRulesIds = Arrays.asList(OverrideAnnotationRule.OVERRIDE_ANNOTATION_RULE_ID,
-				TryWithResourceRule.TRY_WITH_RESOURCE_RULE_ID,
-				LambdaToMethodReferenceRule.LAMBDA_TO_METHOD_REFERENCE_RULE_ID,
-				RemoveUnnecessaryThrownExceptionsRule.REMOVE_UNNECESSARY_THROWN_EXCEPTIONS_RULE_ID,
-				MultiVariableDeclarationLineRule.MULTI_VARIABLE_DECLARATION_LINE_RULE_ID,
-				InefficientConstructorRule.INEFFICIENT_CONSTRUCTOR_RULE_ID,
-				RemoveEmptyStatementRule.REMOVE_EMPTY_STATEMENT_RULE_ID,
-				RemoveDoubleNegationRule.REMOVE_DOUBLE_NEGATION_RULE_ID,
-				ForToForEachRule.FOR_TO_FOR_EACH_RULE_ID,
-				RemoveToStringOnStringRule.REMOVE_TO_STRING_ON_STRING_RULE_ID,
-				StringLiteralEqualityCheckRule.STRING_LITERAL_EQUALITY_CHECK_RULE_ID,
-				EnumsWithoutEqualsRule.ENUMS_WITHOUT_EQUALS_RULE_ID,
-				UseIsEmptyOnCollectionsRule.USE_IS_EMPTY_ON_COLLECTIONS_RULE_ID,
-				PrimitiveBoxedForStringRule.PRIMITIVE_BOXED_FOR_STRING_RULE_ID,
-				OrganiseImportsRuleBase.ORGANISE_IMPORTS_RULE_ID);
+		enabledRulesIds = RulesContainer.getAllRules(false)
+			.stream()
+			.filter(RefactoringRule::isFree)
+			.map(RefactoringRule::getId)
+			.collect(Collectors.toList());
 	}
 
 	@Override
