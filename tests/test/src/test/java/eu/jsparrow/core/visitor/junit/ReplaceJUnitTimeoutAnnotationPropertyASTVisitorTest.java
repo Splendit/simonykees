d33b02eaf212c540ea532a99960e93138d7768bd
Ardit Ymeri <ardit.ymeri@splendit.at>
2021-01-12 18:20:41 +0100

    SIM-1858 UPDATE more visitor tests

--- tests/test/src/test/java/eu/jsparrow/core/visitor/junit/ReplaceJUnitTimeoutAnnotationPropertyASTVisitorTest.java
+++ tests/test/src/test/java/eu/jsparrow/core/visitor/junit/ReplaceJUnitTimeoutAnnotationPropertyASTVisitorTest.java
@@ -5,8 +5,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import eu.jsparrow.core.visitor.impl.UsesJDTUnitFixture;
-
-public class ReplaceJUnitTimeoutAnnotationPropertyASTVisitorTest extends UsesJDTUnitFixture {
+class ReplaceJUnitTimeoutAnnotationPropertyASTVisitorTest extends UsesJDTUnitFixture {
 	
 	@BeforeEach
 	public void setUpVisitor() throws Exception {
@@ -59,6 +58,80 @@ public class ReplaceJUnitTimeoutAnnotationPropertyASTVisitorTest extends UsesJDT
 				+ "}";
 		assertChange(original, expected);
 	}
+	
+	@Test
+	void visit_combinedAnnotationProperties_shouldTransform() throws Exception {
+		String original = ""
+				+ "@Test(expected=IOException.class, timeout = 500)\n"
+				+ "public void combinedAnnotationProperties() throws InterruptedException, IOException {\n"
+				+ "	Thread.sleep(2);\n"
+				+ "	throw new IOException();\n"
+				+ "}";
+		String expected = ""
+				+ "@Test(expected=IOException.class)\n"
+				+ "public void combinedAnnotationProperties() throws InterruptedException, IOException {\n"
+				+ "	assertTimeout(ofMillis(500), () -> {\n"
+				+ "		Thread.sleep(2);\n"
+				+ "		throw new IOException();\n"
+				+ "	});\n"
+				+ "}";
+		assertChange(original, expected);
+	}
+	
+	@Test
+	void visit_ifStatement_shouldTransform() throws Exception {
+		defaultFixture.addImport("java.io.IOExceptio");
+		String original = ""
+				+ "@Test(timeout = 500)\n"
+				+ "public void ifStatement() throws InterruptedException, IOException {\n"
+				+ "	if(true) {\n"
+				+ "		Thread.sleep(2);\n"
+				+ "		throw new IOException();\n"
+				+ "	}\n"
+				+ "}";
+		String expected = ""
+				+ "@Test\n"
+				+ "public void ifStatement() throws InterruptedException, IOException {\n"
+				+ "	assertTimeout(ofMillis(500), () -> {\n"
+				+ "		if(true) {\n"
+				+ "			Thread.sleep(2);\n"
+				+ "			throw new IOException();\n"
+				+ "		}\n"
+				+ "	});\n"
+				+ "}\n"
+				+ "";
+		assertChange(original, expected);
+	}
+	
+	@Test
+	void visit_missingAnnotationProperties_shouldNotTransform() throws Exception {
+		String original = ""
+				+ "@Test\n"
+				+ "public void missingAnnotationProperty() {\n"
+				+ "		Thread.sleep(500);\n"
+				+ "}";
+		assertNoChange(original);
+	}
+	
+	@Test
+	void visit_missingTimeoutAnnotationProperty_shouldNotTransform() throws Exception {
+		defaultFixture.addImport("java.io.IOException");
+		String original = ""
+				+ "@Test(expected=IOException.class)\n"
+				+ "public void missingTimeoutAnnotationProperty() throws IOException {\n"
+				+ "		Thread.sleep(500);\n"
+				+ "}";
+		assertNoChange(original);
+	}
+	
+	@Test
+	void visit_missingTestAnnotation_shouldNotTransform() throws Exception {
+		String original = ""
+				+ "@Override"
+				+ "public String toString() {\n"
+				+ "		return \"\";\n"
+				+ "}";
 
-
+		assertNoChange(original);
+	}
 }
