a938e049e58ff23fc8206096feca0a15a244cf24
gregor.balint <gregor.balint@splendit.at>
2021-02-03 19:19:38 +0100

    SIM-1873 UPDATE improve and re-arrange JUnit-tests

--- tests/test/src/test/java/eu/jsparrow/core/visitor/junit/jupiter/MigrateJUnit4ToJupiterASTVisitorNegativeTest.java
+++ tests/test/src/test/java/eu/jsparrow/core/visitor/junit/jupiter/MigrateJUnit4ToJupiterASTVisitorNegativeTest.java
@@ -6,9 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import eu.jsparrow.core.visitor.impl.UsesJDTUnitFixture;
-
-class MigrateJUnit4ToJupiterASTVisitorNegativeTest extends UsesJDTUnitFixture {
+class MigrateJUnit4ToJupiterASTVisitorNegativeTest extends AbstractMigrateJUnit4ToJupiterASTVisitorTest {
 
 	@BeforeEach
 	public void setUpVisitor() throws Exception {
@@ -21,42 +19,72 @@ class MigrateJUnit4ToJupiterASTVisitorNegativeTest extends UsesJDTUnitFixture {
 		fixtureProject.clear();
 	}
 
+	/**
+	 * Expected to fail as soon as both import on demand and the
+	 * {@link org.junit.Assert} - class will be supported.
+	 */
 	@Test
 	public void visit_JUnit4AssertImportedOnDemand_shouldNotTransform() throws Exception {
 		defaultFixture.addImport("org.junit", false, true);
 
 		String original = "" +
-				"	@Test\n" +
-				"	public void test() {\n" +
+				"	@BeforeClass\n" +
+				"	public void beforeAll() {\n" +
 				"		Assert.assertEquals(\"1\", \"1\");" +
 				"	}";
 
 		assertNoChange(original);
 	}
 
+	/**
+	 * Expected to fail as soon as the {@link org.junit.Assert} - class will be
+	 * supported.
+	 */
 	@Test
 	public void visit_JUnit4AssertImportedExplicitly_shouldNotTransform() throws Exception {
 		defaultFixture.addImport(org.junit.Assert.class.getName());
-		defaultFixture.addImport(org.junit.Test.class.getName());
+		defaultFixture.addImport(org.junit.BeforeClass.class.getName());
 
 		String original = "" +
-				"	@Test\n" +
-				"	public void test() {\n" +
+				"	@BeforeClass\n" +
+				"	public void beforeAll() {\n" +
 				"		Assert.assertEquals(\"1\", \"1\");" +
 				"	}";
 
 		assertNoChange(original);
 	}
 
+	/**
+	 * Expected to fail as soon as both the {@link org.junit.Assert} - class and
+	 * the static import of its methods will be supported.
+	 */
 	@Test
 	public void visit_JUnit4AssertEqualsStaticImport_shouldNotTransform() throws Exception {
 		defaultFixture.addImport("org.junit.Assert.assertEquals", true, false);
-		defaultFixture.addImport(org.junit.Test.class.getName());
+		defaultFixture.addImport(org.junit.BeforeClass.class.getName());
 
 		String original = "" +
-				"	@Test\n" +
-				"	public void test() {\n" +
+				"	@BeforeClass\n" +
+				"	public void beforeAll() {\n" +
 				"		assertEquals(\"1\", \"1\");" +
+				"	}";
+
+		assertNoChange(original);
+	}
+
+	/**
+	 * Expected to fail as soon as both the {@link org.junit.Assert} - class and
+	 * the static on-demand import of its methods will be supported.
+	 */
+	@Test
+	public void visit_JUnit4AssertEqualsStaticImportOnDemand_shouldNotTransform() throws Exception {
+		defaultFixture.addImport("org.junit.Assert", true, true);
+		defaultFixture.addImport(org.junit.BeforeClass.class.getName());
+
+		String original = "" +
+				"	@BeforeClass\n" +
+				"	public void beforeAll() throws Exception {\n" +
+				"		assertEquals(\"1\", \"1\");\n" +
 				"	}";
 
 		assertNoChange(original);
@@ -84,16 +112,103 @@ class MigrateJUnit4ToJupiterASTVisitorNegativeTest extends UsesJDTUnitFixture {
 	})
 	public void visit_unexpectedJUnitTypeReferences_shouldNotTransform(Class<?> clazz) throws Exception {
 
-		defaultFixture.addImport(org.junit.Test.class.getName());
+		defaultFixture.addImport(org.junit.BeforeClass.class.getName());
 
 		String toStringInvocation = clazz.getName() + ".toString();";
 		String original = "" +
-				"	@Test\n" +
-				"	public void test() {\n" +
+				"	@BeforeClass\n" +
+				"	public void beforeClass() {\n" +
 				"	" + toStringInvocation +
 				"	}";
 
 		assertNoChange(original);
+	}
 
+	@Test
+	public void visit_JUnit4TestAnnotation_shouldNotTransform() throws Exception {
+		defaultFixture.addImport(org.junit.Test.class.getName());
+
+		String original = "" +
+				"\n" +
+				"	@Test\n" +
+				"	public void test1() {\n" +
+				"	}\n" +
+				"\n" +
+				"	@Test()\n" +
+				"	public void test2() {\n" +
+				"	}\n" +
+				"\n" +
+				"	@Test(timeout=1000L)\n" +
+				"	public void test3() {\n" +
+				"	}";
+
+		assertNoChange(original);
+
+	}
+
+	@Test
+	public void visit_NotJUnit4TestAnnotation_shouldNotTransform() throws Exception {
+		defaultFixture.addImport(java.lang.annotation.Retention.class.getName());
+		defaultFixture.addImport(java.lang.annotation.RetentionPolicy.class.getName());
+		defaultFixture.addImport(java.lang.annotation.Target.class.getName());
+		defaultFixture.addImport(java.lang.annotation.ElementType.class.getName());
+
+		String original = "" +
+				"\n" +
+				"	@BeforeClass\n" +
+				"	public void beforeAll() {\n" +
+				"	}\n" +
+				"\n" +
+				"	@Retention(RetentionPolicy.RUNTIME)\n"
+				+ "	@Target({ElementType.METHOD})\n"
+				+ "	@interface BeforeClass {\n"
+				+ "		\n"
+				+ "	}";
+
+		assertNoChange(original);
+	}
+
+	/*
+	 * SIM-1873: expected to fail as soon import on demand will be supported
+	 */
+	@Test
+	public void visit_IgnoreImportedOnDemand_shouldNotTransform() throws Exception {
+		defaultFixture.addImport("org.junit", false, true);
+
+		String original = "" +
+				"	@Ignore\n" +
+				"	public void test() {\n" +
+				"	}";
+
+		assertNoChange(original);
+	}
+
+	/*
+	 * SIM-1873: expected to fail as soon qualified annotation names will be
+	 * supported
+	 */
+	@Test
+	public void visit_JUnit4TestAnnotationNotSimpleTypeName_shouldNotTransform() throws Exception {
+
+		String original = "" +
+				"\n" +
+				"	@org.junit.BeforeClass\n" +
+				"	public void beforeAll() {\n" +
+				"	}";
+
+		assertNoChange(original);
+	}
+
+	@Test
+	public void visit_SingleMemberAnnotationRunWith_shouldNotTransform() throws Exception {
+		defaultFixture.addImport(org.junit.runner.RunWith.class.getName());
+		defaultFixture.addImport(org.junit.runners.Parameterized.class.getName());
+
+		String original = "" +
+				"	@RunWith(Parameterized.class)\n" +
+				"	class TestRunningWithParameterized {\n" +
+				"		\n" +
+				"	}";
+		assertNoChange(original);
 	}
 }
--- tests/test/src/test/java/eu/jsparrow/core/visitor/junit/jupiter/MigrateJUnit4ToJupiterASTVisitorTest.java
+++ tests/test/src/test/java/eu/jsparrow/core/visitor/junit/jupiter/MigrateJUnit4ToJupiterASTVisitorTest.java
@@ -1,12 +1,13 @@
 package eu.jsparrow.core.visitor.junit.jupiter;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import eu.jsparrow.core.visitor.impl.UsesJDTUnitFixture;
-
-class MigrateJUnit4ToJupiterASTVisitorTest extends UsesJDTUnitFixture {
+class MigrateJUnit4ToJupiterASTVisitorTest extends AbstractMigrateJUnit4ToJupiterASTVisitorTest {
 
 	@BeforeEach
 	public void setUpVisitor() throws Exception {
@@ -20,63 +21,16 @@ class MigrateJUnit4ToJupiterASTVisitorTest extends UsesJDTUnitFixture {
 	}
 
 	@Test
-	public void visit_JUnit4TestAnnotation__shouldNotTransform() throws Exception {
+	public void visit_MarkerAnnotationTest_shouldTransform() throws Exception {
 		defaultFixture.addImport(org.junit.Test.class.getName());
 
 		String original = "" +
-				"\n" +
 				"	@Test\n" +
-				"	public void test1() {\n" +
-				"	}\n" +
-				"\n" +
-				"	@Test()\n" +
-				"	public void test2() {\n" +
-				"	}\n" +
-				"\n" +
-				"	@Test(timeout=1000L)\n" +
-				"	public void test3() {\n" +
+				"	public void test() {\n" +
 				"	}";
-
-		assertNoChange(original);
+		assertChange(original, original, Arrays.asList("import org.junit.jupiter.api.Test;"));
 	}
 
-	@Test
-	public void visit_JUnit4TestAnnotationNotSimpleTypeName_shouldNotTransform() throws Exception {
-
-		String original = "" +
-				"\n" +
-				"	@org.junit.Test\n" +
-				"	public void test1() {\n" +
-				"	}";
-
-		assertNoChange(original);
-	}
-
-	@Test
-	public void visit_NotJUnit4TestAnnotation__shouldNotTransform() throws Exception {
-		defaultFixture.addImport(java.lang.annotation.Retention.class.getName());
-		defaultFixture.addImport(java.lang.annotation.RetentionPolicy.class.getName());
-		defaultFixture.addImport(java.lang.annotation.Target.class.getName());
-		defaultFixture.addImport(java.lang.annotation.ElementType.class.getName());
-		String original = "" +
-				"\n" +
-				"	@Test\n" +
-				"	public void test1() {\n" +
-				"	}\n" +
-				"\n" +
-				"	@Retention(RetentionPolicy.RUNTIME)\n"
-				+ "	@Target({ElementType.METHOD})\n"
-				+ "	@interface Test {\n"
-				+ "		\n"
-				+ "	}";
-
-		assertNoChange(original);
-	}
-
-	/**
-	 * SIM-1873: Expected to fail as soon as {@link org.junit.Ignore} is
-	 * transformed to {@link org.junit.jupiter.api.Disabled}.
-	 */
 	@Test
 	public void visit_MarkerAnnotationIgnore_shouldTransform() throws Exception {
 		defaultFixture.addImport(org.junit.Ignore.class.getName());
@@ -91,13 +45,9 @@ class MigrateJUnit4ToJupiterASTVisitorTest extends UsesJDTUnitFixture {
 				"	public void test() {\n" +
 				"	}";
 
-		assertChange(original, expected);
+		assertChange(original, expected, Arrays.asList("import org.junit.jupiter.api.Disabled;"));
 	}
 
-	/**
-	 * SIM-1873: Expected to fail as soon as {@link org.junit.Ignore} is
-	 * transformed to {@link org.junit.jupiter.api.Disabled}.
-	 */
 	@Test
 	public void visit_SingleMemberAnnotationIgnore_shouldTransform() throws Exception {
 		defaultFixture.addImport(org.junit.Ignore.class.getName());
@@ -111,20 +61,7 @@ class MigrateJUnit4ToJupiterASTVisitorTest extends UsesJDTUnitFixture {
 				"	@Disabled(\"Test is not carried out.\")\n" +
 				"	public void test() {\n" +
 				"	}";
-		assertChange(original, expected);
-	}
-
-	@Test
-	public void visit_SingleMemberAnnotationRunWith_shouldNotTransform() throws Exception {
-		defaultFixture.addImport(org.junit.runner.RunWith.class.getName());
-		defaultFixture.addImport(org.junit.runners.Parameterized.class.getName());
-
-		String original = "" +
-				"	@RunWith(Parameterized.class)\n" +
-				"	class TestRunningWithParameterized {\n" +
-				"		\n" +
-				"	}";
-		assertNoChange(original);
+		assertChange(original, expected, Arrays.asList("import org.junit.jupiter.api.Disabled;"));
 
 	}
 
@@ -136,6 +73,14 @@ class MigrateJUnit4ToJupiterASTVisitorTest extends UsesJDTUnitFixture {
 		defaultFixture.addImport(org.junit.BeforeClass.class.getName());
 		defaultFixture.addImport(org.junit.Ignore.class.getName());
 		defaultFixture.addImport(org.junit.Test.class.getName());
+		
+		List<String> importsToStringExpected = Arrays.asList(
+				"import " + org.junit.jupiter.api.AfterEach.class.getName() + ";",
+				"import " + org.junit.jupiter.api.AfterAll.class.getName() + ";",
+				"import " + org.junit.jupiter.api.BeforeEach.class.getName() + ";",
+				"import " + org.junit.jupiter.api.BeforeAll.class.getName() + ";",
+				"import " + org.junit.jupiter.api.Disabled.class.getName() + ";",
+				"import " + org.junit.jupiter.api.Test.class.getName() + ";");
 
 		String original = "" +
 				"class TestStub {\n"
@@ -223,18 +168,7 @@ class MigrateJUnit4ToJupiterASTVisitorTest extends UsesJDTUnitFixture {
 				+ "	}\n"
 				+ "}";
 
-		assertChange(original, expected);
+		assertChange(original, expected, importsToStringExpected);
 	}
 
-	@Test
-	public void visit_IgnoreImportedOnDemand_shouldNotTransform() throws Exception {
-		defaultFixture.addImport("org.junit", false, true);
-
-		String original = "" +
-				"	@Ignore\n" +
-				"	public void test() {\n" +
-				"	}";
-
-		assertNoChange(original);
-	}
 }
