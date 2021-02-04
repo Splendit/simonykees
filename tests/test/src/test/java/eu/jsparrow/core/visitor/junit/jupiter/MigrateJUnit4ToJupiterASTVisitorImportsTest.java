8d6447bca7d041d6973ca8c6480d509f3fcb7b98
gregor.balint <gregor.balint@splendit.at>
2021-02-04 19:18:08 +0100

    SIM-1873 ADD test in connection with imports

---
+++ tests/test/src/test/java/eu/jsparrow/core/visitor/junit/jupiter/MigrateJUnit4ToJupiterASTVisitorImportsTest.java
@@ -0,0 +1,114 @@
+package eu.jsparrow.core.visitor.junit.jupiter;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class MigrateJUnit4ToJupiterASTVisitorImportsTest extends AbstractMigrateJUnit4ToJupiterASTVisitorTest {
+
+	@BeforeEach
+	public void setUpVisitor() throws Exception {
+		addDependency("junit", "junit", "4.13");
+		setDefaultVisitor(new MigrateJUnit4ToJupiterASTVisitor());
+	}
+
+	@AfterEach
+	public void tearDown() throws Exception {
+		fixtureProject.clear();
+	}
+
+	@SuppressWarnings("restriction")
+	@ParameterizedTest
+	@ValueSource(classes = {
+			junit.extensions.ActiveTestSuite.class,
+			junit.framework.TestResult.class,
+			junit.runner.BaseTestRunner.class,
+			junit.textui.TestRunner.class,
+			org.junit.ClassRule.class,
+			org.junit.experimental.categories.Category.class,
+			org.junit.experimental.ParallelComputer.class,
+			org.junit.function.ThrowingRunnable.class,
+			org.junit.internal.TextListener.class,
+			org.junit.internal.runners.JUnit38ClassRunner.class,
+			org.junit.internal.builders.AllDefaultPossibilitiesBuilder.class,
+			org.junit.matchers.JUnitMatchers.class,
+			org.junit.rules.DisableOnDebug.class,
+			org.junit.runner.Computer.class,
+			org.junit.runners.AllTests.class,
+			org.junit.validator.AnnotationsValidator.class
+	})
+	public void visit_unexpectedJUnitTypeImports_shouldNotTransform(Class<?> clazz) throws Exception {
+
+		defaultFixture.addImport(org.junit.BeforeClass.class.getName());
+		defaultFixture.addImport(clazz.getName());
+
+		String original = "" +
+				"	@BeforeClass\n" +
+				"	public void beforeClass() {\n" +
+				"	}";
+
+		assertNoChange(original);
+	}
+
+	@SuppressWarnings("restriction")
+	@ParameterizedTest
+	@ValueSource(classes = {
+			junit.extensions.ActiveTestSuite.class,
+			junit.framework.TestResult.class,
+			junit.runner.BaseTestRunner.class,
+			junit.textui.TestRunner.class,
+			org.junit.experimental.categories.Category.class,
+			org.junit.experimental.ParallelComputer.class,
+			org.junit.function.ThrowingRunnable.class,
+			org.junit.internal.TextListener.class,
+			org.junit.internal.runners.JUnit38ClassRunner.class,
+			org.junit.internal.builders.AllDefaultPossibilitiesBuilder.class,
+			org.junit.matchers.JUnitMatchers.class,
+			org.junit.rules.DisableOnDebug.class,
+			org.junit.runner.Computer.class,
+			org.junit.runners.AllTests.class,
+			org.junit.validator.AnnotationsValidator.class
+	})
+	public void visit_unexpectedJUnitImportsOnDemand_shouldNotTransform(Class<?> clazz) throws Exception {
+
+		defaultFixture.addImport(org.junit.BeforeClass.class.getName());
+		defaultFixture.addImport(clazz.getPackage()
+			.getName(), false, true);
+
+		String original = "" +
+				"	@BeforeClass\n" +
+				"	public void beforeClass() {\n" +
+				"	}";
+
+		assertNoChange(original);
+	}
+
+	@Test
+	public void visit_supportedImportsOnDemand_shouldTransform() throws Exception {
+
+		defaultFixture.addImport(org.junit.BeforeClass.class.getName());
+		defaultFixture.addImport("org.junit", false, true);
+
+		String original = "" +
+				"	@BeforeClass\n" +
+				"	public void beforeClass() {\n" +
+				"	}";
+		
+		String expected = "" +
+				"	@BeforeAll\n" +
+				"	public void beforeClass() {\n" +
+				"	}";
+		
+		List<String> importsToStringExpected = Arrays.asList(
+				"import org.junit.*;",
+				"import org.junit.jupiter.api.BeforeAll;");		
+
+		assertChange(original, expected,importsToStringExpected );
+	}
+
+}
--- tests/test/src/test/java/eu/jsparrow/core/visitor/junit/jupiter/MigrateJUnit4ToJupiterASTVisitorNegativeTest.java
+++ tests/test/src/test/java/eu/jsparrow/core/visitor/junit/jupiter/MigrateJUnit4ToJupiterASTVisitorNegativeTest.java
@@ -3,8 +3,6 @@ package eu.jsparrow.core.visitor.junit.jupiter;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
 
 class MigrateJUnit4ToJupiterASTVisitorNegativeTest extends AbstractMigrateJUnit4ToJupiterASTVisitorTest {
 
@@ -32,47 +30,6 @@ class MigrateJUnit4ToJupiterASTVisitorNegativeTest extends AbstractMigrateJUnit4
 				"	public void beforeAll() {\n" +
 				"		Assert.assertEquals(\"1\", \"1\");" +
 				"	}";
-
-		assertNoChange(original);
-	}
-
-	@SuppressWarnings("restriction")
-	@ParameterizedTest
-	@ValueSource(classes = {
-			junit.extensions.ActiveTestSuite.class,
-			junit.framework.TestResult.class,
-			junit.runner.BaseTestRunner.class,
-			junit.textui.TestRunner.class,
-			org.junit.Assert.class,
-			org.junit.experimental.categories.Category.class,
-			org.junit.experimental.ParallelComputer.class,
-			org.junit.function.ThrowingRunnable.class,
-			org.junit.internal.TextListener.class,
-			org.junit.internal.runners.JUnit38ClassRunner.class,
-			org.junit.internal.builders.AllDefaultPossibilitiesBuilder.class,
-			org.junit.matchers.JUnitMatchers.class,
-			org.junit.rules.DisableOnDebug.class,
-			org.junit.runner.Computer.class,
-			org.junit.runners.AllTests.class,
-			org.junit.validator.AnnotationsValidator.class
-	})
-	public void visit_unexpectedJUnitTypeReferences_shouldNotTransform(Class<?> clazz) throws Exception {
-
-		defaultFixture.addImport(org.junit.BeforeClass.class.getName());
-
-		String toStringInvocation = clazz.getName() + ".class.toString();";
-		String original = "" +
-				"	@BeforeClass\n" +
-				"	public void beforeClass() {\n" +
-				"	" + toStringInvocation +
-				"	}";
-
-		// org.junit.Assert.toString();
-		if (clazz.getSimpleName()
-			.equals("Assert")) {
-			toStringInvocation.length();
-			// org.junit.Assert.toString();
-		}
 
 		assertNoChange(original);
 	}
