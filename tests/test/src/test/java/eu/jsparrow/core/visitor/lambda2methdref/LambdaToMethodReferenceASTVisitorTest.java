f1bdd72a3e06f2ad33908b3bccc9e65998d54e9f
Ardit <ardit.ymeri@splendit.at>
2021-02-05 13:52:19 +0100

    SIM-1826 UPDATE merging the visitor tests in one class
    Two test cases that were expecting changes should have actually expected no change

--- tests/test/src/test/java/eu/jsparrow/core/visitor/lambda2methdref/LambdaToMethodReferenceASTVisitorTest.java
+++ tests/test/src/test/java/eu/jsparrow/core/visitor/lambda2methdref/LambdaToMethodReferenceASTVisitorTest.java
@@ -40,7 +40,7 @@ public class LambdaToMethodReferenceASTVisitorTest extends UsesJDTUnitFixture {
 	}
 
 	@Test
-	public void visit_AmbiguousIntegerToString_shouldNotTransform() throws Exception {
+	void visit_AmbiguousIntegerToString_shouldNotTransform() throws Exception {
 
 		defaultFixture.addImport(java.util.function.Function.class.getName());
 
@@ -53,7 +53,7 @@ public class LambdaToMethodReferenceASTVisitorTest extends UsesJDTUnitFixture {
 	}
 
 	@Test
-	public void visit_AmbiguousStaticIntegerToString_shouldNotTransform() throws Exception {
+	void visit_AmbiguousStaticIntegerToString_shouldNotTransform() throws Exception {
 
 		defaultFixture.addImport(java.util.function.Function.class.getName());
 
@@ -66,7 +66,7 @@ public class LambdaToMethodReferenceASTVisitorTest extends UsesJDTUnitFixture {
 	}
 
 	@Test
-	public void visit_AmbiguousTestAmbiguity_shouldNotTransform() throws Exception {
+	void visit_AmbiguousTestAmbiguity_shouldNotTransform() throws Exception {
 
 		defaultFixture.addImport(java.util.function.Function.class.getName());
 
@@ -79,7 +79,7 @@ public class LambdaToMethodReferenceASTVisitorTest extends UsesJDTUnitFixture {
 	}
 
 	@Test
-	public void visit_AmbiguousStaticTestAmbiguity_shouldNotTransform() throws Exception {
+	void visit_AmbiguousStaticTestAmbiguity_shouldNotTransform() throws Exception {
 
 		defaultFixture.addImport(java.util.function.Function.class.getName());
 
@@ -88,6 +88,64 @@ public class LambdaToMethodReferenceASTVisitorTest extends UsesJDTUnitFixture {
 				+ "\n"
 				+ CLASS_AMBIGUOUS_METHODS_DECLARATION;
 
+		assertNoChange(original);
+	}
+	
+	@Test
+	void visit_FunctionOfDequeOfIntegerAsAssignmentLHS_shouldTransform() throws Exception {
+
+		defaultFixture.addImport(java.util.Deque.class.getName());
+		defaultFixture.addImport(java.util.function.Function.class.getName());
+
+		String original = "" +
+				"public void visit_FunctionOfDequeOfIntegerAsAssignmentLHS_shouldTransform() {\n" +
+				"	Function<Deque<Integer>, Integer> dequeToInteger;\n" +
+				"	dequeToInteger = (Deque<Integer> x1) -> x1.getFirst();\n" +
+				"}";
+
+		String expected = "" +
+				"public void visit_FunctionOfDequeOfIntegerAsAssignmentLHS_shouldTransform() {\n" +
+				"	Function<Deque<Integer>, Integer> dequeToInteger;\n" +
+				"	dequeToInteger = Deque<Integer>::getFirst;\n" +
+				"}";
+
+		assertChange(original, expected);
+	}
+	
+	@Test
+	void visit_FunctionOfDequeOfIntegerInitialization_shouldTransform() throws Exception {
+
+		defaultFixture.addImport(java.util.Deque.class.getName());
+		defaultFixture.addImport(java.util.function.Function.class.getName());
+
+		String original = "Function<Deque<Integer>, Integer> dequeToInteger = (Deque<Integer> x1) -> x1.getFirst();";
+		String expected = "Function<Deque<Integer>, Integer> dequeToInteger = Deque<Integer>::getFirst;";
+
+		assertChange(original, expected);
+	}
+
+	@Test
+	void visit_FunctionOfJokerUsingDequeOfIntegerInitialization_shouldNotTransform() throws Exception {
+
+		defaultFixture.addImport(java.util.Deque.class.getName());
+		defaultFixture.addImport(java.util.function.Function.class.getName());
+
+		String original = "Function<?, Integer> dequeToInteger = (Deque<Integer> x1) -> x1.getFirst();";
+
+		assertNoChange(original);
+	}
+
+	@Test
+	void visit_FunctionOfJokerUsingDequeOfIntegerAsAssignmentLHS_shouldNotTransform() throws Exception {
+
+		defaultFixture.addImport(java.util.Deque.class.getName());
+		defaultFixture.addImport(java.util.function.Function.class.getName());
+
+		String original = "" +
+				"public void visit_FunctionOfJokerUsingDequeOfIntegerAsAssignmentLHS_shouldNotTransform() {\n" +
+				"	Function<?, Integer> dequeToInteger;\n" +
+				"	dequeToInteger = (Deque<Integer> x1) -> x1.getFirst();\n" +
+				"}";
 		assertNoChange(original);
 	}
 
