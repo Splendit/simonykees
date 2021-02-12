9a2481d69820f9b0bd8adab7126c7d74cb2ad137
Ardit Ymeri <ardit.ymeri@splendit.at>
2021-02-12 13:40:50 +0100

    SIM-1889 FIX generic functions cannot be transformed to lambdas

--- tests/sample/src/test/java/eu/jsparrow/sample/preRule/TestFunctionalInterfaceRule.java
+++ tests/sample/src/test/java/eu/jsparrow/sample/preRule/TestFunctionalInterfaceRule.java
@@ -440,6 +440,16 @@ public class TestFunctionalInterfaceRule {
 		}; 
 	}
 
+	public void usingFunctionsWithTypeParameters() {
+		// SIM-1889
+		FunctionWithTypeParameters foo = new FunctionWithTypeParameters() {
+			@Override
+			public String foo(int a, String b) {
+				return "";
+			}
+		};
+	}
+
 	private interface AFunctionalInterface {
 		public void method(int a);
 	}
@@ -469,5 +479,12 @@ public class TestFunctionalInterfaceRule {
 	private void sampleMethodAcceptingFunction(GenericFoo foo) {
 		foo.hashCode();
 		// do nothing
+	}
+
+	/**
+	 * SIM-1889
+	 */
+	public interface FunctionWithTypeParameters {
+		<T> T foo (int a, String b);
 	}
 }
