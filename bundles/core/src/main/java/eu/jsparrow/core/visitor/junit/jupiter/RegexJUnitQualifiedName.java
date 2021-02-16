892b6317126cd7d222fea8b1c231d2dacd3934dc
Ardit Ymeri <ardit.ymeri@splendit.at>
2021-02-16 16:53:38 +0100

    SIM-1873 ADD missing javadocs

--- bundles/core/src/main/java/eu/jsparrow/core/visitor/junit/jupiter/RegexJUnitQualifiedName.java
+++ bundles/core/src/main/java/eu/jsparrow/core/visitor/junit/jupiter/RegexJUnitQualifiedName.java
@@ -3,6 +3,13 @@ package eu.jsparrow.core.visitor.junit.jupiter;
 import java.util.function.Predicate;
 import java.util.regex.Pattern;
 
+/**
+ * A wrapper for a REGEX for defining whether a type is declared in
+ * {@code org.junit} package but not in {@code org.junit.jupiter}.
+ * 
+ * @since 3.27.0
+ *
+ */
 class RegexJUnitQualifiedName {
 	private static final String PATTERN_ORG_JUNIT_4_QUALIFIED_NAME = "^(junit|(org\\.junit))(\\..+)?$"; //$NON-NLS-1$
 	private static final String PATTERN_ORG_JUNIT_JUPITER_QUALIFIED_NAME = "^org\\.junit\\.jupiter\\.api(\\..+)?$"; //$NON-NLS-1$
@@ -22,5 +29,8 @@ class RegexJUnitQualifiedName {
 	}
 
 	private RegexJUnitQualifiedName() {
+		/*
+		 * Hide the default constructor.
+		 */
 	}
 }
