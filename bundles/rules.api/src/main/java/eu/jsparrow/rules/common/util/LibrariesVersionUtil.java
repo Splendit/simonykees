8628265db4e50f179b7b4b8340e83cfb5116f5f0
Ardit Ymeri <ardit.ymeri@splendit.at>
2021-02-16 21:59:45 +0100

    SIM-1893 FIX write a utility to validate and compare version libraries

---
+++ bundles/rules.api/src/main/java/eu/jsparrow/rules/common/util/LibrariesVersionUtil.java
@@ -0,0 +1,71 @@
+package eu.jsparrow.rules.common.util;
+
+import java.util.function.Predicate;
+
+import org.osgi.framework.Version;
+
+import eu.jsparrow.rules.common.exception.InvalidLibraryVersionException;
+
+/**
+ * A utility class for parsing and comparing the versions of libraries.
+ * 
+ * @since 3.27.0
+ *
+ */
+public class LibrariesVersionUtil {
+
+	private LibrariesVersionUtil() {
+		/*
+		 * Hide Default constructor.
+		 */
+	}
+
+	/**
+	 * Validates the given version, parses it to a {@link Version}, and verifies
+	 * if the predicate is satisfied
+	 * 
+	 * @param version
+	 *            the version to be verified
+	 * @param condition
+	 *            a predicate to verify if the version is satisfied
+	 * @return if the predicate is satisfied
+	 * @throws InvalidLibraryVersionException
+	 *             if the given version does not match the pattern:
+	 * 
+	 *             <pre>
+	 *             ^\d+(\.\d+){0,3}[-.*]*$
+	 */
+	public static boolean satisfies(String version, Predicate<Version> condition)
+			throws InvalidLibraryVersionException {
+		if (version == null) {
+			throw new InvalidLibraryVersionException("No version provided.");//$NON-NLS-1$
+		}
+		String numericVersion = chopNonNumeric(version);
+		validate(numericVersion);
+		Version actualVersion = Version.parseVersion(numericVersion);
+		return condition.test(actualVersion);
+	}
+
+	private static void validate(String version) throws InvalidLibraryVersionException {
+		if (version.isEmpty()) {
+			String message = "No version provided."; //$NON-NLS-1$
+			throw new InvalidLibraryVersionException(message);
+		}
+
+		boolean isVersionPattern = version.matches("\\d+(\\.\\d+){0,3}"); //$NON-NLS-1$
+		if (!isVersionPattern) {
+			String message = String.format("Invalid library version %s.", version); //$NON-NLS-1$
+			throw new InvalidLibraryVersionException(message);
+		}
+	}
+
+	private static String chopNonNumeric(String version) {
+		boolean hasDash = version.matches("^\\d+[\\.\\d+]*-.*$"); //$NON-NLS-1$
+		if (!hasDash) {
+			return version;
+		}
+		String[] parts = version.split("-"); //$NON-NLS-1$
+		return parts[0];
+	}
+
+}
---
+++ tests/eu.jsparrow.rules.api.test/src/test/java/org/eu/jsparrow/rules/common/util/LibraryVersionUtilTest.java
@@ -0,0 +1,46 @@
+package org.eu.jsparrow.rules.common.util;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.function.Predicate;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.osgi.framework.Version;
+
+import eu.jsparrow.rules.common.exception.InvalidLibraryVersionException;
+import eu.jsparrow.rules.common.util.LibrariesVersionUtil;
+
+public class LibraryVersionUtilTest {
+
+	private Predicate<Version> versionComparator;
+
+	@BeforeEach
+	public void setUp() {
+		versionComparator = version -> version.compareTo(Version.parseVersion("5.0.0")) >= 0;
+	}
+
+	@ParameterizedTest
+	@ValueSource(strings = { "5.7.0-M2", "5", "5.0", "5.0.0", "5.0.0.0" })
+	void validVersions_shouldBeSatisfied(String version) throws InvalidLibraryVersionException {
+		boolean satisfied = LibrariesVersionUtil.satisfies(version, versionComparator);
+		assertTrue(satisfied);
+	}
+
+	@ParameterizedTest
+	@ValueSource(strings = { "4.7.0-M2", "4", "4.0", "4.0.0", "4.0.0.0" })
+	void validVersions_shouldBeNotSatisfied(String version) throws InvalidLibraryVersionException {
+		boolean satisfied = LibrariesVersionUtil.satisfies(version, versionComparator);
+		assertFalse(satisfied);
+	}
+
+	@ParameterizedTest
+	@ValueSource(strings = { "INVALID_VERSION", "5.0.0.0.0", "", "-", "5.3.r" })
+	void invalidVersion_shouldThrowException(String version) {
+		assertThrows(InvalidLibraryVersionException.class,
+				() -> LibrariesVersionUtil.satisfies(version, versionComparator));
+	}
+}
