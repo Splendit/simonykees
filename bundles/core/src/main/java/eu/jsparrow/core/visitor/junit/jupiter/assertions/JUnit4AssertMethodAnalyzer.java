bb42615577c866a77875c7880079a0030c9d3d16
gregor.balint <gregor.balint@splendit.at>
2021-02-24 17:02:01 +0100

    SIM-1892 UPDATE more simple implementation

--- bundles/core/src/main/java/eu/jsparrow/core/visitor/junit/jupiter/assertions/JUnit4AssertMethodAnalyzer.java
+++ bundles/core/src/main/java/eu/jsparrow/core/visitor/junit/jupiter/assertions/JUnit4AssertMethodAnalyzer.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 
 import org.eclipse.jdt.core.dom.Expression;
 import org.eclipse.jdt.core.dom.IMethodBinding;
+import org.eclipse.jdt.core.dom.ITypeBinding;
 import org.eclipse.jdt.core.dom.MethodInvocation;
 
 import eu.jsparrow.rules.common.util.ASTNodeUtil;
@@ -66,6 +67,13 @@ class JUnit4AssertMethodAnalyzer {
 				assertionMessage = arguments.get(0);
 				expected = arguments.get(1);
 				actual = arguments.get(2);
+				if (isAssertEqualsComparingObjectArrays(methodBinding)) {
+					/*
+					 * {@code assertEquals(String, Object[], Object[])}
+					 */
+					return Optional.of(new AssertTransformationData("assertArrayEquals",
+							Arrays.asList(expected, actual), assertionMessage));
+				}
 				return Optional.of(new AssertTransformationData(Arrays.asList(expected, actual), assertionMessage));
 			} else {
 				expected = arguments.get(0);
@@ -78,10 +86,63 @@ class JUnit4AssertMethodAnalyzer {
 		if (arguments.size() == 2) {
 			expected = arguments.get(0);
 			actual = arguments.get(1);
+			if (isAssertEqualsComparingObjectArrays(methodBinding)) {
+				/*
+				 * {@code assertEquals(Object[], Object[])}
+				 */
+				return Optional.of(new AssertTransformationData("assertArrayEquals", Arrays.asList(expected, actual)));
+			}
 			return Optional.of(new AssertTransformationData(Arrays.asList(expected, actual)));
 		}
 
 		return Optional.empty();
+	}
+
+	/**
+	 * This applies to the following signatures:<br>
+	 * {@code assertEquals(Object[], Object[])}
+	 * {@code assertEquals(String, Object[], Object[])} where a corresponding
+	 * method with the name "assertArrayEquals" is available
+	 * 
+	 * @param methodBinding
+	 * @return
+	 */
+	private boolean isAssertEqualsComparingObjectArrays(IMethodBinding methodBinding) {
+		if (!methodBinding.getName()
+			.equals("assertEquals")) {
+			return false;
+		}
+		ITypeBinding[] parameterTypes = methodBinding.getParameterTypes();
+		/*
+		 * applies to {@code assertEquals(Object[], Object[])}
+		 */
+		if (parameterTypes.length == 2) {
+			return parameterTypes[0].getComponentType()
+				.getQualifiedName()
+				.equals("java.lang.Object")
+					&& parameterTypes[0].getDimensions() == 1
+					&& parameterTypes[1].getComponentType()
+						.getQualifiedName()
+						.equals("java.lang.Object")
+					&& parameterTypes[1].getDimensions() == 1;
+		}
+		/*
+		 * applies to {@code assertEquals(String, Object[], Object[])}
+		 */
+		if (parameterTypes.length == 3) {
+			return parameterTypes[0].getComponentType()
+				.getQualifiedName()
+				.equals("java.lang.String")
+					&& parameterTypes[1].getComponentType()
+						.getQualifiedName()
+						.equals("java.lang.Object")
+					&& parameterTypes[1].getDimensions() == 1
+					&& parameterTypes[2].getComponentType()
+						.getQualifiedName()
+						.equals("java.lang.Object")
+					&& parameterTypes[2].getDimensions() == 1;
+		}
+		return false;
 	}
 
 	private Optional<AssertTransformationData> findAssertionOnSingleObject(IMethodBinding methodBinding,
--- bundles/core/src/main/java/eu/jsparrow/core/visitor/junit/jupiter/assertions/JUnit4AssertMethodAnalyzerAlternative1.java
+++ bundles/core/src/main/java/eu/jsparrow/core/visitor/junit/jupiter/assertions/JUnit4AssertMethodAnalyzerAlternative1.java
@@ -1,11 +1,11 @@
 package eu.jsparrow.core.visitor.junit.jupiter.assertions;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
 import org.eclipse.jdt.core.dom.Expression;
 import org.eclipse.jdt.core.dom.IMethodBinding;
+import org.eclipse.jdt.core.dom.ITypeBinding;
 import org.eclipse.jdt.core.dom.MethodInvocation;
 
 import eu.jsparrow.rules.common.util.ASTNodeUtil;
@@ -14,23 +14,39 @@ class JUnit4AssertMethodAnalyzerAlternative1 {
 
 	Optional<AssertTransformationData> findAssertTransformationData(MethodInvocation methodInvocation) {
 		IMethodBinding methodBinding = methodInvocation.resolveMethodBinding();
-		String declaringClassQualifiedName = methodBinding.getDeclaringClass()
-			.getQualifiedName();
-		if (!declaringClassQualifiedName.equals("org.junit.Assert")) { //$NON-NLS-1$
+		if (isSupportedJUnit4Method(methodBinding)) {
 			return Optional.empty();
 		}
+		String newMethodName = isAssertEqualsComparingObjectArrays(methodBinding) ? "assertArrayEquals" : null;
+		
+		methodBinding.getParameterTypes();
+		methodBinding.getMethodDeclaration().getParameterTypes();
 
-		List<Expression> arguments = ASTNodeUtil.convertToTypedList(methodInvocation.arguments(), Expression.class);
+		List<Expression> invocationArguments = ASTNodeUtil.convertToTypedList(methodInvocation.arguments(),
+				Expression.class);
+		Expression assertionMessage = null;
+		if (invocationArguments.size() > 0) {
+			Expression invocationArgument = invocationArguments.get(0);
+			if (invocationArgument.resolveTypeBinding()
+				.getQualifiedName()
+				.equals("java.lang.String")) {
+				assertionMessage = invocationArgument;
+			}
+			invocationArguments.remove(0);
+		}
 
-		Optional<AssertTransformationData> data = findAssertionOnTwoObjects(methodBinding, arguments);
-		if (data.isPresent()) {
-			return data;
+		if (assertionMessage != null) {
+			if (newMethodName != null) {
+				return Optional.of(new AssertTransformationData(newMethodName, invocationArguments, assertionMessage));
+			}
+			return Optional.of(new AssertTransformationData(invocationArguments, assertionMessage));
 		}
-		data = findAssertionOnSingleObject(methodBinding, arguments);
-		if (data.isPresent()) {
-			return data;
+
+		if (newMethodName != null) {
+			return Optional.of(new AssertTransformationData(newMethodName, invocationArguments));
 		}
-		return Optional.empty();
+
+		return Optional.of(new AssertTransformationData(invocationArguments));
 	}
 
 	private boolean isSupportedJUnit4Method(IMethodBinding methodBinding) {
@@ -44,80 +60,51 @@ class JUnit4AssertMethodAnalyzerAlternative1 {
 		return false;
 	}
 
-	private Optional<AssertTransformationData> findAssertionOnTwoObjects(IMethodBinding methodBinding,
-			List<Expression> arguments) {
-		String methodName = methodBinding.getName();
-		if (!methodName.equals("assertArrayEquals")
-				&& !methodName.equals("assertEquals")
-				&& !methodName.equals("assertNotEquals")
-				&& !methodName.equals("assertSame")
-				&& !methodName.equals("assertNotSame")) {
-			return Optional.empty();
+	/**
+	 * This applies to the following signatures:<br>
+	 * {@code assertEquals(Object[], Object[])}
+	 * {@code assertEquals(String, Object[], Object[])} where a corresponding
+	 * method with the name "assertArrayEquals" is available
+	 * 
+	 * @param methodBinding
+	 * @return
+	 */
+	private boolean isAssertEqualsComparingObjectArrays(IMethodBinding methodBinding) {
+		if (!methodBinding.getName()
+			.equals("assertEquals")) {
+			return false;
 		}
-
-		Expression assertionMessage;
-		Expression expected;
-		Expression actual;
-		Expression delta;
-
-		if (arguments.size() == 4) {
-			assertionMessage = arguments.get(0);
-			expected = arguments.get(1);
-			actual = arguments.get(2);
-			delta = arguments.get(3);
-			return Optional.of(new AssertTransformationData(Arrays.asList(expected, actual, delta), assertionMessage));
-		}
-
-		if (arguments.size() == 3) {
-			Expression firstArgument = arguments.get(1);
-			if (firstArgument.resolveTypeBinding()
+		ITypeBinding[] parameterTypes = methodBinding.getParameterTypes();
+		/*
+		 * applies to {@code assertEquals(Object[], Object[])}
+		 */
+		if (parameterTypes.length == 2) {
+			return parameterTypes[0].getComponentType()
 				.getQualifiedName()
-				.equals("java.lang.string")) {
-				assertionMessage = arguments.get(0);
-				expected = arguments.get(1);
-				actual = arguments.get(2);
-				return Optional.of(new AssertTransformationData(Arrays.asList(expected, actual), assertionMessage));
-			} else {
-				expected = arguments.get(0);
-				actual = arguments.get(1);
-				delta = arguments.get(2);
-				return Optional.of(new AssertTransformationData(Arrays.asList(expected, actual, delta)));
-			}
+				.equals("java.lang.Object")
+					&& parameterTypes[0].getDimensions() == 1
+					&& parameterTypes[1].getComponentType()
+						.getQualifiedName()
+						.equals("java.lang.Object")
+					&& parameterTypes[1].getDimensions() == 1;
 		}
-
-		if (arguments.size() == 2) {
-			expected = arguments.get(0);
-			actual = arguments.get(1);
-			return Optional.of(new AssertTransformationData(Arrays.asList(expected, actual)));
+		/*
+		 * applies to {@code assertEquals(String, Object[], Object[])}
+		 */
+		if (parameterTypes.length == 3) {
+			return parameterTypes[0].getComponentType()
+				.getQualifiedName()
+				.equals("java.lang.String")
+					&& parameterTypes[1].getComponentType()
+						.getQualifiedName()
+						.equals("java.lang.Object")
+					&& parameterTypes[1].getDimensions() == 1
+					&& parameterTypes[2].getComponentType()
+						.getQualifiedName()
+						.equals("java.lang.Object")
+					&& parameterTypes[2].getDimensions() == 1;
 		}
-
-		return Optional.empty();
-	}
-
-	private Optional<AssertTransformationData> findAssertionOnSingleObject(IMethodBinding methodBinding,
-			List<Expression> arguments) {
-		String methodName = methodBinding.getName();
-		if (!methodName.equals("assertTrue")
-				&& !methodName.equals("assertFalse")
-				&& !methodName.equals("assertNull")
-				&& !methodName.equals("assertNotNull")) {
-			return Optional.empty();
-		}
-		
-		Expression assertionMessage;
-		Expression assertionArgument;
-
-		if (arguments.size() == 2) {
-			assertionMessage = arguments.get(0);
-			assertionArgument = arguments.get(1);
-			return Optional.of(new AssertTransformationData(Arrays.asList(assertionArgument), assertionMessage));
-		}
-		if (arguments.size() == 1) {
-			assertionArgument = arguments.get(0);
-			return Optional.of(new AssertTransformationData(Arrays.asList(assertionArgument)));
-		}
-
-		return Optional.empty();
+		return false;
 	}
 
 }
