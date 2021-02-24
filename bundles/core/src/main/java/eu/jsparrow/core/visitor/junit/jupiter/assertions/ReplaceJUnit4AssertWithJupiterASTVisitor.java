1870b94c56fef2d62289a149ce15ec9c2f42a1da
gregor.balint <gregor.balint@splendit.at>
2021-02-24 17:27:49 +0100

    SIM-1892 UPDATE decision for JUnit4AssertMethodAnalyzerAlternative1

--- bundles/core/src/main/java/eu/jsparrow/core/visitor/junit/jupiter/assertions/ReplaceJUnit4AssertWithJupiterASTVisitor.java
+++ bundles/core/src/main/java/eu/jsparrow/core/visitor/junit/jupiter/assertions/ReplaceJUnit4AssertWithJupiterASTVisitor.java
@@ -27,7 +27,7 @@ public class ReplaceJUnit4AssertWithJupiterASTVisitor extends AbstractAddImportA
 		}
 		MethodInvocationsCollectorVisitor invocationCollectorVisitor = new MethodInvocationsCollectorVisitor();
 		compilationUnit.accept(invocationCollectorVisitor);
-		JUnit4AssertMethodAnalyzer methodAnalyzer = new JUnit4AssertMethodAnalyzer();
+		JUnit4AssertMethodAnalyzerAlternative1 methodAnalyzer = new JUnit4AssertMethodAnalyzerAlternative1();
 		List<AssertTransformationData> assertTransformationDataList = invocationCollectorVisitor.getMethodInvocations()
 			.stream()
 			.map(methodAnalyzer::findAssertTransformationData)
