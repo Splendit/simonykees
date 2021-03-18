8a91f3765a75a84131425aeaf3d14ec991b7ebef
gregor.balint <gregor.balint@splendit.at>
2021-03-18 16:18:36 +0100

    SIM-1932 ADD wrapper class to store analysis results and JavaDocs

--- bundles/core/src/main/java/eu/jsparrow/core/visitor/junit/jupiter/assertthrows/JUnit4AssertThrowsInvocationAnalyzer.java
+++ bundles/core/src/main/java/eu/jsparrow/core/visitor/junit/jupiter/assertthrows/JUnit4AssertThrowsInvocationAnalyzer.java
@@ -4,6 +4,16 @@ import org.eclipse.jdt.core.dom.ASTNode;
 import org.eclipse.jdt.core.dom.MethodDeclaration;
 import org.eclipse.jdt.core.dom.MethodInvocation;
 
+/**
+ * Helper class analyzing a {@link MethodInvocation}-node . If the
+ * {@link MethodInvocation} represents the invocation of
+ * {@code org.junit.Assert.assertThrows}, then all necessary informations for a
+ * possible transformation are collected in an instance of
+ * {@link JUnit4AssertThrowsInvocationAnalysisResult}.
+ * 
+ * @since 3.29.0
+ *
+ */
 public class JUnit4AssertThrowsInvocationAnalyzer {
 
 	boolean isSurroundedWithLambda(MethodDeclaration methodDeclaration, MethodInvocation methodInvocation) {
