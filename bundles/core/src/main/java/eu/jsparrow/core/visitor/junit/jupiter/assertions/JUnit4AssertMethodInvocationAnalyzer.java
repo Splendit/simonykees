7420e543bde33611b6b365b2ea5df720fd4965b2
gregor.balint <gregor.balint@splendit.at>
2021-03-29 12:23:04 +0200

    SIM-1932 REMOVE MethodInvocationInJUnitJupiterAnalyzer by moving logics

--- bundles/core/src/main/java/eu/jsparrow/core/visitor/junit/jupiter/assertions/JUnit4AssertMethodInvocationAnalyzer.java
+++ bundles/core/src/main/java/eu/jsparrow/core/visitor/junit/jupiter/assertions/JUnit4AssertMethodInvocationAnalyzer.java
@@ -1,20 +1,32 @@
 package eu.jsparrow.core.visitor.junit.jupiter.assertions;
 
+import static eu.jsparrow.core.visitor.junit.jupiter.RegexJUnitQualifiedName.isJUnitJupiterName;
+import static eu.jsparrow.core.visitor.junit.jupiter.RegexJUnitQualifiedName.isJUnitName;
 import static eu.jsparrow.rules.common.util.ClassRelationUtil.isContentOfType;
+import static eu.jsparrow.rules.common.util.ClassRelationUtil.isContentOfTypes;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 import org.eclipse.jdt.core.dom.ASTNode;
+import org.eclipse.jdt.core.dom.Annotation;
+import org.eclipse.jdt.core.dom.BodyDeclaration;
 import org.eclipse.jdt.core.dom.CompilationUnit;
 import org.eclipse.jdt.core.dom.Expression;
+import org.eclipse.jdt.core.dom.IAnnotationBinding;
 import org.eclipse.jdt.core.dom.IMethodBinding;
 import org.eclipse.jdt.core.dom.ITypeBinding;
+import org.eclipse.jdt.core.dom.MethodDeclaration;
 import org.eclipse.jdt.core.dom.MethodInvocation;
 import org.eclipse.jdt.core.dom.SuperMethodInvocation;
+import org.eclipse.jdt.core.dom.TypeDeclaration;
 
+import eu.jsparrow.core.visitor.junit.jupiter.AnnotationCollectorVisitor;
+import eu.jsparrow.core.visitor.junit.jupiter.common.MethodDeclarationsCollectorVisitor;
 import eu.jsparrow.core.visitor.junit.jupiter.common.MethodInvocationsCollectorVisitor;
 import eu.jsparrow.rules.common.util.ASTNodeUtil;
 
@@ -30,14 +42,27 @@ import eu.jsparrow.rules.common.util.ASTNodeUtil;
  */
 class JUnit4AssertMethodInvocationAnalyzer {
 	static final String ASSERT_THROWS = "assertThrows"; //$NON-NLS-1$
+	private static final List<String> ORG_JUNIT_JUPITER_API_ANNOTATIONS = Collections.unmodifiableList(Arrays.asList(
+			"org.junit.jupiter.api.Disabled", //$NON-NLS-1$
+			"org.junit.jupiter.api.Test", //$NON-NLS-1$
+			"org.junit.jupiter.api.AfterEach", //$NON-NLS-1$
+			"org.junit.jupiter.api.AfterAll", //$NON-NLS-1$
+			"org.junit.jupiter.api.BeforeEach", //$NON-NLS-1$
+			"org.junit.jupiter.api.BeforeAll" //$NON-NLS-1$
+	));
 
-	private final MethodInvocationInJUnitJupiterAnalyzer invocationInJUnitJupiterAnalyzer;
+	private final List<MethodDeclaration> jUnitJupiterTestMethods;
 	private final Predicate<String> methodNamePredicate;
 
 	public JUnit4AssertMethodInvocationAnalyzer(CompilationUnit compilationUnit,
 			Predicate<String> methodNamePredicate) {
-		this.invocationInJUnitJupiterAnalyzer = new MethodInvocationInJUnitJupiterAnalyzer(compilationUnit);
 		this.methodNamePredicate = methodNamePredicate;
+		MethodDeclarationsCollectorVisitor methodDeclarationsCollectorVisitor = new MethodDeclarationsCollectorVisitor();
+		compilationUnit.accept(methodDeclarationsCollectorVisitor);
+		jUnitJupiterTestMethods = methodDeclarationsCollectorVisitor.getMethodDeclarations()
+			.stream()
+			.filter(this::isJUnitJupiterTestMethod)
+			.collect(Collectors.toList());
 	}
 
 	List<JUnit4AssertMethodInvocationAnalysisResult> collectJUnit4AssertionAnalysisResults(
@@ -136,7 +161,7 @@ class JUnit4AssertMethodInvocationAnalyzer {
 	}
 
 	boolean isTransformableInvocation(MethodInvocation methodInvocation) {
-		if (!invocationInJUnitJupiterAnalyzer.isWithinJUnitJupiterTest(methodInvocation)) {
+		if (!isWithinJUnitJupiterTest(methodInvocation)) {
 			return false;
 		}
 		List<Expression> arguments = ASTNodeUtil.convertToTypedList(methodInvocation.arguments(), Expression.class);
@@ -155,5 +180,57 @@ class JUnit4AssertMethodInvocationAnalyzer {
 		}
 
 		return unambiguousArgumentTypes;
+	}
+
+	private boolean isJUnitJupiterTestMethod(MethodDeclaration methodDeclaration) {
+		if (methodDeclaration.getLocationInParent() != TypeDeclaration.BODY_DECLARATIONS_PROPERTY) {
+			return false;
+		}
+		TypeDeclaration typeDeclaration = (TypeDeclaration) methodDeclaration.getParent();
+		if (typeDeclaration.isLocalTypeDeclaration()) {
+			return false;
+		}
+		boolean containsJUnitJupiterAnnotation = ASTNodeUtil
+			.convertToTypedList(methodDeclaration.modifiers(), Annotation.class)
+			.stream()
+			.map(Annotation::resolveAnnotationBinding)
+			.map(IAnnotationBinding::getAnnotationType)
+			.anyMatch(typeBinding -> isContentOfTypes(typeBinding, ORG_JUNIT_JUPITER_API_ANNOTATIONS));
+
+		if (!containsJUnitJupiterAnnotation) {
+			return false;
+		}
+		AnnotationCollectorVisitor annotationCollector = new AnnotationCollectorVisitor();
+		methodDeclaration.accept(annotationCollector);
+		return annotationCollector.getAnnotations()
+			.stream()
+			.noneMatch(this::isJUnit4Annotation);
+	}
+
+	private boolean isJUnit4Annotation(Annotation annotation) {
+		IAnnotationBinding resolveAnnotationBinding = annotation.resolveAnnotationBinding();
+		String qualifiedTypeName = resolveAnnotationBinding
+			.getAnnotationType()
+			.getQualifiedName();
+		return isJUnitName(qualifiedTypeName) && !isJUnitJupiterName(qualifiedTypeName);
+	}
+
+	public boolean isWithinJUnitJupiterTest(MethodInvocation methodInvocation) {
+		BodyDeclaration bodyDeclarationAncestor = ASTNodeUtil.getSpecificAncestor(methodInvocation,
+				BodyDeclaration.class);
+		ASTNode parent = methodInvocation.getParent();
+		while (parent != null) {
+			if (parent == bodyDeclarationAncestor) {
+				if (parent.getNodeType() == ASTNode.METHOD_DECLARATION) {
+					return jUnitJupiterTestMethods.contains(parent);
+				}
+				return false;
+			}
+			if (parent.getNodeType() == ASTNode.LAMBDA_EXPRESSION) {
+				return false;
+			}
+			parent = parent.getParent();
+		}
+		return false;
 	}
 }
