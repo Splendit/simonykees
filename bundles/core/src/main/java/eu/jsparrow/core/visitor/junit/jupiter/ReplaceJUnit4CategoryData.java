ba27fd7b7279caffe76d695cb0685ffaecb75f43
gregor.balint <gregor.balint@splendit.at>
2021-04-21 16:12:51 +0200

    SIM-1949 ADD wrapper class with data  for replacement of @Category

---
+++ bundles/core/src/main/java/eu/jsparrow/core/visitor/junit/jupiter/ReplaceJUnit4CategoryData.java
@@ -0,0 +1,37 @@
+package eu.jsparrow.core.visitor.junit.jupiter;
+
+import java.util.List;
+
+import org.eclipse.jdt.core.dom.Annotation;
+import org.eclipse.jdt.core.dom.ChildListPropertyDescriptor;
+
+/**
+ * Stores all informations which are necessary to replace a JUnit4
+ * {@code @Category} annotation with a JUnit Jupiter {@code @Tag} annotation
+ * 
+ *
+ */
+public class ReplaceJUnit4CategoryData {
+	private final Annotation categoryAnnotation;
+	private final List<String> categoryNames;
+	private final ChildListPropertyDescriptor locationInParent;
+
+	public ReplaceJUnit4CategoryData(Annotation categoryAnnotation, List<String> categoryNames,
+			ChildListPropertyDescriptor locationInParent) {
+		this.categoryAnnotation = categoryAnnotation;
+		this.categoryNames = categoryNames;
+		this.locationInParent = locationInParent;
+	}
+
+	public Annotation getCategoryAnnotation() {
+		return categoryAnnotation;
+	}
+
+	public List<String> getCategoryNames() {
+		return categoryNames;
+	}
+
+	public ChildListPropertyDescriptor getLocationInParent() {
+		return locationInParent;
+	}
+}
