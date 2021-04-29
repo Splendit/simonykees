5a4cb4fcf36a868c1a11cb36e503223b303741a6
Ardit Ymeri <ardit.ymeri@splendit.at>
2021-04-29 18:39:17 +0200

    SIM-1874 UPDATE clear editor and cleanups

--- bundles/ui/src/main/java/eu/jsparrow/ui/quickfix/Engine.java
+++ bundles/ui/src/main/java/eu/jsparrow/ui/quickfix/Engine.java
@@ -2,12 +2,9 @@ package eu.jsparrow.ui.quickfix;
 
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.HashSet;
 import java.util.List;
 
 import org.eclipse.core.resources.IResource;
-import org.eclipse.core.runtime.preferences.IEclipsePreferences.IPreferenceChangeListener;
-import org.eclipse.core.runtime.preferences.IEclipsePreferences.PreferenceChangeEvent;
 import org.eclipse.jdt.core.ElementChangedEvent;
 import org.eclipse.jdt.core.ICompilationUnit;
 import org.eclipse.jdt.core.IElementChangedListener;
@@ -21,23 +18,88 @@ import org.eclipse.ui.ide.ResourceUtil;
 import eu.jsparrow.rules.common.EventGenerator;
 import eu.jsparrow.rules.common.MarkerEvent;
 
-public class Engine extends EditorTracker implements IElementChangedListener, IPreferenceChangeListener {
-	
-	private IResource currentResource;
-	private static final Collection<String> JAVA_EXTENSIONS = new HashSet<>(Arrays.asList(JavaCore
-			.getJavaLikeExtensions())); // We only need Java. 
-	private MarkerFactory markerFactory = new MarkerFactory();
+public class Engine extends EditorTracker implements IElementChangedListener {
 
-	@Override
-	public void preferenceChange(PreferenceChangeEvent event) {
-		// TODO Auto-generated method stub
-		
-	}
+	private IResource currentResource;
+	private static final Collection<String> JAVA_EXTENSIONS = Arrays.asList("java"); //$NON-NLS-1$
+	private MarkerFactory markerFactory = new MarkerFactory();
 
 	@Override
 	public void elementChanged(ElementChangedEvent event) {
 		IJavaElementDelta delta = event.getDelta();
 		handleDelta(delta);
+	}
+
+	@Override
+	public void editorActivated(IEditorPart part) {
+		currentResource = getResource(part);
+		checkResource(currentResource);
+	}
+
+	@Override
+	public void editorDeactivated(IEditorPart part) {
+		clearEditor(part);
+		IResource resource = getResource(part);
+		if (resource != null && resource.equals(currentResource)) {
+			setCurrentResource(null);
+		}
+	}
+
+	private boolean isCurrent(IJavaElement element) {
+		IResource resource = element.getResource();
+		if (resource == null) {
+			return false;
+		}
+		return resource.equals(currentResource);
+	}
+
+	private void setCurrentResource(IEditorPart editor) {
+		currentResource = getResource(editor);
+		checkResource(currentResource);
+	}
+
+	private void clearEditor(IEditorPart editor) {
+		IResource resource = getResource(editor);
+		if (shouldProcess(resource)) {
+			MarkerJob job = new MarkerJob(resource, () -> markerFactory.clear(resource));
+			job.schedule();
+		}
+	}
+
+	private void checkResource(IResource resource) {
+		if (shouldProcess(resource)) {
+			checkElement(JavaCore.create(resource));
+		}
+	}
+
+	private IResource getResource(IEditorPart editor) {
+		if (editor == null) {
+			return null;
+		}
+		IEditorInput editorInput = editor.getEditorInput();
+		return ResourceUtil.getResource(editorInput);
+	}
+
+	private boolean shouldProcess(IResource resource) {
+		return resource != null && resource.exists() && IResource.FILE == resource.getType()
+				&& JAVA_EXTENSIONS.contains(resource.getFileExtension());
+	}
+
+	public void checkElement(IJavaElement element) {
+		if (element == null) {
+			return;
+		}
+
+		ICompilationUnit cu = (ICompilationUnit) element.getAncestor(IJavaElement.COMPILATION_UNIT);
+		if (cu == null) {
+			return;
+		}
+
+		if (cu.getOwner() != null) {
+			cu = cu.getPrimary();
+		}
+
+		handleParentSourceReference(cu);
 	}
 
 	private void handleDelta(IJavaElementDelta delta) {
@@ -50,86 +112,17 @@ public class Engine extends EditorTracker implements IElementChangedListener, IP
 		if (isCurrent(element)) {
 			checkElement(element);
 		}
-		
-	}
-
-	private boolean isCurrent(IJavaElement element) {
-		IResource resource = element.getResource();
-		if (resource == null) {
-			return false;
-		}
-		return resource.equals(currentResource);
-	}
-	
-	@Override
-	public void editorActivated(IEditorPart part) {
-		currentResource = getResource(part);
-		checkResource(currentResource);
-		
-	}
-
-	@Override
-	public void editorDeactivated(IEditorPart part) {
-		// TODO Auto-generated method stub
-		
-	}
-	
-	private void checkResource(IResource resource) {
-		if (shouldProcess(resource)) {
-			checkElement(JavaCore.create(resource));
-		}
-	}
-	
-	private IResource getResource(IEditorPart editor) {
-		if (editor == null) {
-			return null;
-		}
-		IEditorInput editorInput = editor.getEditorInput();
-		return ResourceUtil.getResource(editorInput);
-	}
-	
-	private boolean shouldProcess(IResource resource) {
-		return resource != null && resource.exists() && IResource.FILE == resource.getType()
-				&& isJavaResource(resource);
-	}
-	
-	private boolean isJavaResource(IResource resource) {
-		return JAVA_EXTENSIONS.contains(resource.getFileExtension());
-	}
-	
-	public void checkElement(IJavaElement element) {
-		if (element == null) {
-			return;
-		}
-
-		ICompilationUnit cu = (ICompilationUnit) element.getAncestor(IJavaElement.COMPILATION_UNIT);
-
-		if (cu == null) {
-			return;
-		}
-
-		if (cu.getOwner() != null) {
-			cu = cu.getPrimary();
-		}
-
-		handleParentSourceReference(cu);
 	}
 
 	private void handleParentSourceReference(ICompilationUnit cu) {
 		List<MarkerEvent> events = EventGenerator.generateAnonymousClassEvents(cu);
 		final IResource resource = cu.getResource();
-		MarkerJob job = new MarkerJob(resource, new MarkerJob.MarkerRunnable() {
-
-			@Override
-			public void run() {
-				markerFactory.clear(resource);
-				for (MarkerEvent event : events) {
-					markerFactory.create(event);
-				}
+		MarkerJob job = new MarkerJob(resource, () -> {
+			markerFactory.clear(resource);
+			for (MarkerEvent event : events) {
+				markerFactory.create(event);
 			}
 		});
 		job.schedule();
-		
 	}
-
 }
