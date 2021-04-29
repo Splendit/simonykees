55fe897616a4bb247a8fecc9397fe062494a1ecc
Ardit Ymeri <ardit.ymeri@splendit.at>
2021-04-29 09:58:02 +0200

    SIM-1874 ADD dummy quickfix processor

---
+++ bundles/ui/src/main/java/eu/jsparrow/ui/quickfix/JSparrowProposal.java
@@ -0,0 +1,61 @@
+package eu.jsparrow.ui.quickfix;
+
+import org.eclipse.jdt.ui.text.java.IJavaCompletionProposal;
+import org.eclipse.jface.text.IDocument;
+import org.eclipse.jface.text.contentassist.ICompletionProposalExtension6;
+import org.eclipse.jface.text.contentassist.IContextInformation;
+import org.eclipse.jface.viewers.StyledString;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.graphics.Point;
+
+public class JSparrowProposal implements  IJavaCompletionProposal, ICompletionProposalExtension6 {
+
+	@Override
+	public int getRelevance() {
+		
+		return 0;
+	}
+
+	@Override
+	public void apply(IDocument document) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Point getSelection(IDocument document) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getAdditionalProposalInfo() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getDisplayString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Image getImage() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IContextInformation getContextInformation() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public StyledString getStyledDisplayString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
