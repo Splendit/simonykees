fec0a2ea39a87dfabcf203031b9587244fb5c882
Ardit Ymeri <ardit.ymeri@splendit.at>
2021-05-03 18:37:12 +0200

    SIM-1874 UPDATE using IMarkerResolution instead of IQuickFix

--- bundles/ui/src/main/java/eu/jsparrow/ui/quickfix/QuickFix.java
+++ bundles/ui/src/main/java/eu/jsparrow/ui/quickfix/QuickFix.java
@@ -1,21 +1,75 @@
 package eu.jsparrow.ui.quickfix;
 
+import java.net.URL;
+import java.util.HashMap;
+
 import org.eclipse.core.resources.IMarker;
-import org.eclipse.jface.dialogs.MessageDialog;
-import org.eclipse.ui.IMarkerResolution;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.FileLocator;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.jdt.core.ICompilationUnit;
+import org.eclipse.jdt.core.IJavaElement;
+import org.eclipse.jdt.core.JavaCore;
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.ui.IMarkerResolution2;
+import org.osgi.framework.Bundle;
 
-public class QuickFix implements IMarkerResolution {
+import eu.jsparrow.core.markers.EventProducer;
+import eu.jsparrow.rules.common.RefactoringEventProducer;
+import eu.jsparrow.ui.Activator;
 
-    String label;
-    QuickFix(String label) {
-       this.label = label;
-    }
-    public String getLabel() {
-       return label;
-    }
-    public void run(IMarker marker) {
-       MessageDialog.openInformation(null, "QuickFix Demo",
-          "This quick-fix is not yet implemented");
-    }
+public class QuickFix implements IMarkerResolution2 {
+
+	private String label;
+	private int offset;
+	
+	QuickFix(IMarker marker) {
+		this.label = marker.getAttribute(IMarker.MESSAGE, "jSparrow QuickFix");
+		this.offset = marker.getAttribute(IMarker.CHAR_START, 0);
+		
+	}
+
+	@Override
+	public String getLabel() {
+		return label;
+	}
+
+	@Override
+	public void run(IMarker marker) {
+		
+		RefactoringEventProducer eventGenerator = new EventProducer();
+		IResource resource = marker.getResource();
+		IJavaElement element = JavaCore.create(resource);
+		if (element == null) {
+			return;
+		}
+
+		ICompilationUnit icu = (ICompilationUnit) element.getAncestor(IJavaElement.COMPILATION_UNIT);
+		if (icu == null) {
+			return;
+		}
+		Integer offset = marker.getAttribute(IMarker.CHAR_START, this.offset);
+		
+		eventGenerator.resolve(icu, offset);
+	}
+
+	@Override
+	public String getDescription() {
+		// TODO: set a new property in the marker for 
+		return "Issue description: " + label;
+	}
+
+	@Override
+	public Image getImage() {
+		//TODO: make the image static
+		Bundle bundle = Platform.getBundle(Activator.PLUGIN_ID);
+		IPath iPathActive = new Path("icons/jSparrow_active_icon_16.png");
+		URL urlActive = FileLocator.find(bundle, iPathActive, new HashMap<>());
+		ImageDescriptor imageDesc = ImageDescriptor.createFromURL(urlActive);
+		return imageDesc.createImage();
+	}
 
 }
--- bundles/ui/src/main/java/eu/jsparrow/ui/quickfix/QuickFixer.java
+++ bundles/ui/src/main/java/eu/jsparrow/ui/quickfix/QuickFixer.java
@@ -3,22 +3,30 @@ package eu.jsparrow.ui.quickfix;
 import org.eclipse.core.resources.IMarker;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.ui.IMarkerResolution;
-import org.eclipse.ui.IMarkerResolutionGenerator;
+import org.eclipse.ui.IMarkerResolutionGenerator2;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-public class QuickFixer implements IMarkerResolutionGenerator {
+public class QuickFixer implements IMarkerResolutionGenerator2 {
+
+	private static final Logger logger = LoggerFactory.getLogger(QuickFixer.class);
 
 	@Override
 	public IMarkerResolution[] getResolutions(IMarker marker) {
 
+		return new IMarkerResolution[] { new QuickFix(marker) };
+
+	}
+
+	@Override
+	public boolean hasResolutions(IMarker marker) {
 		try {
-			Object problem = marker.getAttribute("WhatsUp");
-			return new IMarkerResolution[] {
-					new QuickFix("Fix #1 for " + problem),
-					new QuickFix("Fix #2 for " + problem),
-			};
+			String markerType = marker.getType();
+			return MarkerFactory.JSPARROW_MARKER.equals(markerType);
 		} catch (CoreException e) {
-			return new IMarkerResolution[0];
+			logger.error("Cannot read marker annotation type", e);
 		}
+		return false;
 	}
 
 }
