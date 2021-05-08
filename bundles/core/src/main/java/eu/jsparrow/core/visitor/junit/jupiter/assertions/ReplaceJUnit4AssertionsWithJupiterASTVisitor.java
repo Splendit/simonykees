c56902b651cb606d0ea39396c13721920dc8149b
gregor.balint <gregor.balint@splendit.at>
2021-05-08 16:18:05 +0200

    SIM-1948 FIX SonarQube issue because inheritance tree is too deep
    Inheritance tree of classes should not be too deep (java:S110)

--- bundles/core/src/main/java/eu/jsparrow/core/visitor/junit/jupiter/assertions/ReplaceJUnit4AssertionsWithJupiterASTVisitor.java
+++ bundles/core/src/main/java/eu/jsparrow/core/visitor/junit/jupiter/assertions/ReplaceJUnit4AssertionsWithJupiterASTVisitor.java
@@ -1,12 +1,30 @@
 package eu.jsparrow.core.visitor.junit.jupiter.assertions;
 
+import static eu.jsparrow.core.visitor.junit.jupiter.assertions.JUnit4MethodInvocationAnalyzer.isDeprecatedAssertEqualsComparingObjectArrays;
+import static eu.jsparrow.core.visitor.junit.jupiter.assertions.JUnit4MethodInvocationAnalyzer.isParameterTypeString;
 import static eu.jsparrow.rules.common.util.ClassRelationUtil.isContentOfType;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
+import java.util.List;
+import java.util.Optional;
 import java.util.Set;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
 
+import org.eclipse.jdt.core.dom.AST;
+import org.eclipse.jdt.core.dom.CompilationUnit;
+import org.eclipse.jdt.core.dom.Expression;
 import org.eclipse.jdt.core.dom.IMethodBinding;
+import org.eclipse.jdt.core.dom.ITypeBinding;
+import org.eclipse.jdt.core.dom.ImportDeclaration;
+import org.eclipse.jdt.core.dom.MethodInvocation;
+import org.eclipse.jdt.core.dom.Name;
+import org.eclipse.jdt.core.dom.SimpleType;
+import org.eclipse.jdt.core.dom.Type;
+
+import eu.jsparrow.rules.common.util.ASTNodeUtil;
 
 /**
  * Replaces invocations of methods of the JUnit-4-class {@code org.junit.Assert}
@@ -16,7 +34,10 @@ import org.eclipse.jdt.core.dom.IMethodBinding;
  * @since 3.28.0
  * 
  */
-public class ReplaceJUnit4AssertionsWithJupiterASTVisitor extends AbstractJUnit4MethodInvocationToJupiterASTVisitor {
+public class ReplaceJUnit4AssertionsWithJupiterASTVisitor extends AbstractReplaceJUnit4MethodInvocationsASTVisitor {
+	
+	private static final String ORG_JUNIT_JUPITER_API_FUNCTION_EXECUTABLE = "org.junit.jupiter.api.function.Executable"; //$NON-NLS-1$
+
 
 	private final Set<String> potentialMethodNameReplacements;
 
@@ -26,6 +47,111 @@ public class ReplaceJUnit4AssertionsWithJupiterASTVisitor extends AbstractJUnit4
 		tmp.add("assertArrayEquals"); //$NON-NLS-1$
 		potentialMethodNameReplacements = Collections.unmodifiableSet(tmp);
 
+	}
+	
+	@Override
+	public boolean visit(CompilationUnit compilationUnit) {
+
+		super.visit(compilationUnit);
+
+		verifyImport(compilationUnit, classDeclaringJUnit4MethodReplacement);
+		verifyImport(compilationUnit, ORG_JUNIT_JUPITER_API_FUNCTION_EXECUTABLE);
+
+		List<JUnit4MethodInvocationAnalysisResult> allSupportedJUnit4InvocationDataList = collectJUnit4MethodInvocationAnalysisResult(
+				compilationUnit);
+
+		List<Type> throwingRunnableTypesToReplace = allSupportedJUnit4InvocationDataList.stream()
+			.map(JUnit4MethodInvocationAnalysisResult::getTypeOfThrowingRunnableToReplace)
+			.filter(Optional::isPresent)
+			.map(Optional::get)
+			.collect(Collectors.toList());
+
+		List<ImportDeclaration> staticMethodImportsToRemove = collectStaticMethodImportsToRemove(compilationUnit,
+				allSupportedJUnit4InvocationDataList);
+
+		Set<String> supportedNewStaticMethodImports = findSupportedStaticImports(staticMethodImportsToRemove,
+				allSupportedJUnit4InvocationDataList);
+
+		List<JUnit4MethodInvocationReplacementData> jUnit4AssertTransformationDataList = allSupportedJUnit4InvocationDataList
+			.stream()
+			.map(data -> this.findTransformationData(data, supportedNewStaticMethodImports))
+			.filter(Optional::isPresent)
+			.map(Optional::get)
+			.collect(Collectors.toList());
+
+		Set<String> newStaticAssertionMethodImports = jUnit4AssertTransformationDataList.stream()
+			.map(JUnit4MethodInvocationReplacementData::getStaticMethodImport)
+			.filter(Optional::isPresent)
+			.map(Optional::get)
+			.collect(Collectors.toSet());
+
+		transform(staticMethodImportsToRemove, newStaticAssertionMethodImports, jUnit4AssertTransformationDataList);
+
+		AST ast = astRewrite.getAST();
+		throwingRunnableTypesToReplace.forEach(typeToReplace -> {
+			Name executableTypeName = addImport(ORG_JUNIT_JUPITER_API_FUNCTION_EXECUTABLE, typeToReplace);
+			SimpleType typeReplacement = ast.newSimpleType(executableTypeName);
+			astRewrite.replace(typeToReplace, typeReplacement, null);
+		});
+
+		return false;
+	}
+
+	private Optional<JUnit4MethodInvocationReplacementData> findTransformationData(
+			JUnit4MethodInvocationAnalysisResult invocationData,
+			Set<String> supportedNewStaticMethodImports) {
+
+		if (!invocationData.isTransformable()) {
+			return Optional.empty();
+		}
+		MethodInvocation methodInvocation = invocationData.getMethodInvocation();
+		IMethodBinding originalMethodBinding = invocationData.getMethodBinding();
+		String originalMethodName = originalMethodBinding.getName();
+
+		ITypeBinding[] declaredParameterTypes = originalMethodBinding
+			.getMethodDeclaration()
+			.getParameterTypes();
+		String newMethodName;
+		if (isDeprecatedAssertEqualsComparingObjectArrays(originalMethodName, declaredParameterTypes)) {
+			newMethodName = "assertArrayEquals"; //$NON-NLS-1$
+		} else {
+			newMethodName = originalMethodName;
+		}
+
+		List<Expression> originalArguments = ASTNodeUtil.convertToTypedList(methodInvocation.arguments(),
+				Expression.class);
+
+		boolean messageMovingToLastPosition = declaredParameterTypes.length > 0
+				&& isParameterTypeString(declaredParameterTypes[0]);
+
+		List<Expression> newArguments;
+		if (messageMovingToLastPosition && originalArguments.size() > 1) {
+			newArguments = new ArrayList<>();
+			Expression messageArgument = originalArguments.remove(0);
+			newArguments.addAll(originalArguments);
+			newArguments.add(messageArgument);
+		} else {
+			newArguments = originalArguments;
+		}
+
+		String newMethodStaticImport = classDeclaringJUnit4MethodReplacement + "." + newMethodName; //$NON-NLS-1$
+		if (supportedNewStaticMethodImports.contains(newMethodStaticImport)) {
+			if (methodInvocation.getExpression() == null
+					&& newArguments == originalArguments
+					&& newMethodName.equals(originalMethodName)) {
+				return Optional.of(new JUnit4MethodInvocationReplacementData(methodInvocation, newMethodStaticImport));
+			}
+			Supplier<List<Expression>> newArgumentsSupplier = () -> createNewMethodArguments(newArguments);
+			return Optional.of(new JUnit4MethodInvocationReplacementData(methodInvocation,
+					() -> createNewInvocationWithoutQualifier(newMethodName, newArgumentsSupplier),
+					newMethodStaticImport));
+		}
+		Supplier<List<Expression>> newArgumentsSupplier = () -> createNewMethodArguments(newArguments);
+		Supplier<MethodInvocation> newMethodInvocationSupplier = () -> createNewInvocationWithQualifier(
+				methodInvocation,
+				newMethodName, newArgumentsSupplier);
+
+		return Optional.of(new JUnit4MethodInvocationReplacementData(methodInvocation, newMethodInvocationSupplier));
 	}
 
 	@Override
--- bundles/core/src/main/java/eu/jsparrow/core/visitor/junit/jupiter/assertions/ReplaceJUnit4AssumptionsWithJupiterASTVisitor.java
+++ bundles/core/src/main/java/eu/jsparrow/core/visitor/junit/jupiter/assertions/ReplaceJUnit4AssumptionsWithJupiterASTVisitor.java
@@ -1,11 +1,25 @@
 package eu.jsparrow.core.visitor.junit.jupiter.assertions;
 
+import static eu.jsparrow.core.visitor.junit.jupiter.assertions.JUnit4MethodInvocationAnalyzer.isDeprecatedAssertEqualsComparingObjectArrays;
+import static eu.jsparrow.core.visitor.junit.jupiter.assertions.JUnit4MethodInvocationAnalyzer.isParameterTypeString;
 import static eu.jsparrow.rules.common.util.ClassRelationUtil.isContentOfType;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
 import java.util.Set;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
 
+import org.eclipse.jdt.core.dom.CompilationUnit;
+import org.eclipse.jdt.core.dom.Expression;
 import org.eclipse.jdt.core.dom.IMethodBinding;
+import org.eclipse.jdt.core.dom.ITypeBinding;
+import org.eclipse.jdt.core.dom.ImportDeclaration;
+import org.eclipse.jdt.core.dom.MethodInvocation;
+
+import eu.jsparrow.rules.common.util.ASTNodeUtil;
 
 /**
  * Replaces the JUnit 4 method invocations {@code org.junit.Assume.assumeFalse}
@@ -15,10 +29,100 @@ import org.eclipse.jdt.core.dom.IMethodBinding;
  * @since 3.30.0
  * 
  */
-public class ReplaceJUnit4AssumptionsWithJupiterASTVisitor extends AbstractJUnit4MethodInvocationToJupiterASTVisitor {
+public class ReplaceJUnit4AssumptionsWithJupiterASTVisitor extends AbstractReplaceJUnit4MethodInvocationsASTVisitor {
 
 	public ReplaceJUnit4AssumptionsWithJupiterASTVisitor() {
 		super(ORG_J_UNIT_JUPITER_API_ASSUMPTIONS);
+	}
+
+	@Override
+	public boolean visit(CompilationUnit compilationUnit) {
+
+		super.visit(compilationUnit);
+
+		verifyImport(compilationUnit, classDeclaringJUnit4MethodReplacement);
+
+		List<JUnit4MethodInvocationAnalysisResult> allSupportedJUnit4InvocationDataList = collectJUnit4MethodInvocationAnalysisResult(
+				compilationUnit);
+
+		List<ImportDeclaration> staticMethodImportsToRemove = collectStaticMethodImportsToRemove(compilationUnit,
+				allSupportedJUnit4InvocationDataList);
+
+		Set<String> supportedNewStaticMethodImports = findSupportedStaticImports(staticMethodImportsToRemove,
+				allSupportedJUnit4InvocationDataList);
+
+		List<JUnit4MethodInvocationReplacementData> jUnit4AssertTransformationDataList = allSupportedJUnit4InvocationDataList
+			.stream()
+			.map(data -> this.findTransformationData(data, supportedNewStaticMethodImports))
+			.filter(Optional::isPresent)
+			.map(Optional::get)
+			.collect(Collectors.toList());
+
+		Set<String> newStaticAssertionMethodImports = jUnit4AssertTransformationDataList.stream()
+			.map(JUnit4MethodInvocationReplacementData::getStaticMethodImport)
+			.filter(Optional::isPresent)
+			.map(Optional::get)
+			.collect(Collectors.toSet());
+
+		transform(staticMethodImportsToRemove, newStaticAssertionMethodImports, jUnit4AssertTransformationDataList);
+		return false;
+	}
+
+	private Optional<JUnit4MethodInvocationReplacementData> findTransformationData(
+			JUnit4MethodInvocationAnalysisResult invocationData,
+			Set<String> supportedNewStaticMethodImports) {
+
+		if (!invocationData.isTransformable()) {
+			return Optional.empty();
+		}
+		MethodInvocation methodInvocation = invocationData.getMethodInvocation();
+		IMethodBinding originalMethodBinding = invocationData.getMethodBinding();
+		String originalMethodName = originalMethodBinding.getName();
+
+		ITypeBinding[] declaredParameterTypes = originalMethodBinding
+			.getMethodDeclaration()
+			.getParameterTypes();
+		String newMethodName;
+		if (isDeprecatedAssertEqualsComparingObjectArrays(originalMethodName, declaredParameterTypes)) {
+			newMethodName = "assertArrayEquals"; //$NON-NLS-1$
+		} else {
+			newMethodName = originalMethodName;
+		}
+
+		List<Expression> originalArguments = ASTNodeUtil.convertToTypedList(methodInvocation.arguments(),
+				Expression.class);
+
+		boolean messageMovingToLastPosition = declaredParameterTypes.length > 0
+				&& isParameterTypeString(declaredParameterTypes[0]);
+
+		List<Expression> newArguments;
+		if (messageMovingToLastPosition && originalArguments.size() > 1) {
+			newArguments = new ArrayList<>();
+			Expression messageArgument = originalArguments.remove(0);
+			newArguments.addAll(originalArguments);
+			newArguments.add(messageArgument);
+		} else {
+			newArguments = originalArguments;
+		}
+
+		String newMethodStaticImport = classDeclaringJUnit4MethodReplacement + "." + newMethodName; //$NON-NLS-1$
+		if (supportedNewStaticMethodImports.contains(newMethodStaticImport)) {
+			if (methodInvocation.getExpression() == null
+					&& newArguments == originalArguments
+					&& newMethodName.equals(originalMethodName)) {
+				return Optional.of(new JUnit4MethodInvocationReplacementData(methodInvocation, newMethodStaticImport));
+			}
+			Supplier<List<Expression>> newArgumentsSupplier = () -> createNewMethodArguments(newArguments);
+			return Optional.of(new JUnit4MethodInvocationReplacementData(methodInvocation,
+					() -> createNewInvocationWithoutQualifier(newMethodName, newArgumentsSupplier),
+					newMethodStaticImport));
+		}
+		Supplier<List<Expression>> newArgumentsSupplier = () -> createNewMethodArguments(newArguments);
+		Supplier<MethodInvocation> newMethodInvocationSupplier = () -> createNewInvocationWithQualifier(
+				methodInvocation,
+				newMethodName, newArgumentsSupplier);
+
+		return Optional.of(new JUnit4MethodInvocationReplacementData(methodInvocation, newMethodInvocationSupplier));
 	}
 
 	@Override
