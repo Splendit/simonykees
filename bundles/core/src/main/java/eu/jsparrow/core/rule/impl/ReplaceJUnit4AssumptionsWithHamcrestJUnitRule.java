89ff0726f53f7bae9f6e03e53f5bf78be72728e9
gregor.balint <gregor.balint@splendit.at>
2021-05-31 20:24:22 +0200

    SIM-1948 UPDATE corrections in connection with the requirements
    ReplaceJUnit4AssumptionsWithHamcrestJUnitRule requires Java 5, org.hamcrest.junit.MatcherAssume and org.hamcrest.CoreMatchers

--- bundles/core/src/main/java/eu/jsparrow/core/rule/impl/ReplaceJUnit4AssumptionsWithHamcrestJUnitRule.java
+++ bundles/core/src/main/java/eu/jsparrow/core/rule/impl/ReplaceJUnit4AssumptionsWithHamcrestJUnitRule.java
@@ -23,9 +23,10 @@ import eu.jsparrow.rules.common.Tag;
 public class ReplaceJUnit4AssumptionsWithHamcrestJUnitRule
 		extends RefactoringRuleImpl<ReplaceJUnit4AssumptionsWithHamcrestJUnitASTVisitor> {
 
+	private static final String ORG_HAMCREST_CORE_MATCHERS = "org.hamcrest.CoreMatchers"; //$NON-NLS-1$
+	private static final String MIN_HAMCREST_VERSION = "1.3"; //$NON-NLS-1$
+
 	private static final String ORG_HAMCREST_JUNIT_JUPITER_MATCHER_ASSUME = "org.hamcrest.junit.MatcherAssume"; //$NON-NLS-1$
-	private static final String ORG_JUNIT_ASSUME = "org.junit.Assume"; //$NON-NLS-1$
-	private static final String MIN_JUNIT_4_VERSION = "4.13"; //$NON-NLS-1$
 	private static final String MIN_HAMCREST_JUNIT_VERSION = "1.0.0.0"; //$NON-NLS-1$
 
 	public ReplaceJUnit4AssumptionsWithHamcrestJUnitRule() {
@@ -34,32 +35,33 @@ public class ReplaceJUnit4AssumptionsWithHamcrestJUnitRule
 		this.ruleDescription = new RuleDescription(
 				Messages.ReplaceJUnit4AssumptionsWithHamcrestJUnitRule_name,
 				Messages.ReplaceJUnit4AssumptionsWithHamcrestJUnitRule_description,
-				Duration.ofMinutes(2), Arrays.asList(Tag.JAVA_1_8, Tag.TESTING));
+				Duration.ofMinutes(2), Arrays.asList(Tag.JAVA_1_5, Tag.TESTING));
 	}
 
 	@Override
 	protected String provideRequiredJavaVersion() {
 		/*
-		 * Hamcrest JUnit Requires Java 8.
+		 * Hamcrest JUnit Requires Java 5.
 		 * 
 		 */
-		return JavaCore.VERSION_1_8;
+		return JavaCore.VERSION_1_5;
 	}
 
 	@Override
 	public String requiredLibraries() {
-		return "JUnit 4.13 and Hamcrest JUnit 1.0"; //$NON-NLS-1$
+		return "Hamcrest 1.3 and Hamcrest JUnit 1.0"; //$NON-NLS-1$
 	}
 
 	@Override
 	public boolean ruleSpecificImplementation(IJavaProject project) {
-		Predicate<Version> jupiterVersionComparator = version -> version
+		Predicate<Version> hamcrestJUnitVersionComparator = version -> version
 			.compareTo(Version.parseVersion(MIN_HAMCREST_JUNIT_VERSION)) >= 0;
-		Predicate<Version> junitVersionComparator = version -> version
-			.compareTo(Version.parseVersion(MIN_JUNIT_4_VERSION)) >= 0;
 
-		return isInProjectLibraries(project, ORG_HAMCREST_JUNIT_JUPITER_MATCHER_ASSUME, jupiterVersionComparator)
-				&& isInProjectLibraries(project, ORG_JUNIT_ASSUME, junitVersionComparator);
+		Predicate<Version> hamcrestVersionComparator = version -> version
+			.compareTo(Version.parseVersion(MIN_HAMCREST_VERSION)) >= 0;
+
+		return isInProjectLibraries(project, ORG_HAMCREST_JUNIT_JUPITER_MATCHER_ASSUME, hamcrestJUnitVersionComparator)
+				&& isInProjectLibraries(project, ORG_HAMCREST_CORE_MATCHERS, hamcrestVersionComparator);
 
 	}
 }
