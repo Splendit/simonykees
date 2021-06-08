5a605de73e17a72050c77bf3119441390211a3da
gregor.balint <gregor.balint@splendit.at>
2021-06-08 23:17:40 +0200

    SIM-1965 ADD negative test cases

--- tests/test/src/test/java/eu/jsparrow/core/visitor/junit/dedicated/UseDedicatedAssertionsNegativeASTVisitorTest.java
+++ tests/test/src/test/java/eu/jsparrow/core/visitor/junit/dedicated/UseDedicatedAssertionsNegativeASTVisitorTest.java
@@ -10,8 +10,51 @@ class UseDedicatedAssertionsNegativeASTVisitorTest extends UsesSimpleJDTUnitFixt
 	@BeforeEach
 	public void setUpVisitor() throws Exception {
 		addDependency("junit", "junit", "4.13");
-		addDependency("org.junit.jupiter", "junit-jupiter-api", "5.0.0");
 		setVisitor(new UseDedicatedAssertionsASTVisitor());
+	}
+
+	@Test
+	void visit_NotResolvedMethod_shouldNotTransform() throws Exception {
+
+		String original = ""
+				+ "		Object a = new Object();\n"
+				+ "		Object b = a;\n"
+				+ "		assertTrue(a == b);";
+
+		assertNoChange(original);
+	}
+
+	@Test
+	void visit_AssertTrueNotDeclaredInJUnit_shouldNotTransform() throws Exception {
+
+		fixture.addMethod("assertTrue");
+
+		String original = "assertTrue();";
+
+		assertNoChange(original);
+	}
+
+	@Test
+	void visit_NotSupportedAssertionMethodName_shouldNotTransform() throws Exception {
+		fixture.addImport("org.junit.Assert.assertEquals", true, false);
+
+		String original = ""
+				+ "		Object a = new Object();\n"
+				+ "		Object b = a;\n"
+				+ "		assertEquals(a,b);";
+
+		assertNoChange(original);
+	}
+
+	@Test
+	void visit_AssertTrueOnBooleanVariable_shouldNotTransform() throws Exception {
+		fixture.addImport("org.junit.Assert.assertTrue", true, false);
+
+		String original = ""
+				+ "		boolean condition = true;\n"
+				+ "		assertTrue(condition);";
+
+		assertNoChange(original);
 	}
 
 	@Test
@@ -32,6 +75,97 @@ class UseDedicatedAssertionsNegativeASTVisitorTest extends UsesSimpleJDTUnitFixt
 		String original = ""
 				+ "		double doubleValue = 100.0;\n"
 				+ "		assertTrue(doubleValue == 100);";
+
+		assertNoChange(original);
+	}
+
+	@Test
+	void visit_AssertionWithEqualsWithoutExpression_shouldNotTransform() throws Exception {
+		fixture.addImport("org.junit.Assert.assertTrue", true, false);
+
+		String original = ""
+				+ "		class LocalClass {\n"
+				+ "			public boolean equals(Object o) {\n"
+				+ "				return false;\n"
+				+ "			}\n"
+				+ "\n"
+				+ "			void test() {\n"
+				+ "				Object o = new Object();\n"
+				+ "				assertTrue(equals(o));\n"
+				+ "			}\n"
+				+ "		}";
+
+		assertNoChange(original);
+	}
+
+	@Test
+	void visit_AssertionWithEqualsWithoutArgument_shouldNotTransform() throws Exception {
+		fixture.addImport("org.junit.Assert.assertTrue", true, false);
+
+		String original = ""
+				+ "		class LocalClass {\n"
+				+ "			public boolean equals() {\n"
+				+ "				return false;\n"
+				+ "			}\n"
+				+ "\n"
+				+ "			void test() {\n"
+				+ "				Object o = new Object();\n"
+				+ "				assertTrue(this.equals());\n"
+				+ "			}\n"
+				+ "		}";
+
+		assertNoChange(original);
+	}
+
+	@Test
+	void visit_AssertionWithNotSupportedMethod_shouldNotTransform() throws Exception {
+		fixture.addImport("org.junit.Assert.assertTrue", true, false);
+
+		String original = ""
+				+ "		class LocalClass {\n"
+				+ "			public boolean isEqual(Object o) {\n"
+				+ "				return false;\n"
+				+ "			}\n"
+				+ "\n"
+				+ "			void test() {\n"
+				+ "				Object o = new Object();\n"
+				+ "				assertTrue(this.isEqual(o));\n"
+				+ "			}\n"
+				+ "		}";
+
+		assertNoChange(original);
+	}
+
+	@Test
+	void visit_AssertionWithInfixAnd_shouldNotTransform() throws Exception {
+		fixture.addImport("org.junit.Assert.assertTrue", true, false);
+
+		String original = ""
+				+ "		boolean condition1 = true;\n"
+				+ "		boolean condition2 = true;\n"
+				+ "		assertTrue(condition1 && condition2);";
+
+		assertNoChange(original);
+	}
+	
+	
+	@Test
+	void visit_IntegerObjectInfixEqualsPrimitiveInt_shouldNotTransform() throws Exception {
+		fixture.addImport("org.junit.Assert.assertTrue", true, false);
+
+		String original = ""
+				+ "		Integer integerZero = Integer.valueOf(0);\n"
+				+ "		int intZero = 0;\n"
+				+ "		assertTrue(integerZero == intZero);";
+
+		assertNoChange(original);
+	}
+	
+	@Test
+	void visit_UnresolvedEquals_shouldNotTransform() throws Exception {
+		fixture.addImport("org.junit.Assert.assertTrue", true, false);
+
+		String original = "assertTrue(a.equals(a));";
 
 		assertNoChange(original);
 	}
