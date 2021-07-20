7600baa30c8df97d0fc161f34ac074e4db215f8c
Ardit Ymeri <ardit.ymeri@splendit.at>
2021-07-20 16:31:30 +0200

    SIM-1984 UPDATE remove unused import

--- bundles/rules.java16/src/main/java/eu/jsparrow/rules/java16/UsePatternMatchingForInstanceofASTVisitor.java
+++ bundles/rules.java16/src/main/java/eu/jsparrow/rules/java16/UsePatternMatchingForInstanceofASTVisitor.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.eclipse.jdt.core.dom.AST;
 import org.eclipse.jdt.core.dom.ASTNode;
-import org.eclipse.jdt.core.dom.ASTRequestor;
 import org.eclipse.jdt.core.dom.Block;
 import org.eclipse.jdt.core.dom.Expression;
 import org.eclipse.jdt.core.dom.IfStatement;
