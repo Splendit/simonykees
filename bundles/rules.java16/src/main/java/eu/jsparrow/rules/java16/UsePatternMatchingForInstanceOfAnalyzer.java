6a72e8a11a55c6538ac9058e805e6a6eb9c17ee6
gregor.balint <gregor.balint@splendit.at>
2021-08-12 15:39:48 +0200

    SIM-1985 UPDATE rearrange logics
    for example, structural check before check of type bindings and extraction of logics to separate methods

--- bundles/rules.java16/src/main/java/eu/jsparrow/rules/java16/UsePatternMatchingForInstanceOfAnalyzer.java
+++ bundles/rules.java16/src/main/java/eu/jsparrow/rules/java16/UsePatternMatchingForInstanceOfAnalyzer.java
@@ -2,6 +2,7 @@ package eu.jsparrow.rules.java16;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
 import org.eclipse.jdt.core.dom.ASTNode;
 import org.eclipse.jdt.core.dom.Block;
@@ -12,7 +13,6 @@ import org.eclipse.jdt.core.dom.IfStatement;
 import org.eclipse.jdt.core.dom.InstanceofExpression;
 import org.eclipse.jdt.core.dom.SimpleName;
 import org.eclipse.jdt.core.dom.Statement;
-import org.eclipse.jdt.core.dom.Type;
 import org.eclipse.jdt.core.dom.VariableDeclarationFragment;
 import org.eclipse.jdt.core.dom.VariableDeclarationStatement;
 
@@ -30,63 +30,83 @@ class UsePatternMatchingForInstanceOfAnalyzer {
 	}
 
 	boolean analyzeInstanceOfExpression() {
-		if (instanceOf.getLocationInParent() == IfStatement.EXPRESSION_PROPERTY) {
-			Expression instanceOfLeftOperand = instanceOf.getLeftOperand();
-			Type instanceOfRightOperand = instanceOf.getRightOperand();
-			String leftOperandTypeQualifiedName = instanceOfLeftOperand.resolveTypeBinding()
-				.getErasure()
-				.getQualifiedName();
-			ITypeBinding rightOperandTypeBinding = instanceOfRightOperand.resolveBinding();
 
-			if (!ClassRelationUtil.isInheritingContentOfTypes(rightOperandTypeBinding,
-					Collections.singletonList(leftOperandTypeQualifiedName))) {
-				return false;
-			}
-
-			IfStatement ifStatement = (IfStatement) instanceOf.getParent();
-			Statement thenStatement = ifStatement.getThenStatement();
-			if (thenStatement.getNodeType() != ASTNode.BLOCK) {
-				return false;
-			}
-			Block block = (Block) thenStatement;
-			List<Statement> statements = ASTNodeUtil.convertToTypedList(block.statements(), Statement.class);
-			if (statements.isEmpty()) {
-				return false;
-			}
-			Statement first = statements.get(0);
-			if (first.getNodeType() != ASTNode.VARIABLE_DECLARATION_STATEMENT) {
-				return false;
-			}
-			VariableDeclarationStatement varDecl = (VariableDeclarationStatement) first;
-			VariableDeclarationFragment fragment = (VariableDeclarationFragment) varDecl.fragments()
-				.get(0);
-
-			Expression initializer = fragment.getInitializer();
-			if (initializer == null || initializer.getNodeType() != ASTNode.CAST_EXPRESSION) {
-				return false;
-			}
-
-			if (!ClassRelationUtil.compareITypeBinding(rightOperandTypeBinding, initializer.resolveTypeBinding())) {
-				return false;
-			}
-
-			CastExpression castExpression = (CastExpression) initializer;
-			Expression castOperand = castExpression.getExpression();
-
-			ExpressionMatcher expressionMatcher = new ExpressionMatcher();
-			if (!expressionMatcher.match(castOperand, instanceOfLeftOperand)) {
-				return false;
-			}
-
-			if (varDecl.fragments()
-				.size() > 1) {
-				declarationNodeToRemove = fragment;
-			} else {
-				declarationNodeToRemove = varDecl;
-			}			
-			patternInstanceOfName = fragment.getName();
+		if (instanceOf.getLocationInParent() != IfStatement.EXPRESSION_PROPERTY) {
+			return false;
 		}
+
+		VariableDeclarationStatement varDecl = findVariableDeclarationStatementInThenBlock(
+				(IfStatement) instanceOf.getParent()).orElse(null);
+		if (varDecl == null) {
+			return false;
+		}
+
+		Expression instanceOfLeftOperand = instanceOf.getLeftOperand();
+		String leftOperandTypeQualifiedName = instanceOfLeftOperand.resolveTypeBinding()
+			.getErasure()
+			.getQualifiedName();
+		ITypeBinding rightOperandTypeBinding = instanceOf.getRightOperand().resolveBinding();
+
+		if (!ClassRelationUtil.isInheritingContentOfTypes(rightOperandTypeBinding,
+				Collections.singletonList(leftOperandTypeQualifiedName))) {
+			return false;
+		}
+
+		VariableDeclarationFragment fragment = (VariableDeclarationFragment) varDecl.fragments()
+			.get(0);
+
+		if (!isFragmentWithExpectedTypeCastInitializer(fragment, instanceOfLeftOperand, rightOperandTypeBinding)) {
+			return false;
+		}
+
+		if (varDecl.fragments()
+			.size() > 1) {
+			declarationNodeToRemove = fragment;
+		} else {
+			declarationNodeToRemove = varDecl;
+		}
+		patternInstanceOfName = fragment.getName();
+
 		return true;
+	}
+
+	private Optional<VariableDeclarationStatement> findVariableDeclarationStatementInThenBlock(
+			IfStatement ifStatement) {
+
+		Statement thenStatement = ifStatement.getThenStatement();
+		if (thenStatement.getNodeType() != ASTNode.BLOCK) {
+			return Optional.empty();
+		}
+		Block block = (Block) thenStatement;
+		List<Statement> statements = ASTNodeUtil.convertToTypedList(block.statements(), Statement.class);
+		if (statements.isEmpty()) {
+			return Optional.empty();
+		}
+		Statement first = statements.get(0);
+		if (first.getNodeType() != ASTNode.VARIABLE_DECLARATION_STATEMENT) {
+			return Optional.empty();
+		}
+
+		return Optional.of((VariableDeclarationStatement) first);
+	}
+
+	private boolean isFragmentWithExpectedTypeCastInitializer(VariableDeclarationFragment fragment, Expression instanceOfLeftOperand,
+			ITypeBinding expectedTypeBinding) {
+		Expression initializer = fragment.getInitializer();
+		if (initializer == null || initializer.getNodeType() != ASTNode.CAST_EXPRESSION) {
+			return false;
+		}
+
+		CastExpression castExpression = (CastExpression) initializer;
+		Expression castOperand = castExpression.getExpression();
+
+		ExpressionMatcher expressionMatcher = new ExpressionMatcher();
+		if (!expressionMatcher.match(castOperand, instanceOfLeftOperand)) {
+			return false;
+		}
+
+		return ClassRelationUtil.compareITypeBinding(expectedTypeBinding, initializer.resolveTypeBinding());
+
 	}
 
 	public InstanceofExpression getInstanceOf() {
