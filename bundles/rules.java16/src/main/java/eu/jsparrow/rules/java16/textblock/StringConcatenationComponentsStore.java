2e7ace76a120fecaa8a833313a2554dc9369eb6f
gregor.balint <gregor.balint@splendit.at>
2021-08-25 12:55:04 +0200

    SIM-1990 consider primitive type literals

--- bundles/rules.java16/src/main/java/eu/jsparrow/rules/java16/textblock/StringConcatenationComponentsStore.java
+++ bundles/rules.java16/src/main/java/eu/jsparrow/rules/java16/textblock/StringConcatenationComponentsStore.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.eclipse.jdt.core.dom.ASTNode;
 import org.eclipse.jdt.core.dom.Expression;
 import org.eclipse.jdt.core.dom.InfixExpression;
+import org.eclipse.jdt.core.dom.ParenthesizedExpression;
 
 import eu.jsparrow.rules.common.util.ASTNodeUtil;
 
@@ -21,34 +22,39 @@ import eu.jsparrow.rules.common.util.ASTNodeUtil;
  */
 public class StringConcatenationComponentsStore {
 
-	private List<Expression> components = new ArrayList<>();
-
 	/**
 	 * Recursive Method dividing a {@link String} concatenation expression into
 	 * its components and storing them in a list.
 	 * 
-	 * @param initializer
+	 * @param unwrappedExpression
 	 *            expected to be a {@link String} concatenation expression.
 	 */
-	public void storeComponents(Expression initializer) {
-		if (initializer.getNodeType() == ASTNode.INFIX_EXPRESSION) {
-			InfixExpression infixExpression = (InfixExpression) initializer;
+	public List<Expression> collectComponents(Expression expression) {
+		Expression unwrappedExpression = removeSurroundingParenthesis(expression);
+		List<Expression> componentList = new ArrayList<>();
+
+		if (unwrappedExpression.getNodeType() == ASTNode.INFIX_EXPRESSION) {
+			InfixExpression infixExpression = (InfixExpression) unwrappedExpression;
 			Expression left = infixExpression.getLeftOperand();
-			storeComponents(left);
+			componentList.addAll(collectComponents(left));
 			Expression right = infixExpression.getRightOperand();
-			storeComponents(right);
+			componentList.addAll(collectComponents(right));
 			if (infixExpression.hasExtendedOperands()) {
 				List<Expression> extendedOperands = ASTNodeUtil
 					.convertToTypedList(infixExpression.extendedOperands(), Expression.class);
-				extendedOperands.forEach(this::storeComponents);
+				extendedOperands.forEach(operand -> componentList.addAll(collectComponents(operand)));
 			}
 		} else {
-			components.add(initializer);
+			componentList.add(unwrappedExpression);
 		}
+		return componentList;
 	}
 
-	public List<Expression> getComponents() {
-		return components;
+	private Expression removeSurroundingParenthesis(Expression expression) {
+		if (expression.getNodeType() == ASTNode.PARENTHESIZED_EXPRESSION) {
+			ParenthesizedExpression parenthesizedExpression = (ParenthesizedExpression) expression;
+			return removeSurroundingParenthesis(parenthesizedExpression.getExpression());
+		}
+		return expression;
 	}
-
 }
--- bundles/rules.java16/src/main/java/eu/jsparrow/rules/java16/textblock/UseTextBlockASTVisitor.java
+++ bundles/rules.java16/src/main/java/eu/jsparrow/rules/java16/textblock/UseTextBlockASTVisitor.java
@@ -1,6 +1,14 @@
 package eu.jsparrow.rules.java16.textblock;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.eclipse.jdt.core.dom.ASTNode;
+import org.eclipse.jdt.core.dom.BooleanLiteral;
+import org.eclipse.jdt.core.dom.CharacterLiteral;
+import org.eclipse.jdt.core.dom.Expression;
 import org.eclipse.jdt.core.dom.InfixExpression;
+import org.eclipse.jdt.core.dom.NumberLiteral;
 import org.eclipse.jdt.core.dom.StringLiteral;
 import org.eclipse.jdt.core.dom.TextBlock;
 
@@ -47,29 +55,52 @@ public class UseTextBlockASTVisitor extends AbstractASTRewriteASTVisitor {
 	 */
 	@Override
 	public boolean visit(InfixExpression infixExpresssion) {
+
 		StringConcatenationComponentsStore componentStore = new StringConcatenationComponentsStore();
 
-		componentStore.storeComponents(infixExpresssion);
+		List<Expression> components = componentStore.collectComponents(infixExpresssion);
+		String textBlockContent = findValidTextBlockContent(components).orElse(null);
 
-		StringBuilder sb = new StringBuilder();
-		sb.append(TEXT_BLOCK_TRIPLE_QUOTES);
-		sb.append('\n');
-		componentStore.getComponents()
-			.stream()
-			.filter(StringLiteral.class::isInstance)
-			.map(StringLiteral.class::cast)
-			.map(StringLiteral::getLiteralValue)
-			.forEach(sb::append);
-		sb.append(TEXT_BLOCK_TRIPLE_QUOTES);
+		if (textBlockContent == null) {
+			return true;
+		}
 
 		TextBlock textBlock = astRewrite.getAST()
 			.newTextBlock();
-		String escapedValue = sb.toString();
 
-		textBlock.setEscapedValue(escapedValue);
+		textBlock.setEscapedValue(textBlockContent);
 
 		astRewrite.replace(infixExpresssion, textBlock, null);
 		onRewrite();
 		return false;
 	}
+
+	private Optional<String> findValidTextBlockContent(List<Expression> components) {
+
+		StringBuilder sb = new StringBuilder();
+		sb.append(TEXT_BLOCK_TRIPLE_QUOTES);
+		sb.append('\n');
+
+		for (Expression component : components) {
+			if (component.getNodeType() == ASTNode.STRING_LITERAL) {
+				StringLiteral stringLiteral = (StringLiteral) component;
+				sb.append(stringLiteral.getLiteralValue());
+			} else if (component.getNodeType() == ASTNode.NUMBER_LITERAL) {
+				NumberLiteral numberLiteral = (NumberLiteral) component;
+				sb.append(numberLiteral.getToken());
+			} else if (component.getNodeType() == ASTNode.CHARACTER_LITERAL) {
+				CharacterLiteral characterLiteral = (CharacterLiteral) component;
+				sb.append(characterLiteral.charValue());
+			} else if (component.getNodeType() == ASTNode.BOOLEAN_LITERAL) {
+				BooleanLiteral booleanLiteral = (BooleanLiteral) component;
+				sb.append(booleanLiteral.booleanValue());
+			} else if (component.getNodeType() == ASTNode.NULL_LITERAL) {
+				sb.append("null"); //$NON-NLS-1$
+			}
+		}
+
+		sb.append(TEXT_BLOCK_TRIPLE_QUOTES);
+		return Optional.of(sb.toString());
+	}
+
 }
