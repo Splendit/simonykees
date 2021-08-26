533967d283df7ac7868d3250f5bcb577c8e982a0
Ardit Ymeri <ardit.ymeri@splendit.at>
2021-08-26 18:46:15 +0200

    SIM-1458 ADD visitor searching for unexpected switch-case breaks

---
+++ bundles/rules.java16/src/main/java/eu/jsparrow/rules/java16/switchexpression/SwitchCaseBreakStatementsVisitor.java
@@ -0,0 +1,62 @@
+package eu.jsparrow.rules.java16.switchexpression;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.jdt.core.dom.ASTVisitor;
+import org.eclipse.jdt.core.dom.BreakStatement;
+import org.eclipse.jdt.core.dom.DoStatement;
+import org.eclipse.jdt.core.dom.EnhancedForStatement;
+import org.eclipse.jdt.core.dom.ForStatement;
+import org.eclipse.jdt.core.dom.SwitchExpression;
+import org.eclipse.jdt.core.dom.WhileStatement;
+import org.eclipse.jdt.core.dom.SwitchStatement;
+
+public class SwitchCaseBreakStatementsVisitor extends ASTVisitor {
+
+	private List<BreakStatement> breakStatements = new ArrayList<>();
+	
+	@Override
+	public boolean visit(SwitchStatement switchStatement) {
+		return false;
+	}
+	
+	@Override
+	public boolean visit(SwitchExpression switchExpression) {
+		return false;
+	}
+	
+	@Override
+	public boolean visit(WhileStatement whileStatement) {
+		return false;
+	}
+	
+	@Override
+	public boolean visit(ForStatement forStatement) {
+		return false;
+	}
+	
+	@Override
+	public boolean visit(EnhancedForStatement forStatement) {
+		return false;
+	}
+	
+	@Override
+	public boolean visit(DoStatement doStatement) {
+		return false;
+	}
+	
+	@Override
+	public boolean visit(BreakStatement breakStatement) {
+		breakStatements.add(breakStatement);
+		return true;
+	}
+	
+	public List<BreakStatement> getBreakStatements() {
+		return this.breakStatements;
+	}
+	
+	public boolean hasBreakStatements() {
+		return !this.breakStatements.isEmpty();
+	}
+}
