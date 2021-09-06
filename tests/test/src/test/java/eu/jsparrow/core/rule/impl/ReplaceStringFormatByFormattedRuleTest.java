a989978c31f45bff2603288a4d8594ca6bc2088e
gregor.balint <gregor.balint@splendit.at>
2021-09-06 15:59:47 +0200

    SIM-1998 ADD ReplaceStringFormatByFormattedRuleTest

---
+++ tests/test/src/test/java/eu/jsparrow/core/rule/impl/ReplaceStringFormatByFormattedRuleTest.java
@@ -0,0 +1,49 @@
+package eu.jsparrow.core.rule.impl;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.equalTo;
+
+import java.time.Duration;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import eu.jsparrow.core.SingleRuleTest;
+import eu.jsparrow.core.util.RulesTestUtil;
+import eu.jsparrow.rules.common.RuleDescription;
+import eu.jsparrow.rules.common.Tag;
+import eu.jsparrow.rules.java16.ReplaceStringFormatByFormattedRule;
+
+class ReplaceStringFormatByFormattedRuleTest extends SingleRuleTest {
+
+	private ReplaceStringFormatByFormattedRule rule;
+
+	@BeforeEach
+	public void setUp() throws Exception {
+		rule = new ReplaceStringFormatByFormattedRule();
+		testProject = RulesTestUtil.createJavaProject("javaVersionTestProject", "bin");
+	}
+
+	@Test
+	void test_ruleId() {
+		String ruleId = rule.getId();
+		assertThat(ruleId, equalTo("ReplaceStringFormatByFormatted"));
+	}
+
+	@Test
+	void test_ruleDescription() {
+		RuleDescription description = rule.getRuleDescription();
+		assertThat(description.getName(), equalTo("Replace String.format by String.formatted"));
+		assertThat(description.getTags(),
+				contains(Tag.JAVA_15, Tag.OLD_LANGUAGE_CONSTRUCTS, Tag.READABILITY));
+		assertThat(description.getRemediationCost(), equalTo(Duration.ofMinutes(2)));
+		assertThat(description.getDescription(),
+				equalTo("This rule replaces invocations of the static method String.format(String, Object...) by invocations of the Java 15 instance method String.formatted(Object...). This way, readability of code is improved."));
+	}
+
+	@Test
+	void test_requiredJavaVersion() throws Exception {
+		assertThat(rule.getRequiredJavaVersion(), equalTo("15"));
+	}
+}
