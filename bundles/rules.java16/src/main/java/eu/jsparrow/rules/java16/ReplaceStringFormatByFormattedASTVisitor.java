a35ed4354016460fe2f6a03066e3052532433809
gregor.balint <gregor.balint@splendit.at>
2021-09-06 12:31:28 +0200

    SIM-1998 UPDATE implementation of visit method

--- bundles/rules.java16/src/main/java/eu/jsparrow/rules/java16/ReplaceStringFormatByFormattedASTVisitor.java
+++ bundles/rules.java16/src/main/java/eu/jsparrow/rules/java16/ReplaceStringFormatByFormattedASTVisitor.java
@@ -1,13 +1,25 @@
 package eu.jsparrow.rules.java16;
 
-import org.eclipse.jdt.core.dom.MethodInvocation;
+import java.util.Collections;
+import java.util.List;
 
+import org.eclipse.jdt.core.dom.AST;
+import org.eclipse.jdt.core.dom.Expression;
+import org.eclipse.jdt.core.dom.IMethodBinding;
+import org.eclipse.jdt.core.dom.ITypeBinding;
+import org.eclipse.jdt.core.dom.MethodInvocation;
+import org.eclipse.jdt.core.dom.SimpleName;
+import org.eclipse.jdt.core.dom.rewrite.ListRewrite;
+
+import eu.jsparrow.rules.common.util.ASTNodeUtil;
+import eu.jsparrow.rules.common.util.ClassRelationUtil;
 import eu.jsparrow.rules.common.visitor.AbstractASTRewriteASTVisitor;
 
 /**
  * This visitor looks for {@link MethodInvocation}-nodes which represent
- * invocations of the method {@link String#format(String, Object...)} and replaces them by
- * invocations of the Java 15 method {@code String#formatted(Object...)}
+ * invocations of the method {@link String#format(String, Object...)} and
+ * replaces them by invocations of the Java 15 method
+ * {@code String#formatted(Object...)}
  * 
  * Example:
  * 
@@ -32,6 +44,62 @@ public class ReplaceStringFormatByFormattedASTVisitor extends AbstractASTRewrite
 	@Override
 	public boolean visit(MethodInvocation invocation) {
 
+		List<Expression> stringFormatArguments = findStringFormatArguments(invocation);
+		if (!stringFormatArguments.isEmpty()) {
+			AST ast = astRewrite.getAST();
+			MethodInvocation formattedMethodInvocation = ast.newMethodInvocation();
+			SimpleName formattedMethodName = ast.newSimpleName("formatted"); //$NON-NLS-1$
+			formattedMethodInvocation.setName(formattedMethodName);
+
+			Expression firstArgument = stringFormatArguments.get(0);
+			Expression stringInstanceExpression = (Expression) astRewrite.createMoveTarget(firstArgument);
+			// astRewrite.remove(firstArgument, null); // ??
+
+			formattedMethodInvocation.setExpression(stringInstanceExpression);
+			int formatArgumentsSsize = stringFormatArguments.size();
+			if (formatArgumentsSsize > 1) {
+				ListRewrite listRewrite = astRewrite.getListRewrite(formattedMethodInvocation,
+						MethodInvocation.ARGUMENTS_PROPERTY);
+
+				for (int i = 1; i < formatArgumentsSsize; i++) {
+					Expression objectArgument = stringFormatArguments.get(i);
+					Expression movedObjectArgument = (Expression) astRewrite.createMoveTarget(objectArgument);
+					// astRewrite.remove(objectArgument, null); // ??
+					listRewrite.insertLast(movedObjectArgument, null);
+				}
+			}
+			astRewrite.replace(invocation, formattedMethodInvocation, null);
+			onRewrite();
+		}
 		return true;
+	}
+
+	private List<Expression> findStringFormatArguments(MethodInvocation invocation) {
+		IMethodBinding methodBinding = invocation.resolveMethodBinding();
+		if (methodBinding == null) {
+			return Collections.emptyList();
+		}
+
+		if (!"format".equals(methodBinding.getName())) { //$NON-NLS-1$
+			return Collections.emptyList();
+		}
+
+		if (!ClassRelationUtil.isContentOfType(methodBinding.getDeclaringClass(), String.class.getName())) {
+			return Collections.emptyList();
+		}
+
+		IMethodBinding methodDeclaration = methodBinding.getMethodDeclaration();
+
+		ITypeBinding[] parameterTypes = methodDeclaration.getParameterTypes();
+
+		if (parameterTypes.length != 2) {
+			return Collections.emptyList();
+		}
+
+		if (!ClassRelationUtil.isContentOfType(parameterTypes[0], String.class.getName())) {
+			return Collections.emptyList();
+		}
+
+		return ASTNodeUtil.convertToTypedList(invocation.arguments(), Expression.class);
 	}
 }
--- tests/test/src/test/java/eu/jsparrow/core/visitor/impl/ReplaceStringFormatByFormattedASTVisitorTest.java
+++ tests/test/src/test/java/eu/jsparrow/core/visitor/impl/ReplaceStringFormatByFormattedASTVisitorTest.java
@@ -15,17 +15,86 @@ public class ReplaceStringFormatByFormattedASTVisitorTest extends UsesSimpleJDTU
 		setVisitor(new ReplaceStringFormatByFormattedASTVisitor());
 	}
 
-	/**
-	 * SIM-1998: this test will fail as soon as
-	 * {@link ReplaceStringFormatByFormattedASTVisitor} is implemented.
-	 * 
-	 */
 	@Test
-	public void visit_StringFormatInvocation_notTransformingYet() throws Exception {
+	public void visit_StringFormatInvocation_shouldTransform() throws Exception {
 		String original = "" +
-				"		String output = String.format(\n" +
-				"				\"Name: %s, Phone: %s, Address: %s, Salary: $%.2f\",\n" +
-				"				name, phone, address, salary);";
+				"		String name = \"<name>\";\n"
+				+ "		String phone = \"<phone>\";\n"
+				+ "		String address = \"<address>\";\n"
+				+ "		float salary = 10000.0f;\n"
+				+ "\n"
+				+ "		String output = String.format(\n"
+				+ "				\"Name: %s, Phone: %s, Address: %s, Salary: $%.2f\",\n"
+				+ "				name, phone, address, salary);";
+
+		String expected = "" +
+				"		String name = \"<name>\";\n"
+				+ "		String phone = \"<phone>\";\n"
+				+ "		String address = \"<address>\";\n"
+				+ "		float salary = 10000.0f;\n"
+				+ "\n"
+				+ "		String output = \"Name: %s, Phone: %s, Address: %s, Salary: $%.2f\".formatted(name, phone, address, salary);\n"
+				+ "";
+
+		assertChange(original, expected);
+	}
+
+	@Test
+	public void visit_NoObjectArguments_shouldTransform() throws Exception {
+		String original = "String output = String.format(\"line-1 %nline-2 %nline-3\");";
+		String expected = "String output = \"line-1 %nline-2 %nline-3\".formatted();";
+
+		assertChange(original, expected);
+	}
+
+	@Test
+	public void visit_MethodBindingNotResolved_shouldNotTransform() throws Exception {
+		String original = "String output = format(\"line-1 %nline-2 %nline-3\");";
+
+		assertNoChange(original);
+	}
+
+	@Test
+	public void visit_MethodNameFormatted_shouldNotTransform() throws Exception {
+		String original = ""
+				+ "		String output = \"line-1 %nline-2 %nline-3\";\n"
+				+ "		output = output.formatted();";
+
+		assertNoChange(original);
+	}
+
+	@Test
+	public void visit_LocaleAsFirstArgument_shouldNotTransform() throws Exception {
+		fixture.addImport(java.util.Locale.class.getName());
+		String original = "" +
+				"		String name = \"<name>\";\n"
+				+ "		String phone = \"<phone>\";\n"
+				+ "		String address = \"<address>\";\n"
+				+ "		float salary = 10000.0f;\n"
+				+ "\n"
+				+ "		String output = String.format(\n"
+				+ "				Locale.ENGLISH,\n"
+				+ "				\"Name: %s, Phone: %s, Address: %s, Salary: $%.2f\",\n"
+				+ "				name, phone, address, salary);";
+
+		assertNoChange(original);
+	}
+
+	@Test
+	public void visit_FormatIsNoStringMethod_shouldNotTransform() throws Exception {
+		fixture.addMethodDeclarationFromString(""
+				+ "	String format(String format, Object... args) {\n"
+				+ "		return \"\";\n"
+				+ "	}");
+		String original = "" +
+				"		String name = \"<name>\";\n"
+				+ "		String phone = \"<phone>\";\n"
+				+ "		String address = \"<address>\";\n"
+				+ "		float salary = 10000.0f;\n"
+				+ "\n"
+				+ "		String output = format(\"Name: %s, Phone: %s, Address: %s, Salary: $%.2f\", name, phone,\n"
+				+ "				address, salary);";
+
 		assertNoChange(original);
 	}
 
@@ -41,7 +110,7 @@ public class ReplaceStringFormatByFormattedASTVisitorTest extends UsesSimpleJDTU
 	// }
 
 	// @Test
-	// public void visit_CastingOtherVariable_shouldNotTransform() throws
+	// public void visit__shouldNotTransform() throws
 	// Exception {
 	// String original = "" +
 	// "";
