06cd7bac456de44e3073ae40d65d4b4ccd155919
Ardit Ymeri <ardit.ymeri@splendit.at>
2021-09-07 16:42:59 +0200

    SIM-1458 UPDATE removing unused code and empty lines

--- bundles/i18n/src/main/java/eu/jsparrow/i18n/Messages.java
+++ bundles/i18n/src/main/java/eu/jsparrow/i18n/Messages.java
@@ -140,9 +140,7 @@ public class Messages extends NLS {
 	public static String EnhancedForLoopToStreamForEachRule_description;
 	public static String EnhancedForLoopToStreamForEachRule_name;
 	public static String EnumsWithoutEqualsResolver_message;
-
 	public static String EnumsWithoutEqualsResolver_name;
-
 	public static String EnumsWithoutEqualsRule_name;
 	public static String EnumsWithoutEqualsRule_description;
 	public static String EnhancedForLoopToStreamSumRule_description;
@@ -192,9 +190,7 @@ public class Messages extends NLS {
 	public static String ForToForEachRule_description;
 	public static String ForToForEachRule_name;
 	public static String FunctionalInterfaceResolver_message;
-
 	public static String FunctionalInterfaceResolver_name;
-
 	public static String FunctionalInterfaceRule_description;
 	public static String FunctionalInterfaceRule_name;
 	public static String GuardConditionRule_description;
@@ -206,9 +202,7 @@ public class Messages extends NLS {
 	public static String LambdaForEachMapRule_description;
 	public static String LambdaForEachMapRule_name;
 	public static String LambdaToMethodReferenceResolver_message;
-
 	public static String LambdaToMethodReferenceResolver_name;
-
 	public static String LambdaToMethodReferenceRule_description;
 	public static String LambdaToMethodReferenceRule_name;
 	public static String LicenseManager_cannot_reach_licensing_provider_on_checkin;
@@ -287,9 +281,7 @@ public class Messages extends NLS {
 	public static String IndexOfToContainsRule_description;
 	public static String IndexOfToContainsRule_name;
 	public static String InefficientConstructorResolver_message;
-
 	public static String InefficientConstructorResolver_name;
-
 	public static String InefficientConstructorRule_description;
 	public static String InefficientConstructorRule_name;
 	public static String InsertBreakStatementInLoopsRule_description;
@@ -314,9 +306,7 @@ public class Messages extends NLS {
 	public static String OverrideAnnotationRule_description;
 	public static String OverrideAnnotationRule_name;
 	public static String PrimitiveBoxedForStringResolver_message;
-
 	public static String PrimitiveBoxedForStringResolver_name;
-
 	public static String PrimitiveBoxedForStringRule_description;
 	public static String PrimitiveBoxedForStringRule_name;
 	public static String PrimitiveObjectUseEqualsRule_description;
@@ -411,9 +401,7 @@ public class Messages extends NLS {
 	public static String RemoveNewStringConstructorRule_description;
 	public static String RemoveNewStringConstructorRule_name;
 	public static String RemoveNullCheckBeforeInstanceofResolver_message;
-
 	public static String RemoveNullCheckBeforeInstanceofResolver_name;
-
 	public static String RemoveNullCheckBeforeInstanceofRule_description;
 	public static String RemoveNullCheckBeforeInstanceofRule_name;
 	public static String RemoveRedundantTypeCastRule_description;
@@ -568,9 +556,7 @@ public class Messages extends NLS {
 	public static String StringConcatToPlusRule_description;
 	public static String StringConcatToPlusRule_name;
 	public static String StringLiteralEqualityCheckResolver_message;
-
 	public static String StringLiteralEqualityCheckResolver_name;
-
 	public static String StringLiteralEqualityCheckRule_description;
 	public static String StringLiteralEqualityCheckRule_name;
 	public static String StringUtilsRule_classNotInClassPath;
@@ -602,9 +588,7 @@ public class Messages extends NLS {
 	public static String UseCollectionsSingletonListRule_description;
 	public static String UseCollectionsSingletonListRule_name;
 	public static String UseComparatorMethodsResolver_message;
-
 	public static String UseComparatorMethodsResolver_name;
-
 	public static String UseComparatorMethodsRule_description;
 	public static String UseComparatorMethodsRule_name;
 	public static String UseDedicatedAssertionsRule_description;
@@ -616,9 +600,7 @@ public class Messages extends NLS {
 	public static String UseFilesWriteStringRule_description;
 	public static String UseFilesWriteStringRule_name;
 	public static String UseIsEmptyOnCollectionsResolver_message;
-
 	public static String UseIsEmptyOnCollectionsResolver_name;
-
 	public static String UseIsEmptyOnCollectionsRule_description;
 	public static String UseIsEmptyOnCollectionsRule_name;
 	public static String UseListSortRule_description;
@@ -638,9 +620,7 @@ public class Messages extends NLS {
 	public static String UseStringJoinRule_description;
 	public static String UseStringJoinRule_name;
 	public static String UseSwitchExpressionRule_description;
-
 	public static String UseSwitchExpressionRule_name;
-
 	public static String ValidateExecutor_shutting_down_validation_scheduler;
 	public static String ValidateExecutor_validation_scheduler_started;
 	public static String WelcomeEditor_title;
@@ -656,9 +636,7 @@ public class Messages extends NLS {
 	public static String WhileToForEachRule_name;
 	public static String WizardMessageDialog_selectionDidNotContainAnyJavaFiles;
 	public static String PutIfAbsentResolver_message;
-
 	public static String PutIfAbsentResolver_name;
-
 	public static String PutIfAbsentRule_description;
 	public static String PutIfAbsentRule_name;
 	public static String YAMLConfigurationWrapper_configurationFileNotFoundAtPath;
