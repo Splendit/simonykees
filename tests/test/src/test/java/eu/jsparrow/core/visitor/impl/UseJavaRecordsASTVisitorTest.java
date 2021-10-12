0b20fd119dafcd7f5d82805bf2c73a9a83531e1a
gregor.balint <gregor.balint@splendit.at>
2021-10-12 17:30:36 +0200

    SIM-2017 UPDATE workaround asserting sting equality
    ASTMatcher#match(RecordDeclaration, Object) causes java.lang.ClassCastException

--- tests/test/src/test/java/eu/jsparrow/core/visitor/impl/UseJavaRecordsASTVisitorTest.java
+++ tests/test/src/test/java/eu/jsparrow/core/visitor/impl/UseJavaRecordsASTVisitorTest.java
@@ -1,10 +1,18 @@
 package eu.jsparrow.core.visitor.impl;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.eclipse.jdt.core.JavaCore;
+import org.eclipse.jdt.core.JavaModelException;
+import org.eclipse.jdt.core.dom.TypeDeclaration;
+import org.eclipse.jface.text.BadLocationException;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import eu.jsparrow.jdtunit.JdtUnitException;
+import eu.jsparrow.jdtunit.util.ASTNodeBuilder;
+import eu.jsparrow.rules.common.visitor.AbstractASTRewriteASTVisitor;
 import eu.jsparrow.rules.java16.javarecords.UseJavaRecordsASTVisitor;
 
 @SuppressWarnings("nls")
@@ -21,52 +29,53 @@ public class UseJavaRecordsASTVisitorTest extends UsesJDTUnitFixture {
 		fixtureProject.clear();
 	}
 
-	/**
-	 * SIM-2017: This test is expected to fail as soon as
-	 * {@link UseJavaRecordsASTVisitor#visit(org.eclipse.jdt.core.dom.TypeDeclaration)}
-	 * is implemented.
-	 */
 	@Test
-	public void visit_LocalClassToRecord_research() throws Exception {
+	public void visit_LocalClassToRecord_shouldTransform() throws Exception {
 		String original = "" +
-				"	public void methodWithLocalClassPoint() {\n" +
-				"		class Point {\n" +
-				"			private final int x;\n" +
-				"			private final int y;\n" +
-				"\n" +
-				"			Point(int x, int y) {\n" +
-				"				this.x = x;\n" +
-				"				this.y = y;\n" +
-				"			}\n" +
-				"\n" +
-				"			public int x() {\n" +
-				"				return x;\n" +
-				"			}\n" +
-				"\n" +
-				"			public int y() {\n" +
-				"				return y;\n" +
-				"			}\n" +
-				"		}\n" +
-				"	}";
-
-		// assertNoChange(original);
-
-		String expected = "" +
 				"	public void methodWithLocalClassPoint() {\n"
-				+ "		record Point(int x, int y) {\n"
+				+ "		if (true) {\n"
+				+ "			class Point {\n"
+				+ "				private final int x;\n"
+				+ "				private final int y;\n"
+				+ "\n"
+				+ "				Point(int x, int y) {\n"
+				+ "					this.x = x;\n"
+				+ "					this.y = y;\n"
+				+ "				}\n"
+				+ "\n"
+				+ "				public int x() {\n"
+				+ "					return x;\n"
+				+ "				}\n"
+				+ "\n"
+				+ "				public int y() {\n"
+				+ "					return y;\n"
+				+ "				}\n"
+				+ "			}\n"
 				+ "		}\n"
 				+ "	}";
 
-		String expectedCompilationUnitFormat = "" +
-				"package %s;\n" +
-				"public class %s {\n" +
-				" %s \n" +
-				"}";
+		String expected = "" +
+				"	public void methodWithLocalClassPoint() {\n"
+				+ "		if (true) {\n"
+				+ "			record Point(int x, int y) {\n"
+				+ "			}\n"
+				+ "		}\n"
+				+ "	}";
 
-		assertCompilationUnitMatch(original, original,
-				expectedCompilationUnitFormat);
-		// assertCompilationUnitMatch(original, expected,
-		// expectedCompilationUnitFormat);
-		// assertChange(original, expected);
+		assertChange(original, expected);
+	}
+
+	@Override
+	protected void assertChange(String actual, String expected)
+			throws JdtUnitException, JavaModelException, BadLocationException {
+		defaultFixture.addTypeDeclarationFromString(DEFAULT_TYPE_DECLARATION_NAME, actual);
+		AbstractASTRewriteASTVisitor defaultVisitor = getDefaultVisitor();
+		defaultVisitor.setASTRewrite(defaultFixture.getAstRewrite());
+		defaultFixture.accept(defaultVisitor);
+
+		TypeDeclaration expectedNode = ASTNodeBuilder.createTypeDeclarationFromString(DEFAULT_TYPE_DECLARATION_NAME,
+				expected);
+		TypeDeclaration actualNode = defaultFixture.getTypeDeclaration();
+		assertEquals(expectedNode.toString(), actualNode.toString());
 	}
 }
