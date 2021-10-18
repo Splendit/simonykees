2e790ccb8ad9121a283e6ec823e914adf7f01308
Ardit Ymeri <ardit.ymeri@splendit.at>
2021-10-18 15:08:57 +0200

    SIM-2019 UPDATE move UseTextBlock rule tests to java16.tests

--- tests/eu.jsparrow.rules.java16.test/src/test/java/org/eu/jsparrow/rules/java16/textblock/UseTextBlockRuleTest.java
+++ tests/eu.jsparrow.rules.java16.test/src/test/java/org/eu/jsparrow/rules/java16/textblock/UseTextBlockRuleTest.java
@@ -1,4 +1,4 @@
-package eu.jsparrow.core.rule.impl;
+package org.eu.jsparrow.rules.java16.textblock;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.contains;
