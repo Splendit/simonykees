b67a6c01e66c33c0d65022d30f815d4fb1fa5418
Ardit Ymeri <ardit.ymeri@splendit.at>
2021-11-25 13:34:45 +0100

    SIM-1974 ADD new empty preference page for jSparrow Markers

---
+++ bundles/ui/src/main/java/eu/jsparrow/ui/preference/SimonykeesMarkersPreferencePage.java
@@ -0,0 +1,29 @@
+package eu.jsparrow.ui.preference;
+
+import org.eclipse.jface.preference.PreferencePage;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.RowLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchPreferencePage;
+
+public class SimonykeesMarkersPreferencePage extends PreferencePage implements IWorkbenchPreferencePage {
+
+	@Override
+	public void init(IWorkbench workbench) {
+		// required by the parent
+	}
+
+	@Override
+	protected Control createContents(Composite parent) {
+		Composite composite = new Composite(parent, SWT.NONE);
+		composite.setFont(parent.getFont());
+		GridData gd = new GridData(SWT.LEFT, SWT.CENTER, false, true);
+		composite.setLayoutData(gd);
+		composite.setLayout(new RowLayout(SWT.VERTICAL));
+		return composite;
+	}
+
+}
