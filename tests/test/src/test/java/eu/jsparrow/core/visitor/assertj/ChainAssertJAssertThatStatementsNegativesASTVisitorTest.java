c64b7a383db4ab142f4893eb59928a8e8719afeb
gregor.balint <gregor.balint@splendit.at>
2021-11-28 19:05:14 +0100

    SIM-2027 ADD negative visitor test cases

--- tests/test/src/test/java/eu/jsparrow/core/visitor/assertj/ChainAssertJAssertThatStatementsNegativesASTVisitorTest.java
+++ tests/test/src/test/java/eu/jsparrow/core/visitor/assertj/ChainAssertJAssertThatStatementsNegativesASTVisitorTest.java
@@ -12,27 +12,218 @@ public class ChainAssertJAssertThatStatementsNegativesASTVisitorTest extends Use
 	@BeforeEach
 	public void setUpVisitor() throws Exception {
 		addDependency("org.assertj", "assertj-core", "3.21.0");
-
 		setDefaultVisitor(new ChainAssertJAssertThatStatementsASTVisitor());
 	}
-	
+
 	@AfterEach
 	public void tearDown() throws Exception {
 		fixtureProject.clear();
 	}
 
 	@Test
-	public void visit_AssertThatOnElementsOfSameList_shouldNotTransform() throws Exception {
-		
-		defaultFixture.addImport("org.assertj.core.api.Assertions.assertThat", true, false);
+	public void visit_ExpressionStatementNotWithInvocation_shouldNotTransform() throws Exception {
+		defaultFixture.addImport("org.assertj.core.api.Assertions.assertThat",
+				true, false);
 		defaultFixture.addImport(java.util.List.class.getName());
+		defaultFixture.addImport(java.util.Arrays.class.getName());
+		defaultFixture.addImport("org.assertj.core.api.ListAssert");
 
-		String original = "" + //
-				"	public void assertThatOnDifferentListElements(List<String> stringList) {\n"
-				+ "		assertThat(stringList).element(0).asString().contains(\"-1\");\n"
-				+ "		assertThat(stringList).element(1).asString().contains(\"-2\");\n"
+		String original = "" +
+				"	List<String> stringList = Arrays.asList(\"String-1\", \"String-2\", \"String-3\", \"String-4\");\n"
+				+ "\n"
+				+ "	public void testExpressionStatementNotWithInvocation() {\n"
+				+ "		ListAssert<String> listAssert = assertThat(stringList).isNotNull();\n"
+				+ "		listAssert = assertThat(stringList).isNotEmpty();\n"
 				+ "	}";
 
 		assertNoChange(original);
-	}	
+	}
+	
+	@Test
+	public void visit_AssertThatWithoutSubsequentChain_shouldNotTransform() throws Exception {
+		defaultFixture.addImport("org.assertj.core.api.Assertions.assertThat",
+				true, false);
+		defaultFixture.addImport(java.util.List.class.getName());
+		defaultFixture.addImport(java.util.Arrays.class.getName());
+
+		String original = "" +
+				"	List<String> stringList = Arrays.asList(\"String-1\", \"String-2\", \"String-3\", \"String-4\");\n"
+				+ "\n"
+				+ "	public void testAssertThatWithoutSubsequentChain() {\n"
+				+ "		assertThat(stringList);\n"
+				+ "		assertThat(stringList);\n"
+				+ "	}";
+
+		assertNoChange(original);
+	}
+	
+	@Test
+	public void visit_MethodNameNotAssertThat_shouldNotTransform() throws Exception {
+		defaultFixture.addImport(java.util.List.class.getName());
+		defaultFixture.addImport(java.util.Arrays.class.getName());
+		defaultFixture.addImport("org.assertj.core.api.ListAssert");
+		defaultFixture.addImport("org.assertj.core.api.Assertions");
+
+		String original = "" +
+				"	List<String> stringList = Arrays.asList(\"String-1\", \"String-2\", \"String-3\", \"String-4\");\n"
+				+ "	\n"
+				+ "	ListAssert<String> assertStringList(List<String> stringList) {\n"
+				+ "		return Assertions.assertThat(stringList);\n"
+				+ "	}\n"
+				+ "\n"
+				+ "	public void testMethodNameNotAssertThat() {\n"
+				+ "		assertStringList(stringList).isNotNull();\n"
+				+ "		assertStringList(stringList).isNotEmpty();\n"
+				+ "	}";
+
+		assertNoChange(original);
+	}
+	
+	@Test
+	public void visit_AssertThatWithMultipleArguments_shouldNotTransform() throws Exception {
+		defaultFixture.addImport(java.util.Arrays.class.getName());
+		defaultFixture.addImport("org.assertj.core.api.ListAssert");
+		defaultFixture.addImport("org.assertj.core.api.Assertions");
+
+		String original = "" +
+				"	ListAssert<String> assertThat(String... strings) {\n"
+				+ "		return Assertions.assertThat(Arrays.asList(strings));\n"
+				+ "	}\n"
+				+ "\n"
+				+ "	public void testAssertThatWithMultipleArguments() {\n"
+				+ "		assertThat(\"String-1\", \"String-2\").isNotNull();\n"
+				+ "		assertThat(\"String-1\", \"String-2\").isNotEmpty();\n"
+				+ "	}";
+
+		assertNoChange(original);
+	}
+
+	@Test
+	public void visit_AssertThatNotResolved_shouldNotTransform() throws Exception {
+		defaultFixture.addImport(java.util.List.class.getName());
+		defaultFixture.addImport(java.util.Arrays.class.getName());
+
+		String original = "" +
+				"	List<String> stringList = Arrays.asList(\"String-1\", \"String-2\", \"String-3\", \"String-4\");\n"
+				+ "\n"
+				+ "	public void testAssertThatNotResolved() {\n"
+				+ "		assertThat(stringList).isNotNull();\n"
+				+ "		assertThat(stringList).isNotEmpty();\n"
+				+ "	}";
+
+		assertNoChange(original);
+	}
+
+	@Test
+	public void visit_AssertThatNotDeclaredByAssertJAssertions_shouldNotTransform() throws Exception {
+		defaultFixture.addImport(java.util.List.class.getName());
+		defaultFixture.addImport(java.util.Arrays.class.getName());
+		defaultFixture.addImport("org.assertj.core.api.ListAssert");
+		defaultFixture.addImport("org.assertj.core.api.Assertions");
+
+		String original = "" +
+				"	List<String> stringList = Arrays.asList(\"String-1\", \"String-2\", \"String-3\", \"String-4\");\n"
+				+ "	\n"
+				+ "	ListAssert<String> assertThat(List<String> stringList) {\n"
+				+ "		return Assertions.assertThat(stringList);\n"
+				+ "	}\n"
+				+ "\n"
+				+ "	public void testAssertThatNotDeclaredByAssertJAssertions() {\n"
+				+ "		assertThat(stringList).isNotNull();\n"
+				+ "		assertThat(stringList).isNotEmpty();\n"
+				+ "	}";
+
+		assertNoChange(original);
+	}
+
+	@Test
+	public void visit_AssertThatOnElementsOfSameList_shouldNotTransform() throws Exception {
+		defaultFixture.addImport("org.assertj.core.api.Assertions.assertThat", true, false);
+		defaultFixture.addImport(java.util.List.class.getName());
+		defaultFixture.addImport(java.util.Arrays.class.getName());
+
+		String original = "" +
+				"	private List<String> stringList = Arrays.asList(\"String-1\", \"String-2\", \"String-3\", \"String-4\");\n"
+				+ "\n"
+				+ "	public void assertThatOnDifferentListElements() {\n"
+				+ "		assertThat(stringList)\n"
+				+ "				.element(0)\n"
+				+ "				.asString()\n"
+				+ "				.contains(\"-1\");\n"
+				+ "		assertThat(stringList)\n"
+				+ "				.element(1)\n"
+				+ "				.asString()\n"
+				+ "				.contains(\"-2\");\n"
+				+ "	}";
+
+		assertNoChange(original);
+	}
+	
+	@Test
+	public void visit_FirstAssertThatWithoutFollowing_shouldNotTransform() throws Exception {
+		defaultFixture.addImport("org.assertj.core.api.Assertions.assertThat",
+				true, false);
+		defaultFixture.addImport(java.util.List.class.getName());
+		defaultFixture.addImport(java.util.Arrays.class.getName());
+
+		String original = "" +
+				"	List<String> stringList = Arrays.asList(\"String-1\", \"String-2\", \"String-3\", \"String-4\");\n"
+				+ "\n"
+				+ "	public void testAssertThatWithoutFollowing() {\n"
+				+ "		assertThat(stringList).isNotNull();\n"
+				+ "	}";
+
+		assertNoChange(original);
+	}
+	
+	@Test
+	public void visit_AssertThatOnCreateListInvocation_shouldNotTransform() throws Exception {
+		defaultFixture.addImport("org.assertj.core.api.Assertions.assertThat",
+				true, false);
+		defaultFixture.addImport(java.util.List.class.getName());
+		defaultFixture.addImport(java.util.Arrays.class.getName());
+
+
+		String original = "" +
+				"	List<String> createList() {\n"
+				+ "		return Arrays.asList(\"String-1\", \"String-2\", \"String-3\", \"String-4\");\n"
+				+ "	}\n"
+				+ "\n"
+				+ "	public void assertThatOnCreateListInvocation() {\n"
+				+ "		assertThat(createList()).isNotNull();\n"
+				+ "		assertThat(createList()).isNotEmpty();\n"
+				+ "	}";
+
+		assertNoChange(original);
+	}
+	
+	
+	@Test
+	public void visit_AssertThatOnDifferentLists_shouldNotTransform() throws Exception {
+		defaultFixture.addImport("org.assertj.core.api.Assertions.assertThat",
+				true, false);
+		defaultFixture.addImport(java.util.List.class.getName());
+		defaultFixture.addImport(java.util.Arrays.class.getName());
+		
+		String original = "" +
+				"	List<String> stringList1 = Arrays.asList(\"String-1\", \"String-2\", \"String-3\", \"String-4\");\n"
+				+ "	List<String> stringList2 = Arrays.asList(\"String-1\", \"String-2\", \"String-3\", \"String-4\");\n"
+				+ "	\n"
+				+ "	public void testMultipleAssertThatWithoutInvocationChain() {\n"
+				+ "		assertThat(stringList1).isNotNull();\n"
+				+ "		assertThat(stringList2).isNotEmpty();\n"
+				+ "	}";
+
+		assertNoChange(original);
+	}
+	
+//	@Test
+//	public void visit__shouldNotTransform() throws Exception {
+//
+//
+//		String original = "" +
+//				"";
+//
+//		assertNoChange(original);
+//	}
 }
