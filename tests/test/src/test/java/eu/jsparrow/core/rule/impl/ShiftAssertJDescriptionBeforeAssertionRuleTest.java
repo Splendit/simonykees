053900e5c046a9ef57d9301fdda328c3b9392c55
Ardit Ymeri <ardit.ymeri@splendit.at>
2021-12-06 13:57:14 +0100

    SIM-2026 UPDATE rule description and required library

--- tests/test/src/test/java/eu/jsparrow/core/rule/impl/ShiftAssertJDescriptionBeforeAssertionRuleTest.java
+++ tests/test/src/test/java/eu/jsparrow/core/rule/impl/ShiftAssertJDescriptionBeforeAssertionRuleTest.java
@@ -2,20 +2,21 @@ package eu.jsparrow.core.rule.impl;
 
 import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.isEmptyOrNullString;
 import static org.junit.Assert.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.time.Duration;
+import java.util.Arrays;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static eu.jsparrow.common.util.RulesTestUtil.*;
 
 import org.eclipse.jdt.core.JavaCore;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import eu.jsparrow.common.SingleRuleTest;
-import eu.jsparrow.common.util.RulesTestUtil;
+
 import eu.jsparrow.rules.common.RuleDescription;
 import eu.jsparrow.rules.common.Tag; 
 
@@ -26,13 +27,13 @@ class ShiftAssertJDescriptionBeforeAssertionRuleTest extends SingleRuleTest {
 	@BeforeEach
 	void setUp() throws Exception {
 		rule = new ShiftAssertJDescriptionBeforeAssertionRule();
-		testProject = RulesTestUtil.createJavaProject("javaVersionTestProject", "bin");
+		testProject = createJavaProject("javaVersionTestProject", "bin");
 	}
 	
 	@Test
 	void test_ruleId() {
 		String ruleId = rule.getId();
-		assertThat(ruleId, equalTo("ShiftAssertJDescriptionBeforeAssertions"));
+		assertThat(ruleId, equalTo("ShiftAssertJDescriptionBeforeAssertion"));
 	}
 	
 	@Test
@@ -43,7 +44,10 @@ class ShiftAssertJDescriptionBeforeAssertionRuleTest extends SingleRuleTest {
 				contains(Tag.JAVA_1_5, Tag.TESTING, Tag.ASSERTJ, Tag.CODING_CONVENTIONS));
 		assertThat(description.getRemediationCost(), equalTo(Duration.ofMinutes(5)));
 		assertThat(description.getDescription(),
-				equalTo("AssertJ Description only make senese to be invoked before the assertion itself. Otherwise it has no effect."));
+				equalTo("AssertJ provides methods for setting descriptions or error messages of assertions, e.g.: as, describedAs, withFailMessage, overridingErrorMessage. "
+						+ "\nThese methods should always be invoked before the actual assertion they intend to describe, otherwise, they have no effect. "
+						+ "\nThis rule, swaps the invocation of the assertion methods with the invocation of the methods setting descriptions or the error "
+						+ "messages for the corresponding assertions."));
 	}
 	
 	@Test
@@ -53,7 +57,7 @@ class ShiftAssertJDescriptionBeforeAssertionRuleTest extends SingleRuleTest {
 
 		rule.calculateEnabledForProject(testProject);
 
-		assertThat(rule.requiredLibraries(), isEmptyOrNullString());
+		assertThat(rule.requiredLibraries(), equalTo("AssertJ"));
 	}
 	
 	@Test
@@ -62,7 +66,9 @@ class ShiftAssertJDescriptionBeforeAssertionRuleTest extends SingleRuleTest {
 	}
 
 	@Test
-	void calculateEnabledForProjectShouldBeDisabled() {
+	void calculateEnabledForProject_ShouldBeDisabled() throws Exception {
+		addToClasspath(testProject, Arrays
+				.asList(generateMavenEntryFromDepedencyString("org.assertj", "assertj-core", "3.21.0")));
 		testProject.setOption(JavaCore.COMPILER_COMPLIANCE, JavaCore.VERSION_1_4);
 
 		rule.calculateEnabledForProject(testProject);
@@ -71,7 +77,9 @@ class ShiftAssertJDescriptionBeforeAssertionRuleTest extends SingleRuleTest {
 	}
 
 	@Test
-	void calculateEnabledForProjectShouldBeEnabled() {
+	void calculateEnabledForProject_ShouldBeEnabled() throws Exception {
+		addToClasspath(testProject, Arrays
+				.asList(generateMavenEntryFromDepedencyString("org.assertj", "assertj-core", "3.21.0")));
 		testProject.setOption(JavaCore.COMPILER_COMPLIANCE, JavaCore.VERSION_1_5);
 
 		rule.calculateEnabledForProject(testProject);
@@ -79,4 +87,12 @@ class ShiftAssertJDescriptionBeforeAssertionRuleTest extends SingleRuleTest {
 		assertTrue(rule.isEnabled());
 	}
 
+	@Test
+	void calculateEnabledForProject_missingDependency_ShouldBeEnabled() throws Exception {
+		testProject.setOption(JavaCore.COMPILER_COMPLIANCE, JavaCore.VERSION_1_5);
+
+		rule.calculateEnabledForProject(testProject);
+
+		assertFalse(rule.isEnabled());
+	}
 }
