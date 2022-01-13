7c77bc9c9a6f881836eaa1bd783d61ecfb03e3f0
gregor.balint <gregor.balint@splendit.at>
2022-01-13 11:50:22 +0100

    SIM-2035 ADD rule test and correction on rule

--- bundles/core/src/main/java/eu/jsparrow/core/rule/impl/UseDedicatedAssertJAssertionsRule.java
+++ bundles/core/src/main/java/eu/jsparrow/core/rule/impl/UseDedicatedAssertJAssertionsRule.java
@@ -6,7 +6,6 @@ import java.util.function.Predicate;
 
 import org.eclipse.jdt.core.IJavaProject;
 import org.eclipse.jdt.core.JavaCore;
-import org.eclipse.jdt.core.JavaModelException;
 import org.osgi.framework.Version;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,7 +16,6 @@ import eu.jsparrow.i18n.Messages;
 import eu.jsparrow.rules.common.RefactoringRuleImpl;
 import eu.jsparrow.rules.common.RuleDescription;
 import eu.jsparrow.rules.common.Tag;
-import eu.jsparrow.rules.common.exception.runtime.ITypeNotFoundRuntimeException;
 
 /**
  * @see ChainAssertJAssertThatStatementsASTVisitor
@@ -29,15 +27,13 @@ import eu.jsparrow.rules.common.exception.runtime.ITypeNotFoundRuntimeException;
 public class UseDedicatedAssertJAssertionsRule
 		extends RefactoringRuleImpl<UseDedicatedAssertJAssertionsASTVisitor> {
 
-	private static final Logger logger = LoggerFactory.getLogger(UseDedicatedAssertJAssertionsRule.class);
-
 	public UseDedicatedAssertJAssertionsRule() {
 		this.visitorClass = UseDedicatedAssertJAssertionsASTVisitor.class;
 		this.id = "UseDedicatedAssertJAssertions"; //$NON-NLS-1$
 		this.ruleDescription = new RuleDescription(Messages.UseDedicatedAssertJAssertionsRule_name,
 				Messages.UseDedicatedAssertJAssertionsRule_description,
 				Duration.ofMinutes(5),
-				Arrays.asList(Tag.JAVA_1_8, Tag.CODING_CONVENTIONS, Tag.READABILITY));
+				Arrays.asList(Tag.JAVA_1_8, Tag.TESTING, Tag.ASSERTJ,  Tag.CODING_CONVENTIONS, Tag.READABILITY));
 	}
 
 	@Override
@@ -47,13 +43,13 @@ public class UseDedicatedAssertJAssertionsRule
 
 	@Override
 	public String requiredLibraries() {
-		return "AssertJ [3.20.2, 3.21.x]"; //$NON-NLS-1$
+		return "AssertJ [3.20.2, 3.22.x]"; //$NON-NLS-1$
 	}
 
 	@Override
 	public boolean ruleSpecificImplementation(IJavaProject project) {
 		Predicate<Version> assertJVersionComparator = version -> version.compareTo(Version.parseVersion("3.20.2")) >= 0
-				&& version.compareTo(Version.parseVersion("3.21.9")) <= 0;
+				&& version.compareTo(Version.parseVersion("3.22.9")) <= 0;
 
 		return isInProjectLibraries(project, "org.assertj.core.api.Assertions", assertJVersionComparator);
 	}
---
+++ tests/test/src/test/java/eu/jsparrow/core/rule/impl/UseDedicatedAssertJAssertionsRuleTest.java
@@ -0,0 +1,94 @@
+package eu.jsparrow.core.rule.impl;
+
+import static eu.jsparrow.common.util.RulesTestUtil.addToClasspath;
+import static eu.jsparrow.common.util.RulesTestUtil.createJavaProject;
+import static eu.jsparrow.common.util.RulesTestUtil.generateMavenEntryFromDepedencyString;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.time.Duration;
+import java.util.Arrays;
+
+import org.eclipse.jdt.core.JavaCore;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import eu.jsparrow.common.SingleRuleTest;
+import eu.jsparrow.rules.common.RuleDescription;
+import eu.jsparrow.rules.common.Tag;
+
+@SuppressWarnings("nls")
+class UseDedicatedAssertJAssertionsRuleTest extends SingleRuleTest {
+
+	private UseDedicatedAssertJAssertionsRule rule;
+
+	@BeforeEach
+	public void setUp() throws Exception {
+		rule = new UseDedicatedAssertJAssertionsRule();
+		testProject = createJavaProject("javaVersionTestProject", "bin");
+	}
+
+	@Test
+	void test_ruleId() {
+		String ruleId = rule.getId();
+		assertThat(ruleId, equalTo("UseDedicatedAssertJAssertions"));
+	}
+
+	@Test
+	void test_ruleDescription() {
+		RuleDescription description = rule.getRuleDescription();
+		assertThat(description.getName(), equalTo("Use Dedicated AssertJ Assertions"));
+		assertThat(description.getTags(),
+				contains(Tag.JAVA_1_8, Tag.TESTING, Tag.ASSERTJ, Tag.CODING_CONVENTIONS, Tag.READABILITY));
+		assertThat(description.getRemediationCost(), equalTo(Duration.ofMinutes(5)));
+		assertThat(description.getDescription(),
+				equalTo("Replaces AssertJ assertions with the corresponding dedicated AssertJ assertions. For example, 'assertThat(string1.equals(string2)).isTrue();' can be replaced by 'assertThat(string1).isEqualTo(string2)', or 'assertThat(o1 == o2).isTrue();' can be replaced by 'assertThat(o1).isSameAs(o2)', or 'assertThat(o1 == null).isTrue();' can be replaced by 'assertThat(o1).isNull();'."));
+	}
+
+	@Test
+	void test_requiredLibraries() throws Exception {
+		assertThat(rule.requiredLibraries(), equalTo("AssertJ [3.20.2, 3.22.x]"));
+	}
+
+	@Test
+	void test_requiredJavaVersion() throws Exception {
+		assertThat(rule.getRequiredJavaVersion(), equalTo("1.8"));
+	}
+
+	@Test
+	void calculateEnabledForProject_supportLibraryVersion_3_21_0_shouldAllReturnTrue() throws Exception {
+		addToClasspath(testProject, Arrays
+			.asList(generateMavenEntryFromDepedencyString("org.assertj", "assertj-core", "3.21.0")));
+		testProject.setOption(JavaCore.COMPILER_COMPLIANCE, JavaCore.VERSION_1_8);
+
+		rule.calculateEnabledForProject(testProject);
+		assertTrue(rule.isEnabled());
+		assertTrue(rule.isSatisfiedLibraries());
+		assertTrue(rule.isSatisfiedJavaVersion());
+	}
+
+	@Test
+	void calculateEnabledForProject_supportLibraryVersion_3_22_0_shouldAllReturnTrue() throws Exception {
+		addToClasspath(testProject, Arrays
+			.asList(generateMavenEntryFromDepedencyString("org.assertj", "assertj-core", "3.22.0")));
+		testProject.setOption(JavaCore.COMPILER_COMPLIANCE, JavaCore.VERSION_1_8);
+
+		rule.calculateEnabledForProject(testProject);
+		assertTrue(rule.isEnabled());
+		assertTrue(rule.isSatisfiedLibraries());
+		assertTrue(rule.isSatisfiedJavaVersion());
+	}
+
+	@Test
+	void calculateEnabledForProject_notSupportedLibraryVersion_3_20_1_shouldReturnFalse() throws Exception {
+		addToClasspath(testProject, Arrays
+			.asList(generateMavenEntryFromDepedencyString("org.assertj", "assertj-core", "3.20.1")));
+		testProject.setOption(JavaCore.COMPILER_COMPLIANCE, JavaCore.VERSION_1_8);
+
+		rule.calculateEnabledForProject(testProject);
+		assertFalse(rule.isEnabled());
+	}
+}
