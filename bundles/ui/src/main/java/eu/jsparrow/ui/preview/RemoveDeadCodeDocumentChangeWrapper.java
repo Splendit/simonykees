5d306965328e60bc342e7e0e4157b7d8accc23bf
Ardit Ymeri <ardit.ymeri@splendit.at>
2022-01-28 20:33:54 +0100

    SIM-2059 ADD preview wizard for unused code similar to Rename Wizard
    Many parts of code are duplicated. It has to be cleaned up.

---
+++ bundles/ui/src/main/java/eu/jsparrow/ui/preview/RemoveDeadCodeDocumentChangeWrapper.java
@@ -0,0 +1,80 @@
+package eu.jsparrow.ui.preview;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.jface.text.Document;
+import org.eclipse.ltk.core.refactoring.DocumentChange;
+
+import eu.jsparrow.core.visitor.unused.UnusedFieldWrapper;
+
+public class RemoveDeadCodeDocumentChangeWrapper {
+	
+	private DocumentChange documentChange;
+	private boolean isParent;
+	private RemoveDeadCodeDocumentChangeWrapper parent;
+	private String identifier;
+	private String compilationUnitName;
+	private List<RemoveDeadCodeDocumentChangeWrapper> children = new ArrayList<>();
+	private UnusedFieldWrapper fieldData;
+	private Document originalDocument;
+
+	public RemoveDeadCodeDocumentChangeWrapper(DocumentChange documentChange, RemoveDeadCodeDocumentChangeWrapper parent, Document originalDocument, UnusedFieldWrapper fieldData) {
+		this.documentChange = documentChange;
+		this.parent = parent;
+		this.isParent = null == parent;
+		this.identifier = fieldData.getFieldName();
+		this.compilationUnitName = fieldData.getClassDeclarationName();
+		this.fieldData = fieldData;
+		this.originalDocument = originalDocument;
+	}
+
+	private RemoveDeadCodeDocumentChangeWrapper(DocumentChange documentChange, RemoveDeadCodeDocumentChangeWrapper parent, String identifier, 
+			String compilationUnitName, Document compilationUnitSource, UnusedFieldWrapper fieldData) {
+		this.documentChange = documentChange;
+		this.parent = parent;
+		this.isParent = null == parent;
+		this.identifier = identifier;
+		this.compilationUnitName = compilationUnitName;
+		this.originalDocument = compilationUnitSource;
+		this.fieldData = fieldData;
+	}
+
+	public DocumentChange getDocumentChange() {
+		return documentChange;
+	}
+
+	public boolean isParent() {
+		return isParent;
+	}
+
+	public void addChild(DocumentChange child, String compilationUnitName, Document document) {
+		this.children.add(new RemoveDeadCodeDocumentChangeWrapper(child, this, this.identifier, 
+				compilationUnitName, document, this.fieldData));
+	}
+
+	public RemoveDeadCodeDocumentChangeWrapper[] getChildren() {
+		return children.toArray(new RemoveDeadCodeDocumentChangeWrapper[] {});
+	}
+
+	public RemoveDeadCodeDocumentChangeWrapper getParent() {
+		return parent;
+	}
+
+	public String getIdentifier() {
+		return identifier;
+	}
+
+	public String getCompilationUnitName() {
+		return compilationUnitName;
+	}
+	
+	public Document getOriginalDocument() {
+		return this.originalDocument;
+	}
+
+	public UnusedFieldWrapper getFieldData() {
+		return fieldData;
+	}
+
+}
