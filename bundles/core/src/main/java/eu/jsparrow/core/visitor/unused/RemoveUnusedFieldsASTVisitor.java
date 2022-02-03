1032ac37352fccb6d44ed0dcd79515b66737a7cd
Ardit Ymeri <ardit.ymeri@splendit.at>
2022-02-03 12:47:53 +0100

    SIM-2059 UPDATE removing declarations and reassignments of unused fields

--- bundles/core/src/main/java/eu/jsparrow/core/visitor/unused/RemoveUnusedFieldsASTVisitor.java
+++ bundles/core/src/main/java/eu/jsparrow/core/visitor/unused/RemoveUnusedFieldsASTVisitor.java
@@ -1,15 +1,17 @@
 package eu.jsparrow.core.visitor.unused;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.jdt.core.ICompilationUnit;
-import org.eclipse.jdt.core.dom.ASTMatcher;
 import org.eclipse.jdt.core.dom.Assignment;
 import org.eclipse.jdt.core.dom.CompilationUnit;
+import org.eclipse.jdt.core.dom.ExpressionStatement;
 import org.eclipse.jdt.core.dom.FieldDeclaration;
-import org.eclipse.jdt.core.dom.SimpleName;
 import org.eclipse.jdt.core.dom.VariableDeclarationFragment;
 import org.eclipse.text.edits.TextEditGroup;
 
@@ -19,16 +21,9 @@ import eu.jsparrow.rules.common.visitor.AbstractASTRewriteASTVisitor;
 public class RemoveUnusedFieldsASTVisitor extends AbstractASTRewriteASTVisitor {
 	
 	private List<UnusedFieldWrapper> unusedFields;
-	/*
-	 * Gets a list of unused field wrappers.
-	 * Finds the relevant ones for the given compilation unit. 
-	 * Removes them. 
-	 */
+	private Map<VariableDeclarationFragment, UnusedFieldWrapper> relevantFragments;
+	private Map<ExpressionStatement, UnusedFieldWrapper> relevantReassignments;
 	
-	/*
-	 * TODO: make a list of fragments to be removed. 
-	 * TODO: make a list of assignment statements to be removed. 
-	 */
 	
 	public RemoveUnusedFieldsASTVisitor(List<UnusedFieldWrapper> unusedFields) {
 		this.unusedFields = unusedFields;
@@ -36,69 +31,100 @@ public class RemoveUnusedFieldsASTVisitor extends AbstractASTRewriteASTVisitor {
 	
 	@Override
 	public boolean visit(CompilationUnit compilationUnit) {
-		// TODO: find all relevant declaration fragments and reassignments that should be removed from this compilation unit. 
 		IPath currentPath = compilationUnit.getJavaElement().getPath();
 		List<UnusedFieldWrapper> relevantUnusedFields = new ArrayList<>();
+		Map<VariableDeclarationFragment, UnusedFieldWrapper> fragments = new HashMap<>();
+		Map<ExpressionStatement, UnusedFieldWrapper> reassignments = new HashMap<>();
 		for(UnusedFieldWrapper unusedField : unusedFields) {
 			IPath path = unusedField.getDeclarationPath();
 			if (path.equals(currentPath)) {
 				relevantUnusedFields.add(unusedField);
+				VariableDeclarationFragment fragment = unusedField.getFragment();
+				fragments.put(fragment, unusedField);
+				List<ExpressionStatement> internalReassignment = unusedField.getUnusedReassignments();
+				for(ExpressionStatement reassignment : internalReassignment) {
+					reassignments.put(reassignment, unusedField);
+				}
+				
 			} else {
-				List<ICompilationUnit> targetCompilationUnits = unusedField.getTargetICompilationUnits();
-				for (ICompilationUnit targetICU : targetCompilationUnits) {
+				for(UnusedExternalReferences reference : unusedField.getUnusedExternalReferences()) {
+					ICompilationUnit targetICU = reference.getICompilationUnit();
 					if(currentPath.equals(targetICU.getPath())) {
-						relevantUnusedFields.add(unusedField);
-						break;
+						for(ExpressionStatement reassignment : reference.getUnusedReassignments()) {
+							reassignments.put(reassignment, unusedField);
+						}
+						
 					}
 				}
 			}
 			
 		}
+		this.relevantFragments = fragments;
+		this.relevantReassignments = reassignments;
 		return super.visit(compilationUnit);
 	}
 	
 	@Override
 	public boolean visit(FieldDeclaration fieldDeclaration) {
 		List<VariableDeclarationFragment> fragments = ASTNodeUtil.convertToTypedList(fieldDeclaration.fragments(), VariableDeclarationFragment.class);
-		for (UnusedFieldWrapper unusedField : unusedFields) {
-			VariableDeclarationFragment fragment = unusedField.getFragment();
-			
-			for(VariableDeclarationFragment declaratingFragment : fragments) {
-				if(declaratingFragment.subtreeMatch(new ASTMatcher(), fragment)) {
-					TextEditGroup editGroup = unusedField.getTextEditGroup((ICompilationUnit)this.getCompilationUnit().getJavaElement());
-					if(fragments.size() == 1) {
-						astRewrite.remove(fieldDeclaration, editGroup);
-						
-					} else {
-						astRewrite.remove(declaratingFragment, editGroup);
-					}
-					onRewrite();
+		for(VariableDeclarationFragment declaratingFragment : fragments) {
+			isDesignatedForRemoval(declaratingFragment).ifPresent(unusedField -> {
+				TextEditGroup editGroup = unusedField.getTextEditGroup((ICompilationUnit)this.getCompilationUnit().getJavaElement());
+				if(fragments.size() == 1) {
+					astRewrite.remove(fieldDeclaration, editGroup);
+					
+				} else {
+					astRewrite.remove(declaratingFragment, editGroup);
 				}
-			}
-			
+				onRewrite();
+			});
 		}
+			
+
+		return true;
+	}
+
+	@Override
+	public boolean visit(Assignment assignment) {
+		if(assignment.getLocationInParent() != ExpressionStatement.EXPRESSION_PROPERTY) {
+			return true;
+		}
+		
+		ExpressionStatement statement = (ExpressionStatement)assignment.getParent();
+		isDesignatedForRemoval(statement).ifPresent(unusedField -> {
+			TextEditGroup editGroup = unusedField.getTextEditGroup((ICompilationUnit)this.getCompilationUnit().getJavaElement());
+			astRewrite.remove(assignment.getParent(), editGroup);
+		});
+
 		return true;
 	}
 	
-//	@Override
-//	public boolean visit(SimpleName simpleName) {
-//		// TODO: here we need a map from assignments to UnusedFieldWrapper. We should visit assignments instead of simple names. it is more efficient. 
-//		if(simpleName.getLocationInParent() == Assignment.LEFT_HAND_SIDE_PROPERTY) {
-//			TextEditGroup editGroup = unusedFields.get(0).getTextEditGroup((ICompilationUnit)this.getCompilationUnit().getJavaElement());
-//			astRewrite.remove(simpleName.getParent().getParent(), editGroup);
-//		}
-//		return true;
-//	}
-	
-	@Override
-	public boolean visit(Assignment assignment) {
-		if(assignment.getLeftHandSide().toString().contains(unusedFields.get(0).getFieldName())) {
-			TextEditGroup editGroup = unusedFields.get(0)
-					.getTextEditGroup((ICompilationUnit)this.getCompilationUnit().getJavaElement());
-			astRewrite.remove(assignment.getParent(), editGroup);
+	private Optional<UnusedFieldWrapper> isDesignatedForRemoval(VariableDeclarationFragment fragment) {
+		int startPosition = fragment.getStartPosition();
+		int length = fragment.getLength();
+		for(Map.Entry<VariableDeclarationFragment, UnusedFieldWrapper> entry : relevantFragments.entrySet()) {
+			VariableDeclarationFragment candidate = entry.getKey();
+			int candidateStart = candidate.getStartPosition();
+			int candidateLength = candidate.getLength();
+			if(candidateStart == startPosition && candidateLength == length) {
+				return Optional.of(entry.getValue());
+			}
 		}
-
-		return true;
+		return Optional.empty();
+	}
+	
+	private Optional<UnusedFieldWrapper> isDesignatedForRemoval(ExpressionStatement statement) {
+		int startPosition = statement.getStartPosition();
+		int length = statement.getLength();
+		for(Map.Entry<ExpressionStatement, UnusedFieldWrapper> entry : relevantReassignments.entrySet()) {
+			ExpressionStatement candidate = entry.getKey();
+			int candidateStart = candidate.getStartPosition();
+			int candidateLength = candidate.getLength();
+			if(candidateStart == startPosition && candidateLength == length) {
+				return Optional.of(entry.getValue());
+			}
+		}
+		return Optional.empty();
 	}
 
 }
