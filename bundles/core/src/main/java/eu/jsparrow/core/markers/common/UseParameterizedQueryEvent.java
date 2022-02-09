cac78847fc9ade96f462df3f52f82ad0f584daa8
Ardit Ymeri <ardit.ymeri@splendit.at>
2022-02-09 13:19:45 +0100

    SIM-2057 UPDATE missing docs and addMarkerEvent call

--- bundles/core/src/main/java/eu/jsparrow/core/markers/common/UseParameterizedQueryEvent.java
+++ bundles/core/src/main/java/eu/jsparrow/core/markers/common/UseParameterizedQueryEvent.java
@@ -2,9 +2,23 @@ package eu.jsparrow.core.markers.common;
 
 import org.eclipse.jdt.core.dom.MethodInvocation;
 
+import eu.jsparrow.core.visitor.security.AbstractDynamicQueryASTVisitor;
+
+/**
+ * An interface to add {@link RefactoringMarkerEvent}s for
+ * {@link AbstractDynamicQueryASTVisitor}.
+ * 
+ * @since 4.8.0
+ *
+ */
 public interface UseParameterizedQueryEvent {
 
+	/**
+	 * 
+	 * @param methodInvocation
+	 *            the invocation of a dynamic query.
+	 */
 	default void addMarkerEvent(MethodInvocation methodInvocation) {
-		
+
 	}
 }
