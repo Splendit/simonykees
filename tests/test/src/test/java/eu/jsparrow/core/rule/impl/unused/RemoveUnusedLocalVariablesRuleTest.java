fc9d15cc40f5803e5b18026020129372d34dc783
gregor.balint <gregor.balint@splendit.at>
2022-03-09 13:07:14 +0100

    SIM-2065 ADD RemoveUnusedLocalVariablesRuleTest

---
+++ tests/test/src/test/java/eu/jsparrow/core/rule/impl/unused/RemoveUnusedLocalVariablesRuleTest.java
@@ -0,0 +1,123 @@
+package eu.jsparrow.core.rule.impl.unused;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.nullValue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.apache.commons.lang3.StringUtils;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.jdt.core.ICompilationUnit;
+import org.eclipse.jdt.core.IPackageFragment;
+import org.eclipse.jdt.core.IPackageFragmentRoot;
+import org.eclipse.jdt.core.JavaCore;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import eu.jsparrow.common.SingleRuleTest;
+import eu.jsparrow.common.util.RulesTestUtil;
+import eu.jsparrow.core.refactorer.RefactoringPipeline;
+import eu.jsparrow.rules.common.RuleDescription;
+import eu.jsparrow.rules.common.Tag;
+
+class RemoveUnusedLocalVariablesRuleTest extends SingleRuleTest {
+
+	private RemoveUnusedLocalVariablesRule rule;
+
+	@BeforeEach
+	public void setUp() throws Exception {
+		rule = new RemoveUnusedLocalVariablesRule();
+		testProject = RulesTestUtil.createJavaProject("javaVersionTestProject", "bin");
+	}
+
+	@Test
+	void test_ruleId() {
+		String ruleId = rule.getId();
+		assertThat(ruleId, equalTo("RemoveUnusedLocalVariables"));
+	}
+	
+	@Test
+	void test_ruleDescription() {
+		RuleDescription description = rule.getRuleDescription();
+		assertThat(description.getName(), equalTo("Remove Unused Local Variables"));
+		assertThat(description.getTags(),
+				contains(Tag.JAVA_1_1, Tag.READABILITY, Tag.CODING_CONVENTIONS));
+		assertThat(description.getRemediationCost(), equalTo(Duration.ofMinutes(2)));
+		assertThat(description.getDescription(),
+				equalTo("Finds and removes local variables that are never used actively."));
+	}
+	
+	@Test
+	void test_requiredLibraries() throws Exception {
+
+		testProject.setOption(JavaCore.COMPILER_COMPLIANCE, JavaCore.VERSION_1_8);
+
+		rule.calculateEnabledForProject(testProject);
+
+		assertThat(rule.requiredLibraries(), nullValue());
+	}
+	
+	@Test
+	void test_requiredJavaVersion() throws Exception {
+		assertThat(rule.getRequiredJavaVersion(), equalTo("1.1"));
+	}
+
+	@Test
+	void calculateEnabledForProject_ShouldBeEnabled() throws Exception {
+		testProject.setOption(JavaCore.COMPILER_COMPLIANCE, JavaCore.VERSION_1_1);
+
+		rule.calculateEnabledForProject(testProject);
+
+		assertTrue(rule.isEnabled());
+	}
+	
+	@Test
+	void testTransformation() throws Exception {
+		Path preRule = getPreRuleFile("unused/UnusedLocalVariables.java");
+		Path postRule = getPostRuleFile("UnusedLocalVariables.java", "unused");
+		
+		RemoveUnusedLocalVariablesRule rule = new RemoveUnusedLocalVariablesRule();
+		
+		String refactoring = applyRemoveUnusedFieldRefactoring(rule, "eu.jsparrow.sample.preRule.unused", preRule, root);
+		String postRulePackage = getPostRulePackage("unused");
+		String actual = StringUtils.replace(refactoring, "package eu.jsparrow.sample.preRule.unused",
+				postRulePackage);
+		String expected = new String(Files.readAllBytes(postRule), StandardCharsets.UTF_8);
+		
+		assertEquals(expected, actual);
+	}
+	
+	private String applyRemoveUnusedFieldRefactoring(RemoveUnusedLocalVariablesRule rule, String packageString,
+			Path preFile, IPackageFragmentRoot root) throws Exception {
+
+		IPackageFragment packageFragment = root.createPackageFragment(packageString, true, null);
+		String fileName = preFile.getFileName()
+			.toString();
+		String content = new String(Files.readAllBytes(preFile), StandardCharsets.UTF_8);
+		ICompilationUnit compilationUnit = packageFragment.createCompilationUnit(fileName, content, true, null);
+		List<ICompilationUnit> compilationUnits = new ArrayList<>();
+		compilationUnits.add(compilationUnit);
+
+		RefactoringPipeline refactoringPipeline = new RefactoringPipeline(Arrays.asList(rule));
+		IProgressMonitor monitor = new NullProgressMonitor();
+
+		refactoringPipeline.prepareRefactoring(compilationUnits, monitor);
+		refactoringPipeline.doRefactoring(monitor);
+		refactoringPipeline.commitRefactoring();
+
+		return compilationUnit.getSource();
+	}
+	
+
+}
