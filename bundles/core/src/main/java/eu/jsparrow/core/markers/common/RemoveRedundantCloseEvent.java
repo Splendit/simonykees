456670e87e71e4ca223dce43a65f941244b97611
Ardit Ymeri <ardit.ymeri@splendit.at>
2022-05-18 10:26:45 +0200

    SIM-1311 UPDATE missing javadocs and correcting the rule description

--- bundles/core/src/main/java/eu/jsparrow/core/markers/common/RemoveRedundantCloseEvent.java
+++ bundles/core/src/main/java/eu/jsparrow/core/markers/common/RemoveRedundantCloseEvent.java
@@ -2,8 +2,24 @@ package eu.jsparrow.core.markers.common;
 
 import org.eclipse.jdt.core.dom.ExpressionStatement;
 
+import eu.jsparrow.core.visitor.impl.trycatch.close.RemoveRedundantCloseASTVisitor;
+import eu.jsparrow.rules.common.markers.RefactoringMarkerEvent;
+
+/**
+ * * An interface to add {@link RefactoringMarkerEvent}s for
+ * {@link RemoveRedundantCloseASTVisitor}.
+ * 
+ * 4.11.0
+ *
+ */
 public interface RemoveRedundantCloseEvent {
 
+	/**
+	 * 
+	 * @param closeStatement
+	 *            the statement representing a {@code resource.close()}
+	 *            invocation.
+	 */
 	default void addMarkerEvent(ExpressionStatement closeStatement) {
 	}
 
