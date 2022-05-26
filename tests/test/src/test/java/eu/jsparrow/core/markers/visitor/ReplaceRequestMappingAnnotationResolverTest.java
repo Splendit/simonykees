32678567d3de0cb0b9f830267b64f42fb3114e7e
gregor.balint <gregor.balint@splendit.at>
2022-05-26 14:10:39 +0200

    SIM-2104 UPDATE  change rule description

--- tests/test/src/test/java/eu/jsparrow/core/markers/visitor/ReplaceRequestMappingAnnotationResolverTest.java
+++ tests/test/src/test/java/eu/jsparrow/core/markers/visitor/ReplaceRequestMappingAnnotationResolverTest.java
@@ -67,9 +67,11 @@ class ReplaceRequestMappingAnnotationResolverTest extends UsesJDTUnitFixture {
 		List<RefactoringMarkerEvent> events = RefactoringMarkers.getAllEvents();
 		assertEquals(1, events.size());
 		RefactoringMarkerEvent event = events.get(0);
-		String description = "This rule looks for '@RequestMapping' annotations defined by the spring framework and replaces them by more specific ones."
-				+ " For example, the annotation '@RequestMapping(value = \"/hello\", method = RequestMethod.GET)' can be replaced by '@GetMapping(value = \"/hello\")'.";
-
+		String description = "Version 4.3 of the Spring Framework introduced annotations like '@GetMapping' or '@PostMapping'"
+				+ " to be used instead of '@RequestMapping' when a certain request method is specified."
+				+ " Accordingly, this rule tries to replace annotations."
+				+ " For example,  '@RequestMapping(value = \"/hello\", method = RequestMethod.GET)'"
+				+ " can be replaced by '@GetMapping(value = \"/hello\")'.";
 		assertAll(
 				() -> assertEquals("Replace Request Mapping Annotation", event.getName()),
 				() -> assertEquals(description, event.getMessage()),
--- tests/test/src/test/java/eu/jsparrow/core/rule/impl/ReplaceRequestMappingAnnotationRuleTest.java
+++ tests/test/src/test/java/eu/jsparrow/core/rule/impl/ReplaceRequestMappingAnnotationRuleTest.java
@@ -86,8 +86,10 @@ class ReplaceRequestMappingAnnotationRuleTest extends SingleRuleTest {
 				contains(Tag.JAVA_1_5, Tag.CODING_CONVENTIONS, Tag.READABILITY));
 		assertThat(description.getRemediationCost(), equalTo(Duration.ofMinutes(2)));
 		assertThat(description.getDescription(),
-				equalTo("This rule looks for '@RequestMapping' annotations defined by the spring framework and replaces them by more specific ones."
-						+ " For example, the annotation '@RequestMapping(value = \"/hello\", method = RequestMethod.GET)'"
+				equalTo("Version 4.3 of the Spring Framework introduced annotations like '@GetMapping' or '@PostMapping'"
+						+ " to be used instead of '@RequestMapping' when a certain request method is specified."
+						+ " Accordingly, this rule tries to replace annotations."
+						+ " For example,  '@RequestMapping(value = \"/hello\", method = RequestMethod.GET)'"
 						+ " can be replaced by '@GetMapping(value = \"/hello\")'."));
 	}
 
