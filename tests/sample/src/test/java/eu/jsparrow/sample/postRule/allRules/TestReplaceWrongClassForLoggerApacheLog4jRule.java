692c0ddde65d1c9c5b65cec740a28c647376c70c
gregor.balint <gregor.balint@splendit.at>
2022-07-18 00:41:21 +0200

    SIM-2118 UPDATE rule tests

--- tests/sample/src/test/java/eu/jsparrow/sample/postRule/allRules/TestReplaceWrongClassForLoggerApacheLog4jRule.java
+++ tests/sample/src/test/java/eu/jsparrow/sample/postRule/allRules/TestReplaceWrongClassForLoggerApacheLog4jRule.java
@@ -2,6 +2,7 @@ package eu.jsparrow.sample.postRule.allRules;
 
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
+import org.apache.log4j.Priority;
 
 import eu.jsparrow.sample.utilities.HelloWorld;
 
@@ -12,4 +13,28 @@ public class TestReplaceWrongClassForLoggerApacheLog4jRule {
 
 	static final Class<HelloWorld> HRLLO_WORLD_CLASS = HelloWorld.class;
 	static final Logger LOGGER = LogManager.getLogger(HelloWorld.class);
+
+	static class ClassWithLoggingMethodInvocations {
+
+		Object message;
+		Priority priority;
+
+		void callLoggingMethods() {
+
+			LogManager.getLogger(ClassWithLoggingMethodInvocations.class)
+				.debug(message);
+			LogManager.getLogger(ClassWithLoggingMethodInvocations.class)
+				.error(message);
+			LogManager.getLogger(ClassWithLoggingMethodInvocations.class)
+				.fatal(message);
+			LogManager.getLogger(ClassWithLoggingMethodInvocations.class)
+				.info(message);
+			LogManager.getLogger(ClassWithLoggingMethodInvocations.class)
+				.log(priority, message);
+			LogManager.getLogger(ClassWithLoggingMethodInvocations.class)
+				.trace(message);
+			LogManager.getLogger(ClassWithLoggingMethodInvocations.class)
+				.warn(message);
+		}
+	}
 }
