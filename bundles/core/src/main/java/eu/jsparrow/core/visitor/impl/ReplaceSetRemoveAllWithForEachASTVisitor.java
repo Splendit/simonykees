38202de5ff90beb478abdd3aeea30834ebb44dc2
gregor.balint <gregor.balint@splendit.at>
2022-08-10 22:03:21 +0200

    SIM-2117 UPDATE corrections on main visitor, additional visitor tests

--- bundles/core/src/main/java/eu/jsparrow/core/visitor/impl/ReplaceSetRemoveAllWithForEachASTVisitor.java
+++ bundles/core/src/main/java/eu/jsparrow/core/visitor/impl/ReplaceSetRemoveAllWithForEachASTVisitor.java
@@ -10,7 +10,6 @@ import org.eclipse.jdt.core.dom.ExpressionStatement;
 import org.eclipse.jdt.core.dom.ITypeBinding;
 import org.eclipse.jdt.core.dom.MethodInvocation;
 
-import eu.jsparrow.core.exception.visitor.UnresolvedTypeBindingException;
 import eu.jsparrow.core.markers.common.ReplaceSetRemoveAllWithForEachEvent;
 import eu.jsparrow.rules.common.util.ASTNodeUtil;
 import eu.jsparrow.rules.common.util.ClassRelationUtil;
@@ -23,12 +22,9 @@ import eu.jsparrow.rules.common.visitor.AbstractASTRewriteASTVisitor;
 public class ReplaceSetRemoveAllWithForEachASTVisitor extends AbstractASTRewriteASTVisitor
 		implements ReplaceSetRemoveAllWithForEachEvent {
 
+	private static final List<String> JAVA_UTIL_SET = Collections.singletonList(java.util.Set.class.getName());
+	private static final List<String> JAVA_UTIL_LIST = Collections.singletonList(java.util.List.class.getName());
 
-	private static final List<String> LIST_WITH_JAVA_LANG_ITERABLE_CLASS_NAME = Collections
-		.singletonList(java.lang.Iterable.class.getName());
-
-	private static final List<String> LIST_WITH_JAVA_UTIL_SET_CLASS_NAME = Collections
-		.singletonList(java.util.Set.class.getName());
 
 	private static final String REMOVE_ALL = "removeAll"; //$NON-NLS-1$
 	private static final String REMOVE = "remove"; //$NON-NLS-1$
@@ -36,6 +32,11 @@ public class ReplaceSetRemoveAllWithForEachASTVisitor extends AbstractASTRewrite
 
 	@Override
 	public boolean visit(MethodInvocation methodInvocation) {
+		if (!methodInvocation.getName()
+			.getIdentifier()
+			.equals(REMOVE_ALL)) {
+			return true;
+		}
 		if (methodInvocation.getLocationInParent() != ExpressionStatement.EXPRESSION_PROPERTY) {
 			return true;
 		}
@@ -43,64 +44,31 @@ public class ReplaceSetRemoveAllWithForEachASTVisitor extends AbstractASTRewrite
 		if (methodInvocationExpression == null) {
 			return true;
 		}
-		if (!methodInvocation.getName()
-			.getIdentifier()
-			.equals(REMOVE_ALL)) {
-			return true;
-		}
 		List<Expression> arguments = ASTNodeUtil.convertToTypedList(methodInvocation.arguments(), Expression.class);
 		if (arguments.size() != 1) {
 			return true;
 		}
 		Expression removeAllArgument = arguments.get(0);
-		try {
-			if (methodInvocation.resolveMethodBinding() == null) {
-				String message = "Could not resolve type binding of method invocation" //$NON-NLS-1$
-						+ methodInvocation.toString();
-				throw new UnresolvedTypeBindingException(message);
-			}
-			if (isRemoveAllInvocationWithSlowPerformance(methodInvocationExpression, removeAllArgument)) {
-				transform(methodInvocation, methodInvocationExpression, removeAllArgument);
-				return false;
-			}
-		} catch (Exception exc) {
+
+		if (isSet(methodInvocationExpression) && isList(removeAllArgument)) {
+			transform(methodInvocation, methodInvocationExpression, removeAllArgument);
 			return false;
 		}
 		return true;
 	}
 
-	private boolean isRemoveAllInvocationWithSlowPerformance(Expression methodInvocationExpression,
-			Expression removeAllArgument) throws UnresolvedTypeBindingException {
-		ITypeBinding invocationExpressionType = methodInvocationExpression.resolveTypeBinding();
-		if (invocationExpressionType == null) {
-			String message = "Could not resolve type binding of method invocation expression: " //$NON-NLS-1$
-					+ methodInvocationExpression.toString();
-			throw new UnresolvedTypeBindingException(message);
-		}
-		ITypeBinding invocationArgumentType = removeAllArgument.resolveTypeBinding();
-		if (invocationArgumentType == null) {
-			String message = "Could not resolve type binding of method invocation argument: " //$NON-NLS-1$
-					+ removeAllArgument.toString();
-			throw new UnresolvedTypeBindingException(message);
-		}
-		return isInvocationOnSet(invocationExpressionType) && isIterableArgument(invocationArgumentType);
-
+	private boolean isSet(Expression expression) {
+		ITypeBinding typeBinding = expression.resolveTypeBinding();
+		return ClassRelationUtil.isInheritingContentOfTypes(typeBinding, JAVA_UTIL_SET)
+				|| ClassRelationUtil.isContentOfTypes(typeBinding, JAVA_UTIL_SET);
 	}
 
-	private boolean isInvocationOnSet(ITypeBinding invocationExpressionType) {
-		return ClassRelationUtil.isInheritingContentOfTypes(invocationExpressionType,
-				LIST_WITH_JAVA_UTIL_SET_CLASS_NAME)
-				|| ClassRelationUtil.isContentOfTypes(invocationExpressionType, LIST_WITH_JAVA_UTIL_SET_CLASS_NAME);
+	private boolean isList(Expression expression) {
+		ITypeBinding typeBinding = expression.resolveTypeBinding();
+		return ClassRelationUtil.isInheritingContentOfTypes(typeBinding, JAVA_UTIL_LIST) ||
+				ClassRelationUtil.isContentOfTypes(typeBinding, JAVA_UTIL_LIST);
 	}
 
-	private boolean isIterableArgument(ITypeBinding invocationArgumentType) {
-		return ClassRelationUtil.isInheritingContentOfTypes(invocationArgumentType,
-				LIST_WITH_JAVA_LANG_ITERABLE_CLASS_NAME) ||
-				ClassRelationUtil.isContentOfTypes(invocationArgumentType,
-						LIST_WITH_JAVA_LANG_ITERABLE_CLASS_NAME);
-	}
-
-	
 	@SuppressWarnings({ "rawtypes", "unchecked" })
 	private void transform(MethodInvocation methodInvocationToReplace, Expression removeAllInvocationExpression,
 			Expression removeAllArgument) {
@@ -111,22 +79,17 @@ public class ReplaceSetRemoveAllWithForEachASTVisitor extends AbstractASTRewrite
 		Expression newSetExpression = (Expression) astRewrite.createCopyTarget(removeAllInvocationExpression);
 		newExpressionMethodReference.setExpression(newSetExpression);
 		newExpressionMethodReference.setName(ast.newSimpleName(REMOVE));
-		
+
 		MethodInvocation removeAllInvocationReplacement = ast.newMethodInvocation();
 		Expression newIterableExpression = (Expression) astRewrite.createCopyTarget(removeAllArgument);
 		removeAllInvocationReplacement.setExpression(newIterableExpression);
 		removeAllInvocationReplacement.setName(ast.newSimpleName(FOR_EACH));
-		
+
 		List newArguments = removeAllInvocationReplacement.arguments();
 		newArguments.add(newExpressionMethodReference);
-		
+
 		astRewrite.replace(methodInvocationToReplace, removeAllInvocationReplacement, null);
 		onRewrite();
 		addMarkerEvent(methodInvocationToReplace);
-
-		// This method is not implemented yet !!!
-		methodInvocationToReplace.toString();
-		removeAllInvocationExpression.toString();
-		removeAllArgument.toString();
 	}
 }
--- tests/test/src/test/java/eu/jsparrow/core/visitor/impl/ReplaceSetRemoveAllWithForEachASTVisitorTest.java
+++ tests/test/src/test/java/eu/jsparrow/core/visitor/impl/ReplaceSetRemoveAllWithForEachASTVisitorTest.java
@@ -1,8 +1,14 @@
 package eu.jsparrow.core.visitor.impl;
 
+import java.util.stream.Stream;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import eu.jsparrow.common.UsesJDTUnitFixture;
 
@@ -18,26 +24,103 @@ class ReplaceSetRemoveAllWithForEachASTVisitorTest extends UsesJDTUnitFixture {
 		fixtureProject.clear();
 	}
 
-	/*
-	 * This test will fail as soon as {@link
-	 * ReplaceSetRemoveAllWithForEachASTVisitor} will have been fully
-	 * implemented.
-	 * 
-	 */
+	public static Stream<Arguments> arguments_removeAllWithSupportedTypes() {
+		return Stream.of(
+				Arguments.of(java.util.Set.class, java.util.List.class),
+				Arguments.of(java.util.AbstractSet.class, java.util.List.class),
+				Arguments.of(java.util.HashSet.class, java.util.List.class),
+				Arguments.of(java.util.HashSet.class, java.util.ArrayList.class));
+	}
+
+	@ParameterizedTest
+	@MethodSource("arguments_removeAllWithSupportedTypes")
+	void visit_SetRemoveAll_shouldTransform(Class<?> setType, Class<?> listType) throws Exception {
+		defaultFixture.addImport(setType.getName());
+		defaultFixture.addImport(listType.getName());
+
+		String testMethodParameters = String.format("%s<String> strings, %s<String> stringsToRemove",
+				setType.getSimpleName(), listType.getSimpleName());
+		String original = "" +
+				"	void removeElementsFromSet(" + testMethodParameters + ") {\n" +
+				"		strings.removeAll(stringsToRemove);\n" +
+				"	}";
+
+		String expected = "" +
+				"	void removeElementsFromSet(" + testMethodParameters + ") {\n" +
+				"		stringsToRemove.forEach(strings::remove);\n" +
+				"	}";
+
+		assertChange(original, expected);
+	}
+
+	public static Stream<Arguments> arguments_removeAllWithUnsupportedTypes() {
+		return Stream.of(
+				Arguments.of(java.util.List.class, java.util.List.class),
+				Arguments.of(java.util.Set.class, java.util.Collection.class),
+				Arguments.of(java.util.Set.class, java.util.Set.class));
+	}
+
+	@ParameterizedTest
+	@MethodSource("arguments_removeAllWithUnsupportedTypes")
+	void visit_RemoveAllWithUnsupportedTypes_shouldNotTransform(Class<?> setType, Class<?> listType) throws Exception {
+		defaultFixture.addImport(setType.getName());
+		defaultFixture.addImport(listType.getName());
+
+		String testMethodParameters = String.format("%s<String> strings, %s<String> stringsToRemove",
+				setType.getSimpleName(), listType.getSimpleName());
+		String original = "" +
+				"	void removeElementsFromSet(" + testMethodParameters + ") {\n" +
+				"		strings.removeAll(stringsToRemove);\n" +
+				"	}";
+
+		assertNoChange(original);
+	}
+
 	@Test
-	void visit_SetRemoveAll_shouldTransform() throws Exception {
-		defaultFixture.addImport(java.util.List.class.getName());
+	void visit_InvocationArgumentTypeNotImported_shouldNotTransform() throws Exception {
 		defaultFixture.addImport(java.util.Set.class.getName());
 		String original = "" +
 				"	void exampleWithParametersForSetAndList(Set<String> stringSet, List<String> stringsToRemove) {\n" +
 				"		stringSet.removeAll(stringsToRemove);\n" +
 				"	}";
 
-		String expected = "" +
-				"	void exampleWithParametersForSetAndList(Set<String> stringSet, List<String> stringsToRemove) {\n" +
-				"		stringsToRemove.forEach(stringSet::remove);\n" +
-				"	}";
-
-		assertChange(original, expected);
+		assertNoChange(original);
 	}
+
+	@Test
+	void visit_InvocationExpressionTypeNotImported_shouldNotTransform() throws Exception {
+		defaultFixture.addImport(java.util.List.class.getName());
+		String original = "" +
+				"	void exampleWithParametersForSetAndList(Set<String> stringSet, List<String> stringsToRemove) {\n" +
+				"		stringSet.removeAll(stringsToRemove);\n" +
+				"	}";
+		assertNoChange(original);
+	}
+
+	@Test
+	void visit_MethodNameNotRemoveAll_shouldNotTransform() throws Exception {
+		String original = "" +
+				"Integer integer = Integer.valueOf(0);";
+		assertNoChange(original);
+	}
+
+	@ParameterizedTest
+	@ValueSource(strings = {
+			"boolean removed = removeAll(1);",
+			"removeAll(1);",
+			"this.removeAll(1,2,3);"
+	})
+	void visit_RemoveAllMethodNotInExpressionStatement_shouldNotTransform(String statement) throws Exception {
+
+		String original = "" +
+				"	void callRemoveAll() {\n" +
+				"		" + statement + "\n" +
+				"	}\n" +
+				"	\n" +
+				"	boolean removeAll(Integer... arguments) {\n" +
+				"		return true;\n" +
+				"	}";
+		assertNoChange(original);
+	}
+
 }
