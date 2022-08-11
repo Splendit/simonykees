daabd39dd5613f840ce2082108a60421ebada789
gregor.balint <gregor.balint@splendit.at>
2022-08-11 17:56:00 +0200

    SIM-2117 UPDATE JUnit tests in connection with the Markers

--- tests/ui.test/src/test/java/eu/jsparrow/ui/preference/profile/DefaultActiveMarkersTest.java
+++ tests/ui.test/src/test/java/eu/jsparrow/ui/preference/profile/DefaultActiveMarkersTest.java
@@ -21,7 +21,7 @@ class DefaultActiveMarkersTest {
 	@Test
 	void testDefaultMarkersSize() {
 		List<String> actual = defaultActiveMarkers.getActiveMarkers();
-		assertThat(actual, hasSize(87));
+		assertThat(actual, hasSize(88));
 	}
 	@Test
 	void testDefaultActiveMarkers_shouldReturnAllMarkers() {
