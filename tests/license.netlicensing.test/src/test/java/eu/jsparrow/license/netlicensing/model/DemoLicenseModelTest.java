89370ff41d14bceb8c408ad96f1591482e5d3477
gregor.balint <gregor.balint@splendit.at>
2023-02-28 16:49:36 +0100

    SIM-2185 UPDATE correction of DemoLicenseModelTest

--- tests/license.netlicensing.test/src/test/java/eu/jsparrow/license/netlicensing/model/DemoLicenseModelTest.java
+++ tests/license.netlicensing.test/src/test/java/eu/jsparrow/license/netlicensing/model/DemoLicenseModelTest.java
@@ -9,15 +9,26 @@ import org.junit.jupiter.api.Test;
 import eu.jsparrow.license.api.LicenseType;
 
 class DemoLicenseModelTest {
-	
+
 	@Test
 	void testGetExpirationDate() {
 		DemoLicenseModel model = new DemoLicenseModel();
-		assertEquals(999999999, model.getExpirationDate().getYear());
-		assertEquals(Month.DECEMBER, model.getExpirationDate().getMonth());
+		assertEquals(999999999, model.getExpirationDate()
+			.getYear());
+		assertEquals(Month.DECEMBER, model.getExpirationDate()
+			.getMonth());
+		assertEquals(1, model.getExpirationDate()
+			.getDayOfMonth());
+		assertEquals(0, model.getExpirationDate()
+			.getHour());
+		assertEquals(0, model.getExpirationDate()
+			.getMinute());
+		assertEquals(0, model.getExpirationDate()
+			.getSecond());
+		assertEquals(0, model.getExpirationDate()
+			.getNano());
 	}
-	
-	
+
 	@Test
 	void testGetType() {
 		DemoLicenseModel model = new DemoLicenseModel();
