520f50a941cb27d0046b6005682f038b7aded782
gregor.balint <gregor.balint@splendit.at>
2023-05-31 23:18:35 +0200

    SIM-2204 UPDATE externalize rule name and description

--- bundles/core/src/main/java/eu/jsparrow/core/rule/impl/UseTernaryOperatorRule.java
+++ bundles/core/src/main/java/eu/jsparrow/core/rule/impl/UseTernaryOperatorRule.java
@@ -7,6 +7,7 @@ import org.eclipse.jdt.core.JavaCore;
 
 import eu.jsparrow.core.visitor.impl.CollapseIfStatementsASTVisitor;
 import eu.jsparrow.core.visitor.impl.UseTernaryOperatorASTVisitor;
+import eu.jsparrow.i18n.Messages;
 import eu.jsparrow.rules.common.RefactoringRuleImpl;
 import eu.jsparrow.rules.common.RuleDescription;
 import eu.jsparrow.rules.common.Tag;
@@ -24,15 +25,9 @@ public class UseTernaryOperatorRule extends RefactoringRuleImpl<UseTernaryOperat
 	public UseTernaryOperatorRule() {
 		this.visitorClass = UseTernaryOperatorASTVisitor.class;
 		this.id = RULE_ID;
-		String name = "Use Ternary Operator"; //$NON-NLS-1$
-		// TODO: externalize to messages.properties in
-		// eu.jsparrow.i18n.Messages;
-		String description = "Replaces simple if statements by statements containing the ternary operator where this is possible."; //$NON-NLS-1$
-		// TODO: externalize to messages.properties in
-		// eu.jsparrow.i18n.Messages;
-
-		this.ruleDescription = new RuleDescription(name, description, Duration.ofMinutes(10),
-				Arrays.asList(Tag.JAVA_1_1, Tag.CODING_CONVENTIONS, Tag.READABILITY));
+		this.ruleDescription = new RuleDescription(Messages.UseTernaryOperatorRule_name,
+				Messages.UseTernaryOperatorRule_description, Duration.ofMinutes(10),
+				Arrays.asList(Tag.JAVA_1_1, Tag.READABILITY));
 	}
 
 	@Override
