ee6f3f9497277a5424a8b73c6ec00e5f528834ec
gregor.balint <gregor.balint@splendit.at>
2023-06-12 23:39:49 +0200

    SIM-2204 ADD negative test cases

--- tests/test/src/test/java/eu/jsparrow/core/visitor/impl/ternary/UseTernaryOperatorASTVisitorNegativesTest.java
+++ tests/test/src/test/java/eu/jsparrow/core/visitor/impl/ternary/UseTernaryOperatorASTVisitorNegativesTest.java
@@ -20,6 +20,64 @@ public class UseTernaryOperatorASTVisitorNegativesTest extends UsesJDTUnitFixtur
 	}
 
 	@Test
+	void visit_AssignmentWhenTrueWithoutElse_shouldNotTransform() throws Exception {
+		String original = ""
+				+ "		void test() {\n"
+				+ "			boolean condition = true;\n"
+				+ "			int x;\n"
+				+ "			if (condition) {\n"
+				+ "				x = 1;\n"
+				+ "			}\n"
+				+ "		}";
+		assertNoChange(original);
+	}
+
+	@Test
+	void visit_AssignmentWhenTrueEmptyElse_shouldNotTransform() throws Exception {
+		String original = ""
+				+ "		void test() {\n"
+				+ "			boolean condition = true;\n"
+				+ "			int x;\n"
+				+ "			if (condition) {\n"
+				+ "				x = 1;\n"
+				+ "			} else {\n"
+				+ "			}\n"
+				+ "		}";
+		assertNoChange(original);
+	}
+
+	@Test
+	void visit_AssignmentWhenTrueTwoAssignmentsWhenElse_shouldNotTransform() throws Exception {
+		String original = ""
+				+ "		void test() {\n"
+				+ "			boolean condition = true;\n"
+				+ "			int x;\n"
+				+ "			if (condition) {\n"
+				+ "				x = 1;\n"
+				+ "			} else {\n"
+				+ "				x = 2;\n"
+				+ "				x = 3;\n"
+				+ "			}\n"
+				+ "		}";
+		assertNoChange(original);
+	}
+
+	@Test
+	void visit_AssignmentWhenTrueMethodInvocationWhenElse_shouldNotTransform() throws Exception {
+		String original = ""
+				+ "		void test() {\n"
+				+ "			boolean condition = true;\n"
+				+ "			int x;\n"
+				+ "			if (condition) {\n"
+				+ "				x = 1;\n"
+				+ "			} else {\n"
+				+ "				System.out.println();\n"
+				+ "			}\n"
+				+ "		}";
+		assertNoChange(original);
+	}
+
+	@Test
 	void visit_AssigningDifferentVariables_shouldNotTransform() throws Exception {
 		String original = ""
 				+ "	void test() {\n"
@@ -32,6 +90,108 @@ public class UseTernaryOperatorASTVisitorNegativesTest extends UsesJDTUnitFixtur
 				+ "			x2 = 0;\n"
 				+ "		}\n"
 				+ "	}";
+		assertNoChange(original);
+	}
+
+	@Test
+	void visit_usingDifferentAssignOperators_shouldNotTransform() throws Exception {
+		String original = ""
+				+ "	void test() {\n"
+				+ "		boolean condition = true;\n"
+				+ "		int x;\n"
+				+ "		if (condition) {\n"
+				+ "			x += 1;\n"
+				+ "		} else {\n"
+				+ "			x -= 1;\n"
+				+ "		}\n"
+				+ "	}";
+		assertNoChange(original);
+	}
+
+	@Test
+	void visit_assignIntWhenTrueIntegerWhenFalse_shouldNotTransform() throws Exception {
+		String original = ""
+				+ "		void test() {\n"
+				+ "			boolean condition = true;\n"
+				+ "			int x;\n"
+				+ "			if (condition) {\n"
+				+ "				x = 1;\n"
+				+ "			} else {\n"
+				+ "				x = Integer.valueOf(0);\n"
+				+ "			}\n"
+				+ "		}";
+		assertNoChange(original);
+	}
+
+	@Test
+	void visit_assignIntegerWhenTrueIntWhenFalse_shouldNotTransform() throws Exception {
+		String original = ""
+				+ "		void test() {\n"
+				+ "			boolean condition = true;\n"
+				+ "			int x;\n"
+				+ "			if (condition) {\n"
+				+ "				x = Integer.valueOf(1);\n"
+				+ "			} else {\n"
+				+ "				x = 0;\n"
+				+ "			}\n"
+				+ "		}";
+		assertNoChange(original);
+	}
+	
+	@Test
+	void visit_returnWhenTrueEmptyElse_shouldNotTransform() throws Exception {
+		String original = ""
+				+ "		int test() {\n"
+				+ "			boolean condition = true;\n"
+				+ "			if (condition) {\n"
+				+ "				return 1;\n"
+				+ "			} else {\n"
+				+ "			}\n"
+				+ "			return 0;\n"
+				+ "		}";
+		assertNoChange(original);
+	}
+	
+	@Test
+	void visit_returnWhenTrueMethodCallWhenElse_shouldNotTransform() throws Exception {
+		String original = ""
+				+ "		int test() {\n"
+				+ "			boolean condition = true;\n"
+				+ "			if (condition) {\n"
+				+ "				return 1;\n"
+				+ "			} else {\n"
+				+ "				System.out.println();\n"
+				+ "			}\n"
+				+ "			return 0;\n"
+				+ "		}";
+		assertNoChange(original);
+	}
+	
+	@Test
+	void visit_returnWhenTrueNoElseAndNoReturnAfterIf_shouldNotTransform() throws Exception {
+		String original = ""
+				+ "		int test() {\n"
+				+ "			boolean condition = true;\n"
+				+ "			if (condition) {\n"
+				+ "				return 1;\n"
+				+ "			}\n"
+				+ "			System.out.println();\n"
+				+ "			return 0;\n"
+				+ "		}";
+		assertNoChange(original);
+	}
+	
+	@Test
+	void visit_returnIntWhenTrueIntegerWhenFalse_shouldNotTransform() throws Exception {
+		String original = ""
+				+ "		int test() {\n"
+				+ "			boolean condition = true;\n"
+				+ "			if (condition) {\n"
+				+ "				return 1;\n"
+				+ "			} else {\n"
+				+ "				return Integer.valueOf(0);\n"
+				+ "			}\n"
+				+ "		}";
 		assertNoChange(original);
 	}
 }
