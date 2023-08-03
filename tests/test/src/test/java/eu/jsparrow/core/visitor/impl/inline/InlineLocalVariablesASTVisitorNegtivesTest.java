c883c2245608fdabb162b4d5b87294604a943d27
gregor.balint <gregor.balint@splendit.at>
2023-08-03 17:48:52 +0200

    SIM-2143 ADD positive and negative visitor tests

---
+++ tests/test/src/test/java/eu/jsparrow/core/visitor/impl/inline/InlineLocalVariablesASTVisitorNegtivesTest.java
@@ -0,0 +1,130 @@
+package eu.jsparrow.core.visitor.impl.inline;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import eu.jsparrow.common.UsesJDTUnitFixture;
+
+@SuppressWarnings({ "nls" })
+public class InlineLocalVariablesASTVisitorNegtivesTest extends UsesJDTUnitFixture {
+
+	@BeforeEach
+	public void setUp() {
+		setDefaultVisitor(new InlineLocalVariablesASTVisitor());
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		fixtureProject.clear();
+	}
+
+	@Test
+	void visit_NoInitializer_shouldNotTransform() throws Exception {
+		String original = "" +
+				"	void noInitializer() {\n" +
+				"		int sum;\n" +
+				"	}";
+		assertNoChange(original);
+	}
+
+	@Test
+	void visit_DeclarationAsForLoopCounter_shouldNotTransform() throws Exception {
+		String original = "" +
+				"	void declarationAsForLoopCounter() {\n" +
+				"		for (int i = 0; i < 10; i++) {\n" +
+				"		}\n" +
+				"	}";
+		assertNoChange(original);
+	}
+
+	@Test
+	void visit_MultipleVariableDeclaration_shouldNotTransform() throws Exception {
+		String original = "" +
+				"	int multipleVariableDecaration(int a) {\n" +
+				"		int square = a * a, cube = a * a * a;\n" +
+				"		return square;\n" +
+				"	}";
+		assertNoChange(original);
+	}
+
+	@ParameterizedTest
+	@ValueSource(strings = {
+			"int a, int b",
+			"int a, Integer b",
+			"Integer a, int b",
+			"Integer a, Integer b",
+	})
+	void visit_BoxingInitializer_shouldNotTransform(String parameters) throws Exception {
+		String initializer = "a + b";
+		String original = "" +
+				"	Integer boxingInitializer(" + parameters + ") {\n" +
+				"		Integer result = " + initializer + ";\n" +
+				"		return result;\n" +
+				"	}";
+		assertNoChange(original);
+	}
+
+	@Test
+	void visit_UnboxingInitializer_shouldNotTransform() throws Exception {
+		String original = "" +
+				"	int unboxingInitializer(Integer a) {\n" +
+				"		int result = a;\n" +
+				"		return result;\n" +
+				"	}";
+		assertNoChange(original);
+	}
+
+	@Test
+	void visit_UsedMoreThanOnce_shouldNotTransform() throws Exception {
+		String original = "" +
+				"	int usedMoreThanOnce() {\n" +
+				"		int x = 1;\n" +
+				"		int y = x;\n" +
+				"		return x;\n" +
+				"	}";
+		assertNoChange(original);
+	}
+
+	@Test
+	void visit_IncrementAsOnlyUse_shouldNotTransform() throws Exception {
+		String original = "" +
+				"	void incrementAsOnlyUse() {\n" +
+				"		int x = 0;\n" +
+				"		++x;\n" +
+				"	}";
+		assertNoChange(original);
+	}
+
+	@Test
+	void visit_StatementBetweenDeclarationAndReturn_shouldNotTransform() throws Exception {
+		String original = "" +
+				"	int statementBetweenDeclarationAndReturn(int a) {\n" +
+				"		int square = a * a;\n" +
+				"		a += 1;\n" +
+				"		return square;\n" +
+				"	}";
+		assertNoChange(original);
+	}
+
+	@Test
+	void visit_StatementBetweenDeclarationAndThrow_shouldNotTransform() throws Exception {
+		String original = "" +
+				"	void statementBetweenDeclarationAndThrow(String message) throws Exception {\n" +
+				"		Exception exception = new Exception(message);\n" +
+				"		message = \"***\" + message + \"***\";\n" +
+				"		throw exception;\n" +
+				"	}";
+		assertNoChange(original);
+	}
+
+	// @Test
+	// void visit_8_shouldNotTransform() throws Exception {
+	// String original = "" +
+	// "";
+	// assertNoChange(original);
+	// }
+
+}
--- tests/test/src/test/java/eu/jsparrow/core/visitor/impl/inline/InlineLocalVariablesASTVisitorTest.java
+++ tests/test/src/test/java/eu/jsparrow/core/visitor/impl/inline/InlineLocalVariablesASTVisitorTest.java
@@ -1,8 +1,13 @@
 package eu.jsparrow.core.visitor.impl.inline;
 
+import java.util.stream.Stream;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import eu.jsparrow.common.UsesJDTUnitFixture;
 
@@ -20,27 +25,97 @@ public class InlineLocalVariablesASTVisitorTest extends UsesJDTUnitFixture {
 	}
 
 	@Test
-	void visit_usedForReturn_shouldTransform() throws Exception {
+	void visit_PrimitiveIntVariableUsedForReturn_shouldTransform() throws Exception {
 		String original = "" +
-				"	int useForReturn() {\n" +
-				"		int x = 1;\n" +
-				"		return x;\n" +
-				"	}";
+				"		int getResult(int a, int b) {\n" +
+				"			int result = a + b;\n" +
+				"			return result;\n" +
+				"		}";
 		String expected = "" +
-				"	int useForReturn() {\n" +
-				"		return 1;\n" +
-				"	}";
+				"		int getResult(int a, int b) {\n" +
+				"			return a + b;\n" +
+				"		}";
 
 		assertChange(original, expected);
 	}
 
 	@Test
-	void visit_usedAsInitializer_shouldNotTransform() throws Exception {
+	void visit_StringVariableUsedForReturn_shouldTransform() throws Exception {
 		String original = "" +
-				"	void useAsInitializer() {\n" +
-				"		int x = 1;\n" +
-				"		int x2 = x;\n" +
-				"	}";
-		assertNoChange(original);
+				"		String getResult(String a, String b) {\n" +
+				"			String result = a + b;\n" +
+				"			return result;\n" +
+				"		}";
+		String expected = "" +
+				"		String getResult(String a, String b) {\n" +
+				"			return a + b;\n" +
+				"		}";
+
+		assertChange(original, expected);
 	}
+
+	@Test
+	void visit_ExceptionUsedForThrow_shouldTransform() throws Exception {
+		String original = "" +
+				"		void throwException(String message) throws Exception {\n" +
+				"			Exception exception = new Exception(message);\n" +
+				"			throw exception;\n" +
+				"		}";
+		String expected = "" +
+				"		void throwException(String message) throws Exception {\n" +
+				"			throw new Exception(message);\n" +
+				"		}";
+
+		assertChange(original, expected);
+	}
+
+	private static Stream<Arguments> arrayInitializerToArrayCreation() {
+		String testWithParam = "test (int a)";
+		return Stream.of(
+				Arguments.of(
+						"" +
+								"	int[] " + testWithParam + " {\n" +
+								"		int[] counts = { a, a + a, a * 3 };\n" +
+								"		return counts;\n" +
+								"	}",
+						"" +
+								"	int[] " + testWithParam + " {\n" +
+								"		return new int[]{ a, a + a, a * 3 };\n" +
+								"	}"),
+				Arguments.of(
+						"" +
+								"	int[] " + testWithParam + " {\n" +
+								"		int counts[] = { a, a + a, a * 3 };\n" +
+								"		return counts;\n" +
+								"	}",
+						"" +
+								"	int[] " + testWithParam + " {\n" +
+								"		return new int[]{ a, a + a, a * 3 };\n" +
+								"	}"),
+				Arguments.of(
+						"" +
+								"	int[][] " + testWithParam + " {\n" +
+								"		int[] counts[] = { { a, a * 2 }, { a * 3, a * 4 } };\n" +
+								"		return counts;\n" +
+								"	}",
+						"" +
+								"	int[][] " + testWithParam + " {\n" +
+								"		return new int[][]{ { a, a * 2 }, { a * 3, a * 4 } };\n" +
+								"	}"));
+	}
+
+	@ParameterizedTest
+	@MethodSource("arrayInitializerToArrayCreation")
+	void visit_arrayInitializerToArrayCreation_shouldTransform(String original, String expected) throws Exception {
+		assertChange(original, expected);
+	}
+
+	// @Test
+	// void visit__shouldTransform() throws Exception {
+	// String original = "" +
+	// "";
+	// String expected = "" +
+	// "";
+	// assertChange(original, expected);
+	// }
 }
