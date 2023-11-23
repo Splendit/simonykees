8e94ece686634462673ab447dfcc3af99de8f1d6
gregor.balint <gregor.balint@splendit.at>
2023-11-23 21:18:18 +0100

    SIM-2215 UPDATE correction of  configuration and system property

--- jsparrow-independent-main/src/main/java/eu/jsparrow/independent/main/JSparrowIndependentMain.java
+++ jsparrow-independent-main/src/main/java/eu/jsparrow/independent/main/JSparrowIndependentMain.java
@@ -1,34 +1,35 @@
 package eu.jsparrow.independent.main;
 
+import java.time.Instant;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.logging.Logger;
-//TODO: uncomment 
-//import org.osgi.framework.BundleException;
+
+import org.osgi.framework.BundleException;
 import org.osgi.framework.Constants;
+
 
 @SuppressWarnings("nls")
 public class JSparrowIndependentMain {
 
-	public static final String SAMPLE_PROJECT_PATH = "/home/gregor/eclipse/rcp-2022-03/workspace/example-project";
-	private static final Logger log = Logger.getLogger("JSparrowIndependentMain");
-	
-	public static void main(String[] args) {
-		log.info("JSparrowIndependentMain#main#main begin");
+	public static final String SAMPLE_PROJECT_PATH = "/home/gregor/minimal-sample-projects/simple-maven-projects/example-project";
 
+	public static void main(String[] args) {
 		Map<String, String> configuration = new HashMap<>();
+
+		String val = Instant.now()
+			.toString();
+
+		final String tempWorkspacePath = "/home/gregor/minimal-sample-projects/temp-workspace/" + val;
+
 		configuration.put("debug.enabled", "true");
 		configuration.put("STANDALONE.MODE", "REFACTOR");
-		// TODO:
-		// as soon as the new mode is present:
-		// configuration.put("STANDALONE.MODE", "SELECT_SOURCES");
-		//
 		configuration.put("LICENSE", "IT43A7PPH");
 		configuration.put("ROOT.PROJECT.BASE.PATH", SAMPLE_PROJECT_PATH);
 
 		configuration.put(Constants.FRAMEWORK_STORAGE_CLEAN, Constants.FRAMEWORK_STORAGE_CLEAN_ONFIRSTINIT);
 		configuration.put(Constants.FRAMEWORK_STORAGE, "osgi.instance.area.default");
-		configuration.put("osgi.instance.area.default", System.getProperty("user.dir"));
+		configuration.put("osgi.instance.area.default", tempWorkspacePath);
+		System.setProperty("user.dir", tempWorkspacePath);
 
 		/*
 		 * This is solution B from this article:
@@ -38,15 +39,12 @@ public class JSparrowIndependentMain {
 		configuration.put(Constants.FRAMEWORK_BOOTDELEGATION,
 				"javax.*,org.xml.*,sun.*,com.sun.*,jdk.internal.reflect,jdk.internal.reflect.*"); //$NON-NLS-1$
 
-		// TODO: uncomment following code:
-		// BundleStarter bundleStarter = new BundleStarter();
-		// try {
-		// bundleStarter.runStandalone(configuration);
-		// } catch (BundleException | InterruptedException e) {
-		// e.printStackTrace();
-		// }
-
-		log.info("JSparrowIndependentMain#main#main end");
+		BundleStarter bundleStarter = new BundleStarter();
+		try {
+			bundleStarter.runStandalone(configuration);
+		} catch (BundleException | InterruptedException e) {
+			e.printStackTrace();
+		}
 
 	}
 
