0d12d840996c004a62b871b5be809dfba706bf29
gregor.balint <gregor.balint@splendit.at>
2023-11-23 20:57:22 +0100

    SIM-2215 UPDATE rename DepencenciesToSrcMainRessources
    Rename DepencenciesToSrcMainRessources to ProductPlugInHelper

--- jsparrow-independent-main/src/main/java/eu/jsparrow/independent/main/ProductPlugInHelper.java
+++ jsparrow-independent-main/src/main/java/eu/jsparrow/independent/main/ProductPlugInHelper.java
@@ -5,7 +5,7 @@ package eu.jsparrow.independent.main;
  * creates a manifest.independent file
  * https://docs.oracle.com/javase/tutorial/essential/io/copy.html#:~:text=You%20can%20copy%20a%20file,Directories%20can%20be%20copied.
  */
-public class DepencenciesToSrcMainRessources {
+public class ProductPlugInHelper {
 	// TODO: implement method to copy dependencies to main resources
 	// TODO: implement method to create manifest.independent file
 }
