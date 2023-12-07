189893869088f342db14bac4e10c9decd7d6e4e6
gregor.balint <gregor.balint@splendit.at>
2023-12-07 13:44:44 +0100

    SIM-2215 ADD method getRepositoryPlugInsDirectory()
    Add method getRepositoryPlugInsDirectory() to class ProductPlugInHelper

--- jsparrow-independent-main/src/main/java/eu/jsparrow/independent/main/ProductPlugInHelper.java
+++ jsparrow-independent-main/src/main/java/eu/jsparrow/independent/main/ProductPlugInHelper.java
@@ -16,24 +16,11 @@ import java.util.stream.Stream;
  * Copies Dependencies to src/main/resources and <br>
  * creates a manifest.independent file
  * https://docs.oracle.com/javase/tutorial/essential/io/copy.html#:~:text=You%20can%20copy%20a%20file,Directories%20can%20be%20copied.
- * // TODO: implement method to copy dependencies to main resources<br>
- * // TODO: implement method to create manifest.independent file
  */
 @SuppressWarnings("nls")
 public class ProductPlugInHelper {
 
-	// static Optional<Path> findSourceMainResourcesDirectory() throws
-	// URISyntaxException {
-	// File srcMainResources = new File("src/main/resources");
-	// String absolutePathAsString = srcMainResources.getAbsolutePath();
-	// URI uri;
-	// uri = new URI("file://" + absolutePathAsString);
-	// Path path = Paths.get(uri);
-	// if (Files.isDirectory(path)) {
-	// return Optional.of(path);
-	// }
-	// return Optional.empty();
-	// }
+	private static File productRepositoryPlugInsDirectory;
 
 	private static void clearSourceMainResources(Path surceMainResourcesPath) throws IOException {
 		try (Stream<Path> childStream = Files.list(surceMainResourcesPath);) {
@@ -57,44 +44,19 @@ public class ProductPlugInHelper {
 	 * @throws IOException
 	 */
 	static List<String> getProductPlugInNames() throws IOException {
-		File simonykees = new File("..").getCanonicalFile();
-
-		Path plugInsPath = Paths.get(simonykees.getAbsolutePath(), //
-				"releng", //
-				"eu.jsparrow.independent.product", //
-				"target", //
-				"repository", //
-				"plugins");
-
-		List<String> pluginNames = Arrays.asList(plugInsPath.toFile()
-			.list());
-
+		List<String> pluginNames = Arrays.asList(getRepositoryPlugInsDirectory().list());
 		Collections.sort(pluginNames);
-
 		return pluginNames;
 	}
 
 	static void copyProductPlugIns() throws URISyntaxException, IOException {
+
 		Path sourceMainResourcesAbsolutePath = Paths.get("src", "main", "resources")
 			.toAbsolutePath();
 		clearSourceMainResources(sourceMainResourcesAbsolutePath);
-
-		// File jSparrowIndependentMain = new File(".").getCanonicalFile();
-		// Path srcMainResources = Paths.get("src", "main",
-		// "resources").toAbsolutePath();
-		File simonykees = new File("..").getCanonicalFile();
-
-		Path plugInsPath = Paths.get(simonykees.getAbsolutePath(), //
-				"releng", //
-				"eu.jsparrow.independent.product", //
-				"target", //
-				"repository", //
-				"plugins");
-
 		File sourceMainResourcesAbsolutePath2File = sourceMainResourcesAbsolutePath.toFile();
 
-		File[] pluginFiles = plugInsPath.toFile()
-			.listFiles();
+		File[] pluginFiles = getRepositoryPlugInsDirectory().listFiles();
 		for (File file : pluginFiles) {
 			Path source = file.toPath();
 			Path target = (new File(sourceMainResourcesAbsolutePath2File, file.getName())).toPath();
@@ -102,13 +64,25 @@ public class ProductPlugInHelper {
 		}
 	}
 
+	static File getRepositoryPlugInsDirectory() throws IOException {
+		if (productRepositoryPlugInsDirectory == null) {
+			File simonykees = new File("..").getCanonicalFile();
+			Path pathToRepositoryPlugins = Paths.get(simonykees.getAbsolutePath(), //
+					"releng", //
+					"eu.jsparrow.independent.product", //
+					"target", //
+					"repository", //
+					"plugins");
+			productRepositoryPlugInsDirectory = pathToRepositoryPlugins.toFile();
+		}
+		return productRepositoryPlugInsDirectory;
+	}
+
 	public static void main(String[] args) {
 		try {
 			copyProductPlugIns();
 		} catch (URISyntaxException | IOException e) {
-
 			e.printStackTrace();
 		}
-
 	}
 }
