a6f030f0329169eaefa0de71d31fd9fe57afd5c9
gregor.balint <gregor.balint@splendit.at>
2023-12-15 23:36:57 +0100

    SIM-2215 REMOVE obsolete code

--- jsparrow-independent-main/src/main/java/eu/jsparrow/independent/main/JSparrowIndependentMain.java
+++ jsparrow-independent-main/src/main/java/eu/jsparrow/independent/main/JSparrowIndependentMain.java
@@ -24,7 +24,7 @@ public class JSparrowIndependentMain {
 	public static final String OPENSOURCE_PROJECT_BIOJAVA = "/home/gregor/opensource/biojava/";
 	public static final String SIMPLE_MAVEN_PROJECT = "/home/gregor/opensource/eclipse-plugin-tests/workspace/jsparrow-independent-simple-test/";
 
-	public static final String ROOT_PROJECT_BASE_PATH_VALUE = SIMPLE_MAVEN_PROJECT;
+	public static final String ROOT_PROJECT_BASE_PATH_VALUE = OPENSOURCE_PROJECT_BIOJAVA;
 	private static final String FALSE_AS_STRING = Boolean.FALSE.toString();
 	private static final String TRUE_AS_STRING = Boolean.TRUE.toString();
 	public static final String ROOT_PROJECT_BASE_PATH = "ROOT.PROJECT.BASE.PATH";
@@ -45,12 +45,12 @@ public class JSparrowIndependentMain {
 		// REFACTOR
 		// SELECT_SOURCES
 
-		configuration.put("RULES.FROM.RULES_CONTAINER", FALSE_AS_STRING); //$NON-NLS-1$
+		configuration.put("RULES.FROM.RULES_CONTAINER", TRUE_AS_STRING); //$NON-NLS-1$
 		// possible values:
 		// TRUE_AS_STRING
 		// FALSE_AS_STRING
 
-		configuration.put("RULES.FILTER", RULES_NO_FILTER); //$NON-NLS-1$
+		configuration.put("RULES.FILTER", RULE_DIAMOND_OPERATOR); //$NON-NLS-1$
 		// possible values:
 		// RULES_NO_FILTER
 		// RULE_DIAMOND_OPERATOR
