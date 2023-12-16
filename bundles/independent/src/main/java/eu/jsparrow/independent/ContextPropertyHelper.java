88ec28517b1356b160bf245572656a5e52c47aa4
Gregor Balint <gregor.balint@splendit.at>
2023-12-16 01:38:12 +0100

    SIM-2215 UPDATE marked method as deprecated, correction of test
    ContextPropertyHelper#getProperty(BundleContext, String) is deprecated and should be removed, ContextPropertyHelper should be reworked

--- bundles/independent/src/main/java/eu/jsparrow/independent/ContextPropertyHelper.java
+++ bundles/independent/src/main/java/eu/jsparrow/independent/ContextPropertyHelper.java
@@ -6,6 +6,12 @@ import java.util.Map;
 
 import org.osgi.framework.BundleContext;
 
+/**
+ * This class contains obsolete code. Remove
+ * {@link #getProperty(BundleContext, String) and move constants to another
+ * class if this is necessary, and then remove this class.
+ *
+ */
 @SuppressWarnings("nls")
 public class ContextPropertyHelper {
 	private static final String TRUE_VALUE = Boolean.TRUE.toString();
@@ -14,7 +20,7 @@ public class ContextPropertyHelper {
 	public static final String STANDALONE_MODE_KEY = "STANDALONE.MODE";
 	public static final String USE_DEFAULT_CONFIGURATION = "DEFAULT.CONFIG"; //$NON-NLS-1$
 	public static final String SELECTED_SOURCES = "SELECTED.SOURCES"; //$NON-NLS-1$
-	
+
 	public static final String ROOT_PROJECT_BASE_PATH = "ROOT.PROJECT.BASE.PATH";
 	private static final String SELECTED_PROJECT_PATH = "/home/gregor/minimal-sample-projects/simple-maven-projects/example-project/";
 
@@ -23,6 +29,10 @@ public class ContextPropertyHelper {
 	private static final Map<String, String> JSPARROW_INDEPENDENT_CONFIG;
 
 	static {
+		/**
+		 * obsolete code.
+		 *
+		 */
 		HashMap<String, String> temp = new HashMap<>();
 		temp.put(ROOT_PROJECT_BASE_PATH, SELECTED_PROJECT_PATH);
 		temp.put(DEBUG_ENABLED, TRUE_VALUE);
@@ -33,6 +43,7 @@ public class ContextPropertyHelper {
 		JSPARROW_INDEPENDENT_CONFIG = Collections.unmodifiableMap(temp);
 	}
 
+	@Deprecated
 	public static String getProperty(BundleContext context, String key) {
 		if (!isContextContainingJSparrowProperties(context)) {
 			String value = JSPARROW_INDEPENDENT_CONFIG.get(key);
