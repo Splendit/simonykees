9e050d503d4d156f903c967cbcf8f8b58a35fa8e
Gregor Balint <gregor.balint@splendit.at>
2024-03-14 22:35:09 +0100

    SIM-2212 UPDATE repair logger

--- bundles/logging/src/main/java/eu/jsparrow/logging/Activator.java
+++ bundles/logging/src/main/java/eu/jsparrow/logging/Activator.java
@@ -1,15 +1,7 @@
 package eu.jsparrow.logging;
 
-import java.io.IOException;
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
-
-import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
-
-import ch.qos.logback.core.joran.spi.JoranException;
 
 /**
  * The activator class controls the plug-in life cycle
@@ -18,7 +10,6 @@ import ch.qos.logback.core.joran.spi.JoranException;
  * @since 1.2
  */
 public class Activator implements BundleActivator {
-	private static final String DEBUG_ENABLED = "debug.enabled"; //$NON-NLS-1$
 	// The plug-in ID
 	public static final String PLUGIN_ID = "eu.jsparrow.logging"; //$NON-NLS-1$
 
@@ -40,57 +31,8 @@ public class Activator implements BundleActivator {
 	@Override
 	public void start(BundleContext context) throws Exception {
 		plugin = this;
-		configureLogger(context);
-	}
-
-	void configureLogger(BundleContext context) throws JoranException, IOException {
-		List<String> jSparrowBundleSymbolicNames = getJSparrowBundleSymbolicNames(context);
-		boolean debugEnabled = isDebugEnabled(context);
-		if (containsTest(jSparrowBundleSymbolicNames)) {
-			LoggingUtil.configureLoggerForTesting();
-		} else if (isStandalone(jSparrowBundleSymbolicNames)) {
-			LoggingUtil.configureLogger(debugEnabled);
-		} else {
-			LoggingUtil.configureLogger();
-		}
-
-	}
-
-	private List<String> getJSparrowBundleSymbolicNames(BundleContext context) {
-		return Arrays.stream(context.getBundles())
-			.map(Bundle::getSymbolicName)
-			.filter(symbolicName -> symbolicName.startsWith("eu.jsparrow."))
-			.collect(Collectors.toList());
-	}
-
-	private boolean isDebugEnabled(BundleContext context) {
-		String debugEnabledValue = context.getProperty(DEBUG_ENABLED);
-		if (debugEnabledValue == null) {
-			return false;
-		}
-		return Boolean.parseBoolean(debugEnabledValue);
-	}
-
-	private boolean containsTest(List<String> symbolicNames) {
-		for (String symbolicName : symbolicNames) {
-			if (symbolicName.endsWith(".test")) {
-				return true;
-			}
-		}
-		return false;
-	}
-
-	private boolean isStandalone(List<String> symbolicNames) {
-		boolean jSparrowStandaloneFound = false;
-		for (String symbolicName : symbolicNames) {
-			if (symbolicName.endsWith(".ui")) {
-				return false;
-			}
-			if (symbolicName.endsWith(".standalone")) {
-				jSparrowStandaloneFound = true;
-			}
-		}
-		return jSparrowStandaloneFound;
+		LoggingUtil.setBundle(context.getBundle());
+		LoggingUtil.configureLogger();
 	}
 
 	/*
